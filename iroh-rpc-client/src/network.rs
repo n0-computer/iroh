d6adf5efc38fc21ca6bb574b77b78e4fd8cad7a7
Kasey <klhuizinga@gmail.com>
2022-05-19 21:06:17 -0400

    fix(rpc): clone rpc_client to send concurrent requests

--- iroh-rpc-client/src/network.rs
+++ iroh-rpc-client/src/network.rs
@@ -1,6 +1,5 @@
 use std::collections::{HashMap, HashSet};
 use std::net::SocketAddr;
-use std::sync::Arc;
 
 use anyhow::{Context, Result};
 use bytes::Bytes;
@@ -9,11 +8,10 @@ use iroh_rpc_types::p2p::{
     self, BitswapRequest, ConnectRequest, DisconnectRequest, Empty, Key, Providers,
 };
 use libp2p::{Multiaddr, PeerId};
-use tokio::sync::Mutex;
-use tracing::warn;
+use tracing::{debug, warn};
 
 #[derive(Debug, Clone)]
-pub struct P2pClient(Arc<Mutex<p2p::p2p_client::P2pClient<tonic::transport::Channel>>>);
+pub struct P2pClient(p2p::p2p_client::P2pClient<tonic::transport::Channel>);
 
 impl P2pClient {
     pub async fn new(addr: SocketAddr) -> Result<Self> {
@@ -23,7 +21,7 @@ impl P2pClient {
 
         let client = p2p::p2p_client::P2pClient::new(conn);
 
-        Ok(P2pClient(Arc::new(Mutex::new(client))))
+        Ok(P2pClient(client))
     }
 
     // Fetches a block directly from the network.
@@ -33,6 +31,7 @@ impl P2pClient {
         cid: Cid,
         providers: Option<HashSet<PeerId>>,
     ) -> Result<Bytes> {
+        debug!("rpc p2p client fetch_bitswap: {:?}", cid);
         let providers = providers.map(|p| {
             let list = p.into_iter().map(|id| id.to_bytes()).collect::<Vec<_>>();
             Providers { providers: list }
@@ -42,7 +41,7 @@ impl P2pClient {
             cid: cid.to_bytes(),
             providers,
         });
-        let res = self.0.lock().await.fetch_bitswap(req).await?;
+        let res = self.0.clone().fetch_bitswap(req).await?;
         Ok(res.into_inner().data)
     }
 
@@ -51,7 +50,7 @@ impl P2pClient {
         let req = iroh_metrics::req::trace_tonic_req(Key {
             key: key.hash().to_bytes(),
         });
-        let res = self.0.lock().await.fetch_provider(req).await?;
+        let res = self.0.clone().fetch_provider(req).await?;
         let mut providers = HashSet::new();
         for provider in res.into_inner().providers.into_iter() {
             providers.insert(PeerId::from_bytes(&provider[..])?);
@@ -62,13 +61,7 @@ impl P2pClient {
     #[tracing::instrument(skip(self))]
     pub async fn get_listening_addrs(&self) -> Result<(PeerId, Vec<Multiaddr>)> {
         let req = iroh_metrics::req::trace_tonic_req(Empty {});
-        let res = self
-            .0
-            .lock()
-            .await
-            .get_listening_addrs(req)
-            .await?
-            .into_inner();
+        let res = self.0.clone().get_listening_addrs(req).await?.into_inner();
         let peer_id = PeerId::from_bytes(&res.peer_id[..])?;
         let addrs = addrs_from_bytes(res.addrs)?;
         Ok((peer_id, addrs))
@@ -77,7 +70,7 @@ impl P2pClient {
     #[tracing::instrument(skip(self))]
     pub async fn get_peers(&self) -> Result<HashMap<PeerId, Vec<Multiaddr>>> {
         let req = iroh_metrics::req::trace_tonic_req(Empty {});
-        let peers = self.0.lock().await.get_peers(req).await?.into_inner().peers;
+        let peers = self.0.clone().get_peers(req).await?.into_inner().peers;
         let mut peers_map = HashMap::new();
         for (peer, addrs) in peers.into_iter() {
             let peer = peer.parse()?;
@@ -93,7 +86,7 @@ impl P2pClient {
             peer_id: peer_id.to_bytes(),
             addrs: addrs.iter().map(|a| a.to_vec()).collect(),
         });
-        let res = self.0.lock().await.peer_connect(req).await?.into_inner();
+        let res = self.0.clone().peer_connect(req).await?.into_inner();
         Ok(res.success)
     }
 
@@ -103,7 +96,7 @@ impl P2pClient {
         let req = iroh_metrics::req::trace_tonic_req(DisconnectRequest {
             peer_id: peer_id.to_bytes(),
         });
-        let res = self.0.lock().await.peer_disconnect(req).await?.into_inner();
+        let res = self.0.clone().peer_disconnect(req).await?.into_inner();
         Ok(res)
     }
 }
