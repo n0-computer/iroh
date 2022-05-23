f85423729bdc6b759a9d7a529daeb6062400b79a
dignifiedquire <me@dignifiedquire.com>
2022-05-23 13:29:13 +0200

    refactor(bitswap): restructure code layout

--- iroh-bitswap/src/query.rs
+++ iroh-bitswap/src/query.rs
@@ -1,8 +1,10 @@
 use ahash::AHashSet;
+use bytes::Bytes;
 use cid::Cid;
 use libp2p::PeerId;
+use tracing::{error, trace};
 
-use crate::{Block, Priority};
+use crate::{BitswapMessage, Block, Priority};
 
 #[derive(Default, Debug)]
 pub struct QueryManager {
@@ -10,7 +12,7 @@ pub struct QueryManager {
 }
 
 impl QueryManager {
-    pub fn new_query(&mut self, query: Query) {
+    fn new_query(&mut self, query: Query) {
         self.queries.push(query);
     }
 
@@ -22,43 +24,214 @@ impl QueryManager {
         self.queries.len()
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Query> {
-        self.queries.iter_mut()
+    pub fn get(&mut self, cid: Cid, priority: Priority, providers: AHashSet<PeerId>) {
+        self.new_query(Query::Get {
+            providers,
+            cid,
+            priority,
+            state: State::New,
+        });
     }
 
-    pub fn retain<F>(&mut self, f: F)
-    where
-        F: FnMut(&Query) -> bool,
-    {
-        self.queries.retain(f);
+    pub fn send(&mut self, receiver: PeerId, cid: Cid, data: Bytes) {
+        self.new_query(Query::Send {
+            receiver,
+            block: Block { cid, data },
+            state: State::New,
+        });
+    }
+
+    pub fn cancel(&mut self, cid: &Cid) {
+        let mut cancels = Vec::new();
+        self.queries.retain(|query| match query {
+            Query::Get {
+                providers: _,
+                cid: c,
+                priority: _,
+                state,
+            } => {
+                let to_remove = cid == c;
+                if to_remove {
+                    if let State::Sent(providers) = state {
+                        // send out cancels to the providers
+                        cancels.push((providers.clone(), *cid));
+                    }
+                }
+                !to_remove
+            }
+            Query::Cancel { .. } => true,
+            Query::Send { .. } => true,
+        });
+
+        for (providers, cid) in cancels.into_iter() {
+            self.new_query(Query::Cancel {
+                providers,
+                cid,
+                state: State::New,
+            });
+        }
+    }
+
+    pub fn process_block(&mut self, sender: &PeerId, block: &Block) -> (Vec<PeerId>, bool) {
+        let mut cancels = Vec::new();
+        let mut unused_providers = Vec::new();
+        let mut wanted_block = false;
+
+        self.queries.retain(|query| {
+            match query {
+                Query::Get {
+                    providers,
+                    cid,
+                    priority: _,
+                    state,
+                } => {
+                    if &block.cid == cid {
+                        wanted_block = true;
+                        for provider in providers {
+                            unused_providers.push(*provider);
+                        }
+
+                        if let State::Sent(providers) = state {
+                            // send out cancels to the providers
+                            let mut providers = providers.clone();
+                            providers.remove(sender);
+                            if !providers.is_empty() {
+                                cancels.push((providers, block.cid));
+                            }
+                        }
+                        false
+                    } else {
+                        true
+                    }
+                }
+                Query::Cancel { .. } => true,
+                Query::Send { .. } => true,
+            }
+        });
+
+        for (providers, cid) in cancels.into_iter() {
+            self.new_query(Query::Cancel {
+                providers,
+                cid,
+                state: State::New,
+            });
+        }
+
+        (unused_providers, wanted_block)
+    }
+
+    pub fn poll(&mut self, peer_id: &PeerId) -> Option<BitswapMessage> {
+        if self.is_empty() {
+            return None;
+        }
+
+        // Aggregate all queries for this peer
+        let mut msg = BitswapMessage::default();
+
+        trace!("connected {}, looking for queries: {}", peer_id, self.len());
+        let mut num_queries = 0;
+        for query in self.queries.iter_mut().filter(|query| match query {
+            Query::Get { providers, .. } | Query::Cancel { providers, .. } => {
+                providers.contains(peer_id)
+            }
+            Query::Send { receiver, .. } => receiver == peer_id,
+        }) {
+            num_queries += 1;
+            match query {
+                Query::Get {
+                    providers,
+                    cid,
+                    priority,
+                    state,
+                } => {
+                    msg.wantlist_mut().want_block(cid, *priority);
+
+                    providers.remove(peer_id);
+
+                    // update state
+                    match state {
+                        State::New => {
+                            *state = State::Sent([*peer_id].into_iter().collect());
+                        }
+                        State::Sent(sent_providers) => {
+                            sent_providers.insert(*peer_id);
+                        }
+                    }
+                }
+                Query::Cancel {
+                    providers,
+                    cid,
+                    state,
+                } => {
+                    msg.wantlist_mut().cancel_block(cid);
+
+                    providers.remove(peer_id);
+
+                    // update state
+                    match state {
+                        State::New => {
+                            *state = State::Sent([*peer_id].into_iter().collect());
+                        }
+                        State::Sent(sent_providers) => {
+                            sent_providers.insert(*peer_id);
+                        }
+                    }
+                }
+                Query::Send {
+                    block,
+                    state,
+                    receiver,
+                } => match state {
+                    State::New => {
+                        msg.add_block(block.clone());
+                        *state = State::Sent([*receiver].into_iter().collect());
+                    }
+                    State::Sent(_) => {
+                        // nothing to do anymore
+                        num_queries -= 1;
+                    }
+                },
+            }
+        }
+
+        if num_queries > 0 {
+            if msg.is_empty() {
+                error!("{} queries, but message is empty: {:?}", num_queries, msg);
+            } else {
+                trace!("sending message to {} {:?}", peer_id, msg);
+                return Some(msg);
+            }
+        }
+
+        None
     }
 }
 
 #[derive(Debug)]
-pub enum Query {
+enum Query {
     /// Fetch a single CID.
     Get {
         providers: AHashSet<PeerId>,
         cid: Cid,
         priority: Priority,
-        state: QueryState,
+        state: State,
     },
     /// Cancel a single CID.
     Cancel {
         providers: AHashSet<PeerId>,
         cid: Cid,
-        state: QueryState,
+        state: State,
     },
     /// Sends a single Block.
     Send {
         receiver: PeerId,
         block: Block,
-        state: QueryState,
+        state: State,
     },
 }
 
 #[derive(Debug)]
-pub enum QueryState {
+enum State {
     New,
     Sent(AHashSet<PeerId>),
 }
--- iroh-bitswap/src/session.rs
+++ iroh-bitswap/src/session.rs
@@ -1,25 +1,63 @@
+use std::num::NonZeroU8;
+
 use ahash::AHashMap;
-use libp2p::PeerId;
+use libp2p::{
+    swarm::{
+        dial_opts::{DialOpts, PeerCondition},
+        NetworkBehaviourAction, NotifyHandler,
+    },
+    PeerId,
+};
 use tracing::trace;
+
+use crate::{behaviour::BitswapHandler, query::QueryManager, BitswapEvent};
 
 #[derive(Default, Debug)]
 pub struct SessionManager {
     sessions: AHashMap<PeerId, Session>,
+    config: Config,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Config {
+    /// Limit of how many providers are concurrently dialed.
+    pub dial_concurrency_factor_providers: NonZeroU8,
+    /// Limit of how many dials are done per provider peer.
+    pub dial_concurrency_factor_peer: NonZeroU8,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            dial_concurrency_factor_providers: 32.try_into().unwrap(),
+            dial_concurrency_factor_peer: 32.try_into().unwrap(),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Session {
-    pub state: SessionState,
+    state: State,
+    query_count: usize,
 }
 
 impl SessionManager {
-    pub fn unconfirmed_connection(&mut self, peer_id: &PeerId) {
+    pub fn new(config: Config) -> Self {
+        Self {
+            sessions: Default::default(),
+            config,
+        }
+    }
+
+    pub fn new_connection(&mut self, peer_id: &PeerId) {
         let session = self.sessions.entry(*peer_id).or_insert(Session {
-            state: SessionState::Available,
+            state: State::Connected,
+            query_count: 0,
         });
+
         match session.state {
-            SessionState::Dialing(count) | SessionState::New(count) => {
-                session.state = SessionState::Connected(count);
+            State::Dialing | State::New => {
+                session.state = State::Connected;
             }
             _ => {}
         }
@@ -29,58 +67,85 @@ impl SessionManager {
         self.sessions.remove(peer_id);
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&PeerId, &mut Session)> {
-        self.sessions.iter_mut()
-    }
-
     pub fn create_session(&mut self, peer_id: &PeerId) {
         let session = self.sessions.entry(*peer_id).or_insert(Session {
-            state: SessionState::New(1),
+            state: State::New,
+            query_count: 0,
         });
-        match session.state {
-            SessionState::Available => {
-                // already connected
-                trace!("already connected to {}", peer_id);
-                session.state = SessionState::Connected(1);
-            }
-            SessionState::Connected(ref mut count) => {
-                *count += 1;
-            }
-            _ => {}
-        }
+        session.query_count += 1;
     }
 
     pub fn current_dials(&self) -> usize {
         self.sessions
             .values()
-            .filter(|s| matches!(s.state, SessionState::Dialing(_)))
+            .filter(|s| matches!(s.state, State::Dialing))
             .count()
     }
 
     pub fn destroy_session(&mut self, peer_id: &PeerId) {
         if let Some(session) = self.sessions.get_mut(peer_id) {
             match session.state {
-                SessionState::Connected(ref mut count)
-                | SessionState::New(ref mut count)
-                | SessionState::Dialing(ref mut count) => {
-                    *count -= 1;
-                    if *count == 0 {
-                        session.state = SessionState::Available;
-                    }
+                State::Connected | State::New | State::Dialing => {
+                    session.query_count -= 1;
                 }
-                _ => {}
             }
         }
+    }
+
+    pub fn poll(
+        &mut self,
+        queries: &mut QueryManager,
+    ) -> Option<NetworkBehaviourAction<BitswapEvent, BitswapHandler>> {
+        // limit parallel dials
+        let skip_dialing =
+            self.current_dials() >= self.config.dial_concurrency_factor_providers.get() as _;
+
+        for (peer_id, session) in self.sessions.iter_mut() {
+            match session.state {
+                State::New => {
+                    if skip_dialing {
+                        // no dialing this round
+                        continue;
+                    }
+                    trace!("Dialing {}", peer_id);
+                    let handler = Default::default();
+                    session.state = State::Dialing;
+                    return Some(NetworkBehaviourAction::Dial {
+                        opts: DialOpts::peer_id(*peer_id)
+                            .condition(PeerCondition::Always)
+                            .override_dial_concurrency_factor(
+                                self.config.dial_concurrency_factor_peer,
+                            )
+                            .build(),
+                        handler,
+                    });
+                }
+                State::Dialing => {
+                    // Nothing to do yet
+                }
+                State::Connected => {
+                    if let Some(event) = queries.poll(peer_id) {
+                        return Some(NetworkBehaviourAction::NotifyHandler {
+                            peer_id: *peer_id,
+                            handler: NotifyHandler::Any,
+                            event,
+                        });
+                    }
+                }
+            }
+        }
+
+        None
     }
 }
 
 #[derive(Debug)]
-pub enum SessionState {
-    /// Connected, but not used in a query.
-    Available,
+enum State {
     /// Requested in a query, but not connected.
-    New(usize),
-    Dialing(usize),
-    Connected(usize),
+    New,
+    /// Currently Dialing
+    Dialing,
+    /// Connected
+    Connected,
     // Disconnected will be removed from the list
 }
