1f47d7c946a2e4497d5c17058908cc34c03cad4b
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2022-06-20 10:09:15 +0200

    fix: params (#128)

--- iroh-gateway/src/core.rs
+++ iroh-gateway/src/core.rs
@@ -126,8 +126,8 @@ impl Core {
                     .layer(Extension(Arc::clone(&self.state)))
                     .layer(HandleErrorLayer::new(middleware_error_handler))
                     .load_shed()
-                    .concurrency_limit(1024)
-                    .timeout(Duration::from_secs(120))
+                    .concurrency_limit(2048)
+                    .timeout(Duration::from_secs(60))
                     .into_inner(),
             )
             .layer(
