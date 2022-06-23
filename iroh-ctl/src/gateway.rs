d3fbde8e2bb21747d8e055ec0dc74a8b66668173
Kasey <klhuizinga@gmail.com>
2022-06-23 18:24:54 -0400

    stub in requested gateway dev commands

--- iroh-ctl/src/gateway.rs
+++ iroh-ctl/src/gateway.rs
@@ -12,6 +12,22 @@ pub struct Gateway {
 pub enum GatewayCommands {
     #[clap(about = "Version of the iroh gateway binary")]
     Version,
+    Dev(Dev),
+}
+
+#[derive(Args, Debug, Clone)]
+#[clap(hide = true)]
+pub struct Dev {
+    #[clap(subcommand)]
+    command: DevCommands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum DevCommands {
+    #[clap(hide = true)]
+    Get { curl: String },
+    #[clap(hide = true)]
+    Head,
 }
 
 pub async fn run_command(rpc: Client, g: Gateway) -> Result<()> {
@@ -20,6 +36,14 @@ pub async fn run_command(rpc: Client, g: Gateway) -> Result<()> {
             let v = rpc.gateway.version().await?;
             println!("v{}", v);
         }
+        GatewayCommands::Dev(dev) => match dev.command {
+            DevCommands::Get { curl } => {
+                todo!("Get not yet implemented: {}", curl);
+            }
+            DevCommands::Head => {
+                todo!("Head not yet implemented");
+            }
+        },
     }
     Ok(())
 }
