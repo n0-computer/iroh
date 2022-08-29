1bd19598058938ee3a3ecd1632d1f253cfc8116c
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2022-08-29 22:00:38 +0200

    bitswap metrics

--- iroh-metrics/src/bitswap.rs
+++ iroh-metrics/src/bitswap.rs
@@ -15,6 +15,22 @@ pub(crate) struct Metrics {
     sent_block_bytes: Counter,
     received_block_bytes: Counter,
     providers_total: Counter,
+
+    // new metrics
+    known_peers: Counter,
+    forgotten_peers: Counter,
+    wanted_blocks: Counter,
+    want_have_blocks: Counter,
+    cancel_blocks: Counter,
+    cancel_want_blocks: Counter,
+    connected_peers: Counter,
+    disconnected_peers: Counter,
+    messages_sent: Counter,
+    events_backpressure_in: Counter,
+    events_backpressure_out: Counter,
+    poll_action_connected_wants: Counter,
+    poll_action_connected: Counter,
+    poll_action_not_connected: Counter,
 }
 
 impl fmt::Debug for Metrics {
@@ -61,12 +77,84 @@ impl Metrics {
             Box::new(providers_total.clone()),
         );
 
+        // new metrics
+        let known_peers = Counter::default();
+        sub_registry.register("known_peers", "", Box::new(known_peers.clone()));
+        let forgotten_peers = Counter::default();
+        sub_registry.register("forgotten_peers", "", Box::new(forgotten_peers.clone()));
+        let wanted_blocks = Counter::default();
+        sub_registry.register("wanted_blocks", "", Box::new(wanted_blocks.clone()));
+        let want_have_blocks = Counter::default();
+        sub_registry.register("want_have_blocks", "", Box::new(want_have_blocks.clone()));
+        let cancel_blocks = Counter::default();
+        sub_registry.register("cancel_blocks", "", Box::new(cancel_blocks.clone()));
+        let cancel_want_blocks = Counter::default();
+        sub_registry.register(
+            "cancel_want_blocks",
+            "",
+            Box::new(cancel_want_blocks.clone()),
+        );
+        let connected_peers = Counter::default();
+        sub_registry.register("connected_peers", "", Box::new(connected_peers.clone()));
+        let disconnected_peers = Counter::default();
+        sub_registry.register(
+            "disconnected_peers",
+            "",
+            Box::new(disconnected_peers.clone()),
+        );
+        let messages_sent = Counter::default();
+        sub_registry.register("messages_sent", "", Box::new(messages_sent.clone()));
+        let events_backpressure_in = Counter::default();
+        sub_registry.register(
+            "events_backpressure_in",
+            "",
+            Box::new(events_backpressure_in.clone()),
+        );
+        let events_backpressure_out = Counter::default();
+        sub_registry.register(
+            "events_backpressure_out",
+            "",
+            Box::new(events_backpressure_out.clone()),
+        );
+        let poll_action_connected_wants = Counter::default();
+        sub_registry.register(
+            "poll_action_connected_wants",
+            "",
+            Box::new(poll_action_connected_wants.clone()),
+        );
+        let poll_action_connected = Counter::default();
+        sub_registry.register(
+            "poll_action_connected",
+            "",
+            Box::new(poll_action_connected.clone()),
+        );
+        let poll_action_not_connected = Counter::default();
+        sub_registry.register(
+            "poll_action_not_connected",
+            "",
+            Box::new(poll_action_not_connected.clone()),
+        );
+
         Self {
             requests_total,
             canceled_total,
             sent_block_bytes,
             received_block_bytes,
             providers_total,
+            known_peers,
+            forgotten_peers,
+            wanted_blocks,
+            want_have_blocks,
+            cancel_blocks,
+            cancel_want_blocks,
+            connected_peers,
+            disconnected_peers,
+            messages_sent,
+            events_backpressure_in,
+            events_backpressure_out,
+            poll_action_connected_wants,
+            poll_action_connected,
+            poll_action_not_connected,
         }
     }
 }
@@ -86,6 +174,34 @@ impl MetricsRecorder for Metrics {
             self.received_block_bytes.inc_by(value);
         } else if m.name() == BitswapMetrics::Providers.name() {
             self.providers_total.inc_by(value);
+        } else if m.name() == BitswapMetrics::KnownPeers.name() {
+            self.known_peers.inc_by(value);
+        } else if m.name() == BitswapMetrics::ForgottenPeers.name() {
+            self.forgotten_peers.inc_by(value);
+        } else if m.name() == BitswapMetrics::WantedBlocks.name() {
+            self.wanted_blocks.inc_by(value);
+        } else if m.name() == BitswapMetrics::WantHaveBlocks.name() {
+            self.want_have_blocks.inc_by(value);
+        } else if m.name() == BitswapMetrics::CancelBlocks.name() {
+            self.cancel_blocks.inc_by(value);
+        } else if m.name() == BitswapMetrics::CancelWantBlocks.name() {
+            self.cancel_want_blocks.inc_by(value);
+        } else if m.name() == BitswapMetrics::ConnectedPeers.name() {
+            self.connected_peers.inc_by(value);
+        } else if m.name() == BitswapMetrics::DisconnectedPeers.name() {
+            self.disconnected_peers.inc_by(value);
+        } else if m.name() == BitswapMetrics::MessagesSent.name() {
+            self.messages_sent.inc_by(value);
+        } else if m.name() == BitswapMetrics::EventsBackpressureIn.name() {
+            self.events_backpressure_in.inc_by(value);
+        } else if m.name() == BitswapMetrics::EventsBackpressureOut.name() {
+            self.events_backpressure_out.inc_by(value);
+        } else if m.name() == BitswapMetrics::PollActionConnectedWants.name() {
+            self.poll_action_connected_wants.inc_by(value);
+        } else if m.name() == BitswapMetrics::PollActionConnected.name() {
+            self.poll_action_connected.inc_by(value);
+        } else if m.name() == BitswapMetrics::PollActionNotConnected.name() {
+            self.poll_action_not_connected.inc_by(value);
         } else {
             error!("record (bitswap): unknown metric {}", m.name());
         }
@@ -106,6 +222,21 @@ pub enum BitswapMetrics {
     BlockBytesOut,
     BlockBytesIn,
     Providers,
+
+    KnownPeers,
+    ForgottenPeers,
+    WantedBlocks,
+    WantHaveBlocks,
+    CancelBlocks,
+    CancelWantBlocks,
+    ConnectedPeers,
+    DisconnectedPeers,
+    MessagesSent,
+    EventsBackpressureIn,
+    EventsBackpressureOut,
+    PollActionConnectedWants,
+    PollActionConnected,
+    PollActionNotConnected,
 }
 
 impl MetricType for BitswapMetrics {
@@ -116,6 +247,21 @@ impl MetricType for BitswapMetrics {
             BitswapMetrics::BlockBytesOut => METRICS_CNT_BLOCK_BYTES_OUT,
             BitswapMetrics::BlockBytesIn => METRICS_CNT_BLOCK_BYTES_IN,
             BitswapMetrics::Providers => METRICS_CNT_PROVIDERS_TOTAL,
+
+            BitswapMetrics::KnownPeers => METRICS_CNT_KNOWN_PEERS,
+            BitswapMetrics::ForgottenPeers => METRICS_CNT_FORGOTTEN_PEERS,
+            BitswapMetrics::WantedBlocks => METRICS_CNT_WANTED_BLOCKS,
+            BitswapMetrics::WantHaveBlocks => METRICS_CNT_WANT_HAVE_BLOCKS,
+            BitswapMetrics::CancelBlocks => METRICS_CNT_CANCEL_BLOCKS,
+            BitswapMetrics::CancelWantBlocks => METRICS_CNT_CANCEL_WANT_BLOCKS,
+            BitswapMetrics::ConnectedPeers => METRICS_CNT_CONNECTED_PEERS,
+            BitswapMetrics::DisconnectedPeers => METRICS_CNT_DISCONNECTED_PEERS,
+            BitswapMetrics::MessagesSent => METRICS_CNT_MESSAGES_SENT,
+            BitswapMetrics::EventsBackpressureIn => METRICS_CNT_EVENTS_BACKPRESSURE_IN,
+            BitswapMetrics::EventsBackpressureOut => METRICS_CNT_EVENTS_BACKPRESSURE_OUT,
+            BitswapMetrics::PollActionConnectedWants => METRICS_CNT_POLL_ACTION_CONNECTED_WANTS,
+            BitswapMetrics::PollActionConnected => METRICS_CNT_POLL_ACTION_CONNECTED,
+            BitswapMetrics::PollActionNotConnected => METRICS_CNT_POLL_ACTION_NOT_CONNECTED,
         }
     }
 }
@@ -137,3 +283,17 @@ pub const METRICS_CNT_CANCEL_TOTAL: &str = "canceled";
 pub const METRICS_CNT_BLOCK_BYTES_OUT: &str = "block_bytes_out";
 pub const METRICS_CNT_BLOCK_BYTES_IN: &str = "block_bytes_in";
 pub const METRICS_CNT_PROVIDERS_TOTAL: &str = "providers";
+pub const METRICS_CNT_KNOWN_PEERS: &str = "known_peers";
+pub const METRICS_CNT_FORGOTTEN_PEERS: &str = "forgotten_peers";
+pub const METRICS_CNT_WANTED_BLOCKS: &str = "wanted_blocks";
+pub const METRICS_CNT_WANT_HAVE_BLOCKS: &str = "want_have_blocks";
+pub const METRICS_CNT_CANCEL_BLOCKS: &str = "cancel_blocks";
+pub const METRICS_CNT_CANCEL_WANT_BLOCKS: &str = "cancel_want_blocks";
+pub const METRICS_CNT_CONNECTED_PEERS: &str = "connected_peers";
+pub const METRICS_CNT_DISCONNECTED_PEERS: &str = "disconnected_peers";
+pub const METRICS_CNT_MESSAGES_SENT: &str = "messages_sent";
+pub const METRICS_CNT_EVENTS_BACKPRESSURE_IN: &str = "events_backpressure_in";
+pub const METRICS_CNT_EVENTS_BACKPRESSURE_OUT: &str = "events_backpressure_out";
+pub const METRICS_CNT_POLL_ACTION_CONNECTED_WANTS: &str = "poll_action_connected_wants";
+pub const METRICS_CNT_POLL_ACTION_CONNECTED: &str = "poll_action_connected";
+pub const METRICS_CNT_POLL_ACTION_NOT_CONNECTED: &str = "poll_action_not_connected";
