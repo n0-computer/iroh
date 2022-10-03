b006c2f2652f8ae75cc52d4a16ba5c2f84bcb3ef
Martijn Faassen <faassen@startifact.com>
2022-10-03 16:17:59 +0200

    Exposing status is really tricky, sketch out some complexity

--- iroh/src/api.rs
+++ iroh/src/api.rs
@@ -6,9 +6,9 @@ use crate::store::{ClientStore, MockStore, Store};
 use anyhow::Result;
 use async_trait::async_trait;
 use cid::Cid;
-use futures::Stream;
 use iroh_resolver::resolver::Path as IpfsPath;
 use iroh_rpc_client::Client;
+use iroh_rpc_client::{StatusRow, StatusTable};
 use mockall::automock;
 
 pub struct ClientApi<'a> {
@@ -21,13 +21,15 @@ pub trait Api {
     type P: P2p;
     type S: Store;
 
+    // type StatusTableStream: futures::Stream<Item = Result<iroh_rpc_client::StatusTable>>;
+
     fn p2p(&self) -> Result<Self::P>;
     fn store(&self) -> Result<Self::S>;
     async fn get<'a>(&self, ipfs_path: &IpfsPath, output: Option<&'a Path>) -> Result<()>;
     async fn add(&self, path: &Path, recursive: bool, no_wrap: bool) -> Result<Cid>;
     async fn check(&self) -> iroh_rpc_client::StatusTable;
     // This won't work
-    // async fn watch(&self) -> futures::Stream<Item = iroh_rpc_client::StatusTable>;
+    // async fn watch(&self) -> Self::StatusTableStream;
 }
 
 impl<'a> ClientApi<'a> {
@@ -40,6 +42,7 @@ impl<'a> ClientApi<'a> {
 impl<'a> Api for ClientApi<'a> {
     type P = ClientP2p<'a>;
     type S = ClientStore<'a>;
+    // type StatusTableStream = Box<dyn Stream<Item = StatusRow>>;
 
     fn p2p(&self) -> Result<ClientP2p<'a>> {
         let p2p_client = self.client.try_p2p()?;
