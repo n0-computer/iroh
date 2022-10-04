082c4184e66dee0dda761907a23b0313021170b3
Martijn Faassen <faassen@startifact.com>
2022-10-04 11:22:56 +0200

    Clarify this comment

--- iroh-ctl/src/main.rs
+++ iroh-ctl/src/main.rs
@@ -4,7 +4,11 @@ use clap::Parser;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     let cli = iroh_ctl::run::Cli::parse();
-    // run_cli exists in two versions, one for real client interaction,
-    // and one for testing purposes using mocked fixtures of the API
+    // `run_cli` exists in two versions:
+    // When using the `fixture` feature, the
+    // version of `run_cli` designed for testing purposes using mocked test
+    // fixtures is invoked.
+    // Without the `fixture` feature, the version of
+    // `run_cli` that interacts with the real Iroh API is used.
     iroh_ctl::run::run_cli(cli).await
 }
