372e79e040baeeacdc553e78ab620e9b4996542b
Martijn Faassen <faassen@startifact.com>
2022-10-06 16:29:31 +0200

    refactor: make things less public where I can

--- iroh-one/src/lib.rs
+++ iroh-one/src/lib.rs
@@ -1,15 +1,15 @@
 pub mod cli;
-pub mod config_ctl; // does this need to be pub?
+mod config_ctl;
 pub mod config_one;
 pub mod content_loader;
 #[cfg(feature = "testing")]
-pub mod fixture; // XXX remove pub once move is complete
-pub mod gateway_cli; // XXX remove pub once move is complete
+mod fixture;
+mod gateway_cli;
 pub mod mem_p2p;
 pub mod mem_store;
-pub mod p2p_cli;
+mod p2p_cli;
 mod start;
-pub mod status; // XXX remove pub once move is complete
-pub mod store_cli; // XXX remove pub once move is complete
+mod status;
+mod store_cli;
 #[cfg(feature = "uds-gateway")]
 pub mod uds; // XXX remove pub once move is complete
