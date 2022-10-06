6f5562e28474356268678598e49dd1e0f64a48bf
Rüdiger Klaehn <rklaehn@protonmail.com>
2022-10-06 15:55:12 +0300

    test(unixfs): property based tests for unixfs (#289)
    * test(unixfs): add property based roundtrip tests for files and directories
    
    * refactor(unixfs): DRY manual tests by calling file_roundtrip_test
    
    * refactor(unixfs): separate creation of TestDir from actual test
    
    * perf(tests): use Arc to make cloning resover cheaper
    
    This avoids having to clone the entire store multiple times in tests.
    I measured it, it makes a big difference with large file roundtrip tests.
    
    Also directly control the size of the file for the file roundtrip test.
    
    * refactor(tests): Use FnvHashMap instead of the default randomizing HashMap
    
    Makes a bit of difference in performance (~10%), and eliminates a source of nondeterminism from the
    tests.
    
    To make this work, implement ContentLoader for any hashmap, not just the one with the default
    BuildHasher.

--- iroh-resolver/src/unixfs_builder.rs
+++ iroh-resolver/src/unixfs_builder.rs
@@ -532,14 +532,15 @@ async fn make_dir_from_path<P: Into<PathBuf>>(path: P, recursive: bool) -> Resul
 
 #[cfg(test)]
 mod tests {
-    use crate::resolver::{OutMetrics, Resolver};
+    use crate::resolver::{ContentLoader, Out, OutMetrics, Resolver};
 
     use super::*;
     use anyhow::{Context, Result};
     use futures::TryStreamExt;
+    use proptest::prelude::*;
     use rand::prelude::*;
     use rand_chacha::ChaCha8Rng;
-    use std::{collections::HashMap, io::prelude::*};
+    use std::{collections::BTreeMap, io::prelude::*, sync::Arc};
     use tokio::io::AsyncReadExt;
 
     #[tokio::test]
@@ -662,38 +663,132 @@ mod tests {
     }
 
     // read an AsyncRead into a vec completely
-    async fn read_to_vec<T: AsyncRead + Unpin>(mut reader: T) -> Vec<u8> {
+    async fn read_to_vec<T: AsyncRead + Unpin>(mut reader: T) -> Result<Vec<u8>> {
         let mut out = Vec::new();
-        reader.read_to_end(&mut out).await.unwrap();
-        out
+        reader.read_to_end(&mut out).await?;
+        Ok(out)
     }
 
     /// Read a stream of (cid, block) pairs into an in memory store and return the store and the root cid
     async fn stream_to_resolver(
         stream: impl Stream<Item = Result<(Cid, Bytes)>>,
-    ) -> Result<(Cid, Resolver<HashMap<Cid, Bytes>>)> {
+    ) -> Result<(Cid, Resolver<Arc<fnv::FnvHashMap<Cid, Bytes>>>)> {
         tokio::pin!(stream);
         let items: Vec<_> = stream.try_collect().await?;
         let (root, _) = items.last().context("no root")?.clone();
-        let store: HashMap<Cid, Bytes> = items.into_iter().collect();
-        println!("{}", store.len());
-        let resolver = Resolver::new(store);
+        let store: fnv::FnvHashMap<Cid, Bytes> = items.into_iter().collect();
+        let resolver = Resolver::new(Arc::new(store));
         Ok((root, resolver))
     }
 
-    // test is ignored because it currently fails
-    #[tokio::test]
-    async fn test_builder_roundtrip_complex_tree_1() -> Result<()> {
-        // fill with random data so we get distinct cids for all blocks
-        let mut rng = ChaCha8Rng::from_seed([0; 32]);
-        let mut data = vec![0u8; 1024 * 128];
-        rng.fill(data.as_mut_slice());
-        let data: Bytes = data.into();
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    enum TestDirEntry {
+        File(Bytes),
+        Directory(TestDir),
+    }
+    type TestDir = BTreeMap<String, TestDirEntry>;
+
+    /// builds an unixfs directory out of a TestDir
+    #[async_recursion(?Send)]
+    async fn build_directory(name: &str, dir: &TestDir) -> Result<Directory> {
+        let mut builder = DirectoryBuilder::new();
+        builder.recursive();
+        builder.name(name);
+        for (name, entry) in dir {
+            match entry {
+                TestDirEntry::File(content) => {
+                    let mut file = FileBuilder::new();
+                    file.name(name).content_bytes(content.to_vec());
+                    builder.add_file(file.build().await?);
+                }
+                TestDirEntry::Directory(dir) => {
+                    let dir = build_directory(name, dir).await?;
+                    builder.add_dir(dir)?;
+                }
+            }
+        }
+        builder.build()
+    }
+
+    /// builds a TestDir out of a stream of blocks and a resolver
+    async fn build_testdir(
+        stream: impl Stream<Item = Result<(crate::resolver::Path, Out)>>,
+        resolver: Resolver<impl ContentLoader + Unpin>,
+    ) -> Result<TestDir> {
+        tokio::pin!(stream);
+
+        /// recursively create directories for a path
+        fn mkdir(dir: &mut TestDir, path: &[String]) -> Result<()> {
+            if let Some((first, rest)) = path.split_first() {
+                if let TestDirEntry::Directory(child) = dir
+                    .entry(first.clone())
+                    .or_insert_with(|| TestDirEntry::Directory(Default::default()))
+                {
+                    mkdir(child, rest)?;
+                } else {
+                    anyhow::bail!("not a directory");
+                }
+            }
+            Ok(())
+        }
+
+        /// create a file in a directory hierarchy
+        fn mkfile(dir: &mut TestDir, path: &[String], data: Bytes) -> Result<()> {
+            if let Some((first, rest)) = path.split_first() {
+                if rest.is_empty() {
+                    dir.insert(first.clone(), TestDirEntry::File(data));
+                } else if let TestDirEntry::Directory(child) = dir
+                    .entry(first.clone())
+                    .or_insert_with(|| TestDirEntry::Directory(Default::default()))
+                {
+                    mkfile(child, rest, data)?;
+                } else {
+                    anyhow::bail!("not a directory");
+                }
+            }
+            Ok(())
+        }
+
+        let reference = stream
+            .try_fold(TestDir::default(), move |mut agg, (path, item)| {
+                let resolver = resolver.clone();
+                async move {
+                    if item.is_dir() {
+                        mkdir(&mut agg, path.tail())?;
+                    } else {
+                        let reader = item.pretty(resolver.clone(), OutMetrics::default())?;
+                        let data = read_to_vec(reader).await?;
+                        mkfile(&mut agg, path.tail(), data.into())?;
+                    }
+                    Ok(agg)
+                }
+            })
+            .await?;
+        Ok(reference)
+    }
+
+    /// a roundtrip test that converts a dir to an unixfs DAG and back
+    async fn dir_roundtrip_test(dir: TestDir) -> Result<bool> {
+        let directory = build_directory("", &dir).await?;
+        let stream = directory.encode();
+        let (root, resolver) = stream_to_resolver(stream).await?;
+        let stream = resolver.resolve_recursive_with_paths(crate::resolver::Path::from_cid(root));
+        let reference = build_testdir(stream, resolver).await?;
+        Ok(dir == reference)
+    }
+
+    /// sync version of dir_roundtrip_test for use in proptest
+    fn dir_roundtrip_test_sync(dir: TestDir) -> bool {
+        futures::executor::block_on(dir_roundtrip_test(dir)).unwrap()
+    }
+
+    /// a roundtrip test that converts a file to an unixfs DAG and back
+    async fn file_roundtrip_test(data: Bytes, chunk_size: usize, degree: usize) -> Result<bool> {
         let mut builder = FileBuilder::new();
         builder
             .name("file.bin")
-            .chunk_size(1024)
-            .degree(4)
+            .chunk_size(chunk_size)
+            .degree(degree)
             .content_bytes(data.clone());
         let file = builder.build().await?;
         let stream = file.encode().await?;
@@ -701,31 +796,67 @@ mod tests {
         let out = resolver
             .resolve(crate::resolver::Path::from_cid(root))
             .await?;
-        let t = read_to_vec(out.pretty(resolver, OutMetrics::default())?).await;
-        assert_eq!(t.len(), data.len());
-        assert_eq!(t, data);
+        let t = read_to_vec(out.pretty(resolver, OutMetrics::default())?).await?;
+        println!("{}", data.len());
+        Ok(t == data)
+    }
+
+    /// sync version of file_roundtrip_test for use in proptest
+    fn file_roundtrip_test_sync(data: Bytes, chunk_size: usize, degree: usize) -> bool {
+        let f = file_roundtrip_test(data, chunk_size, degree);
+        futures::executor::block_on(f).unwrap()
+    }
+
+    fn arb_test_dir() -> impl Strategy<Value = TestDir> {
+        // create an arbitrary nested directory structure
+        fn arb_dir_entry() -> impl Strategy<Value = TestDirEntry> {
+            let leaf = any::<Vec<u8>>().prop_map(|x| TestDirEntry::File(Bytes::from(x)));
+            leaf.prop_recursive(3, 64, 10, |inner| {
+                prop::collection::btree_map(".*", inner, 0..10).prop_map(TestDirEntry::Directory)
+            })
+        }
+        prop::collection::btree_map(".*", arb_dir_entry(), 0..10)
+    }
+
+    fn arb_degree() -> impl Strategy<Value = usize> {
+        // use either the smallest possible degree for complex tree structures, or the default value for realism
+        prop_oneof![Just(2), Just(DEFAULT_DEGREE)]
+    }
+
+    fn arb_chunk_size() -> impl Strategy<Value = usize> {
+        // use either the smallest possible chunk size for complex tree structures, or the default value for realism
+        prop_oneof![Just(1), Just(DEFAULT_CHUNKS_SIZE)]
+    }
+
+    proptest! {
+        #[test]
+        fn test_file_roundtrip(data in proptest::collection::vec(any::<u8>(), 0usize..1024), chunk_size in arb_chunk_size(), degree in arb_degree()) {
+            assert!(file_roundtrip_test_sync(data.into(), chunk_size, degree));
+        }
+
+        #[test]
+        fn test_dir_roundtrip(data in arb_test_dir()) {
+            assert!(dir_roundtrip_test_sync(data));
+        }
+    }
+
+    #[tokio::test]
+    async fn test_builder_roundtrip_complex_tree_1() -> Result<()> {
+        // fill with random data so we get distinct cids for all blocks
+        let mut rng = ChaCha8Rng::from_seed([0; 32]);
+        let mut data = vec![0u8; 1024 * 128];
+        rng.fill(data.as_mut_slice());
+        assert!(file_roundtrip_test(data.into(), 1024, 4).await?);
         Ok(())
     }
 
-    // test is ignored because it currently fails
     #[tokio::test]
     async fn test_builder_roundtrip_128m() -> Result<()> {
         // fill with random data so we get distinct cids for all blocks
         let mut rng = ChaCha8Rng::from_seed([0; 32]);
         let mut data = vec![0u8; 128 * 1024 * 1024];
         rng.fill(data.as_mut_slice());
-        let data: Bytes = data.into();
-        let mut builder = FileBuilder::new();
-        builder.name("128m.bin").content_bytes(data.clone());
-        let file = builder.build().await?;
-        let stream = file.encode().await?;
-        let (root, resolver) = stream_to_resolver(stream).await?;
-        let out = resolver
-            .resolve(crate::resolver::Path::from_cid(root))
-            .await?;
-        let t = read_to_vec(out.pretty(resolver, OutMetrics::default())?).await;
-        assert_eq!(t.len(), data.len());
-        assert_eq!(t, data);
+        assert!(file_roundtrip_test(data.into(), DEFAULT_CHUNKS_SIZE, DEFAULT_DEGREE).await?);
         Ok(())
     }
 
