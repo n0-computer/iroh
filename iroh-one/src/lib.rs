d5dda29425835f98592512cc610b8bef1064b561
Martijn Faassen <faassen@startifact.com>
2022-10-06 15:23:29 +0200

    other: a reminder

--- iroh-one/src/lib.rs
+++ iroh-one/src/lib.rs
@@ -1,11 +1,11 @@
 pub mod cli;
-pub mod config_ctl;
+pub mod config_ctl; // does this need to be pub?
 pub mod config_one;
 pub mod content_loader;
 #[cfg(feature = "testing")]
 pub mod fixture; // XXX remove pub once move is complete
 pub mod mem_p2p;
 pub mod mem_store;
-pub mod status;
+pub mod status; // XXX remove pub once move is complete
 #[cfg(feature = "uds-gateway")]
 pub mod uds;
