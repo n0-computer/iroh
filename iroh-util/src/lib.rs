bddbae90f49c7ea45a2900ad1fa4560d9482bd60
Fabrice Desré <fabrice@desre.org>
2022-10-10 10:51:12 -0700

    feat: provide a mechanism to check if an iroh program is already running. (#293)

--- iroh-util/src/lib.rs
+++ iroh-util/src/lib.rs
@@ -16,6 +16,8 @@ use cid::{
 use config::{Config, ConfigError, Environment, File, Map, Source, Value, ValueKind};
 use tracing::debug;
 
+pub mod lock;
+
 /// name of directory that wraps all iroh files in a given application directory
 const IROH_DIR: &str = "iroh";
 const DEFAULT_NOFILE_LIMIT: u64 = 65536;
