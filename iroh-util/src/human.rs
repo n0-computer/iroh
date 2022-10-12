0e11393bd69187ef8d768de62b69eaa2362342f5
b5 <sparkle_pony_2000@qri.io>
2022-10-12 16:48:41 -0400

    use HashSet, flip back to Decimal units

--- iroh-util/src/human.rs
+++ iroh-util/src/human.rs
@@ -1,9 +1,9 @@
-use humansize::{format_size, BINARY};
+use humansize::{format_size, DECIMAL};
 
 /// Format byte count as a human-readable size string eg: 1_000_000u64 -> "1 MB"
 /// this func isolates a library + configuration choice
 pub fn format_bytes(size: u64) -> String {
-  format_size(size, BINARY)
+  format_size(size, DECIMAL)
 }
 
 
@@ -12,6 +12,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_format_bytes() {
-        assert_eq!(format_bytes(1_048_576u64), "1 MiB");
+        assert_eq!(format_bytes(1_000_000u64), "1 MB");
     }
 }
