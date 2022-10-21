6a05491feda5f59d2a30379642bcd056763323af
b5 <sparkle_pony_2000@qri.io>
2022-10-21 15:15:55 -0400

    test slow fs theory

--- iroh-util/src/lock.rs
+++ iroh-util/src/lock.rs
@@ -282,7 +282,8 @@ mod test {
                 Ok(Parent { child: _ }) => {
                     let _ = std::fs::remove_file("lock_test.result");
 
-                    std::thread::sleep(Duration::from_secs(1));
+                    // This needs to be long because github actions + macos filesystem is waaaaay slow
+                    std::thread::sleep(Duration::from_secs(3));
 
                     let mut result = std::fs::File::open("lock_test.result").unwrap();
                     let mut buf = String::new();
