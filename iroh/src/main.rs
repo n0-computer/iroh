7a945ed602af50f1a5f0d15f2079f351a3037650
Martijn Faassen <faassen@startifact.com>
2022-10-21 02:55:09 +0200

    fix: Improve error handling  (#379)

--- iroh/src/main.rs
+++ iroh/src/main.rs
@@ -1,5 +1,6 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use std::io;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
@@ -10,5 +11,30 @@ async fn main() -> Result<()> {
     // fixtures is invoked.
     // Without the `testing` feature, the version of
     // `run` that interacts with the real Iroh API is used.
-    cli.run().await
+    let r = cli.run().await;
+    let r = transform_error(r);
+    match r {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn transform_error(r: Result<()>) -> Result<()> {
+    match r {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            let io_error = e.root_cause().downcast_ref::<io::Error>();
+            if let Some(io_error) = io_error {
+                if io_error.kind() == io::ErrorKind::ConnectionRefused {
+                    return Err(anyhow!(
+                        "Connection refused. Are `iroh-p2p` and `iroh-store` running?"
+                    ));
+                }
+            }
+            Err(e)
+        }
+    }
 }
