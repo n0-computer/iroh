1bc985ad82f7158bd607fd3bb3a7102710439abe
b5 <sparkle_pony_2000@qri.io>
2022-10-23 12:38:31 -0400

    fmt

--- iroh-api/src/lib.rs
+++ iroh-api/src/lib.rs
@@ -8,6 +8,7 @@ mod p2p;
 pub use crate::api::MockApi;
 pub use crate::api::{Api, Iroh, OutType};
 pub use crate::api_ext::ApiExt;
+pub use crate::error::ApiError;
 #[cfg(feature = "testing")]
 pub use crate::p2p::MockP2p;
 pub use crate::p2p::P2p as P2pApi;
@@ -19,4 +20,3 @@ pub use iroh_resolver::unixfs_builder::AddEvent;
 pub use iroh_rpc_client::{Lookup, ServiceStatus, StatusRow, StatusTable};
 pub use libp2p::gossipsub::MessageId;
 pub use libp2p::{Multiaddr, PeerId};
-pub use crate::error::ApiError;
