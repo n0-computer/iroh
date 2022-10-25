9e441a2e6dc5716b4a23dd27c05cc3ae58631166
dignifiedquire <me@dignifiedquire.com>
2022-10-25 18:29:02 +0100

    filter by transport

--- iroh-resolver/src/indexer.rs
+++ iroh-resolver/src/indexer.rs
@@ -32,6 +32,7 @@ impl Indexer {
         Ok(Self { client, endpoint })
     }
 
+    /// Returns all available bitswap providers.
     pub async fn find_providers(&self, cid: Cid) -> Result<Vec<Provider>> {
         let url = self.endpoint.join(&cid.to_string())?;
         trace!("requesting providers from {}", url);
@@ -46,7 +47,13 @@ impl Indexer {
             .multihash_results
             .into_iter()
             .flat_map(|r| r.provider_results)
-            .map(|r| r.provider)
+            .filter_map(|r| {
+                if r.metadata == Transport::Bitswap {
+                    Some(r.provider)
+                } else {
+                    None
+                }
+            })
             .collect();
         Ok(providers)
     }
@@ -61,7 +68,7 @@ struct FindResult {
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "PascalCase")]
 struct MultihashResult {
-    #[serde(with = "base64")]
+    #[serde(with = "base64_multihash")]
     pub multihash: Multihash,
     pub provider_results: Vec<ProviderResult>,
 }
@@ -71,11 +78,12 @@ struct MultihashResult {
 struct ProviderResult {
     #[serde(rename = "ContextID")]
     pub context_id: String,
-    pub metadata: String,
+    #[serde(with = "base64_provider")]
+    pub metadata: Transport,
     pub provider: Provider,
 }
 
-mod base64 {
+mod base64_multihash {
     use multihash::Multihash;
     use serde::{de, Deserialize, Deserializer, Serializer};
 
@@ -94,6 +102,52 @@ mod base64 {
         let bytes = base64::decode(s).map_err(de::Error::custom)?;
         let multihash = Multihash::from_bytes(&bytes).map_err(de::Error::custom)?;
         Ok(multihash)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum Transport {
+    Bitswap = 0x900,
+    FilecoinGraphsyncV1 = 0x910,
+    Unknown,
+}
+
+impl From<u32> for Transport {
+    fn from(raw: u32) -> Self {
+        if raw == 0x900 {
+            Transport::Bitswap
+        } else if raw == 0x910 {
+            Transport::FilecoinGraphsyncV1
+        } else {
+            Transport::Unknown
+        }
+    }
+}
+
+mod base64_provider {
+    use serde::{de, Deserialize, Deserializer, Serializer};
+
+    use super::Transport;
+
+    pub fn serialize<S>(t: &Transport, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut bytes = unsigned_varint::encode::u32_buffer();
+        unsigned_varint::encode::u32(*t as u32, &mut bytes);
+        serializer.serialize_str(&base64::encode(&bytes))
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Transport, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = <&str>::deserialize(deserializer)?;
+        let bytes = base64::decode(s).map_err(de::Error::custom)?;
+        let (raw, _) = unsigned_varint::decode::u32(&bytes).map_err(de::Error::custom)?;
+        let transport = Transport::from(raw);
+        Ok(transport)
     }
 }
 
@@ -127,6 +181,7 @@ mod tests {
                 .unwrap(),
         );
         assert_eq!(res.provider_results[0].provider.addrs.len(), 1);
+        assert_eq!(res.provider_results[0].metadata, Transport::Bitswap);
     }
 
     #[tokio::test]
