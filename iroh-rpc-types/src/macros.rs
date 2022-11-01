n0-computer/beetle

integrate tarpc instead of grpc

iroh-rpc-types/src/macros.rs
@@ -1,261 +1,73 @@
-macro_rules! proxy_serve {
-    ($label:ident, $($name:ident: $req:ty => $res:ty),+) => {
-        paste::paste! {
-            pub async fn serve<T: $label>(addr: [<$label ServerAddr>], source: T) -> anyhow::Result<()> {
-                match addr {
-                    #[cfg(feature = "grpc")]
-                    $crate::Addr::GrpcHttp2(addr) => {
-                        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-                        health_reporter
-                            .set_serving::<[<$label:lower _server>]::[<$label Server>]<T>>()
-                            .await;
-
-                        tonic::transport::Server::builder()
-                            .add_service(health_service)
-                            .add_service([<$label:lower _server>]::[<$label Server>]::new(source))
-                            .serve(addr)
-                            .await?;
-
-                        Ok(())
-                    }
-
-                    #[cfg(all(feature = "grpc", unix))]
-                    $crate::Addr::GrpcUds(path) => {
-                        use anyhow::Context;
-                        use tokio::net::UnixListener;
-                        use tokio_stream::wrappers::UnixListenerStream;
-
-                        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-                        health_reporter
-                            .set_serving::<[<$label:lower _server>]::[<$label Server>]<T>>()
-                            .await;
-
-                        if path.exists() {
-                            if path.is_dir() {
-                                anyhow::bail!("cannot bind socket to directory: {}", path.display());
-                            } else {
-                                anyhow::bail!("cannot bind socket: already exists: {}", path.display());
-                            }
-                        }
-
-                        // If the parent directory doesn't exist, we'll fail to bind.
-                        // Create a more precise error to recognize that case.
-                        if let Some(parent) = path.parent() {
-                            if !parent.exists() {
-                                anyhow::bail!("socket parent directory doesn't exist: {}", parent.display());
-                            }
-                        }
-
-                        // Delete file on close
-                        struct UdsGuard(std::path::PathBuf);
-                        impl Drop for UdsGuard {
-                            fn drop(&mut self) {
-                                let _ = std::fs::remove_file(&self.0);
-                            }
-                        }
-
-                        let uds = UnixListener::bind(&path)
-                            .with_context(|| format!("failed to bind to {}", path.display()))?;
-                        let _guard = UdsGuard(path.clone().into());
-
-                        let uds_stream = UnixListenerStream::new(uds);
-
-                        tonic::transport::Server::builder()
-                            .add_service(health_service)
-                            .add_service([<$label:lower _server>]::[<$label Server>]::new(source))
-                            .serve_with_incoming(uds_stream)
-                            .await?;
-
-                        Ok(())
-                    }
-                    #[cfg(feature = "mem")]
-                    $crate::Addr::Mem(mut receiver) => {
-                        while let Some((msg, sender)) = receiver.recv().await {
-                            match msg {
-                                $(
-                                    [<$label Request>]::$name(req) => {
-                                        let res = source.$name(req).await.map_err(|e| e.to_string());
-                                        sender.send([<$label Response>]::$name(res)).ok();
-                                    }
-                                )+
-                            }
-                        }
-
-                        Ok(())
-                    }
-                }
-            }
-        }
-    }
-}
-
-macro_rules! proxy_serve_types {
-    ($label:ident, $($name:ident: $req:ty => $res:ty),+) => {
+#[macro_export]
+macro_rules! impl_serve {
+    ($label:ident, $server:ty, $req:ty, $resp:ty) => {
         paste::paste! {
             pub type [<$label ServerAddr>] = $crate::Addr<
-                    tokio::sync::mpsc::Receiver<
-                            ([<$label Request>], tokio::sync::oneshot::Sender<[<$label Response>]>),
-                        >
-                    >;
-            pub type [<$label ClientAddr>] = $crate::Addr<
-                    tokio::sync::mpsc::Sender<
-                            ([<$label Request>], tokio::sync::oneshot::Sender<[<$label Response>]>),
-                        >
-                    >;
+                tarpc::ClientMessage<$req>, tarpc::Response<$resp>
+            >;
 
-            #[derive(Debug, Clone)]
-            #[allow(clippy::large_enum_variant)]
-            pub enum [<$label ClientBackend>] {
-                #[cfg(feature = "grpc")]
-                Grpc {
-                    client: [<$label:lower _client>]::[<$label Client>]<tonic::transport::Channel>,
-                    health: tonic_health::proto::health_client::HealthClient<tonic::transport::Channel>,
-                },
-                #[cfg(feature = "mem")]
-                Mem(
-                    tokio::sync::mpsc::Sender<(
-                        [<$label Request>],
-                        tokio::sync::oneshot::Sender<[<$label Response>]>
-                    )>
-                ),
-            }
-
-            #[allow(non_camel_case_types)]
-            #[derive(Debug, Clone)]
-            pub enum [<$label Request>] {
-                $(
-                    $name($req),
-                )+
-            }
-
-            #[allow(non_camel_case_types)]
-            pub enum [<$label Response>] {
-                $(
-                    $name(Result<$res, String>),
-                )+
-            }
-        }
-    }
-}
-
-macro_rules! proxy_traits {
-    ($label:ident, $($name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?),+) => {
-        paste::paste! {
-            #[async_trait::async_trait]
-            pub trait $label: Send + Sync + 'static {
-                $(
-                    async fn $name(&self, request: $req) -> anyhow::Result<$res>;
-                )+
-            }
-
-            #[async_trait::async_trait]
-            impl $label for [<$label ClientBackend>] {
-                $(
-                    async fn $name(&self, req: $req) -> anyhow::Result<$res> {
-                        match self {
-                            #[cfg(feature = "grpc")]
-                            Self::Grpc { client, .. } => {
-                                let req = iroh_metrics::req::trace_tonic_req(req);
-                                let mut c = client.clone();
-                                let res = [<$label:lower _client>]::[<$label Client>]::$name(&mut c, req).await?;
-                                let res = res.into_inner();
-                                $(
-                                    let res = {
-                                        use futures::StreamExt;
-                                        Box::pin(res.map(|p| {
-                                            p.map_err(|e| anyhow::anyhow!(e))
-                                        }))
-                                    };
-                                    // hack
-                                    #[allow(dead_code)]
-                                    if false {
-                                        let _x = stringify!($stream_type_name);
-                                    }
-                                )?
-                                Ok(res)
-                            }
-                            #[cfg(feature = "mem")]
-                            Self::Mem(s) => {
-                                let (s_res, r_res) = tokio::sync::oneshot::channel();
-                                s.send(([<$label Request>]::$name(req), s_res)).await.map_err(|_| anyhow::anyhow!("send failed"))?;
-
-                                let res = r_res.await?;
-                                #[allow(irrefutable_let_patterns)]
-                                if let [<$label Response>]::$name(res) = res {
-                                    return res.map_err(|e| anyhow::anyhow!(e))
-                                } else {
-                                    anyhow::bail!("invalid response");
-                                }
-                            }
-                        }
-                    }
-                )+
-            }
-        }
-    }
-}
-
-macro_rules! proxy_grpc {
-    ($label:ident, $($name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?),+) => {
-        #[cfg(feature = "grpc")]
-        mod grpc {
-            use super::*;
-            use tonic::{Request, Response, Status};
-
-            paste::paste! {
-                #[async_trait::async_trait]
-                impl<P: $label> [<$label:lower _server>]::$label for P {
-                    $(
-                        $(type $stream_type_name = $tonic_res;)?
-
-                        async fn $name(
-                            &self,
-                            req: Request<$req>,
-                        ) -> Result<Response<$tonic_res>, Status> {
-                            let req = req.into_inner();
-                            let res = $label::$name(self, req).await.map_err(|err| Status::internal(err.to_string()))?;
-
-                            $(
-                                let res = {
-                                    use futures::StreamExt;
-                                    Box::pin(res.map(|s| s.map_err(|e| Status::internal(e.to_string()))))
-                                };
-                                // hack
-                                #[allow(dead_code)]
-                                if false {
-                                    let _x = stringify!($stream_type_name);
-                                }
-                            )?
-
-                            Ok(Response::new(res))
-                        }
-                    )+
+            pub async fn serve(
+                server_addr: [<$label ServerAddr>],
+                server: $server,
+            ) -> anyhow::Result<()> {
+                match server_addr {
+                    $crate::Addr::GrpcHttp2(server_addr) => serve_tcp(server_addr, server).await,
+                    $crate::Addr::GrpcUds(server_addr) => todo!(),
+                    $crate::Addr::Mem(chan) => serve_mem(chan, server).await,
                 }
             }
-        }
-    }
-}
 
-#[macro_export]
-macro_rules! proxy {
-    ($label:ident, $(
-        $name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?
-    ),+) => {
-        proxy_serve!($label, $($name: $req => $res),+);
-        proxy_serve_types!($label, $($name: $req => $res),+);
-        proxy_traits!($label, $($name: $req => $tonic_res => $res $([$stream_type_name])?),+);
-        proxy_grpc!($label, $($name: $req => $tonic_res => $res $([$stream_type_name])?),+);
-    }
-}
+            async fn serve_tcp(
+                server_addr: std::net::SocketAddr,
+                server: $server,
+            ) -> anyhow::Result<()> {
+                use futures::{Sink, StreamExt};
+                use tarpc::server::{incoming::Incoming, Channel};
+                use iroh_rpc_types::[<$label:snake>]::$label;
 
-// Based on tonic::include_proto
-#[macro_export]
-macro_rules! include_proto {
-    ($package: tt) => {
-        #[allow(clippy::all)]
-        mod proto {
-            include!(concat!(env!("OUT_DIR"), concat!("/", $package, ".rs")));
+                // TODO: configurable
+                let max_channels_per_ip = 500;
+                let max_channels = 500;
+
+                let mut listener = tarpc::serde_transport::tcp::listen(
+                    &server_addr,
+                    tarpc::tokio_serde::formats::Bincode::default,
+                )
+                    .await?;
+                listener.config_mut().max_frame_length(usize::MAX);
+                listener
+                // Ignore accept errors.
+                    .filter_map(|r| futures::future::ready(r.ok()))
+                    .map(tarpc::server::BaseChannel::with_defaults)
+                // Limit channels  per IP.
+                    .max_channels_per_key(max_channels_per_ip, |t| {
+                        t.transport().peer_addr().unwrap().ip()
+                    })
+                    .map(|channel| {
+                        // let server = S::new_tcp(&channel);
+                        let server = server.clone();
+                        channel.execute(server.serve())
+                    })
+                    .buffer_unordered(max_channels)
+                    .for_each(|_| async {})
+                    .await;
+
+                Ok(())
+            }
+
+            async fn serve_mem(
+                server_transport: tarpc::transport::channel::Channel<tarpc::ClientMessage<$req>, tarpc::Response<$resp>>,
+                server: $server,
+            ) -> anyhow::Result<()> {
+                use tarpc::server::Channel;
+                use tarpc::server::Serve;
+                use iroh_rpc_types::[<$label:snake>]::$label;
+
+                let transport = tarpc::server::BaseChannel::with_defaults(server_transport);
+                let t = server.serve();
+                transport.execute(|a, b| t.serve(a, b)).await;
+                Ok(())
+            }
         }
-        pub use proto::*;
     };
 }

n0-computer/iroh

iroh-rpc-types/src/macros.rs
@@ -1,261 +1,74 @@
-macro_rules! proxy_serve {
-    ($label:ident, $($name:ident: $req:ty => $res:ty),+) => {
-        paste::paste! {
-            pub async fn serve<T: $label>(addr: [<$label ServerAddr>], source: T) -> anyhow::Result<()> {
-                match addr {
-                    #[cfg(feature = "grpc")]
-                    $crate::Addr::GrpcHttp2(addr) => {
-                        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-                        health_reporter
-                            .set_serving::<[<$label:lower _server>]::[<$label Server>]<T>>()
-                            .await;
-
-                        tonic::transport::Server::builder()
-                            .add_service(health_service)
-                            .add_service([<$label:lower _server>]::[<$label Server>]::new(source))
-                            .serve(addr)
-                            .await?;
-
-                        Ok(())
-                    }
-
-                    #[cfg(all(feature = "grpc", unix))]
-                    $crate::Addr::GrpcUds(path) => {
-                        use anyhow::Context;
-                        use tokio::net::UnixListener;
-                        use tokio_stream::wrappers::UnixListenerStream;
-
-                        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-                        health_reporter
-                            .set_serving::<[<$label:lower _server>]::[<$label Server>]<T>>()
-                            .await;
-
-                        if path.exists() {
-                            if path.is_dir() {
-                                anyhow::bail!("cannot bind socket to directory: {}", path.display());
-                            } else {
-                                anyhow::bail!("cannot bind socket: already exists: {}", path.display());
-                            }
-                        }
-
-                        // If the parent directory doesn't exist, we'll fail to bind.
-                        // Create a more precise error to recognize that case.
-                        if let Some(parent) = path.parent() {
-                            if !parent.exists() {
-                                anyhow::bail!("socket parent directory doesn't exist: {}", parent.display());
-                            }
-                        }
-
-                        // Delete file on close
-                        struct UdsGuard(std::path::PathBuf);
-                        impl Drop for UdsGuard {
-                            fn drop(&mut self) {
-                                let _ = std::fs::remove_file(&self.0);
-                            }
-                        }
-
-                        let uds = UnixListener::bind(&path)
-                            .with_context(|| format!("failed to bind to {}", path.display()))?;
-                        let _guard = UdsGuard(path.clone().into());
-
-                        let uds_stream = UnixListenerStream::new(uds);
-
-                        tonic::transport::Server::builder()
-                            .add_service(health_service)
-                            .add_service([<$label:lower _server>]::[<$label Server>]::new(source))
-                            .serve_with_incoming(uds_stream)
-                            .await?;
-
-                        Ok(())
-                    }
-                    #[cfg(feature = "mem")]
-                    $crate::Addr::Mem(mut receiver) => {
-                        while let Some((msg, sender)) = receiver.recv().await {
-                            match msg {
-                                $(
-                                    [<$label Request>]::$name(req) => {
-                                        let res = source.$name(req).await.map_err(|e| e.to_string());
-                                        sender.send([<$label Response>]::$name(res)).ok();
-                                    }
-                                )+
-                            }
-                        }
-
-                        Ok(())
-                    }
-                }
-            }
-        }
-    }
-}
-
-macro_rules! proxy_serve_types {
-    ($label:ident, $($name:ident: $req:ty => $res:ty),+) => {
+#[macro_export]
+macro_rules! impl_serve {
+    ($label:ident, $server:ty, $req:ty, $resp:ty) => {
         paste::paste! {
             pub type [<$label ServerAddr>] = $crate::Addr<
-                    tokio::sync::mpsc::Receiver<
-                            ([<$label Request>], tokio::sync::oneshot::Sender<[<$label Response>]>),
-                        >
-                    >;
-            pub type [<$label ClientAddr>] = $crate::Addr<
-                    tokio::sync::mpsc::Sender<
-                            ([<$label Request>], tokio::sync::oneshot::Sender<[<$label Response>]>),
-                        >
-                    >;
+                tarpc::ClientMessage<$req>, tarpc::Response<$resp>
+            >;
 
-            #[derive(Debug, Clone)]
-            #[allow(clippy::large_enum_variant)]
-            pub enum [<$label ClientBackend>] {
-                #[cfg(feature = "grpc")]
-                Grpc {
-                    client: [<$label:lower _client>]::[<$label Client>]<tonic::transport::Channel>,
-                    health: tonic_health::proto::health_client::HealthClient<tonic::transport::Channel>,
-                },
-                #[cfg(feature = "mem")]
-                Mem(
-                    tokio::sync::mpsc::Sender<(
-                        [<$label Request>],
-                        tokio::sync::oneshot::Sender<[<$label Response>]>
-                    )>
-                ),
-            }
-
-            #[allow(non_camel_case_types)]
-            #[derive(Debug, Clone)]
-            pub enum [<$label Request>] {
-                $(
-                    $name($req),
-                )+
-            }
-
-            #[allow(non_camel_case_types)]
-            pub enum [<$label Response>] {
-                $(
-                    $name(Result<$res, String>),
-                )+
-            }
-        }
-    }
-}
-
-macro_rules! proxy_traits {
-    ($label:ident, $($name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?),+) => {
-        paste::paste! {
-            #[async_trait::async_trait]
-            pub trait $label: Send + Sync + 'static {
-                $(
-                    async fn $name(&self, request: $req) -> anyhow::Result<$res>;
-                )+
-            }
-
-            #[async_trait::async_trait]
-            impl $label for [<$label ClientBackend>] {
-                $(
-                    async fn $name(&self, req: $req) -> anyhow::Result<$res> {
-                        match self {
-                            #[cfg(feature = "grpc")]
-                            Self::Grpc { client, .. } => {
-                                let req = iroh_metrics::req::trace_tonic_req(req);
-                                let mut c = client.clone();
-                                let res = [<$label:lower _client>]::[<$label Client>]::$name(&mut c, req).await?;
-                                let res = res.into_inner();
-                                $(
-                                    let res = {
-                                        use futures::StreamExt;
-                                        Box::pin(res.map(|p| {
-                                            p.map_err(|e| anyhow::anyhow!(e))
-                                        }))
-                                    };
-                                    // hack
-                                    #[allow(dead_code)]
-                                    if false {
-                                        let _x = stringify!($stream_type_name);
-                                    }
-                                )?
-                                Ok(res)
-                            }
-                            #[cfg(feature = "mem")]
-                            Self::Mem(s) => {
-                                let (s_res, r_res) = tokio::sync::oneshot::channel();
-                                s.send(([<$label Request>]::$name(req), s_res)).await.map_err(|_| anyhow::anyhow!("send failed"))?;
-
-                                let res = r_res.await?;
-                                #[allow(irrefutable_let_patterns)]
-                                if let [<$label Response>]::$name(res) = res {
-                                    return res.map_err(|e| anyhow::anyhow!(e))
-                                } else {
-                                    anyhow::bail!("invalid response");
-                                }
-                            }
-                        }
-                    }
-                )+
-            }
-        }
-    }
-}
-
-macro_rules! proxy_grpc {
-    ($label:ident, $($name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?),+) => {
-        #[cfg(feature = "grpc")]
-        mod grpc {
-            use super::*;
-            use tonic::{Request, Response, Status};
-
-            paste::paste! {
-                #[async_trait::async_trait]
-                impl<P: $label> [<$label:lower _server>]::$label for P {
-                    $(
-                        $(type $stream_type_name = $tonic_res;)?
-
-                        async fn $name(
-                            &self,
-                            req: Request<$req>,
-                        ) -> Result<Response<$tonic_res>, Status> {
-                            let req = req.into_inner();
-                            let res = $label::$name(self, req).await.map_err(|err| Status::internal(err.to_string()))?;
-
-                            $(
-                                let res = {
-                                    use futures::StreamExt;
-                                    Box::pin(res.map(|s| s.map_err(|e| Status::internal(e.to_string()))))
-                                };
-                                // hack
-                                #[allow(dead_code)]
-                                if false {
-                                    let _x = stringify!($stream_type_name);
-                                }
-                            )?
-
-                            Ok(Response::new(res))
-                        }
-                    )+
+            pub async fn serve(
+                server_addr: [<$label ServerAddr>],
+                server: $server,
+            ) -> anyhow::Result<()> {
+                match server_addr {
+                    $crate::Addr::Tcp(server_addr) => serve_tcp(server_addr, server).await,
+                    #[cfg(unix)]
+                    $crate::Addr::Uds(server_addr) => todo!(),
+                    $crate::Addr::Mem(chan) => serve_mem(chan, server).await,
                 }
             }
-        }
-    }
-}
 
-#[macro_export]
-macro_rules! proxy {
-    ($label:ident, $(
-        $name:ident: $req:ty => $tonic_res:ty => $res:ty $([$stream_type_name:ident])?
-    ),+) => {
-        proxy_serve!($label, $($name: $req => $res),+);
-        proxy_serve_types!($label, $($name: $req => $res),+);
-        proxy_traits!($label, $($name: $req => $tonic_res => $res $([$stream_type_name])?),+);
-        proxy_grpc!($label, $($name: $req => $tonic_res => $res $([$stream_type_name])?),+);
-    }
-}
+            async fn serve_tcp(
+                server_addr: std::net::SocketAddr,
+                server: $server,
+            ) -> anyhow::Result<()> {
+                use futures::{Sink, StreamExt};
+                use tarpc::server::{incoming::Incoming, Channel};
+                use iroh_rpc_types::[<$label:snake>]::$label;
 
-// Based on tonic::include_proto
-#[macro_export]
-macro_rules! include_proto {
-    ($package: tt) => {
-        #[allow(clippy::all)]
-        mod proto {
-            include!(concat!(env!("OUT_DIR"), concat!("/", $package, ".rs")));
+                // TODO: configurable
+                let max_channels_per_ip = 500;
+                let max_channels = 500;
+
+                let mut listener = tarpc::serde_transport::tcp::listen(
+                    &server_addr,
+                    tarpc::tokio_serde::formats::Bincode::default,
+                )
+                    .await?;
+                listener.config_mut().max_frame_length(usize::MAX);
+                listener
+                // Ignore accept errors.
+                    .filter_map(|r| futures::future::ready(r.ok()))
+                    .map(tarpc::server::BaseChannel::with_defaults)
+                // Limit channels  per IP.
+                    .max_channels_per_key(max_channels_per_ip, |t| {
+                        t.transport().peer_addr().unwrap().ip()
+                    })
+                    .map(|channel| {
+                        // let server = S::new_tcp(&channel);
+                        let server = server.clone();
+                        channel.execute(server.serve())
+                    })
+                    .buffer_unordered(max_channels)
+                    .for_each(|_| async {})
+                    .await;
+
+                Ok(())
+            }
+
+            async fn serve_mem(
+                server_transport: $crate::Channel<tarpc::ClientMessage<$req>, tarpc::Response<$resp>>,
+                server: $server,
+            ) -> anyhow::Result<()> {
+                use tarpc::server::Channel;
+                use tarpc::server::Serve;
+                use iroh_rpc_types::[<$label:snake>]::$label;
+
+                let transport = tarpc::server::BaseChannel::with_defaults(server_transport);
+                let t = server.serve();
+                transport.execute(|a, b| t.serve(a, b)).await;
+                Ok(())
+            }
         }
-        pub use proto::*;
     };
 }