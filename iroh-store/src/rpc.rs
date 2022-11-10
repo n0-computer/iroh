ac8ec35f0131347ad125e82b43356e16787ecf08
Rüdiger Klaehn <rklaehn@protonmail.com>
2022-11-10 20:19:12 +0200

    fix(store): Make store operations atomic (#480)
    * test(store): add test that shows that the store can get inconsistent
    
    Add test that shows that the store can get internally inconsistent when
    hammering it with concurrent put calls involving the same child cids.
    
    * fix(store): Make writing to the store fully sequential
    
    * refactor(store): split into write and read part
    
    * refactor(store): use spawn_blocking to isolate blocking io from tokio taskpool
    
    * refactor(store): switch to parking lot mutexes
    
    * refactor(store): review comments
    
    * doc(store) update comments
    
    * fix(store): Rip out parking lot
    
    Maybe that will fix the mysterious linker error. Shame, since it is much
    faster than std::sync locks.

--- iroh-store/src/rpc.rs
+++ iroh-store/src/rpc.rs
@@ -30,7 +30,9 @@ impl RpcStore for Store {
     async fn put(&self, req: PutRequest) -> Result<()> {
         let cid = cid_from_bytes(req.cid)?;
         let links = links_from_bytes(req.links)?;
-        let res = self.put(cid, req.blob, links)?;
+        let res = self
+            .spawn_blocking(move |x| x.put(cid, req.blob, links))
+            .await?;
 
         info!("store rpc call: put cid {}", cid);
         Ok(res)
@@ -47,50 +49,61 @@ impl RpcStore for Store {
                 Ok((cid, req.blob, links))
             })
             .collect::<Result<Vec<_>>>()?;
-        self.put_many(req)
+        self.spawn_blocking(move |x| x.put_many(req)).await
     }
 
     #[tracing::instrument(skip(self))]
     async fn get(&self, req: GetRequest) -> Result<GetResponse> {
         let cid = cid_from_bytes(req.cid)?;
-        if let Some(res) = self.get(&cid)? {
-            Ok(GetResponse {
-                data: Some(BytesMut::from(&res[..]).freeze()),
-            })
-        } else {
-            Ok(GetResponse { data: None })
-        }
+        self.spawn_blocking(move |x| {
+            if let Some(res) = x.get(&cid)? {
+                Ok(GetResponse {
+                    data: Some(BytesMut::from(&res[..]).freeze()),
+                })
+            } else {
+                Ok(GetResponse { data: None })
+            }
+        })
+        .await
     }
 
     #[tracing::instrument(skip(self))]
     async fn has(&self, req: HasRequest) -> Result<HasResponse> {
         let cid = cid_from_bytes(req.cid)?;
-        let has = self.has(&cid)?;
-
-        Ok(HasResponse { has })
+        self.spawn_blocking(move |self| {
+            let has = self.has(&cid)?;
+            Ok(HasResponse { has })
+        })
+        .await
     }
 
     #[tracing::instrument(skip(self))]
     async fn get_links(&self, req: GetLinksRequest) -> Result<GetLinksResponse> {
         let cid = cid_from_bytes(req.cid)?;
-        if let Some(res) = self.get_links(&cid)? {
-            let links = res.into_iter().map(|cid| cid.to_bytes()).collect();
-            Ok(GetLinksResponse { links })
-        } else {
-            Ok(GetLinksResponse { links: Vec::new() })
-        }
+        self.spawn_blocking(move |self| {
+            if let Some(res) = self.get_links(&cid)? {
+                let links = res.into_iter().map(|cid| cid.to_bytes()).collect();
+                Ok(GetLinksResponse { links })
+            } else {
+                Ok(GetLinksResponse { links: Vec::new() })
+            }
+        })
+        .await
     }
 
     #[tracing::instrument(skip(self))]
     async fn get_size(&self, req: GetSizeRequest) -> Result<GetSizeResponse> {
         let cid = cid_from_bytes(req.cid)?;
-        if let Some(size) = self.get_size(&cid).await? {
-            Ok(GetSizeResponse {
-                size: Some(size as u64),
-            })
-        } else {
-            Ok(GetSizeResponse { size: None })
-        }
+        self.spawn_blocking(move |self| {
+            if let Some(size) = self.get_size(&cid)? {
+                Ok(GetSizeResponse {
+                    size: Some(size as u64),
+                })
+            } else {
+                Ok(GetSizeResponse { size: None })
+            }
+        })
+        .await
     }
 }
 
