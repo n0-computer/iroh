0d1770468b3cf19ab37e2091bd37f0f7e2729ec6
Floris Bruynooghe <flub@devork.be>
2022-11-16 14:36:33 +0100

    Some basic start-stop test
    This isn't isolated yet.  We need this to be isolated.

--- tests/single_node.rs
+++ tests/single_node.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use std::collections::HashSet;
+use std::process::{Command, Output};
 
 use anyhow::anyhow;
 use camino::Utf8PathBuf;
@@ -8,29 +9,68 @@ use once_cell::sync::Lazy;
 static TARGET_DIR: Lazy<Utf8PathBuf> = Lazy::new(|| target_dir());
 
 #[test]
-fn test_start_stop() {
-    println!("crate: {}", env!("CARGO_CRATE_NAME"));
+fn test_stopped() {
+    let out = run_iroh(&["status"]);
+    let status = IrohStatusOutput::try_from(out.stdout.as_slice()).unwrap();
 
+    assert!(status.services.iter().all(|s| s.status == "Down"));
+    assert!(status.services.iter().all(|s| s.count == 1));
+    assert!(status.services.iter().all(|s| s.total == 1));
+    let mut all_services = HashSet::new();
+    all_services.insert(String::from("gateway"));
+    all_services.insert(String::from("p2p"));
+    all_services.insert(String::from("store"));
+    let found_services: HashSet<String> =
+        status.services.iter().map(|s| s.service.clone()).collect();
+    assert_eq!(found_services, all_services);
+}
+
+#[test]
+fn test_start_stop() {
+    let out = run_iroh(&["status"]);
+    let status = IrohStatusOutput::try_from(out.stdout.as_slice()).unwrap();
+    assert!(status.services.iter().all(|s| s.status == "Down"));
+
+    let out = run_iroh(&["start"]);
+    assert!(out.status.success());
+
+    let out = run_iroh(&["status"]);
+    let status = IrohStatusOutput::try_from(out.stdout.as_slice()).unwrap();
+    assert!(status.services.iter().all(|s| s.status == "Serving"));
+
+    let out = run_iroh(&["stop"]);
+    assert!(out.status.success());
+
+    let out = run_iroh(&["status"]);
+    let status = IrohStatusOutput::try_from(out.stdout.as_slice()).unwrap();
+    assert!(status.services.iter().all(|s| s.status == "Down"));
+}
+
+/// Run `iroh` with given arguments.
+fn run_iroh(args: &[&str]) -> Output {
     let path = std::env::var("PATH").unwrap();
     let path = format!("{}:{}", *TARGET_DIR, path);
 
-    let out = Command::new("iroh")
-        .args(["status"])
+    Command::new("iroh")
+        .args(args)
         .env("PATH", path)
         .output()
-        .unwrap();
-
-    let status = IrohStatusOutput::try_from(out.stdout.as_slice()).unwrap();
-    dbg!(status);
-
-    panic!("boom");
+        .unwrap()
 }
 
+/// Representation of the status output from `iroh`.
+///
+/// This is essentially a single sequence of statuses.
+///
+/// You can parse the output using the [`TryFrom::try_from`] method.
 #[derive(Debug, Clone)]
 struct IrohStatusOutput {
     services: Vec<IrohServiceStatus>,
 }
 
+/// Representation of the status of a single iroh service.
+///
+/// At least to the extend as exposed by the `iroh` command.
 #[derive(Debug, Clone)]
 struct IrohServiceStatus {
     service: String,
@@ -95,7 +135,9 @@ impl TryFrom<&[u8]> for IrohStatusOutput {
 
 /// Returns the target directory.
 ///
-/// This is where you can look for compiled binaries etc.
+/// This is where you can look for compiled binaries etc.  When in a crate that builds the
+/// binary its path is available as a `CARGO_BIN_EXE_<name>` environment variable, however
+/// each binary is built by its own crate and we want to test many binaries together.
 fn target_dir() -> Utf8PathBuf {
     let metadata = cargo_metadata::MetadataCommand::new().exec().unwrap();
     let profile = match cfg!(debug_assertions) {
