8f8219f16a97f724459e01d0942b0483bba8da99
Ruediger Klaehn <rklaehn@protonmail.com>
2022-11-22 12:24:08 +0200

    Translate gateway protos into messages for quic-rpc

--- iroh-rpc-types/src/gateway.rs
+++ iroh-rpc-types/src/gateway.rs
@@ -4,3 +4,40 @@ proxy!(
     Gateway,
     version: () => VersionResponse => VersionResponse
 );
+
+
+mod qrpc {
+    use quic_rpc::{message::RpcMsg, Service};
+    use derive_more::{From, TryInto};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct VersionRequest;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct VersionResponse {
+        version: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, From, TryInto)]
+    enum GatewayRequest {
+        Version(VersionRequest),
+    }
+
+    #[derive(Serialize, Deserialize, Debug, From, TryInto)]
+    enum GatewayResponse {
+        Version(VersionResponse),
+    }
+
+    #[derive(Debug, Clone, Copy)]
+    struct GatewayService;
+
+    impl Service for GatewayService {
+        type Req = GatewayRequest;
+        type Res = GatewayResponse;
+    }
+
+    impl RpcMsg<GatewayService> for VersionRequest {
+        type Response = VersionResponse;
+    }
+}
