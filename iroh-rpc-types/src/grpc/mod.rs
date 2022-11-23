da092ab76a037287e5be75688886ffe5e7f0b2ad
Ruediger Klaehn <rklaehn@protonmail.com>
2022-11-23 10:35:56 +0200

    refactor: get grpc and qrpc stuff separated in types

---
+++ iroh-rpc-types/src/grpc/mod.rs
@@ -0,0 +1,7 @@
+pub mod addr;
+pub mod gateway;
+pub mod store;
+pub mod p2p;
+
+#[cfg(feature = "testing")]
+pub mod test;
--- iroh-rpc-types/src/lib.rs
+++ iroh-rpc-types/src/lib.rs
@@ -1,17 +1,20 @@
+
+#[cfg(any(feature = "grpc", feature = "mem"))]
 #[macro_use]
 mod macros;
-
-pub mod gateway;
-pub mod p2p;
-pub mod qrpc;
-pub mod store;
 
 // Reexport for convenience.
 #[cfg(feature = "grpc")]
 pub use tonic::transport::NamedService;
 
-#[cfg(feature = "testing")]
-pub mod test;
+#[cfg(any(feature = "grpc", feature = "mem"))]
+mod grpc;
+#[cfg(any(feature = "grpc", feature = "mem"))]
+pub use grpc::*;
 
-mod addr;
+#[cfg(any(feature = "qrpc"))]
+pub mod qrpc;
+#[cfg(any(feature = "qrpc"))]
+pub use qrpc::*;
+
 pub use crate::addr::Addr;
