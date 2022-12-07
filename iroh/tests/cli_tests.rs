064e3c057d40051ed298c4cea2c4c710946da15e
Floris Bruynooghe <flub@devork.be>
2022-12-07 18:48:44 +0100

    Tell TryCmd to not produce backtraces
    The backtraces are not part of the user interface, but CI sets
    RUST_BACKTRACE by default.  So surpress this.

--- iroh/tests/cli_tests.rs
+++ iroh/tests/cli_tests.rs
@@ -4,6 +4,7 @@
 #[test]
 fn add_directory_without_r_fails_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "add_directory")
         .case("tests/cmd/add_directory_without_r_fails.trycmd")
         .run();
@@ -12,6 +13,7 @@ fn add_directory_without_r_fails_test() {
 #[test]
 fn add_directory_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "add_directory")
         .case("tests/cmd/add_directory.trycmd")
         .run();
@@ -20,6 +22,7 @@ fn add_directory_test() {
 #[test]
 fn add_file_missing_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "add_file")
         .case("tests/cmd/add_file_missing.trycmd")
         .run();
@@ -28,6 +31,7 @@ fn add_file_missing_test() {
 #[test]
 fn add_file_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "add_file")
         .case("tests/cmd/add_file.trycmd")
         .run();
@@ -36,6 +40,7 @@ fn add_file_test() {
 #[test]
 fn get_cid_directory_overwrite_explicit_failure_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_cid_directory_overwrite_explicit_failure.trycmd")
         .run();
@@ -44,6 +49,7 @@ fn get_cid_directory_overwrite_explicit_failure_test() {
 #[test]
 fn get_cid_directory_overwrite_failure_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_cid_directory_overwrite_failure.trycmd")
         .run();
@@ -52,6 +58,7 @@ fn get_cid_directory_overwrite_failure_test() {
 #[test]
 fn get_cid_explicit_output_path_success_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_cid_explicit_output_path_success.trycmd")
         .run();
@@ -60,6 +67,7 @@ fn get_cid_explicit_output_path_success_test() {
 #[test]
 fn get_cid_success_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_cid_success.trycmd")
         .run();
@@ -68,6 +76,7 @@ fn get_cid_success_test() {
 #[tokio::test]
 async fn get_unwrapped_file_overwrite_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_unwrapped_file")
         .case("tests/cmd/get_unwrapped_file_overwrite.trycmd")
         .run();
@@ -76,6 +85,7 @@ async fn get_unwrapped_file_overwrite_test() {
 #[tokio::test]
 async fn get_failure_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_failure.trycmd")
         .run();
@@ -84,6 +94,7 @@ async fn get_failure_test() {
 #[test]
 fn get_ipfs_path_success_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get")
         .case("tests/cmd/get_ipfs_path_success.trycmd")
         .run();
@@ -92,6 +103,7 @@ fn get_ipfs_path_success_test() {
 #[test]
 fn get_tail_success_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_unwrapped_file")
         .case("tests/cmd/get_tail_success.trycmd")
         .run();
@@ -100,6 +112,7 @@ fn get_tail_success_test() {
 #[test]
 fn get_unwrapped_file_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_unwrapped_file")
         .case("tests/cmd/get_unwrapped_file.trycmd")
         .run();
@@ -108,6 +121,7 @@ fn get_unwrapped_file_test() {
 #[test]
 fn get_wrapped_file_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_wrapped_file")
         .case("tests/cmd/get_wrapped_file.trycmd")
         .run();
@@ -116,6 +130,7 @@ fn get_wrapped_file_test() {
 #[tokio::test]
 async fn get_unwrapped_symlink_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_unwrapped_symlink")
         .case("tests/cmd/get_unwrapped_symlink.trycmd")
         .run();
@@ -124,6 +139,7 @@ async fn get_unwrapped_symlink_test() {
 #[tokio::test]
 async fn get_wrapped_symlink_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "get_wrapped_symlink")
         .case("tests/cmd/get_wrapped_symlink.trycmd")
         .run();
@@ -132,6 +148,7 @@ async fn get_wrapped_symlink_test() {
 #[test]
 fn lookup_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "lookup")
         .case("tests/cmd/lookup.trycmd")
         .run();
@@ -140,6 +157,7 @@ fn lookup_test() {
 #[test]
 fn version_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .case("tests/cmd/version.trycmd")
         .insert_var("[VERSION]", std::env::var("CARGO_PKG_VERSION").unwrap())
         .unwrap();
@@ -148,6 +166,7 @@ fn version_test() {
 #[test]
 fn start_status_stop_test() {
     trycmd::TestCases::new()
+        .env("RUST_BACKTRACE", "0")
         .env("IROH_CTL_FIXTURE", "start_status_stop")
         .case("tests/cmd/start_status_stop.trycmd")
         .run();
