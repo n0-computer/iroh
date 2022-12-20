35394950211444b9e80c7e855bc66dc642619986
Rüdiger Klaehn <rklaehn@protonmail.com>
2022-12-20 13:14:33 +0200

    chore: update quic-rpc version (#627)
    This should give us better termination and also solve some issues with
    rpc messages being too large.

--- iroh-rpc-client/src/lib.rs
+++ iroh-rpc-client/src/lib.rs
@@ -53,8 +53,7 @@ pub async fn create_server<S: Service>(
             // Ok(Some(RpcServer::new(combined::Channel::new(Some(addr), None))))
         }
         Addr::Irpc(addr) => {
-            let (channel, hyper) = quic_rpc::transport::http2::ServerChannel::new(&addr)?;
-            tokio::spawn(hyper);
+            let channel = quic_rpc::transport::http2::ServerChannel::serve(&addr)?;
             let channel = combined::ServerChannel::new(Some(channel), None);
             let server = RpcServer::new(channel);
             Ok(server)
