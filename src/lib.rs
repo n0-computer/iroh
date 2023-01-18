736c5a951958441ea87e5e9d24aec6185e13b55a
dignifiedquire <me@dignifiedquire.com>
2023-01-18 14:37:54 +0100

    happy clippy

--- src/lib.rs
+++ src/lib.rs
@@ -64,11 +64,7 @@ mod tests {
 
         let mut tasks = Vec::new();
         for _i in 0..3 {
-            tasks.push(tokio::task::spawn(run_client(
-                hash,
-                addr.clone(),
-                content.to_vec(),
-            )));
+            tasks.push(tokio::task::spawn(run_client(hash, addr, content.to_vec())));
         }
 
         for task in tasks {
