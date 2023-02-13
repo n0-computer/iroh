7922b8630a195cbad78bf111cad82d9ed456d8e1
dignifiedquire <me@dignifiedquire.com>
2023-02-13 22:05:30 +0100

    use local clock for testing

--- src/hp.rs
+++ src/hp.rs
@@ -6,9 +6,3 @@ pub mod portmapper;
 pub mod stun;
 
 mod clock;
-use once_cell::sync::Lazy;
-
-use self::clock::Clock;
-
-/// Our global clock, gets set to a fake one in tests
-static CLOCK: Lazy<Clock> = Lazy::new(|| Clock::default());
