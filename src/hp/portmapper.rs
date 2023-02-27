0a1c67b70a1377c560e2c36e683d54eed39aadb9
dignifiedquire <me@dignifiedquire.com>
2023-02-27 16:31:58 +0100

    minimal rebind stress tests passing

--- src/hp/portmapper.rs
+++ src/hp/portmapper.rs
@@ -1,6 +1,6 @@
 use std::net::SocketAddr;
 
-use anyhow::Error;
+use anyhow::{bail, Error};
 
 #[derive(Debug, Clone)]
 pub struct PortMapper {}
@@ -18,35 +18,37 @@ pub struct Client {}
 
 impl Client {
     pub fn new() -> Self {
-        todo!()
+        Client {}
     }
 
     pub async fn probe(&self) -> Result<ProbeResult, Error> {
-        todo!()
+        bail!("not implemented yet")
     }
 
     /// Updates the local port number to which we want to port map UDP traffic.
     pub async fn set_local_port(&self, local_port: u16) {
-        todo!()
+        // TODO:
     }
 
     /// Quickly returns with our current cached portmapping, if any.
     /// If there's not one, it starts up a background goroutine to create one.
     /// If the background goroutine ends up creating one, the `on_change` hook registered with the
     /// `Client::new` constructor (if any) will fire.
-    pub async fn get_cached_mapping_or_start_creating_one(&self) -> (SocketAddr, bool) {
-        todo!()
+    pub async fn get_cached_mapping_or_start_creating_one(&self) -> Option<SocketAddr> {
+        // TODO:
+        None
     }
 
     pub fn have_mapping(&self) -> bool {
-        todo!()
+        // TODO:
+        false
     }
 
     pub fn note_network_down(&self) {
-        todo!()
+        // TODO:
     }
 
     pub fn close(&self) {
-        todo!()
+        // TODO:
     }
 }
