8ed912087b068276af08060027b28328a3343581
Kasey <klhuizinga@gmail.com>
2023-03-30 00:32:55 -0400

    refactor `FrameType` to be enum

--- src/hp/derp/client.rs
+++ src/hp/derp/client.rs
@@ -15,10 +15,7 @@ use super::PER_CLIENT_SEND_QUEUE_DEPTH;
 use super::{
     read_frame,
     types::{ClientInfo, MeshKey, RateLimiter, ServerInfo},
-    write_frame, FrameType, FRAME_CLOSE_PEER, FRAME_FORWARD_PACKET, FRAME_HEALTH, FRAME_KEEP_ALIVE,
-    FRAME_NOTE_PREFERRED, FRAME_PEER_GONE, FRAME_PEER_PRESENT, FRAME_PING, FRAME_PONG,
-    FRAME_RECV_PACKET, FRAME_RESTARTING, FRAME_SEND_PACKET, FRAME_SERVER_INFO, FRAME_SERVER_KEY,
-    FRAME_WATCH_CONNS, MAGIC, MAX_FRAME_SIZE, MAX_PACKET_SIZE, NOT_PREFERRED, PREFERRED,
+    write_frame, FrameType, MAGIC, MAX_FRAME_SIZE, MAX_PACKET_SIZE, NOT_PREFERRED, PREFERRED,
     PROTOCOL_VERSION,
 };
 
@@ -206,12 +203,12 @@ impl<R: AsyncRead + Unpin> Client<R> {
                 };
 
             match frame_type {
-                FRAME_KEEP_ALIVE => {
+                FrameType::KeepAlive => {
                     // A one-way keep-alive message that doesn't require an ack.
-                    // This predated FRAME_PING/FRAME_PONG.
+                    // This predated FrameType::Ping/FrameType::Pong.
                     return Ok(ReceivedMessage::KeepAlive);
                 }
-                FRAME_PEER_GONE => {
+                FrameType::PeerGone => {
                     if (frame_len) < PUBLIC_KEY_LENGTH {
                         tracing::warn!(
                             "unexpected: dropping short PEER_GONE frame from DERP server"
@@ -222,7 +219,7 @@ impl<R: AsyncRead + Unpin> Client<R> {
                         &frame_payload[..PUBLIC_KEY_LENGTH],
                     )?));
                 }
-                FRAME_PEER_PRESENT => {
+                FrameType::PeerPresent => {
                     if (frame_len) < PUBLIC_KEY_LENGTH {
                         tracing::warn!(
                             "unexpected: dropping short PEER_PRESENT frame from DERP server"
@@ -233,7 +230,7 @@ impl<R: AsyncRead + Unpin> Client<R> {
                         &frame_payload[..PUBLIC_KEY_LENGTH],
                     )?));
                 }
-                FRAME_RECV_PACKET => {
+                FrameType::RecvPacket => {
                     if (frame_len) < PUBLIC_KEY_LENGTH {
                         tracing::warn!("unexpected: dropping short packet from DERP server");
                         continue;
@@ -245,7 +242,7 @@ impl<R: AsyncRead + Unpin> Client<R> {
                     };
                     return Ok(packet);
                 }
-                FRAME_PING => {
+                FrameType::Ping => {
                     if frame_len < 8 {
                         tracing::warn!("unexpected: dropping short PING frame");
                         continue;
@@ -253,7 +250,7 @@ impl<R: AsyncRead + Unpin> Client<R> {
                     let ping = <[u8; 8]>::try_from(&frame_payload[..8])?;
                     return Ok(ReceivedMessage::Ping(ping));
                 }
-                FRAME_PONG => {
+                FrameType::Pong => {
                     if frame_len < 8 {
                         tracing::warn!("unexpected: dropping short PONG frame");
                         continue;
@@ -261,11 +258,11 @@ impl<R: AsyncRead + Unpin> Client<R> {
                     let pong = <[u8; 8]>::try_from(&frame_payload[..8])?;
                     return Ok(ReceivedMessage::Pong(pong));
                 }
-                FRAME_HEALTH => {
+                FrameType::Health => {
                     let problem = Some(String::from_utf8_lossy(&frame_payload).into());
                     return Ok(ReceivedMessage::Health { problem });
                 }
-                FRAME_RESTARTING => {
+                FrameType::Restarting => {
                     if frame_len < 8 {
                         tracing::warn!("unexpected: dropping short server restarting frame");
                         continue;
@@ -501,7 +498,7 @@ where
         let mut buf = BytesMut::new();
         let (frame_type, _) =
             crate::hp::derp::read_frame(&mut self.reader, MAX_FRAME_SIZE, &mut buf).await?;
-        assert_eq!(FRAME_SERVER_INFO, frame_type);
+        assert_eq!(FrameType::ServerInfo, frame_type);
         let msg = self.secret_key.open_from(&server_key, &buf)?;
         let info: ServerInfo = postcard::from_bytes(&msg)?;
         if info.version != PROTOCOL_VERSION {
@@ -564,7 +561,7 @@ pub(crate) async fn recv_server_key<R: AsyncRead + Unpin>(mut reader: R) -> Resu
     let (frame_type, frame_len) = read_frame(&mut reader, MAX_FRAME_SIZE, &mut buf).await?;
 
     if expected_frame_len != frame_len
-        || frame_type != FRAME_SERVER_KEY
+        || frame_type != FrameType::ServerKey
         || buf[..magic_len] != *MAGIC.as_bytes()
     {
         bail!("invalid server greeting");
@@ -655,7 +652,12 @@ pub(crate) async fn send_packet<W: AsyncWrite + Unpin>(
             return Ok(());
         }
     }
-    write_frame(&mut writer, FRAME_SEND_PACKET, &[dstkey.as_bytes(), packet]).await?;
+    write_frame(
+        &mut writer,
+        FrameType::SendPacket,
+        &[dstkey.as_bytes(), packet],
+    )
+    .await?;
     writer.flush().await?;
     Ok(())
 }
@@ -674,7 +676,7 @@ pub(crate) async fn forward_packet<W: AsyncWrite + Unpin>(
 
     write_frame(
         &mut writer,
-        FRAME_FORWARD_PACKET,
+        FrameType::ForwardPacket,
         &[srckey.as_bytes(), dstkey.as_bytes(), packet],
     )
     .await?;
@@ -683,11 +685,11 @@ pub(crate) async fn forward_packet<W: AsyncWrite + Unpin>(
 }
 
 pub(crate) async fn send_ping<W: AsyncWrite + Unpin>(mut writer: W, data: &[u8; 8]) -> Result<()> {
-    send_ping_or_pong(&mut writer, FRAME_PING, data).await
+    send_ping_or_pong(&mut writer, FrameType::Ping, data).await
 }
 
 async fn send_pong<W: AsyncWrite + Unpin>(mut writer: W, data: &[u8; 8]) -> Result<()> {
-    send_ping_or_pong(&mut writer, FRAME_PONG, data).await
+    send_ping_or_pong(&mut writer, FrameType::Pong, data).await
 }
 
 async fn send_ping_or_pong<W: AsyncWrite + Unpin>(
@@ -711,13 +713,13 @@ pub async fn send_note_preferred<W: AsyncWrite + Unpin>(
             [NOT_PREFERRED]
         }
     };
-    write_frame(&mut writer, FRAME_NOTE_PREFERRED, &[&byte]).await?;
+    write_frame(&mut writer, FrameType::NotePreferred, &[&byte]).await?;
     writer.flush().await?;
     Ok(())
 }
 
 pub(crate) async fn watch_connection_changes<W: AsyncWrite + Unpin>(mut writer: W) -> Result<()> {
-    write_frame(&mut writer, FRAME_WATCH_CONNS, &[]).await?;
+    write_frame(&mut writer, FrameType::WatchConns, &[]).await?;
     writer.flush().await?;
     Ok(())
 }
@@ -726,7 +728,7 @@ pub(crate) async fn close_peer<W: AsyncWrite + Unpin>(
     mut writer: W,
     target: PublicKey,
 ) -> Result<()> {
-    write_frame(&mut writer, FRAME_CLOSE_PEER, &[target.as_bytes()]).await?;
+    write_frame(&mut writer, FrameType::ClosePeer, &[target.as_bytes()]).await?;
     writer.flush().await?;
     Ok(())
 }
