050781df010e06ade35a6fafd5e18b647b40443b
dignifiedquire <me@dignifiedquire.com>
2023-05-11 21:23:36 +0200

    add comment

--- src/config.rs
+++ src/config.rs
@@ -97,19 +97,21 @@ impl Config {
 }
 
 fn default_derp_region() -> DerpRegion {
+    // The default derper run by number0.
+    let default_n0_derp = DerpNode {
+        name: "default-1".into(),
+        region_id: 1,
+        host_name: "derp.iroh.computer".into(),
+        stun_only: false,
+        stun_port: 3478,
+        ipv4: UseIpv4::Some("35.175.99.113".parse().unwrap()),
+        ipv6: UseIpv6::None,
+        derp_port: 3340,
+        stun_test_ip: None,
+    };
     DerpRegion {
         region_id: 1,
-        nodes: vec![DerpNode {
-            name: "default-1".into(),
-            region_id: 1,
-            host_name: "derp.iroh.computer".into(),
-            stun_only: false,
-            stun_port: 3478,
-            ipv4: UseIpv4::Some("35.175.99.113".parse().unwrap()),
-            ipv6: UseIpv6::None,
-            derp_port: 3340,
-            stun_test_ip: None,
-        }],
+        nodes: vec![default_n0_derp],
         avoid: false,
         region_code: "default-1".into(),
     }
