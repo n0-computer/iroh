a9a66935693ed92dd76ec2c256d3e2a4a0e82790
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-06-28 11:35:34 +0200

    fix: MagicEndpoint rebase and cleanup

--- iroh-net/examples/magic.rs
+++ iroh-net/examples/magic.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
+use std::net::SocketAddr;
 
 use clap::Parser;
 use ed25519_dalek::SigningKey as SecretKey;
@@ -8,6 +8,8 @@ use iroh_net::{
     hp::derp::{DerpMap, UseIpv4, UseIpv6},
     tls::{Keypair, PeerId},
 };
+use tracing::{debug, info};
+use url::Url;
 
 const EXAMPLE_ALPN: &[u8] = b"n0/iroh/examples/magic/0";
 
@@ -20,7 +22,7 @@ struct Cli {
     #[clap(short, long, default_value = "0.0.0.0:0")]
     bind_address: SocketAddr,
     #[clap(short, long)]
-    derp_addr: Option<String>,
+    derp_url: Option<Url>,
     #[clap(subcommand)]
     command: Command,
 }
@@ -47,16 +49,21 @@ async fn main() -> anyhow::Result<()> {
         Some(key) => parse_secret(&key)?,
     };
 
-    let derp_map = match args.derp_addr {
+    let derp_map = match args.derp_url {
         None => default_derp_map(),
         Some(url) => {
-            let mut addrs = url.to_socket_addrs()?;
-            let addr = addrs.next().expect("Failed to parse DERP address");
-            let (ip4, ip6) = match addr.ip() {
-                IpAddr::V4(addr) => (UseIpv4::Some(addr), UseIpv6::None),
-                IpAddr::V6(addr) => (UseIpv4::None, UseIpv6::Some(addr)),
+            // TODO: This should be done by the DERP client.
+            let derp_port = match url.port() {
+                Some(port) => port,
+                None => match url.scheme() {
+                    "http" => 80,
+                    "https" => 443,
+                    _ => anyhow::bail!(
+                        "Invalid scheme in DERP URL, only http: and https: schemes are supported."
+                    ),
+                },
             };
-            DerpMap::default_from_node(url, 3478, addr.port(), ip4, ip6)
+            DerpMap::default_from_node(url, 3478, derp_port, UseIpv4::None, UseIpv6::None)
         }
     };
 
@@ -67,32 +74,29 @@ async fn main() -> anyhow::Result<()> {
         .bind(args.bind_address)
         .await?;
 
-    println!("endpoint bound! our peer id: {}", endpoint.peer_id());
     let me = endpoint.peer_id();
+    let local_addr = endpoint.local_addr()?;
+    println!("magic socket listening on {local_addr:?}");
+    println!("our peer id: {me}");
 
     match args.command {
         Command::Listen => {
             while let Some(conn) = endpoint.accept().await {
                 let (peer_id, alpn, conn) = accept_conn(conn).await?;
-                println!(
-                    "> new connection from {peer_id} (on {}) with ALPN {alpn}",
+                info!(
+                    "new connection from {peer_id} with ALPN {alpn} (coming from {})",
                     conn.remote_address()
                 );
                 tokio::spawn(async move {
                     let (mut send, mut recv) = conn.accept_bi().await?;
-                    println!("> accepted bi stream");
-
-                    println!("> waiting to recv...");
+                    debug!("accepted bi stream, waiting for data...");
                     let message = recv.read_to_end(1000).await?;
                     let message = String::from_utf8(message)?;
                     println!("received: {message}");
 
-                    eprintln!("> start to send");
                     let message = format!("hi! you connected to {me}. bye bye");
                     send.write_all(message.as_bytes()).await?;
-                    eprintln!("> all data sent! now calling finish");
                     send.finish().await?;
-                    eprintln!("> sent and finished!");
 
                     Ok::<_, anyhow::Error>(())
                 });
@@ -102,14 +106,13 @@ async fn main() -> anyhow::Result<()> {
             let peer_id: PeerId = peer_id.parse()?;
             let addrs = addrs.unwrap_or_default();
             let conn = endpoint.connect(peer_id, EXAMPLE_ALPN, &addrs).await?;
+            info!("connected");
 
             let (mut send, mut recv) = conn.open_bi().await?;
 
             let message = format!("hello here's {me}");
             send.write_all(message.as_bytes()).await?;
             send.finish().await?;
-            eprintln!("> send finished");
-
             let message = recv.read_to_end(100).await?;
             let message = String::from_utf8(message)?;
             println!("received: {message}");
