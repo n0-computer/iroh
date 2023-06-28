975b650208d7541e392b2c822517ffd252ed6355
dignifiedquire <me@dignifiedquire.com>
2023-06-28 12:26:39 +0200

    fix(iroh): error when path does not exist
    Fixes #1068

--- iroh/src/commands/provide.rs
+++ iroh/src/commands/provide.rs
@@ -5,7 +5,7 @@ use std::{
     str::FromStr,
 };
 
-use anyhow::{Context, Result};
+use anyhow::{ensure, Context, Result};
 use iroh_bytes::{
     provider::{Database, FNAME_PATHS},
     runtime,
@@ -32,6 +32,14 @@ pub async fn run(
     rpc_port: ProviderRpcPort,
     keylog: bool,
 ) -> Result<()> {
+    if let Some(ref path) = path {
+        ensure!(
+            path.exists(),
+            "Cannot provide nonexistent path: {}",
+            path.display()
+        );
+    }
+
     let iroh_data_root = iroh_data_root()?;
     let marker = iroh_data_root.join(FNAME_PATHS);
     let db = {
