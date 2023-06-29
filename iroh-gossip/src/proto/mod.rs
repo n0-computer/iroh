9ce4b86181128d788838afec7377ab8ad824c5c5
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-06-29 19:29:28 +0200

    feat: transmit opaque PeerData on join, forward and neighbor
    ... and wire it up in net module to transmit your endpoint addresses

--- iroh-gossip/src/proto/mod.rs
+++ iroh-gossip/src/proto/mod.rs
@@ -1,6 +1,6 @@
 use std::{fmt, hash::Hash};
 
-use serde::{de::DeserializeOwned, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 pub mod hyparview;
 pub mod plumtree;
@@ -13,3 +13,17 @@ pub use topic::{Command, Config, Event, IO};
 
 pub trait PeerAddress: Hash + Eq + Copy + fmt::Debug + Serialize + DeserializeOwned {}
 impl<T> PeerAddress for T where T: Hash + Eq + Copy + fmt::Debug + Serialize + DeserializeOwned {}
+
+pub type PeerData = bytes::Bytes;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PeerInfo<PA> {
+    pub id: PA,
+    pub data: PeerData,
+}
+
+impl<PA> From<(PA, PeerData)> for PeerInfo<PA> {
+    fn from((id, data): (PA, PeerData)) -> Self {
+        Self { id, data }
+    }
+}
