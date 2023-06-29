6f01042cf7ab5d7a6b39d29972bab2a6f79b7713
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-06-29 11:58:00 +0200

    feat: iroh-gossip initial commit
    This commit imports iroh-gossip into the repo. See https://github.com/n0-computer/iroh-sync/pull/1 for the prehistory.
    
    * From that original PR comes my IO-less implementation of the gossip protocol
      (Plumtree broadcast trees and HyParView cluster membership). The
      implementations are quite strict according to the papers
      [1](https://asc.di.fct.unl.pt/~jleitao/pdf/dsn07-leitao.pdf) and
      [2](https://asc.di.fct.unl.pt/~jleitao/pdf/srds07-leitao.pdf). If you
      find any deviations that are not documented, please report, this is
      likely a bug.
    * The combined protocol for Plumtree and Hyparview is additionally namespaced
      with a 32-byte topic ID to allow multiple broadcast trees (one per
      topic) to coexist. This means: Each topic is a seperate broadcast tree
      with seperate swarm membership and active connections (connections to
      the same peers are still shared on the network layer)
    
    This IO-less protocol implementation is in the `proto` module.
    
    This PR *also* adds my work from the past week: A `net` module that provides networking
    for the IO-less protocol impl, through the `MagicEndpoint` from iroh-net, with a (I think) quite straightforward actor implementation.
    
    The `net` module has a smoke test, and the PR also includes a `chat` example that adds signed messages on the trustless base layer.
    
    What this mostly needs is, I think, more testing and validation of the
    protocol implementation. For this to be practical, I think we should
    formulate some base usecase scenarios.
    
    The `proto` module has a very simple simulator and some tests that can
    take environment variables to increase the cluster size. They passed
    with 10.000 nodes. They also report some stats. The numbers look OK-ish,
    but I'd love to compare them properly with either other implementations
    or estimations from the original paper or other resoures. However, those
    are hard to find.

---
+++ iroh-gossip/examples/chat.rs
@@ -0,0 +1,229 @@
+//! Chat over iroh-gossip
+//!
+//! This broadcasts signed messages over iroh-gossip and verifies
+//! signatures on received messages.
+//!
+//! To connect to a swarm, you need to share a DERP server and know at least
+//! one other peer's peer id.
+//!
+//! By default a new peer id is created when starting the example. To reuse your identity,
+//! set the `--secret-key` CLI flag with the secret printed on a previous invocation.
+//!
+//! You can use this with a local DERP server. To do so, run
+//! `cargo run --bin derper -- --dev`
+//! and then set the `-d http://localhost:3340` flag on this example.
+
+use std::{collections::HashMap, net::SocketAddr};
+
+use bytes::Bytes;
+use clap::Parser;
+use ed25519_dalek::{Signature, Signer, SigningKey, VerifyingKey};
+use iroh_gossip::{
+    net::{GossipHandle, GOSSIP_ALPN},
+    proto::{Event, TopicId},
+};
+use iroh_net::{
+    defaults::default_derp_map,
+    hp::derp::{DerpMap, UseIpv4, UseIpv6},
+    magic_endpoint::accept_conn,
+    tls::{Keypair, PeerId},
+    MagicEndpoint,
+};
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long)]
+    secret_key: Option<String>,
+    #[clap(short, long)]
+    topic: String,
+    #[clap(short, long)]
+    derp_server: Option<Url>,
+    #[clap(short, long)]
+    peers: Vec<PeerId>,
+    #[clap(short, long)]
+    name: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    let keypair = match args.secret_key {
+        None => Keypair::generate(),
+        Some(key) => parse_secret(&key)?,
+    };
+    println!("our secret key: {}", fmt_secret(&keypair));
+
+    // configure our derp map
+    // TODO: this should be a one-liner
+    let derp_map = match args.derp_server {
+        None => default_derp_map(),
+        Some(url) => {
+            let derp_port = match url.port() {
+                Some(port) => port,
+                None => match url.scheme() {
+                    "http" => 80,
+                    "https" => 443,
+                    _ => anyhow::bail!(
+                        "Invalid scheme in DERP URL, only http: and https: schemes are supported."
+                    ),
+                },
+            };
+            DerpMap::default_from_node(url, 3478, derp_port, UseIpv4::None, UseIpv6::None)
+        }
+    };
+
+    // init a peerid -> name hashmap
+    let mut names = HashMap::new();
+
+    // build our magic endpoint
+    let endpoint = MagicEndpoint::builder()
+        .keypair(keypair)
+        .alpns(vec![GOSSIP_ALPN.to_vec()])
+        .derp_map(Some(derp_map))
+        .bind(SocketAddr::new([127, 0, 0, 1].into(), 0))
+        .await?;
+    println!("> listening as {}", endpoint.peer_id());
+
+    // create the gossip protocol
+    let gossip = GossipHandle::from_endpoint(endpoint.clone(), Default::default());
+
+    // spawn our endpoint loop that forwards incoming connections to the gossiper
+    tokio::spawn(endpoint_loop(endpoint.clone(), gossip.clone()));
+
+    // join the topic with the peers provided
+    let topic: TopicId = blake3::hash(args.topic.as_bytes()).into();
+    println!("> joining topic {topic} on peers {:?}", args.peers);
+    gossip.join(topic, args.peers).await?;
+    println!("> joined! now send some gossip...");
+
+    // broadcast our name, if set
+    if let Some(name) = args.name {
+        names.insert(endpoint.peer_id(), name.clone());
+        let message =
+            SignedMessage::sign_and_encode(endpoint.keypair(), &Message::AboutMe { name })?;
+        gossip.broadcast(topic, message).await?;
+    }
+
+    // subscribe and print loop
+    tokio::spawn(subscribe_loop(gossip.clone(), topic, names));
+
+    // spawn an input thread that reads stdin
+    // not using tokio here because they recommend this for "technical reasons"
+    let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
+    std::thread::spawn(move || input_loop(line_tx));
+
+    // broadcast each line we type
+    while let Some(text) = line_rx.recv().await {
+        let message =
+            SignedMessage::sign_and_encode(endpoint.keypair(), &Message::Message { text })?;
+        gossip.broadcast(topic, message).await?;
+    }
+
+    Ok(())
+}
+
+async fn subscribe_loop(
+    gossip: GossipHandle,
+    topic: TopicId,
+    mut names: HashMap<PeerId, String>,
+) -> anyhow::Result<()> {
+    let mut stream = gossip.subscribe(topic).await?;
+    loop {
+        let event = stream.recv().await?;
+        if let Event::Received(data) = event {
+            let (from, message) = SignedMessage::verify_and_decode(&data)?;
+            match message {
+                Message::AboutMe { name } => {
+                    names.insert(from, name.clone());
+                    println!("> {} is now known as {}", fmt_peer_id(&from), name);
+                }
+                Message::Message { text } => {
+                    let name = names
+                        .get(&from)
+                        .map_or_else(|| fmt_peer_id(&from), String::to_string);
+                    println!("{}: {}", name, text);
+                }
+            }
+        }
+    }
+}
+
+async fn endpoint_loop(endpoint: MagicEndpoint, gossip: GossipHandle) -> anyhow::Result<()> {
+    while let Some(conn) = endpoint.accept().await {
+        let (peer_id, alpn, conn) = accept_conn(conn).await?;
+        match alpn.as_bytes() {
+            GOSSIP_ALPN => gossip.handle_connection(conn).await?,
+            _ => println!("> ignoring connection from {peer_id}: unsupported ALPN protocol"),
+        }
+    }
+    Ok(())
+}
+
+fn input_loop(line_tx: tokio::sync::mpsc::Sender<String>) -> anyhow::Result<()> {
+    let mut buffer = String::new();
+    let stdin = std::io::stdin(); // We get `Stdin` here.
+    loop {
+        stdin.read_line(&mut buffer)?;
+        line_tx.blocking_send(buffer.clone())?;
+        buffer.clear();
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct SignedMessage {
+    from: PeerId,
+    data: Bytes,
+    signature: Signature,
+}
+
+impl SignedMessage {
+    pub fn verify_and_decode(bytes: &[u8]) -> anyhow::Result<(PeerId, Message)> {
+        let signed_message: Self = postcard::from_bytes(bytes)?;
+        let key: VerifyingKey = signed_message.from.into();
+        key.verify_strict(&signed_message.data, &signed_message.signature)?;
+        let message: Message = postcard::from_bytes(&signed_message.data)?;
+        Ok((signed_message.from, message))
+    }
+
+    pub fn sign_and_encode(keypair: &Keypair, message: &Message) -> anyhow::Result<Bytes> {
+        let data: Bytes = postcard::to_stdvec(&message)?.into();
+        let signature = keypair.secret().sign(&data);
+        let from: PeerId = keypair.public().into();
+        let signed_message = Self {
+            from,
+            data,
+            signature,
+        };
+        let encoded = postcard::to_stdvec(&signed_message)?;
+        Ok(encoded.into())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+enum Message {
+    AboutMe { name: String },
+    Message { text: String },
+}
+
+fn fmt_peer_id(input: &PeerId) -> String {
+    let text = format!("{}", input);
+    format!("{}…{}", &text[..5], &text[(text.len() - 2)..])
+}
+
+fn fmt_secret(keypair: &Keypair) -> String {
+    let mut text = data_encoding::BASE32_NOPAD.encode(&keypair.secret().to_bytes());
+    text.make_ascii_lowercase();
+    text
+}
+fn parse_secret(secret: &str) -> anyhow::Result<Keypair> {
+    let bytes: [u8; 32] = data_encoding::BASE32_NOPAD
+        .decode(secret.to_ascii_uppercase().as_bytes())?
+        .try_into()
+        .map_err(|_| anyhow::anyhow!("Invalid secret"))?;
+    let key = SigningKey::from_bytes(&bytes);
+    Ok(key.into())
+}
---
+++ iroh-gossip/src/net/util.rs
@@ -0,0 +1,170 @@
+use std::{collections::HashMap, io, pin::Pin, time::Instant};
+
+use anyhow::{anyhow, bail, ensure, Context, Result};
+use bytes::{Bytes, BytesMut};
+use futures::{future::BoxFuture, stream::FuturesUnordered, FutureExt, StreamExt};
+use iroh_net::{tls::PeerId, MagicEndpoint};
+use tokio::{
+    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
+    time::{sleep_until, Sleep},
+};
+use tokio_util::sync::CancellationToken;
+
+use crate::proto::util::TimerMap;
+
+use super::{ProtoMessage, MAX_MESSAGE_SIZE};
+
+/// Write a [ProtoMessage] as a length-prefixed, postcard-encoded message.
+pub async fn write_message<W: AsyncWrite + Unpin>(
+    writer: &mut W,
+    buffer: &mut BytesMut,
+    frame: &ProtoMessage,
+) -> Result<()> {
+    let len = postcard::experimental::serialized_size(&frame)?;
+    ensure!(len < MAX_MESSAGE_SIZE);
+    buffer.clear();
+    buffer.resize(len, 0u8);
+    let slice = postcard::to_slice(&frame, buffer)?;
+    writer.write_u32(len as u32).await?;
+    writer.write_all(slice).await?;
+    Ok(())
+}
+
+/// Read a length-prefixed message and decode as [[ProtoMessage]];
+pub async fn read_message(
+    reader: impl AsyncRead + Unpin,
+    buffer: &mut BytesMut,
+) -> Result<Option<ProtoMessage>> {
+    match read_lp(reader, buffer).await? {
+        None => Ok(None),
+        Some(data) => {
+            let message = postcard::from_bytes(&data)?;
+            Ok(Some(message))
+        }
+    }
+}
+
+/// Reads a length prefixed message.
+///
+/// # Returns
+///
+/// The message as raw bytes.  If the end of the stream is reached and there is no partial
+/// message, returns `None`.
+pub async fn read_lp(
+    mut reader: impl AsyncRead + Unpin,
+    buffer: &mut BytesMut,
+) -> Result<Option<Bytes>> {
+    let size = match reader.read_u32().await {
+        Ok(size) => size,
+        Err(err) if err.kind() == io::ErrorKind::UnexpectedEof => return Ok(None),
+        Err(err) => return Err(err.into()),
+    };
+    let mut reader = reader.take(size as u64);
+    let size = usize::try_from(size).context("frame larger than usize")?;
+    if size > MAX_MESSAGE_SIZE {
+        bail!("Incoming message exceeds MAX_MESSAGE_SIZE");
+    }
+    buffer.reserve(size);
+    loop {
+        let r = reader.read_buf(buffer).await?;
+        if r == 0 {
+            break;
+        }
+    }
+    Ok(Some(buffer.split_to(size).freeze()))
+}
+
+pub type DialFuture = BoxFuture<'static, (PeerId, anyhow::Result<quinn::Connection>)>;
+
+/// Dial peers and maintain a queue of pending dials
+///
+/// This wraps a [MagicEndpoint], connects to peers through the endpoint, stores
+/// the pending connect futures and emits finished connect results.
+pub struct Dialer {
+    endpoint: MagicEndpoint,
+    pending: FuturesUnordered<DialFuture>,
+    pending_peers: HashMap<PeerId, CancellationToken>,
+}
+impl Dialer {
+    pub fn new(endpoint: MagicEndpoint) -> Self {
+        Self {
+            endpoint,
+            pending: Default::default(),
+            pending_peers: Default::default(),
+        }
+    }
+    pub fn queue_dial(&mut self, peer_id: PeerId, alpn_protocol: &'static [u8]) {
+        if self.is_pending(&peer_id) {
+            return;
+        }
+        let cancel = CancellationToken::new();
+        self.pending_peers.insert(peer_id, cancel.clone());
+        let endpoint = self.endpoint.clone();
+        let fut = async move {
+            let res = tokio::select! {
+                biased;
+                _ = cancel.cancelled() => Err(anyhow!("Cancelled")),
+                res = endpoint.connect(peer_id, alpn_protocol, &[]) => res
+            };
+            (peer_id, res)
+        }
+        .boxed();
+        self.pending.push(fut.boxed());
+    }
+
+    pub fn abort_dial(&mut self, peer_id: &PeerId) {
+        if let Some(cancel) = self.pending_peers.remove(peer_id) {
+            cancel.cancel();
+        }
+    }
+
+    pub fn is_pending(&self, peer: &PeerId) -> bool {
+        self.pending_peers.contains_key(peer)
+    }
+
+    pub async fn next(&mut self) -> (PeerId, anyhow::Result<quinn::Connection>) {
+        match self.pending_peers.is_empty() {
+            false => {
+                let (peer_id, res) = self.pending.next().await.unwrap();
+                self.pending_peers.remove(&peer_id);
+                (peer_id, res)
+            }
+            true => futures::future::pending().await,
+        }
+    }
+}
+
+pub struct Timers<T> {
+    next: Option<(Instant, Pin<Box<Sleep>>)>,
+    map: TimerMap<T>,
+}
+impl<T> Timers<T> {
+    pub fn new() -> Self {
+        Self {
+            next: None,
+            map: TimerMap::new(),
+        }
+    }
+    /// Insert a new entry at the specified instant
+    pub fn insert(&mut self, instant: Instant, item: T) {
+        self.map.insert(instant, item);
+        self.reset();
+    }
+
+    pub fn reset(&mut self) {
+        self.next = self
+            .map
+            .first()
+            .map(|(instant, _)| (*instant, Box::pin(sleep_until((*instant).into()))))
+    }
+
+    pub async fn wait_and_drain(&mut self) -> impl Iterator<Item = (Instant, T)> {
+        match self.next.as_mut() {
+            Some((instant, sleep)) => {
+                sleep.await;
+                self.map.drain_until(instant)
+            }
+            None => futures::future::pending().await,
+        }
+    }
+}
