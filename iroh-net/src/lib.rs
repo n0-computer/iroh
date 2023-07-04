1e26bd9adeee5aeffc713ee764502b32c6fdd4f4
Diva M <divma@protonmail.com>
2023-07-04 11:45:01 -0500

    turn on lint based on submodule

--- iroh-net/src/lib.rs
+++ iroh-net/src/lib.rs
@@ -1,11 +1,17 @@
-#![deny(missing_docs, rustdoc::broken_intra_doc_links)]
+// #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 #![recursion_limit = "256"]
 
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod defaults;
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod hp;
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod magic_endpoint;
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod net;
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod tls;
+#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod util;
 
 pub use magic_endpoint::MagicEndpoint;
