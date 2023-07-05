5492adbec39f0f833532fbdbca8b579617717719
Diva M <divma@protonmail.com>
2023-07-05 11:10:58 -0500

    netmap docs

--- iroh-net/src/hp/netmap.rs
+++ iroh-net/src/hp/netmap.rs
@@ -7,5 +7,6 @@ use super::cfg;
 /// This contains all the peers the local node knows about.
 #[derive(Clone, Debug)]
 pub struct NetworkMap {
+    /// Known peers in the network.
     pub peers: Vec<cfg::Node>,
 }
