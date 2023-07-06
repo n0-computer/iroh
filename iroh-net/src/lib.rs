ea80c2ff6a721c2543f32e7e9db1ce3c530b9784
Floris Bruynooghe <flub@devork.be>
2023-07-06 16:24:10 +0200

    docs: iroh-net toplevel
    Add some top-level docs for iroh-net

--- iroh-net/src/lib.rs
+++ iroh-net/src/lib.rs
@@ -1,16 +1,16 @@
+//! iroh-net provides connectivity for iroh.
+//!
+//! This crate is a collection of tools to establish direct connectivity between peers.  At
+//! the high level [`MagicEndpoint`] is used to establish a QUIC connection with
+//! authenticated peers and holepunching support.
 #![recursion_limit = "256"]
+#![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod defaults;
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod hp;
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod magic_endpoint;
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod net;
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod tls;
-#[deny(missing_docs, rustdoc::broken_intra_doc_links)]
 pub mod util;
 
 pub use magic_endpoint::MagicEndpoint;
