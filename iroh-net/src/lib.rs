ab4c2cbc0551248de32d32ba89eae7725afda802
Floris Bruynooghe <flub@devork.be>
2023-07-07 09:08:22 +0200

    Remove direct and mention relaying
    It's really a very rough description, can be improved lots I think.

--- iroh-net/src/lib.rs
+++ iroh-net/src/lib.rs
@@ -1,8 +1,8 @@
 //! iroh-net provides connectivity for iroh.
 //!
-//! This crate is a collection of tools to establish direct connectivity between peers.  At
+//! This crate is a collection of tools to establish connectivity between peers.  At
 //! the high level [`MagicEndpoint`] is used to establish a QUIC connection with
-//! authenticated peers and holepunching support.
+//! authenticated peers, relaying and holepunching support.
 #![recursion_limit = "256"]
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
