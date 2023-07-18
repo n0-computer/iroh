b898900df2190382550b6f9bbfbfb559757a7ee0
Kasey <klhuizinga@gmail.com>
2023-07-18 13:22:44 -0400

    isolate test_mesh_network

--- iroh-net/src/derp/http/mesh_clients.rs
+++ iroh-net/src/derp/http/mesh_clients.rs
@@ -117,10 +117,10 @@ mod tests {
             .ok();
 
         // TODO(ramfox): figure out why this fails on later rounds
-        // for i in 0..10 {
-        // println!("TEST_MESH_NETWORK: round {i}");
-        test_mesh_network_once().await?;
-        // }
+        for i in 0..50 {
+            println!("TEST_MESH_NETWORK: round {i}");
+            test_mesh_network_once().await?;
+        }
         Ok(())
     }
 
