beee85c56b6303c5632b57b3643873823a6cb5eb
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-08-10 16:07:34 +0200

    fix downloader and sync test

--- iroh/tests/sync.rs
+++ iroh/tests/sync.rs
@@ -2,9 +2,10 @@
 
 use std::{net::SocketAddr, time::Duration};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use futures::StreamExt;
 use iroh::{
+    client::DocMem,
     collection::IrohCollectionParser,
     node::{Builder, Node},
     rpc_protocol::ShareMode,
@@ -65,80 +66,51 @@ async fn sync_full_basic() -> Result<()> {
     let nodes = spawn_nodes(rt, 3).await?;
     let clients = nodes.iter().map(|node| node.client()).collect::<Vec<_>>();
 
-    // for (i, node) in nodes.iter().enumerate() {
-    //     println!(
-    //         "node {i}: {} {:?}",
-    //         node.peer_id(),
-    //         node.local_endpoints().await
-    //     );
-    // }
-
     // node1: create doc and ticket
-    let (ticket, doc2) = {
+    let (ticket, doc1) = {
         let iroh = &clients[0];
         let author = iroh.create_author().await?;
         let doc = iroh.create_doc().await?;
         let key = b"k1";
         let value = b"v1";
         doc.set_bytes(author, key.to_vec(), value.to_vec()).await?;
-        let entry = doc.get_latest(author, key.to_vec()).await?;
-        let res = doc.get_content_bytes(&entry).await?;
-        assert_eq!(res.to_vec(), value.to_vec());
+        assert_latest(&doc, key, value).await;
         let ticket = doc.share(ShareMode::Write).await?;
         (ticket, doc)
     };
 
     // node2: join in
-    {
+    let _doc2 = {
         let iroh = &clients[1];
         let author = iroh.create_author().await?;
         let doc = iroh.import_doc(ticket.clone()).await?;
+
         // todo: events over rpc to not use sleep...
         tokio::time::sleep(Duration::from_secs(3)).await;
-
-        let key = b"k1".to_vec();
-        let filter = GetFilter::new().with_key(key);
-        let entry = doc.get(filter).await?.next().await.unwrap()?;
-        let res = doc.get_content_bytes(&entry).await?;
-        assert_eq!(res.to_vec(), b"v1".to_vec());
+        assert_latest(&doc, b"k1", b"v1").await;
 
         let key = b"k2";
         let value = b"v2";
         doc.set_bytes(author, key.to_vec(), value.to_vec()).await?;
+        assert_latest(&doc, key, value).await;
         // todo: events
         tokio::time::sleep(Duration::from_secs(3)).await;
+        assert_latest(&doc1, key, value).await;
+        doc
+    };
 
-        for doc in &[doc, doc2] {
-            let filter = GetFilter::new().with_key(key.to_vec());
-            let entry = doc.get(filter).await?.next().await.unwrap()?;
-            let res = doc.get_content_bytes(&entry).await?;
-            assert_eq!(res.to_vec(), value.to_vec());
-        }
-    }
-
-    {
-        //  node 3 joins & imports the doc from peer 1
+    //  node 3 joins & imports the doc from peer 1
+    let _doc3 = {
         let iroh = &clients[2];
+        println!("!!!! DOC 3 JOIN !!!!!");
         let doc = iroh.import_doc(ticket).await?;
 
         // todo: events
         tokio::time::sleep(Duration::from_secs(3)).await;
-
-        let key = b"k1";
-        let value = b"v1";
-        let filter = GetFilter::new().with_key(key.to_vec());
-        let entry = doc.get(filter).await?.next().await.unwrap()?;
-        let res = doc.get_content_bytes(&entry).await?;
-        assert_eq!(res.to_vec(), value.to_vec());
-
-        let key = b"k2";
-        let value = b"v2";
-        let filter = GetFilter::new().with_key(key.to_vec());
-        let entry = doc.get(filter).await?.next().await.unwrap()?;
-        let res = doc.get_content_bytes(&entry).await?;
-        // TODO: This fails! seems reproviding is not working?
-        assert_eq!(res.to_vec(), value.to_vec());
-    }
+        assert_latest(&doc, b"k1", b"v1").await;
+        assert_latest(&doc, b"k2", b"v2").await;
+        doc
+    };
 
     // TODO:
     // - gossiping between multiple peers
@@ -152,46 +124,22 @@ async fn sync_full_basic() -> Result<()> {
     Ok(())
 }
 
-// async fn report<C: ServiceConnection<ProviderService>>(
-//     client: &Iroh<C>,
-//     id: NamespaceId,
-//     label: impl ToString,
-// ) {
-//     let label = label.to_string();
-//     println!("report: {label} {id}");
-//     match try_report(client, id).await {
-//         Ok(_) => {}
-//         Err(err) => println!("  failed: {err}"),
-//     }
-// }
-//
-// async fn try_report<C: ServiceConnection<ProviderService>>(
-//     client: &Iroh<C>,
-//     id: NamespaceId,
-// ) -> anyhow::Result<()> {
-//     let doc = client.get_doc(id)?;
-//     let filter = GetFilter {
-//         latest: false,
-//         author: None,
-//         key: KeyFilter::All,
-//     };
-//     let mut stream = doc.get(filter).await?;
-//     while let Some(entry) = stream.next().await {
-//         let entry = entry?;
-//         let text = match doc.get_content_bytes(&entry).await {
-//             Ok(bytes) => String::from_utf8(bytes.to_vec())?,
-//             Err(err) => format!("<{err}>"),
-//         };
-//         println!(
-//             "    @{} {} {:4} -- {}",
-//             entry.author(),
-//             entry.content_hash(),
-//             entry.content_len(),
-//             text
-//         );
-//     }
-//     Ok(())
-// }
+async fn assert_latest(doc: &DocMem, key: &[u8], value: &[u8]) {
+    let content = get_latest(doc, key).await.unwrap();
+    assert_eq!(content, value.to_vec());
+}
+
+async fn get_latest(doc: &DocMem, key: &[u8]) -> anyhow::Result<Vec<u8>> {
+    let filter = GetFilter::new().with_key(key.to_vec());
+    let entry = doc
+        .get(filter)
+        .await?
+        .next()
+        .await
+        .ok_or_else(|| anyhow!("entry not found"))??;
+    let content = doc.get_content_bytes(&entry).await?;
+    Ok(content.to_vec())
+}
 
 fn setup_logging() {
     tracing_subscriber::registry()
