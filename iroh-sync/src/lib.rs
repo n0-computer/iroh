9816273a481549db51c7afa2cf61dab66d537007
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-08-16 18:11:50 +0200

    chore: fmt

--- iroh-sync/src/lib.rs
+++ iroh-sync/src/lib.rs
@@ -1,9 +1,9 @@
 //! Set reconciliation for multi-dimensional key-value stores
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
+mod keys;
 #[cfg(feature = "metrics")]
 pub mod metrics;
 mod ranger;
 pub mod store;
 pub mod sync;
-mod keys;
