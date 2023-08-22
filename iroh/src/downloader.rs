03869bd6cf89b9c4299109922609c97734f50130
Diva M <divma@protonmail.com>
2023-08-21 21:30:02 -0500

    add download handle

--- iroh/src/downloader.rs
+++ iroh/src/downloader.rs
@@ -7,6 +7,11 @@ use iroh_bytes::{
     protocol::{RangeSpec, RangeSpecSeq},
     Hash,
 };
+use tokio::sync::oneshot;
+use tokio_util::sync::CancellationToken;
+
+#[derive(Debug)]
+pub struct Downloader;
 
 /// Download requests the [`Downloader`] handles.
 #[derive(Debug)]
@@ -35,4 +40,30 @@ pub enum Download {
         /// Sequence of ranges to be downloaded from this collection.
         range_set_seq: RangeSpecSeq,
     },
+}
+
+/// Handle to interact with a download request.
+#[derive(Debug)]
+pub struct DownloadHandle {
+    /// Id used to identify the request in the [`Downloader`].
+    id: u64,
+    /// Token used to cancel this request.
+    cancellation_token: CancellationToken,
+    /// Receiver to retrieve the return value of this download.
+    // TODO(@divma): what's the return value?
+    receiver: oneshot::Receiver<u64>,
+}
+
+impl DownloadHandle {
+    /// Cancels the request consuming the handle.
+    pub fn cancel(self) {
+        self.cancellation_token.cancel()
+    }
+}
+
+impl Drop for DownloadHandle {
+    fn drop(&mut self) {
+        // directly cancel the request on drop
+        self.cancellation_token.cancel();
+    }
 }
