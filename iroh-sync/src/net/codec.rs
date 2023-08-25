f2eee501053edfb714f91d3b5c4fc108901d2f11
dignifiedquire <me@dignifiedquire.com>
2023-08-25 18:18:40 +0200

    expand test

--- iroh-sync/src/net/codec.rs
+++ iroh-sync/src/net/codec.rs
@@ -288,123 +288,138 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_sync_many_authors() -> Result<()> {
+    async fn test_sync_many_authors_memory() -> Result<()> {
+        let alice_store = store::memory::Store::default();
+        let bob_store = store::memory::Store::default();
+        test_sync_many_authors(alice_store, bob_store).await
+    }
+
+    #[tokio::test]
+    async fn test_sync_many_authors_fs() -> Result<()> {
+        let tmpdir = tempfile::tempdir()?;
+        let alice_store = store::fs::Store::new(tmpdir.path().join("a.db"))?;
+        let bob_store = store::fs::Store::new(tmpdir.path().join("b.db"))?;
+        test_sync_many_authors(alice_store, bob_store).await
+    }
+
+    async fn test_sync_many_authors<S: store::Store>(
+        alice_replica_store: S,
+        bob_replica_store: S,
+    ) -> Result<()> {
         let mut rng = rand::thread_rng();
 
-        const NUM_MESSAGES: usize = 1; //20;
+        for num_messages in &[1, 2, 5, 10, 20] {
+            for num_authors in &[2, 3, 4, 5, 10, 20] {
+                println!("Using {num_authors} authors and {num_messages} messages per side");
 
-        for num_authors in &[2, 3, 4, 5, 10, 20] {
-            println!("Using {num_authors} authors and {NUM_MESSAGES} messages per side");
+                let alice_peer_id = SecretKey::generate().public();
+                let bob_peer_id = SecretKey::generate().public();
+                let namespace = Namespace::new(&mut rng);
 
-            let alice_peer_id = SecretKey::generate().public();
-            let bob_peer_id = SecretKey::generate().public();
-            let namespace = Namespace::new(&mut rng);
+                let authors_alice: Vec<_> = (0..*num_authors)
+                    .map(|_| alice_replica_store.new_author(&mut rng).unwrap())
+                    .collect();
 
-            let alice_replica_store = store::memory::Store::default();
-            let authors_alice: Vec<_> = (0..*num_authors)
-                .map(|_| alice_replica_store.new_author(&mut rng).unwrap())
-                .collect();
-
-            let alice_replica = alice_replica_store.new_replica(namespace.clone()).unwrap();
-            for i in 0..NUM_MESSAGES {
-                for (author_num, author) in authors_alice.iter().enumerate() {
-                    alice_replica
-                        .hash_and_insert(
-                            format!("hello bob {i}"),
-                            author,
-                            format!("from alice: {author_num}"),
-                        )
-                        .unwrap();
+                let alice_replica = alice_replica_store.new_replica(namespace.clone()).unwrap();
+                for i in 0..*num_messages {
+                    for (author_num, author) in authors_alice.iter().enumerate() {
+                        alice_replica
+                            .hash_and_insert(
+                                format!("hello bob {i}"),
+                                author,
+                                format!("from alice: {author_num}"),
+                            )
+                            .unwrap();
+                    }
                 }
-            }
 
-            let bob_replica_store = store::memory::Store::default();
-            let authors_bob: Vec<_> = (0..*num_authors)
-                .map(|_| bob_replica_store.new_author(&mut rng).unwrap())
-                .collect();
-            let bob_replica = bob_replica_store.new_replica(namespace.clone()).unwrap();
-            for i in 0..NUM_MESSAGES {
-                for (author_num, author) in authors_bob.iter().enumerate() {
-                    bob_replica
-                        .hash_and_insert(
-                            format!("hello alice {i}"),
-                            author,
-                            format!("from bob: {author_num}"),
-                        )
-                        .unwrap();
+                let authors_bob: Vec<_> = (0..*num_authors)
+                    .map(|_| bob_replica_store.new_author(&mut rng).unwrap())
+                    .collect();
+                let bob_replica = bob_replica_store.new_replica(namespace.clone()).unwrap();
+                for i in 0..*num_messages {
+                    for (author_num, author) in authors_bob.iter().enumerate() {
+                        bob_replica
+                            .hash_and_insert(
+                                format!("hello alice {i}"),
+                                author,
+                                format!("from bob: {author_num}"),
+                            )
+                            .unwrap();
+                    }
                 }
+
+                assert_eq!(
+                    bob_replica_store
+                        .get(bob_replica.namespace(), GetFilter::all())
+                        .unwrap()
+                        .collect::<Result<Vec<_>>>()
+                        .unwrap()
+                        .len(),
+                    num_messages * num_authors
+                );
+                assert_eq!(
+                    alice_replica_store
+                        .get(alice_replica.namespace(), GetFilter::all())
+                        .unwrap()
+                        .collect::<Result<Vec<_>>>()
+                        .unwrap()
+                        .len(),
+                    num_messages * num_authors
+                );
+
+                let (alice, bob) = tokio::io::duplex(64 + num_messages);
+
+                println!("syncing");
+                let (mut alice_reader, mut alice_writer) = tokio::io::split(alice);
+                let replica = alice_replica.clone();
+                let alice_task = tokio::task::spawn(async move {
+                    run_alice::<S, _, _>(
+                        &mut alice_writer,
+                        &mut alice_reader,
+                        &replica,
+                        bob_peer_id,
+                    )
+                    .await
+                });
+
+                let (mut bob_reader, mut bob_writer) = tokio::io::split(bob);
+                let bob_replica_store_task = bob_replica_store.clone();
+                let bob_task = tokio::task::spawn(async move {
+                    run_bob::<S, _, _>(
+                        &mut bob_writer,
+                        &mut bob_reader,
+                        bob_replica_store_task,
+                        alice_peer_id,
+                    )
+                    .await
+                });
+
+                alice_task.await??;
+                bob_task.await??;
+
+                assert_eq!(
+                    alice_replica_store
+                        .get(alice_replica.namespace(), GetFilter::all())
+                        .unwrap()
+                        .collect::<Result<Vec<_>>>()
+                        .unwrap()
+                        .len(),
+                    2 * num_messages * num_authors,
+                    "alice is missing messages"
+                );
+
+                assert_eq!(
+                    bob_replica_store
+                        .get(bob_replica.namespace(), GetFilter::all())
+                        .unwrap()
+                        .collect::<Result<Vec<_>>>()
+                        .unwrap()
+                        .len(),
+                    2 * num_messages * num_authors,
+                    "bob is missing messages"
+                );
             }
-
-            assert_eq!(
-                bob_replica_store
-                    .get(bob_replica.namespace(), GetFilter::all())
-                    .unwrap()
-                    .collect::<Result<Vec<_>>>()
-                    .unwrap()
-                    .len(),
-                NUM_MESSAGES * num_authors
-            );
-            assert_eq!(
-                alice_replica_store
-                    .get(alice_replica.namespace(), GetFilter::all())
-                    .unwrap()
-                    .collect::<Result<Vec<_>>>()
-                    .unwrap()
-                    .len(),
-                NUM_MESSAGES * num_authors
-            );
-
-            let (alice, bob) = tokio::io::duplex(64 + NUM_MESSAGES);
-
-            println!("syncing");
-            let (mut alice_reader, mut alice_writer) = tokio::io::split(alice);
-            let replica = alice_replica.clone();
-            let alice_task = tokio::task::spawn(async move {
-                run_alice::<store::memory::Store, _, _>(
-                    &mut alice_writer,
-                    &mut alice_reader,
-                    &replica,
-                    bob_peer_id,
-                )
-                .await
-            });
-
-            let (mut bob_reader, mut bob_writer) = tokio::io::split(bob);
-            let bob_replica_store_task = bob_replica_store.clone();
-            let bob_task = tokio::task::spawn(async move {
-                run_bob::<store::memory::Store, _, _>(
-                    &mut bob_writer,
-                    &mut bob_reader,
-                    bob_replica_store_task,
-                    alice_peer_id,
-                )
-                .await
-            });
-
-            alice_task.await??;
-            bob_task.await??;
-
-            assert_eq!(
-                alice_replica_store
-                    .get(alice_replica.namespace(), GetFilter::all())
-                    .unwrap()
-                    .collect::<Result<Vec<_>>>()
-                    .unwrap()
-                    .len(),
-                2 * NUM_MESSAGES * num_authors,
-                "alice is missing messages"
-            );
-
-            assert_eq!(
-                bob_replica_store
-                    .get(bob_replica.namespace(), GetFilter::all())
-                    .unwrap()
-                    .collect::<Result<Vec<_>>>()
-                    .unwrap()
-                    .len(),
-                2 * NUM_MESSAGES * num_authors,
-                "bob is missing messages"
-            );
         }
         Ok(())
     }
