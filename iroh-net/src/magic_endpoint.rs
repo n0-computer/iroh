e35ebba0a9c02005fe365ae6f1daabfe671cb3fb
Diva M <divma@protonmail.com>
2023-09-17 14:29:31 -0500

    fix spelling

--- iroh-net/src/magic_endpoint.rs
+++ iroh-net/src/magic_endpoint.rs
@@ -17,7 +17,7 @@ use crate::{
 
 pub use super::magicsock::EndpointInfo as ConnectionInfo;
 
-/// Adress information for a node.
+/// Address information for a node.
 #[derive(Debug)]
 pub struct NodeAddr {
     /// The node's public key.
