878266f01f5ff19875c4f44a8292729a6be9a01c
Diva M <divma@protonmail.com>
2023-10-24 09:48:58 -0500

    wip

--- iroh/src/ticket.rs
+++ iroh/src/ticket.rs
@@ -1,75 +1,40 @@
-//! The ticket type for the provider.
-//!
-//! This is in it's own module to enforce the invariant that you can not construct a ticket
-//! with an empty address list.
+//! This module manages the different tickets Iroh has.
 
-use std::fmt::{self, Display};
-use std::str::FromStr;
-
-use anyhow::{ensure, Context, Result};
-use iroh_bytes::protocol::RequestToken;
-use iroh_bytes::{BlobFormat, Hash};
-use iroh_net::derp::{DerpMap, DerpMode};
-use iroh_net::key::SecretKey;
+use anyhow::{ensure, Result};
+use iroh_bytes::{protocol::RequestToken, BlobFormat, Hash};
 use iroh_net::PeerAddr;
 use serde::{Deserialize, Serialize};
 
-/// Options for the client
-#[derive(Clone, Debug)]
-pub struct Options {
-    /// The secret key of the node
-    pub secret_key: SecretKey,
-    /// The peer to connect to.
-    pub peer: PeerAddr,
-    /// Whether to log the SSL keys when `SSLKEYLOGFILE` environment variable is set
-    pub keylog: bool,
-    /// The configuration of the derp services
-    pub derp_map: Option<DerpMap>,
+/// Kind of ticket
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, strum::EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum TicketKind {
+    Blob,
+    Doc,
+    Peer,
 }
 
-impl Options {
-    // this makes little sense
-    pub fn from_ticket(
-        ticket: &super::ticket::BlobTicket,
-        secret_key: SecretKey,
-        derp_map: Option<DerpMap>,
-    ) -> Self {
-        Self {
-            secret_key,
-            peer: ticket.node_addr().clone(),
-            keylog: true,
-            derp_map,
-        }
+trait IrohTicket<'de>: Serialize + Deserialize<'de> {
+    const KIND: TicketKind;
+    fn to_bytes(&self) -> Vec<u8> {
+        postcard::to_stdvec(&self).expect("postcard::to_stdvec is infallible")
     }
 }
 
-/// Create a new endpoint and dial a peer, returning the connection
-///
-/// Note that this will create an entirely new endpoint, so it should be only
-/// used for short lived connections. If you want to connect to multiple peers,
-/// it is preferable to create an endpoint and use `connect` on the endpoint.
-pub async fn dial(opts: Options) -> anyhow::Result<quinn::Connection> {
-    let endpoint = iroh_net::MagicEndpoint::builder()
-        .secret_key(opts.secret_key)
-        .keylog(opts.keylog);
-    let derp_mode = match opts.derp_map {
-        Some(derp_map) => DerpMode::Custom(derp_map),
-        None => DerpMode::Default,
-    };
-    let endpoint = endpoint.derp_mode(derp_mode);
-    let endpoint = endpoint.bind(0).await?;
-    endpoint
-        .connect(opts.peer, &iroh_bytes::protocol::ALPN)
-        .await
-        .context("failed to connect to provider")
+fn serialize<'de, T: IrohTicket<'de>>(ticket: &T) -> String {
+    let mut out = IrohTicket::KIND.to_string();
+    let bytes = ticket.to_bytes();
+    ticket.encode_append(&bytes, &mut out);
+    out
 }
+
 
 /// A token containing everything to get a file from the provider.
 ///
 /// It is a single item which can be easily serialized and deserialized.  The [`Display`]
 /// and [`FromStr`] implementations serialize to base32.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-pub struct Ticket {
+pub struct BlobTicket {
     /// The provider to get a file from.
     peer: PeerAddr,
     /// The format of the blob.
@@ -80,7 +45,7 @@ pub struct Ticket {
     token: Option<RequestToken>,
 }
 
-impl Ticket {
+impl BlobTicket {
     const OPT_PREFIX: &'static str = "blob:";
     /// Creates a new ticket.
     pub fn new(
@@ -89,10 +54,7 @@ impl Ticket {
         format: BlobFormat,
         token: Option<RequestToken>,
     ) -> Result<Self> {
-        ensure!(
-            !peer.info.direct_addresses.is_empty(),
-            "addrs list can not be empty"
-        );
+        ensure!(!peer.is_empty(), "addressing information cannot be empty");
         Ok(Self {
             hash,
             format,
@@ -103,11 +65,8 @@ impl Ticket {
 
     /// Deserializes from bytes.
     pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
-        let slf: Ticket = postcard::from_bytes(bytes)?;
-        ensure!(
-            !slf.peer.info.direct_addresses.is_empty(),
-            "Invalid address list in ticket"
-        );
+        let slf: BlobTicket = postcard::from_bytes(bytes)?;
+        ensure!(!slf.is_empty(), "Invalid addressing info in ticket");
         Ok(slf)
     }
 
@@ -148,7 +107,7 @@ impl Ticket {
 
     /// Get the contents of the ticket, consuming it.
     pub fn into_parts(self) -> (PeerAddr, Hash, BlobFormat, Option<RequestToken>) {
-        let Ticket {
+        let Self {
             peer,
             hash,
             format,
@@ -156,18 +115,9 @@ impl Ticket {
         } = self;
         (peer, hash, format, token)
     }
-
-    /// Convert this ticket into a [`Options`], adding the given secret key.
-    pub fn as_get_options(&self, secret_key: SecretKey, derp_map: Option<DerpMap>) -> Options {
-        Options {
-            peer: self.peer.clone(),
-            secret_key,
-            keylog: true,
-            derp_map,
-        }
-    }
 }
 
+/*
 /// Serializes to base32.
 impl Display for Ticket {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -192,7 +142,7 @@ impl FromStr for Ticket {
         Self::from_bytes(&bytes)
     }
 }
-
+*/
 #[cfg(test)]
 mod tests {
     use std::net::SocketAddr;
