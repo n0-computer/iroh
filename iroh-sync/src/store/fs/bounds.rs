508316220c259048e9c441959fdd5960a6fa79f0
Franz Heinzmann (Frando) <frando@unbiskant.org>
2023-11-06 14:10:06 +0100

    tests: add tests for records bounds

--- iroh-sync/src/store/fs/bounds.rs
+++ iroh-sync/src/store/fs/bounds.rs
@@ -1,4 +1,4 @@
-use std::ops::Bound;
+use std::ops::{Bound, RangeBounds};
 
 use bytes::Bytes;
 
@@ -9,11 +9,11 @@ use super::{RecordsByKeyId, RecordsByKeyIdOwned, RecordsId, RecordsIdOwned};
 /// Bounds on the records table.
 ///
 /// Supports bounds by author, key
-pub struct RecordsBounds((Bound<RecordsIdOwned>, Bound<RecordsIdOwned>));
+pub struct RecordsBounds(Bound<RecordsIdOwned>, Bound<RecordsIdOwned>);
 
 impl RecordsBounds {
     pub fn new(start: Bound<RecordsIdOwned>, end: Bound<RecordsIdOwned>) -> Self {
-        Self((start, end))
+        Self(start, end)
     }
 
     pub fn author_key(ns: NamespaceId, author: AuthorId, key_matcher: KeyFilter) -> Self {
@@ -43,7 +43,7 @@ impl RecordsBounds {
             Bound::Unbounded
         };
 
-        Self((Bound::Included(start), end))
+        Self(Bound::Included(start), end)
     }
 
     pub fn author_prefix(ns: NamespaceId, author: AuthorId, prefix: Bytes) -> Self {
@@ -63,7 +63,10 @@ impl RecordsBounds {
     }
 
     pub fn as_ref(&self) -> (Bound<RecordsId>, Bound<RecordsId>) {
-        map_bounds(&self.0, records_id_ref)
+        fn map(id: &RecordsIdOwned) -> RecordsId {
+            (&id.0, &id.1, &id.2[..])
+        }
+        (map_bound(&self.0, map), map_bound(&self.1, map))
     }
 
     fn namespace_start(namespace: &NamespaceId) -> Bound<RecordsIdOwned> {
@@ -80,6 +83,16 @@ impl RecordsBounds {
     }
 }
 
+impl RangeBounds<RecordsIdOwned> for RecordsBounds {
+    fn start_bound(&self) -> Bound<&RecordsIdOwned> {
+        map_bound(&self.0, |s| s)
+    }
+
+    fn end_bound(&self) -> Bound<&RecordsIdOwned> {
+        map_bound(&self.1, |s| s)
+    }
+}
+
 impl From<(Bound<RecordsIdOwned>, Bound<RecordsIdOwned>)> for RecordsBounds {
     fn from(value: (Bound<RecordsIdOwned>, Bound<RecordsIdOwned>)) -> Self {
         Self::new(value.0, value.1)
@@ -89,7 +102,7 @@ impl From<(Bound<RecordsIdOwned>, Bound<RecordsIdOwned>)> for RecordsBounds {
 /// Bounds for the by-key index table.
 ///
 /// Supports bounds by key.
-pub struct ByKeyBounds((Bound<RecordsByKeyIdOwned>, Bound<RecordsByKeyIdOwned>));
+pub struct ByKeyBounds(Bound<RecordsByKeyIdOwned>, Bound<RecordsByKeyIdOwned>);
 impl ByKeyBounds {
     pub fn new(ns: NamespaceId, matcher: &KeyFilter) -> Self {
         match matcher {
@@ -97,7 +110,7 @@ impl ByKeyBounds {
             KeyFilter::Exact(key) => {
                 let start = (ns.to_bytes(), key.clone(), [0u8; 32]);
                 let end = (ns.to_bytes(), key.clone(), [255u8; 32]);
-                Self((Bound::Included(start), Bound::Included(end)))
+                Self(Bound::Included(start), Bound::Included(end))
             }
             KeyFilter::Prefix(ref prefix) => {
                 let start = Bound::Included((ns.to_bytes(), prefix.clone(), [0u8; 32]));
@@ -111,7 +124,7 @@ impl ByKeyBounds {
                 } else {
                     Bound::Unbounded
                 };
-                Self((start, end))
+                Self(start, end)
             }
         }
     }
@@ -124,11 +137,24 @@ impl ByKeyBounds {
         } else {
             Bound::Unbounded
         };
-        Self((start, end))
+        Self(start, end)
     }
 
     pub fn as_ref(&self) -> (Bound<RecordsByKeyId>, Bound<RecordsByKeyId>) {
-        map_bounds(&self.0, records_by_key_id_ref)
+        fn map(id: &RecordsByKeyIdOwned) -> RecordsByKeyId {
+            (&id.0, &id.1[..], &id.2)
+        }
+        (map_bound(&self.0, map), map_bound(&self.1, map))
+    }
+}
+
+impl RangeBounds<RecordsByKeyIdOwned> for ByKeyBounds {
+    fn start_bound(&self) -> Bound<&RecordsByKeyIdOwned> {
+        map_bound(&self.0, |s| s)
+    }
+
+    fn end_bound(&self) -> Bound<&RecordsByKeyIdOwned> {
+        map_bound(&self.1, |s| s)
     }
 }
 
@@ -155,17 +181,116 @@ fn map_bound<'a, T, U: 'a>(bound: &'a Bound<T>, f: impl Fn(&'a T) -> U) -> Bound
     }
 }
 
-fn map_bounds<'a, T, U: 'a>(
-    bounds: &'a (Bound<T>, Bound<T>),
-    f: impl Fn(&'a T) -> U,
-) -> (Bound<U>, Bound<U>) {
-    (map_bound(&bounds.0, &f), map_bound(&bounds.1, f))
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn records_by_key_id_ref(id: &RecordsByKeyIdOwned) -> RecordsByKeyId {
-    (&id.0, &id.1[..], &id.2)
-}
+    #[test]
+    fn records_bounds() {
+        let ns = NamespaceId::from(&[255u8; 32]);
 
-fn records_id_ref(id: &RecordsIdOwned) -> RecordsId {
-    (&id.0, &id.1, &id.2[..])
+        let bounds = RecordsBounds::namespace(ns);
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), [0u8; 32], Bytes::new()))
+        );
+        assert_eq!(bounds.end_bound(), Bound::Unbounded);
+
+        let a = AuthorId::from(&[255u8; 32]);
+
+        let bounds = RecordsBounds::author_key(ns, a, KeyFilter::Any);
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), a.to_bytes(), Bytes::new()))
+        );
+        assert_eq!(bounds.end_bound(), Bound::Unbounded);
+
+        let a = AuthorId::from(&[0u8; 32]);
+        let mut a_end = a.to_bytes();
+        a_end[31] = 1;
+        let bounds = RecordsBounds::author_key(ns, a, KeyFilter::Any);
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Excluded(&(ns.to_bytes(), a_end, Default::default()))
+        );
+
+        let bounds = RecordsBounds::author_key(ns, a, KeyFilter::Prefix(vec![1u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), a.to_bytes(), vec![1u8].into()))
+        );
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Excluded(&(ns.to_bytes(), a.to_bytes(), vec![2u8].into()))
+        );
+
+        let bounds = RecordsBounds::author_key(ns, a, KeyFilter::Exact(vec![1u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), a.to_bytes(), vec![1u8].into()))
+        );
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Included(&(ns.to_bytes(), a.to_bytes(), vec![1u8].into()))
+        );
+    }
+
+    #[test]
+    fn by_key_bounds() {
+        let ns = NamespaceId::from(&[255u8; 32]);
+
+        let bounds = ByKeyBounds::namespace(ns);
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), Bytes::new(), [0u8; 32]))
+        );
+        assert_eq!(bounds.end_bound(), Bound::Unbounded);
+
+        let bounds = ByKeyBounds::new(ns, &KeyFilter::Any);
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), Bytes::new(), [0u8; 32]))
+        );
+        assert_eq!(bounds.end_bound(), Bound::Unbounded);
+
+        let bounds = ByKeyBounds::new(ns, &KeyFilter::Prefix(vec![1u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), vec![1u8].into(), [0u8; 32]))
+        );
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Excluded(&(ns.to_bytes(), vec![2u8].into(), [0u8; 32]))
+        );
+
+        let bounds = ByKeyBounds::new(ns, &KeyFilter::Prefix(vec![255u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), vec![255u8].into(), [0u8; 32]))
+        );
+        assert_eq!(bounds.end_bound(), Bound::Unbounded);
+
+        let ns = NamespaceId::from(&[2u8; 32]);
+        let mut ns_end = ns.to_bytes();
+        ns_end[31] = 3u8;
+        let bounds = ByKeyBounds::new(ns, &KeyFilter::Prefix(vec![255u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), vec![255u8].into(), [0u8; 32]))
+        );
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Excluded(&(ns_end, Bytes::new(), [0u8; 32]))
+        );
+
+        let bounds = ByKeyBounds::new(ns, &KeyFilter::Exact(vec![1u8].into()));
+        assert_eq!(
+            bounds.start_bound(),
+            Bound::Included(&(ns.to_bytes(), vec![1u8].into(), [0u8; 32]))
+        );
+        assert_eq!(
+            bounds.end_bound(),
+            Bound::Included(&(ns.to_bytes(), vec![1u8].into(), [255u8; 32]))
+        );
+    }
 }
