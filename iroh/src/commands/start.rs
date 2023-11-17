07de2d8f3682d83d7d9a75d9ff2cd9c5ab3f73a1
dignifiedquire <me@dignifiedquire.com>
2023-11-17 21:41:04 +0100

    remove debugging

--- iroh/src/commands/start.rs
+++ iroh/src/commands/start.rs
@@ -171,7 +171,6 @@ impl StartArgs {
         if let Some(t) = token.as_ref() {
             eprintln!("Request token: {}", t);
         }
-        eprintln!("Derp map: {derp_map:?}");
 
         let rpc_status = RpcStatus::load(iroh_data_root()?).await?;
         match rpc_status {
