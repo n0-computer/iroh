065655f7468eb5b5e473550011bf8a144b332e3a
Brendan O'Brien <sparkle_pony_2000@qri.io>
2023-12-06 11:20:28 -0500

    moar methods for runtime (#1868)
    ## Description
    
    <!-- A summary of what this pull request achieves and a rough list of
    changes. -->
    
    ## Notes & open questions
    
    <!-- Any notes, remarks or open questions you have to make about the PR.
    -->
    
    ## Change checklist
    
    - [ ] Self-review.
    - [ ] Documentation updates if relevant.
    - [ ] Tests if relevant.

--- iroh/src/bootstrap.js
+++ iroh/src/bootstrap.js
@@ -1,5 +1,10 @@
-async function status() {
+
+async function node_status() {
   return await Deno[Deno.internal].core.ops.op_node_status();
+}
+
+async function doc_open(id) {
+  return await Deno[Deno.internal].core.ops.op_doc_open(id);
 }
 
 async function* doc_subscribe(doc) {
@@ -10,6 +15,10 @@ async function* doc_subscribe(doc) {
   }
 }
 
+async function doc_get(doc, key) {
+  return await Deno[Deno.internal].core.ops.op_doc_get(doc, key);
+}
+
 async function doc_create() {
   return await Deno[Deno.internal].core.ops.op_doc_create();
 }
@@ -18,8 +27,28 @@ async function doc_set(doc, key, value) {
   return await Deno[Deno.internal].core.ops.op_doc_set(doc, key, value);
 }
 
+async function doc_set_hash(doc, key, hash, size) {
+  return await Deno[Deno.internal].core.ops.op_doc_set_hash(doc, key, hash, size);
+}
+
+async function blob_set(hash, data) {
+  return await Deno[Deno.internal].core.ops.op_blob_set(hash, data);
+}
+
 async function blob_get(hash) {
   return await Deno[Deno.internal].core.ops.op_blob_get(hash);
 }
 
-globalThis.Iroh = { status, doc_subscribe, doc_create, doc_set, blob_get };
+globalThis.Iroh = {
+  node_status, 
+
+  doc_subscribe, 
+  doc_create, 
+  doc_open, 
+  doc_set, 
+  doc_set_hash,
+  doc_get,
+
+  blob_get,
+  blob_set
+};
