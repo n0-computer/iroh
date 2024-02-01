81124e6ae6bb77e5af0ec313204914610fe06c44
Kasey <kasey@n0.computer>
2024-02-01 16:02:48 -0500

    move some examples down to `iroh-bytes` (#1981)
    ## Description
    
    closes #1929 
    
    ### `provide-bytes`
    This example adds either a single blob or collection to the database and
    provides it. It gives instructions on how to run the fetch side of the
    example for `fetch-fsm` and `fetch-stream`.
    
    We could make this example more "advanced" by having the user provide a
    path & serve the blob or collection of that content, but I wanted to
    keep its simple at first.
    
    ### `fetch-fsm`
    This example fetches a blob or collection using the `get` finite state
    machine directly, printing the contents to the terminal.
    
    ### `fetch-stream`
    This example fetches a blob or collection using a helper function to
    turn the `get` finite state machine into a stream. It prints the
    contents to the terminal.
    
    ## Change checklist
    
    - [x] Self-review.
    - [x] Documentation updates if relevant.
    - [ ] Tests if relevant.

---
+++ iroh-bytes/examples/fetch-fsm.rs
@@ -0,0 +1,174 @@
+//! An example how to download a single blob or collection from a node and write it to stdout using the `get` finite state machine directly.
+//!
+//! Since this example does not use `iroh-net::MagicEndpoint`, it does not do any holepunching, and so will only work locally or between two processes that have public IP addresses.
+//!
+//! Run the provide-bytes example first. It will give instructions on how to run this example properly.
+use std::net::SocketAddr;
+
+use anyhow::{Context, Result};
+use iroh_io::ConcatenateSliceWriter;
+use tracing_subscriber::{prelude::*, EnvFilter};
+
+use iroh_bytes::{
+    get::fsm::{AtInitial, ConnectedNext, EndBlobNext},
+    hashseq::HashSeq,
+    protocol::GetRequest,
+    Hash,
+};
+
+mod connect;
+use connect::{load_certs, make_client_endpoint};
+
+// set the RUST_LOG env var to one of {debug,info,warn} to see logging info
+pub fn setup_logging() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr))
+        .with(EnvFilter::from_default_env())
+        .try_init()
+        .ok();
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("\nfetch bytes example!");
+    setup_logging();
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 4 {
+        anyhow::bail!("usage: fetch-bytes [HASH] [SOCKET_ADDR] [FORMAT]");
+    }
+    let hash: Hash = args[1].parse().context("unable to parse [HASH]")?;
+    let addr: SocketAddr = args[2].parse().context("unable to parse [SOCKET_ADDR]")?;
+    let format = {
+        if args[3] != "blob" && args[3] != "collection" {
+            anyhow::bail!(
+                "expected either 'blob' or 'collection' for FORMAT argument, got {}",
+                args[3]
+            );
+        }
+        args[3].clone()
+    };
+
+    // load tls certificates
+    // This will error if you have not run the `provide-bytes` example
+    let roots = load_certs().await?;
+
+    // create an endpoint to listen for incoming connections
+    let endpoint = make_client_endpoint(roots)?;
+    println!("\nlistening on {}", endpoint.local_addr()?);
+    println!("fetching hash {hash} from {addr}");
+
+    // connect
+    let connection = endpoint.connect(addr, "localhost")?.await?;
+
+    if format == "collection" {
+        // create a request for a collection
+        let request = GetRequest::all(hash);
+        // create the initial state of the finite state machine
+        let initial = iroh_bytes::get::fsm::start(connection, request);
+
+        write_collection(initial).await
+    } else {
+        // create a request for a single blob
+        let request = GetRequest::single(hash);
+        // create the initial state of the finite state machine
+        let initial = iroh_bytes::get::fsm::start(connection, request);
+
+        write_blob(initial).await
+    }
+}
+
+async fn write_blob(initial: AtInitial) -> Result<()> {
+    // connect (create a stream pair)
+    let connected = initial.next().await?;
+
+    // we expect a start root message, since we requested a single blob
+    let ConnectedNext::StartRoot(start_root) = connected.next().await? else {
+        panic!("expected start root")
+    };
+    // we can just call next to proceed to the header, since we know the root hash
+    let header = start_root.next();
+
+    // we need to wrap stdout in a struct that implements AsyncSliceWriter. Since we can not
+    // seek in stdout we use ConcatenateSliceWriter which just concatenates all the writes.
+    let writer = ConcatenateSliceWriter::new(tokio::io::stdout());
+
+    // make the spacing nicer in the terminal
+    println!();
+    // use the utility function write_all to write the entire blob
+    let end = header.write_all(writer).await?;
+
+    // we requested a single blob, so we expect to enter the closing state
+    let EndBlobNext::Closing(closing) = end.next() else {
+        panic!("expected closing")
+    };
+
+    // close the connection and get the stats
+    let _stats = closing.next().await?;
+    Ok(())
+}
+
+async fn write_collection(initial: AtInitial) -> Result<()> {
+    // connect
+    let connected = initial.next().await?;
+    // read the first bytes
+    let ConnectedNext::StartRoot(start_root) = connected.next().await? else {
+        anyhow::bail!("failed to parse collection");
+    };
+    // check that we requested the whole collection
+    if !start_root.ranges().is_all() {
+        anyhow::bail!("collection was not requested completely");
+    }
+
+    // move to the header
+    let header: iroh_bytes::get::fsm::AtBlobHeader = start_root.next();
+    let (root_end, hashes_bytes) = header.concatenate_into_vec().await?;
+    let next = root_end.next();
+    let EndBlobNext::MoreChildren(at_meta) = next else {
+        anyhow::bail!("missing meta blob, got {next:?}");
+    };
+    // parse the hashes from the hash sequence bytes
+    let hashes = HashSeq::try_from(bytes::Bytes::from(hashes_bytes))
+        .context("failed to parse hashes")?
+        .into_iter()
+        .collect::<Vec<_>>();
+    let meta_hash = hashes.first().context("missing meta hash")?;
+
+    let (meta_end, _meta_bytes) = at_meta.next(*meta_hash).concatenate_into_vec().await?;
+    let mut curr = meta_end.next();
+    let closing = loop {
+        match curr {
+            EndBlobNext::MoreChildren(more) => {
+                let Some(hash) = hashes.get(more.child_offset() as usize) else {
+                    break more.finish();
+                };
+                let header = more.next(*hash);
+
+                // we need to wrap stdout in a struct that implements AsyncSliceWriter. Since we can not
+                // seek in stdout we use ConcatenateSliceWriter which just concatenates all the writes.
+                let writer = ConcatenateSliceWriter::new(tokio::io::stdout());
+
+                // use the utility function write_all to write the entire blob
+                let end = header.write_all(writer).await?;
+                println!();
+                curr = end.next();
+            }
+            EndBlobNext::Closing(closing) => {
+                break closing;
+            }
+        }
+    };
+    // close the connection
+    let _stats = closing.next().await?;
+    Ok(())
+}
+
+#[derive(Clone)]
+struct MockEventSender;
+
+use futures::future::FutureExt;
+
+impl iroh_bytes::provider::EventSender for MockEventSender {
+    fn send(&self, _event: iroh_bytes::provider::Event) -> futures::future::BoxFuture<()> {
+        async move {}.boxed()
+    }
+}
---
+++ iroh-bytes/examples/provide-bytes.rs
@@ -0,0 +1,119 @@
+//! An example that provides a blob or a collection over a Quinn connection.
+//!
+//! Since this example does not use `iroh-net::MagicEndpoint`, it does not do any holepunching, and so will only work locally or between two processes that have public IP addresses.
+//!
+//! Run this example with
+//!    cargo run --example provide-bytes blob
+//! To provide a blob (single file)
+//!
+//! Run this example with
+//!    cargo run --example provide-bytes collection
+//! To provide a collection (multiple blobs)
+use anyhow::Result;
+use tokio_util::task::LocalPoolHandle;
+use tracing_subscriber::{prelude::*, EnvFilter};
+
+use iroh_bytes::{format::collection::Collection, Hash};
+
+mod connect;
+use connect::{make_and_write_certs, make_server_endpoint, CERT_PATH};
+
+// set the RUST_LOG env var to one of {debug,info,warn} to see logging info
+pub fn setup_logging() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr))
+        .with(EnvFilter::from_default_env())
+        .try_init()
+        .ok();
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        anyhow::bail!(
+            "usage: provide-bytes [FORMAT], where [FORMAT] is either 'blob' or 'collection'\n\nThe 'blob' example demonstrates sending a single blob of bytes. The 'collection' example demonstrates sending multiple blobs of bytes, grouped together in a 'collection'."
+        );
+    }
+    let format = {
+        if args[1] != "blob" && args[1] != "collection" {
+            anyhow::bail!(
+                "expected either 'blob' or 'collection' for FORMAT argument, got {}",
+                args[1]
+            );
+        }
+        args[1].clone()
+    };
+    println!("\nprovide bytes {format} example!");
+
+    let (db, hash) = if format == "collection" {
+        let (mut db, names) = iroh_bytes::store::readonly_mem::Store::new([
+            ("blob1", b"the first blob of bytes".to_vec()),
+            ("blob2", b"the second blob of bytes".to_vec()),
+        ]); // create a collection
+        let collection: Collection = names
+            .into_iter()
+            .map(|(name, hash)| (name, Hash::from(hash)))
+            .collect();
+        // add it to the db
+        let hash = db.insert_many(collection.to_blobs()).unwrap();
+        (db, hash)
+    } else {
+        // create a new database and add a blob
+        let (db, names) =
+            iroh_bytes::store::readonly_mem::Store::new([("hello", b"Hello World!".to_vec())]);
+
+        // get the hash of the content
+        let hash = names.get("hello").unwrap();
+        (db, Hash::from(hash.as_bytes()))
+    };
+
+    // create tls certs and save to CERT_PATH
+    let (key, cert) = make_and_write_certs().await?;
+
+    // create an endpoint to listen for incoming connections
+    let endpoint = make_server_endpoint(key, cert)?;
+    let addr = endpoint.local_addr()?;
+    println!("\nlistening on {addr}");
+    println!("providing hash {hash}");
+
+    println!("\nfetch the content using a finite state machine by running the following example:\n\ncargo run --example fetch-fsm {hash} \"{addr}\" {format}");
+    println!("\nfetch the content using a stream by running the following example:\n\ncargo run --example fetch-stream {hash} \"{addr}\" {format}\n");
+
+    // create a new local pool handle with 1 worker thread
+    let lp = LocalPoolHandle::new(1);
+
+    let accept_task = tokio::spawn(async move {
+        while let Some(conn) = endpoint.accept().await {
+            println!("connection incoming");
+
+            let db = db.clone();
+            let lp = lp.clone();
+
+            // spawn a task to handle the connection
+            tokio::spawn(async move {
+                iroh_bytes::provider::handle_connection(conn, db, MockEventSender, lp).await
+            });
+        }
+    });
+
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => {
+            tokio::fs::remove_dir_all(std::path::PathBuf::from(CERT_PATH)).await?;
+            accept_task.abort();
+            Ok(())
+        }
+        Err(e) => Err(anyhow::anyhow!("unable to listen for ctrl-c: {e}")),
+    }
+}
+
+#[derive(Clone)]
+struct MockEventSender;
+
+use futures::future::FutureExt;
+
+impl iroh_bytes::provider::EventSender for MockEventSender {
+    fn send(&self, _event: iroh_bytes::provider::Event) -> futures::future::BoxFuture<()> {
+        async move {}.boxed()
+    }
+}
