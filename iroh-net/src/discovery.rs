42f1f3b0b0752b495b184de0bf676081f9664df1
Franz Heinzmann (Frando) <frando@unbiskant.org>
2024-03-06 12:03:41 +0100

    tests: add tests for combined discovery

--- iroh-net/src/discovery.rs
+++ iroh-net/src/discovery.rs
@@ -73,10 +73,10 @@ impl CombinedDiscovery {
 
 impl<T> From<T> for CombinedDiscovery
 where
-    T: Iterator<Item = Box<dyn Discovery>>,
+    T: IntoIterator<Item = Box<dyn Discovery>>,
 {
     fn from(iter: T) -> Self {
-        let services = iter.collect::<Vec<_>>();
+        let services = iter.into_iter().collect::<Vec<_>>();
         Self { services }
     }
 }
@@ -99,5 +99,249 @@ impl Discovery for CombinedDiscovery {
             .filter_map(|service| service.resolve(endpoint.clone(), node_id));
         let streams = futures::stream::select_all(streams);
         Some(Box::pin(streams))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::{BTreeSet, HashMap},
+        net::SocketAddr,
+        sync::Arc,
+        time::{Duration, SystemTime},
+    };
+
+    use futures::{stream, StreamExt};
+    use parking_lot::Mutex;
+
+    use crate::{derp::DerpMode, key::SecretKey, NodeAddr};
+
+    use super::*;
+
+    #[derive(Debug, Clone, Default)]
+    struct TestDiscoveryShared {
+        nodes: Arc<Mutex<HashMap<NodeId, (AddrInfo, u64)>>>,
+    }
+    impl TestDiscoveryShared {
+        pub fn create_discovery(&self, node_id: NodeId) -> TestDiscovery {
+            TestDiscovery {
+                node_id,
+                shared: self.clone(),
+                publish: true,
+                resolve_wrong: false,
+                delay: Duration::from_millis(200),
+            }
+        }
+
+        pub fn create_lying_discovery(&self, node_id: NodeId) -> TestDiscovery {
+            TestDiscovery {
+                node_id,
+                shared: self.clone(),
+                publish: false,
+                resolve_wrong: true,
+                delay: Duration::from_millis(100),
+            }
+        }
+    }
+    #[derive(Debug)]
+    struct TestDiscovery {
+        node_id: NodeId,
+        shared: TestDiscoveryShared,
+        publish: bool,
+        resolve_wrong: bool,
+        delay: Duration,
+    }
+
+    impl Discovery for TestDiscovery {
+        fn publish(&self, info: &AddrInfo) {
+            if !self.publish {
+                return;
+            }
+            let now = system_time_now();
+            self.shared
+                .nodes
+                .lock()
+                .insert(self.node_id, (info.clone(), now));
+        }
+
+        fn resolve(
+            &self,
+            endpoint: MagicEndpoint,
+            node_id: NodeId,
+        ) -> Option<BoxStream<'static, Result<DiscoveryItem>>> {
+            let addr_info = match self.resolve_wrong {
+                false => self.shared.nodes.lock().get(&node_id).cloned(),
+                true => {
+                    let ts = system_time_now() - 100_000;
+                    let port: u16 = rand::random();
+                    // "240.0.0.0/4" is reserved and unreachable
+                    let addr: SocketAddr = format!("240.0.0.1:{port}").parse().unwrap();
+                    let addr_info = AddrInfo {
+                        derp_url: None,
+                        direct_addresses: BTreeSet::from([addr]),
+                    };
+                    Some((addr_info, ts))
+                }
+            };
+            let stream = match addr_info {
+                Some((addr_info, ts)) => {
+                    let item = DiscoveryItem {
+                        provenance: "test-disco",
+                        last_updated: Some(ts),
+                        addr_info,
+                    };
+                    let delay = self.delay;
+                    let fut = async move {
+                        tokio::time::sleep(delay).await;
+                        tracing::debug!(
+                            "resolve on {}: {} = {item:?}",
+                            endpoint.node_id().fmt_short(),
+                            node_id.fmt_short()
+                        );
+                        Ok(item)
+                    };
+                    stream::once(fut).boxed()
+                }
+                None => stream::empty().boxed(),
+            };
+            Some(stream)
+        }
+    }
+
+    #[derive(Debug)]
+    struct EmptyDiscovery;
+    impl Discovery for EmptyDiscovery {
+        fn publish(&self, _info: &AddrInfo) {}
+
+        fn resolve(
+            &self,
+            _endpoint: MagicEndpoint,
+            _node_id: NodeId,
+        ) -> Option<BoxStream<'static, Result<DiscoveryItem>>> {
+            Some(stream::empty().boxed())
+        }
+    }
+
+    const TEST_ALPN: &[u8] = b"n0/iroh/test";
+
+    /// This is a smoke test for our discovery mechanism.
+    #[tokio::test]
+    async fn magic_endpoint_discovery_simple_shared() -> anyhow::Result<()> {
+        let _guard = iroh_test::logging::setup();
+        let disco_shared = TestDiscoveryShared::default();
+        let ep1 = {
+            let secret = SecretKey::generate();
+            let disco = disco_shared.create_discovery(secret.public());
+            new_endpoint(secret, disco).await
+        };
+        let ep2 = {
+            let secret = SecretKey::generate();
+            let disco = disco_shared.create_discovery(secret.public());
+            new_endpoint(secret, disco).await
+        };
+        let ep1_addr = NodeAddr::new(ep1.node_id());
+        // wait for out address to be updated and thus published at least once
+        ep1.my_addr().await?;
+        let _conn = ep2.connect(ep1_addr, TEST_ALPN).await?;
+        Ok(())
+    }
+
+    /// This test adds an empty discovery which provides no addresses.
+    #[tokio::test]
+    async fn magic_endpoint_discovery_combined_with_empty() -> anyhow::Result<()> {
+        let _guard = iroh_test::logging::setup();
+        let disco_shared = TestDiscoveryShared::default();
+        let ep1 = {
+            let secret = SecretKey::generate();
+            let disco = disco_shared.create_discovery(secret.public());
+            new_endpoint(secret, disco).await
+        };
+        let ep2 = {
+            let secret = SecretKey::generate();
+            let disco1 = EmptyDiscovery;
+            let disco2 = disco_shared.create_discovery(secret.public());
+            let mut disco = CombinedDiscovery::new();
+            disco.add(disco1);
+            disco.add(disco2);
+            new_endpoint(secret, disco).await
+        };
+        let ep1_addr = NodeAddr::new(ep1.node_id());
+        // wait for out address to be updated and thus published at least once
+        ep1.my_addr().await?;
+        let _conn = ep2.connect(ep1_addr, TEST_ALPN).await?;
+        Ok(())
+    }
+
+    /// This test adds a "lying" discovery which provides a wrong address.
+    /// This is to make sure that as long as one of the discoveries returns a working address, we
+    /// will connect successfully.
+    #[tokio::test]
+    async fn magic_endpoint_discovery_combined_with_empty_and_wrong() -> anyhow::Result<()> {
+        let _guard = iroh_test::logging::setup();
+        let disco_shared = TestDiscoveryShared::default();
+        let ep1 = {
+            let secret = SecretKey::generate();
+            let disco = disco_shared.create_discovery(secret.public());
+            new_endpoint(secret, disco).await
+        };
+        let ep2 = {
+            let secret = SecretKey::generate();
+            let disco1 = EmptyDiscovery;
+            let disco2 = disco_shared.create_lying_discovery(secret.public());
+            let disco3 = disco_shared.create_discovery(secret.public());
+            let mut disco = CombinedDiscovery::new();
+            disco.add(disco1);
+            disco.add(disco2);
+            disco.add(disco3);
+            new_endpoint(secret, disco).await
+        };
+        let ep1_addr = NodeAddr::new(ep1.node_id());
+        // wait for out address to be updated and thus published at least once
+        ep1.my_addr().await?;
+        let _conn = ep2.connect(ep1_addr, TEST_ALPN).await?;
+        Ok(())
+    }
+
+    /// This test only has the "lying" discovery. It is here to make sure that this actually fails.
+    #[tokio::test]
+    async fn magic_endpoint_discovery_combined_wrong_only() -> anyhow::Result<()> {
+        let _guard = iroh_test::logging::setup();
+        let disco_shared = TestDiscoveryShared::default();
+        let ep1 = {
+            let secret = SecretKey::generate();
+            let disco = disco_shared.create_discovery(secret.public());
+            new_endpoint(secret, disco).await
+        };
+        let ep2 = {
+            let secret = SecretKey::generate();
+            let disco1 = disco_shared.create_lying_discovery(secret.public());
+            let mut disco = CombinedDiscovery::new();
+            disco.add(disco1);
+            new_endpoint(secret, disco).await
+        };
+        let ep1_addr = NodeAddr::new(ep1.node_id());
+        // wait for out address to be updated and thus published at least once
+        ep1.my_addr().await?;
+        let res = ep2.connect(ep1_addr, TEST_ALPN).await;
+        assert!(matches!(res, Err(_)));
+        Ok(())
+    }
+
+    async fn new_endpoint(secret: SecretKey, disco: impl Discovery + 'static) -> MagicEndpoint {
+        MagicEndpoint::builder()
+            .secret_key(secret)
+            .discovery(Box::new(disco))
+            .derp_mode(DerpMode::Disabled)
+            .alpns(vec![TEST_ALPN.to_vec()])
+            .bind(0)
+            .await
+            .unwrap()
+    }
+
+    fn system_time_now() -> u64 {
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("time drift")
+            .as_micros() as u64
     }
 }
