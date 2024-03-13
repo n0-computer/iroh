bdbe35ca519984e2ba25439fd438543b44573642
Floris Bruynooghe <flub@n0.computer>
2024-03-13 13:05:09 +0100

    Strip bad DNS servers

--- iroh-net/src/dns.rs
+++ iroh-net/src/dns.rs
@@ -1,4 +1,4 @@
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv6Addr};
 use std::time::Duration;
 
 use anyhow::Result;
@@ -8,15 +8,45 @@ use once_cell::sync::Lazy;
 pub static DNS_RESOLVER: Lazy<TokioAsyncResolver> =
     Lazy::new(|| get_resolver().expect("unable to create DNS resolver"));
 
+/// Deprecated IPv6 site-local anycast addresses still configured by windows.
+///
+/// Windows still configures these site-local addresses as soon even as an IPv6 loopback
+/// interface is configured.  We do not want to use these DNS servers, the chances of them
+/// being usable are almost always close to zero, while the chance of DNS configuration
+/// **only** relying on these servers and not also being configured normally are also almost
+/// zero.  The chance of the DNS resolver accidentally trying one of these and taking a
+/// bunch of timeouts to figure out they're no good are on the other hand very high.
+const WINDOWS_BAD_SITE_LOCAL_DNS_SERVERS: [IpAddr; 3] = [
+    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 1)),
+    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 2)),
+    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 3)),
+];
+
 /// Get resolver to query MX records.
 ///
 /// We first try to read the system's resolver from `/etc/resolv.conf`.
 /// This does not work at least on some Androids, therefore we fallback
 /// to the default `ResolverConfig` which uses eg. to google's `8.8.8.8` or `8.8.4.4`.
 fn get_resolver() -> Result<TokioAsyncResolver> {
-    let (config, mut options) =
+    let (system_config, mut options) =
         hickory_resolver::system_conf::read_system_conf().unwrap_or_default();
+
+    // Copy all of the system config, but strip the bad windows nameservers.  Unfortunately
+    // there is no easy way to do this.
+    let mut config = hickory_resolver::config::ResolverConfig::new();
+    if let Some(name) = system_config.domain() {
+        config.set_domain(name.clone());
+    }
+    for name in system_config.search() {
+        config.add_search(name.clone());
+    }
+    for nameserver_cfg in system_config.name_servers() {
+        if WINDOWS_BAD_SITE_LOCAL_DNS_SERVERS.contains(&nameserver_cfg.socket_addr.ip()) {
+            config.add_name_server(nameserver_cfg.clone());
+        }
+    }
     dbg!(config.name_servers());
+
     // lookup IPv4 and IPv6 in parallel
     options.ip_strategy = hickory_resolver::config::LookupIpStrategy::Ipv4thenIpv6;
 
