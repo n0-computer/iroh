8dcb1969ddca148757bc5c64861e29f107d6d0d9
Franz Heinzmann <frando@unbiskant.org>
2024-03-23 13:11:17 +0100

    refactor(iroh-net): allow to set a custom DNS resolver on the magic endpoint (#2116)
    ## Description
    
    This makes the DNS resolver to be used in the context of a MagicEndpoint
    configurable. Up to now, we used a single, global, unconfigurable DNS
    resolver, stored in a per-process global static. This PR changes this so
    that we can set a DNS resolver in the builder of the MagicEndpoint. This
    resolver is passed through to all places where we need a DNS resolver -
    which is all places where we need to resolve relay URLs.
    
    The default is unchanged: A single, shared DNS resolver is used for all
    endpoints. However, this default can now be changed per-endpoint. The
    global resolver is only used as a default in the endpoint builder if no
    custom resolver is set, and in the doctor, and in tests.
    
    This change will make testing things that use DNS - prominently: #2045 -
    much easier. And we now have the means in place for people to customize
    the DNS resolving, if needed.
    
    ## Notes & open questions
    
    This makes the `hickory_resolver::TokioAsyncResolver` part of the public
    API surface of `iroh-net`.
    
    ## Change checklist
    
    - [x] Self-review.
    - [x] Documentation updates if relevant.
    - [ ] Tests if relevant.

--- iroh-cli/src/commands/doctor.rs
+++ iroh-cli/src/commands/doctor.rs
@@ -24,6 +24,7 @@ use iroh::{
     },
     net::{
         defaults::DEFAULT_RELAY_STUN_PORT,
+        dns::default_resolver,
         key::{PublicKey, SecretKey},
         magic_endpoint,
         magicsock::EndpointInfo,
@@ -289,7 +290,8 @@ async fn report(
     config: &NodeConfig,
 ) -> anyhow::Result<()> {
     let port_mapper = portmapper::Client::default();
-    let mut client = netcheck::Client::new(Some(port_mapper))?;
+    let dns_resolver = default_resolver().clone();
+    let mut client = netcheck::Client::new(Some(port_mapper), dns_resolver)?;
 
     let dm = match stun_host {
         Some(host_name) => {
@@ -775,10 +777,12 @@ async fn relay_urls(count: usize, config: NodeConfig) -> anyhow::Result<()> {
         println!("No relay nodes specified in the config file.");
     }
 
+    let dns_resolver = default_resolver();
     let mut clients = HashMap::new();
     for node in &config.relay_nodes {
         let secret_key = key.clone();
-        let client = iroh::net::relay::http::ClientBuilder::new(node.url.clone()).build(secret_key);
+        let client = iroh::net::relay::http::ClientBuilder::new(node.url.clone())
+            .build(secret_key, dns_resolver.clone());
 
         clients.insert(node.url.clone(), client);
     }
