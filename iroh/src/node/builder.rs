3e2ec4d45c337d92b0cd9fbbc2ba8a949d9cc064
Ruediger Klaehn <rklaehn@protonmail.com>
2024-04-08 10:58:13 +0300

    fix(iroh-bytes): exit gc loop when gc_start notification fails

--- iroh/src/node/builder.rs
+++ iroh/src/node/builder.rs
@@ -511,8 +511,10 @@ where
         tracing::debug!("GC loop starting {:?}", gc_period);
         'outer: loop {
             if let Err(cause) = db.gc_start().await {
-                tracing::error!("Error {} starting GC, skipping GC to be safe", cause);
-                continue 'outer;
+                tracing::debug!(
+                    "unable to notify the db of GC start: {cause}. Shutting down GC loop."
+                );
+                break;
             }
             // do delay before the two phases of GC
             tokio::time::sleep(gc_period).await;
