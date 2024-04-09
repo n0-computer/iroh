375331f48318ef9077294e09390daff0f2141e2d
Diva M <divma@protonmail.com>
2024-04-09 10:54:12 -0500

    add logs path; use strum instead

--- iroh-cli/src/config.rs
+++ iroh-cli/src/config.rs
@@ -2,7 +2,7 @@
 
 use std::{
     collections::HashMap,
-    env, fmt,
+    env,
     net::SocketAddr,
     path::{Path, PathBuf},
     str::FromStr,
@@ -37,37 +37,16 @@ pub(crate) fn env_var(key: &str) -> std::result::Result<String, env::VarError> {
     env::var(format!("{ENV_PREFIX}_{key}"))
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, strum::IntoStaticStr, strum::Display, strum::EnumString)]
 pub(crate) enum ConsolePaths {
+    #[strum(serialize = "default_author.pubkey")]
     DefaultAuthor,
+    #[strum(serialize = "history")]
     History,
+    #[strum(serialize = "logs")]
+    Logs,
 }
 
-impl From<&ConsolePaths> for &'static str {
-    fn from(value: &ConsolePaths) -> Self {
-        match value {
-            ConsolePaths::DefaultAuthor => "default_author.pubkey",
-            ConsolePaths::History => "history",
-        }
-    }
-}
-impl FromStr for ConsolePaths {
-    type Err = anyhow::Error;
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(match s {
-            "default_author.pubkey" => Self::DefaultAuthor,
-            "history" => Self::History,
-            _ => bail!("unknown file or directory"),
-        })
-    }
-}
-
-impl fmt::Display for ConsolePaths {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let s: &str = self.into();
-        write!(f, "{s}")
-    }
-}
 impl AsRef<Path> for ConsolePaths {
     fn as_ref(&self) -> &Path {
         let s: &str = self.into();
