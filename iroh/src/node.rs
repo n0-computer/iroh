b1bb0f2b6a501279ef9cf3d9f822c94734b386ee
dignifiedquire <me@dignifiedquire.com>
2024-04-18 11:56:19 +0200

    more testing

--- iroh/src/node.rs
+++ iroh/src/node.rs
@@ -227,9 +227,9 @@ impl<D: BaoStore> Node<D> {
     }
 
     /// Get the relay server we are connected to.
-    pub fn my_relay(&self) -> Option<RelayUrl> {
+    /*pub fn my_relay(&self) -> Option<RelayUrl> {
         self.inner.endpoint.my_relay()
-    }
+    }*/
 
     /// Cool new function
     pub fn cool_stuff(&self) -> String {
