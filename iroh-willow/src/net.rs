b61e29919b3b9e4f3080a71121a3fbef08476709
Franz Heinzmann (Frando) <frando@unbiskant.org>
2024-04-25 00:53:04 +0200

    more progress

--- iroh-willow/src/net.rs
+++ iroh-willow/src/net.rs
@@ -1,15 +1,17 @@
-use anyhow::ensure;
-use futures::{FutureExt, SinkExt, TryFutureExt};
+use std::{pin::Pin, task::Poll};
+
+use anyhow::{ensure, Context};
+use futures::{FutureExt, SinkExt, Stream, TryFutureExt};
 use iroh_base::hash::Hash;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_stream::StreamExt;
-use tokio_util::codec::{FramedRead, FramedWrite};
-use tracing::debug;
+use tokio_util::codec::{Decoder, FramedRead, FramedWrite};
+use tracing::{debug, instrument};
 
 use crate::{
     proto::wgps::{
         AccessChallenge, ChallengeHash, CHALLENGE_HASH_LENGTH, CHALLENGE_LENGTH,
-        MAXIMUM_PAYLOAD_SIZE_POWER,
+        MAX_PAYLOAD_SIZE_POWER,
     },
     session::{Role, Session, SessionInit},
     store::{MemoryStore, Store},
@@ -19,6 +21,18 @@ use self::codec::WillowCodec;
 
 pub mod codec;
 
+/// Read the next frame from a [`FramedRead`] but only if it is available without waiting on IO.
+async fn next_if_ready<T: tokio::io::AsyncRead + Unpin, D: Decoder>(
+    mut reader: &mut FramedRead<T, D>,
+) -> Option<Result<D::Item, D::Error>> {
+    futures::future::poll_fn(|cx| match Pin::new(&mut reader).poll_next(cx) {
+        Poll::Ready(r) => Poll::Ready(r),
+        Poll::Pending => Poll::Ready(None),
+    })
+    .await
+}
+
+#[instrument(skip_all, fields(role=?role))]
 async fn run<S: Store>(
     store: &mut S,
     conn: quinn::Connection,
@@ -32,53 +46,49 @@ async fn run<S: Store>(
 
     let our_nonce: AccessChallenge = rand::random();
     debug!(?role, "start");
-    let (received_commitment, maximum_payload_size) =
+    let (received_commitment, max_payload_size) =
         exchange_commitments(&mut send, &mut recv, &our_nonce).await?;
     debug!(?role, "exchanged comittments");
 
-    let mut session = Session::new(
-        role,
-        our_nonce,
-        maximum_payload_size,
-        received_commitment,
-        init,
-    );
+    let mut session = Session::new(role, our_nonce, max_payload_size, received_commitment, init);
 
     let mut reader = FramedRead::new(recv, WillowCodec);
     let mut writer = FramedWrite::new(send, WillowCodec);
 
-    // move to store thread for this!
+    // TODO: blocking!
     session.process(store)?;
 
-    // back in network land: send out everything
-    // should be in parallel with reading
+    // send out initial messages
     for message in session.drain_outbox() {
         debug!(role=?role, ?message, "send");
         writer.send(message).await?;
     }
 
-    while let Some(message) = reader.try_next().await? {
-        debug!(?role, ?message, "recv");
-        // TODO: buffer more than a single message here before handing off to store thread
-        // what we should do here:
-        // * notify store thread that we want to process
-        // * keep reading and pushing into session, until session is full
-        // * once store thread is ready for us: be notified of that, and hand over session to store
-        //   thread
+    while let Some(message) = reader.next().await {
+        let message = message.context("error from reader")?;
+        debug!(%message,awaited=true, "recv");
         session.recv(message.into());
 
-        // move to store thread for this!
-        let done = session.process(store)?;
+        // keep pushing already buffered messages
+        while let Some(message) = next_if_ready(&mut reader).await {
+            let message = message.context("error from reader")?;
+        debug!(%message,awaited=false, "recv");
+            // TODO: stop when session is full
+            session.recv(message.into());
+        }
 
-        // back in network land: send out everything
-        // should be in parallel with reading
+        // TODO: blocking!
+        let done = session.process(store)?;
+        debug!(?done, "process done");
+
         for message in session.drain_outbox() {
-            debug!(role=?role, ?message, "send");
+            debug!(%message, "send");
             writer.send(message).await?;
         }
 
         if done {
-            break;
+            debug!("close");
+            writer.close().await?;
         }
     }
     Ok(())
@@ -90,35 +100,36 @@ async fn exchange_commitments(
     our_nonce: &AccessChallenge,
 ) -> anyhow::Result<(ChallengeHash, usize)> {
     let challenge_hash = Hash::new(&our_nonce);
-    send.write_u8(MAXIMUM_PAYLOAD_SIZE_POWER).await?;
+    send.write_u8(MAX_PAYLOAD_SIZE_POWER).await?;
     send.write_all(challenge_hash.as_bytes()).await?;
 
-    let their_maximum_payload_size_power = recv.read_u8().await?;
-    ensure!(
-        their_maximum_payload_size_power <= 64,
-        "maximum payload size too large"
-    );
-    let their_maximum_payload_size = 2usize.pow(their_maximum_payload_size_power as u32);
+    let their_max_payload_size = {
+        let power = recv.read_u8().await?;
+        ensure!(power <= 64, "max payload size too large");
+        2usize.pow(power as u32)
+    };
 
     let mut received_commitment = [0u8; CHALLENGE_HASH_LENGTH];
     recv.read_exact(&mut received_commitment).await?;
-    Ok((received_commitment, their_maximum_payload_size))
+    Ok((received_commitment, their_max_payload_size))
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{collections::HashSet, time::Instant};
+
     use iroh_base::hash::Hash;
     use iroh_net::MagicEndpoint;
     use rand::SeedableRng;
-    use tracing::debug;
+    use tracing::{debug, info};
 
     use crate::{
         net::run,
         proto::{
-            keys::{NamespaceId, NamespaceSecretKey, NamespaceType, UserSecretKey},
+            grouping::{AreaOfInterest, ThreeDRange},
+            keys::{NamespaceId, NamespaceKind, NamespaceSecretKey, UserSecretKey},
             meadowcap::{AccessMode, McCapability, OwnedCapability},
-            wgps::{AreaOfInterest, ReadCapability},
-            willow::{Entry, Path},
+            willow::{AuthorisedEntry, Entry, Path, SubspaceId},
         },
         session::{Role, SessionInit},
         store::{MemoryStore, Store},
@@ -130,6 +141,8 @@ mod tests {
     async fn smoke() -> anyhow::Result<()> {
         iroh_test::logging::setup_multithreaded();
         let mut rng = rand_chacha::ChaCha12Rng::seed_from_u64(1);
+        let n_betty = 10;
+        let n_alfie = 20;
 
         let ep_alfie = MagicEndpoint::builder()
             .alpns(vec![ALPN.to_vec()])
@@ -152,27 +165,28 @@ mod tests {
         );
         let conn_alfie = conn_alfie.unwrap();
         let conn_betty = conn_betty.unwrap();
-        debug!("connected");
+        info!("connected! now start reconciliation");
 
-        let namespace_secret = NamespaceSecretKey::generate(&mut rng, NamespaceType::Owned);
+        let namespace_secret = NamespaceSecretKey::generate(&mut rng, NamespaceKind::Owned);
         let namespace_id: NamespaceId = namespace_secret.public_key().into();
 
+        let start = Instant::now();
+        let mut expected_entries = HashSet::new();
         let mut store_alfie = MemoryStore::default();
         let init_alfie = {
             let secret_key = UserSecretKey::generate(&mut rng);
             let public_key = secret_key.public_key();
-            let read_capability = ReadCapability::Owned(OwnedCapability::new(
+            let read_capability = McCapability::Owned(OwnedCapability::new(
                 &namespace_secret,
                 public_key,
                 AccessMode::Read,
             ));
-            let area_of_interest = AreaOfInterest::full();
             let write_capability = McCapability::Owned(OwnedCapability::new(
                 &namespace_secret,
                 public_key,
                 AccessMode::Write,
             ));
-            for i in 0..3 {
+            for i in 0..n_alfie {
                 let p = format!("alfie{i}");
                 let entry = Entry {
                     namespace_id,
@@ -182,9 +196,11 @@ mod tests {
                     payload_length: 2,
                     payload_digest: Hash::new("cool things"),
                 };
+                expected_entries.insert(entry.clone());
                 let entry = entry.attach_authorisation(write_capability.clone(), &secret_key)?;
                 store_alfie.ingest_entry(&entry)?;
             }
+            let area_of_interest = AreaOfInterest::full();
             SessionInit {
                 user_secret_key: secret_key,
                 capability: read_capability,
@@ -201,13 +217,12 @@ mod tests {
                 public_key,
                 AccessMode::Read,
             ));
-            let area_of_interest = AreaOfInterest::full();
             let write_capability = McCapability::Owned(OwnedCapability::new(
                 &namespace_secret,
                 public_key,
                 AccessMode::Write,
             ));
-            for i in 0..3 {
+            for i in 0..n_betty {
                 let p = format!("betty{i}");
                 let entry = Entry {
                     namespace_id,
@@ -217,9 +232,11 @@ mod tests {
                     payload_length: 2,
                     payload_digest: Hash::new("cool things"),
                 };
+                expected_entries.insert(entry.clone());
                 let entry = entry.attach_authorisation(write_capability.clone(), &secret_key)?;
                 store_betty.ingest_entry(&entry)?;
             }
+            let area_of_interest = AreaOfInterest::full();
             SessionInit {
                 user_secret_key: secret_key,
                 capability: read_capability,
@@ -233,11 +250,47 @@ mod tests {
             run(&mut store_alfie, conn_alfie, Role::Alfie, init_alfie),
             run(&mut store_betty, conn_betty, Role::Betty, init_betty),
         );
-        res_alfie.unwrap();
-        res_betty.unwrap();
-        println!("alfie {:?}", store_alfie);
-        println!("betty {:?}", store_betty);
+        info!(time=?start.elapsed(), "reconciliation finished!");
+
+        info!("alfie res {:?}", res_alfie);
+        info!("betty res {:?}", res_betty);
+        info!(
+            "alfie store {:?}",
+            get_entries_debug(&mut store_alfie, namespace_id)
+        );
+        info!(
+            "betty store {:?}",
+            get_entries_debug(&mut store_betty, namespace_id)
+        );
+
+        assert!(res_alfie.is_ok());
+        assert!(res_betty.is_ok());
+        assert_eq!(
+            get_entries(&mut store_alfie, namespace_id),
+            expected_entries
+        );
+        assert_eq!(
+            get_entries(&mut store_betty, namespace_id),
+            expected_entries
+        );
 
         Ok(())
+    }
+    fn get_entries<S: Store>(store: &mut S, namespace: NamespaceId) -> HashSet<Entry> {
+        store
+            .get_entries(namespace, &ThreeDRange::all())
+            .filter_map(Result::ok)
+            .collect()
+    }
+
+    fn get_entries_debug<S: Store>(
+        store: &mut S,
+        namespace: NamespaceId,
+    ) -> Vec<(SubspaceId, Path)> {
+        store
+            .get_entries(namespace, &ThreeDRange::all())
+            .filter_map(|r| r.ok())
+            .map(|e| (e.subspace_id, e.path))
+            .collect()
     }
 }
--- iroh-willow/src/proto/wgps.rs
+++ iroh-willow/src/proto/wgps.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fmt};
 
 use bytes::Bytes;
 use ed25519_dalek::Signature;
@@ -7,6 +7,7 @@ use iroh_net::key::PublicKey;
 use serde::{Deserialize, Serialize};
 
 use super::{
+    grouping::{Area, AreaOfInterest, SubspaceArea, ThreeDRange},
     keys, meadowcap,
     willow::{
         AuthorisationToken, AuthorisedEntry, Entry, Path, PossiblyAuthorisedEntry, SubspaceId,
@@ -14,13 +15,14 @@ use super::{
     },
 };
 
-pub const MAXIMUM_PAYLOAD_SIZE_POWER: u8 = 12;
+pub const MAX_PAYLOAD_SIZE_POWER: u8 = 12;
+
 /// The maximum payload size limits when the other peer may include Payloads directly when transmitting Entries:
 /// when an Entry’s payload_length is strictly greater than the maximum payload size,
 /// its Payload may only be transmitted when explicitly requested.
 ///
 /// The value is 4096.
-pub const MAXIMUM_PAYLOAD_SIZE: usize = 2usize.pow(MAXIMUM_PAYLOAD_SIZE_POWER as u32);
+pub const MAX_PAYLOAD_SIZE: usize = 2usize.pow(MAX_PAYLOAD_SIZE_POWER as u32);
 
 pub const CHALLENGE_LENGTH: usize = 32;
 pub const CHALLENGE_HASH_LENGTH: usize = DIGEST_LENGTH;
@@ -73,7 +75,9 @@ pub enum LogicalChannel {
     ControlChannel,
     /// Logical channel for performing 3d range-based set reconciliation.
     ReconciliationChannel,
-    // TODO: actually use more channels
+    // TODO: use all the channels
+    // right now everything but reconciliation goes into the control channel
+    //
     // /// Logical channel for transmitting Entries and Payloads outside of 3d range-based set reconciliation.
     // DataChannel,
     //
@@ -138,245 +142,7 @@ pub struct CommitmentReveal {
     pub nonce: AccessChallenge,
 }
 
-// skip: Private Area Intersection
-
-/// A grouping of Entries that are among the newest in some store.
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct AreaOfInterest {
-    /// To be included in this AreaOfInterest, an Entry must be included in the area.
-    pub area: Area,
-    /// To be included in this AreaOfInterest, an Entry’s timestamp must be among the max_count greatest Timestamps, unless max_count is zero.
-    pub max_count: u64,
-    /// The total payload_lengths of all included Entries is at most max_size, unless max_size is zero.
-    pub max_size: u64,
-}
-
-impl AreaOfInterest {
-    pub fn full() -> Self {
-        Self {
-            area: Area::full(),
-            max_count: 0,
-            max_size: 0,
-        }
-    }
-}
-
-/// A grouping of Entries.
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct Area {
-    /// To be included in this Area, an Entry’s subspace_id must be equal to the subspace_id, unless it is any.
-    pub subspace_id: SubspaceArea,
-    /// To be included in this Area, an Entry’s path must be prefixed by the path.
-    pub path: Path,
-    /// To be included in this Area, an Entry’s timestamp must be included in the times.
-    pub times: Range<Timestamp>,
-}
-
-impl Area {
-    pub const fn new(subspace_id: SubspaceArea, path: Path, times: Range<Timestamp>) -> Self {
-        Self {
-            subspace_id,
-            path,
-            times,
-        }
-    }
-
-    pub fn full() -> Self {
-        Self::new(SubspaceArea::Any, Path::empty(), Range::<Timestamp>::FULL)
-    }
-
-    pub fn empty() -> Self {
-        Self::new(SubspaceArea::Any, Path::empty(), Range::<Timestamp>::EMPTY)
-    }
-
-    pub fn subspace(subspace_id: SubspaceId) -> Self {
-        Self::new(
-            SubspaceArea::Id(subspace_id),
-            Path::empty(),
-            Range::<Timestamp>::FULL,
-        )
-    }
-
-    pub fn includes_entry(&self, entry: &Entry) -> bool {
-        self.subspace_id.includes_subspace(&entry.subspace_id)
-            && self.path.is_prefix_of(&entry.path)
-            && self.times.includes(&entry.timestamp)
-    }
-
-    pub fn includes_area(&self, other: &Area) -> bool {
-        self.subspace_id.includes(&other.subspace_id)
-            && self.path.is_prefix_of(&other.path)
-            && self.times.includes_range(&other.times)
-    }
-
-    pub fn includes_range(&self, range: &ThreeDRange) -> bool {
-        let path_start = self.path.is_prefix_of(&range.paths.start);
-        let path_end = match &range.paths.end {
-            RangeEnd::Open => true,
-            RangeEnd::Closed(path) => self.path.is_prefix_of(path),
-        };
-        let subspace_start = self.subspace_id.includes_subspace(&range.subspaces.start);
-        let subspace_end = match range.subspaces.end {
-            RangeEnd::Open => true,
-            RangeEnd::Closed(subspace) => self.subspace_id.includes_subspace(&subspace),
-        };
-        subspace_start
-            && subspace_end
-            && path_start
-            && path_end
-            && self.times.includes_range(&range.times)
-    }
-
-    pub fn into_range(&self) -> ThreeDRange {
-        let subspace_start = match self.subspace_id {
-            SubspaceArea::Any => SubspaceId::zero(),
-            SubspaceArea::Id(id) => id,
-        };
-        let subspace_end = match self.subspace_id {
-            SubspaceArea::Any => RangeEnd::Open,
-            SubspaceArea::Id(id) => subspace_range_end(id),
-        };
-        let path_start = self.path.clone();
-        let path_end = path_range_end(&self.path);
-        ThreeDRange {
-            subspaces: Range::new(subspace_start, subspace_end),
-            paths: Range::new(path_start, path_end),
-            times: self.times.clone(),
-        }
-    }
-
-    pub fn intersection(&self, other: &Area) -> Option<Area> {
-        let subspace_id = self.subspace_id.intersection(&other.subspace_id)?;
-        let path = self.path.intersection(&other.path)?;
-        let times = self.times.intersection(&other.times)?;
-        Some(Self {
-            subspace_id,
-            times,
-            path,
-        })
-    }
-}
-
-fn path_range_end(path: &Path) -> RangeEnd<Path> {
-    if path.is_empty() {
-        RangeEnd::Open
-    } else {
-        let mut out = vec![];
-        for component in path.iter().rev() {
-            // component can be incremented
-            if out.is_empty() && component.iter().any(|x| *x != 0xff) {
-                let mut bytes = Vec::with_capacity(component.len());
-                bytes.copy_from_slice(&component);
-                let incremented = increment_by_one(&mut bytes);
-                debug_assert!(incremented, "checked above");
-                out.push(Bytes::from(bytes));
-                break;
-            // component cannot be incremented
-            } else if out.is_empty() {
-                continue;
-            } else {
-                out.push(component.clone())
-            }
-        }
-        if out.is_empty() {
-            RangeEnd::Open
-        } else {
-            out.reverse();
-            RangeEnd::Closed(Path::from_bytes_unchecked(out))
-        }
-    }
-    // let mut bytes = id.to_bytes();
-    // if increment_by_one(&mut bytes) {
-    //     RangeEnd::Closed(SubspaceId::from_bytes_unchecked(bytes))
-    // } else {
-    //     RangeEnd::Open
-    // }
-}
-
-fn subspace_range_end(id: SubspaceId) -> RangeEnd<SubspaceId> {
-    let mut bytes = id.to_bytes();
-    if increment_by_one(&mut bytes) {
-        RangeEnd::Closed(SubspaceId::from_bytes_unchecked(bytes))
-    } else {
-        RangeEnd::Open
-    }
-}
-
-/// Increment a byte string by one, by incrementing the last byte that is not 255 by one.
-///
-/// Returns false if all bytes are 255.
-fn increment_by_one(value: &mut [u8]) -> bool {
-    for char in value.iter_mut().rev() {
-        if *char != 255 {
-            *char += 1;
-            return true;
-        } else {
-            *char = 0;
-        }
-    }
-    false
-}
-
-impl Range<Timestamp> {
-    pub const FULL: Self = Self {
-        start: 0,
-        end: RangeEnd::Open,
-    };
-
-    pub const EMPTY: Self = Self {
-        start: 0,
-        end: RangeEnd::Closed(0),
-    };
-
-    fn intersection(&self, other: &Self) -> Option<Self> {
-        let start = self.start.max(other.start);
-        let end = match (&self.end, &other.end) {
-            (RangeEnd::Open, RangeEnd::Closed(b)) => RangeEnd::Closed(*b),
-            (RangeEnd::Closed(a), RangeEnd::Closed(b)) => RangeEnd::Closed(*a.min(b)),
-            (RangeEnd::Closed(a), RangeEnd::Open) => RangeEnd::Closed(*a),
-            (RangeEnd::Open, RangeEnd::Open) => RangeEnd::Open,
-        };
-        match end {
-            RangeEnd::Open => Some(Self::new(start, end)),
-            RangeEnd::Closed(t) if t >= start => Some(Self::new(start, end)),
-            RangeEnd::Closed(_) => Some(Self::new(start, end)),
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub enum SubspaceArea {
-    Any,
-    Id(SubspaceId),
-}
-
-impl SubspaceArea {
-    fn includes(&self, other: &SubspaceArea) -> bool {
-        match (self, other) {
-            (SubspaceArea::Any, SubspaceArea::Any) => true,
-            (SubspaceArea::Id(_), SubspaceArea::Any) => false,
-            (_, SubspaceArea::Id(id)) => self.includes_subspace(id),
-        }
-    }
-    fn includes_subspace(&self, subspace_id: &SubspaceId) -> bool {
-        match self {
-            Self::Any => true,
-            Self::Id(id) => id == subspace_id,
-        }
-    }
-
-    fn intersection(&self, other: &Self) -> Option<Self> {
-        match (self, other) {
-            (Self::Any, Self::Any) => Some(Self::Any),
-            (Self::Id(a), Self::Any) => Some(Self::Id(*a)),
-            (Self::Any, Self::Id(b)) => Some(Self::Id(*b)),
-            (Self::Id(a), Self::Id(b)) if a == b => Some(Self::Id(*a)),
-            (Self::Id(_a), Self::Id(_b)) => None,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, derive_more::From, derive_more::Debug)]
+#[derive(Serialize, Deserialize, derive_more::From, derive_more::Debug, strum::Display)]
 pub enum Message {
     #[debug("{:?}", _0)]
     CommitmentReveal(CommitmentReveal),
@@ -485,6 +251,11 @@ pub struct ReconciliationSendFingerprint {
     pub sender_handle: AreaOfInterestHandle,
     /// An AreaOfInterestHandle, bound by the receiver of this message, that fully contains the range.
     pub receiver_handle: AreaOfInterestHandle,
+    /// If this is this the last reply to range received via [`ReconciliationSendFingerprint`] or [`ReconciliationAnnounceEntries`]
+    /// from the other peer, set to that range to indicate to the other peer that no further replies for that range will be sent
+    ///
+    /// TODO: This is a spec deviation, discuss further and remove or upstream
+    pub is_final_reply_for_range: Option<ThreeDRange>,
 }
 
 /// Prepare transmission of the LengthyEntries a peer has in a 3dRange as part of 3d range-based set reconciliation.
@@ -502,6 +273,11 @@ pub struct ReconciliationAnnounceEntries {
     pub sender_handle: AreaOfInterestHandle,
     /// An AreaOfInterestHandle, bound by the receiver of this message, that fully contains the range.
     pub receiver_handle: AreaOfInterestHandle,
+    /// If this is this the last reply to range received via [`ReconciliationSendFingerprint`] or [`ReconciliationAnnounceEntries`]
+    /// from the other peer, set to that range to indicate to the other peer that no further replies for that range will be sent
+    /// 
+    /// TODO: This is a spec deviation, discuss further and remove or upstream
+    pub is_final_reply_for_range: Option<ThreeDRange>,
 }
 
 /// Transmit a LengthyEntry as part of 3d range-based set reconciliation.
@@ -540,8 +316,14 @@ impl LengthyEntry {
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize, Eq, PartialEq, Clone, Copy)]
+#[derive(Default, Serialize, Deserialize, Eq, PartialEq, Clone, Copy)]
 pub struct Fingerprint(pub [u8; 32]);
+
+impl fmt::Debug for Fingerprint {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Fingerprint({})", iroh_base::base32::fmt_short(&self.0))
+    }
+}
 
 impl Fingerprint {
     pub fn add_entry(&mut self, entry: &Entry) {
@@ -560,6 +342,10 @@ impl Fingerprint {
         this.add_entries(iter);
         this
     }
+
+    pub fn is_empty(&self) -> bool {
+        *self == Self::default()
+    }
 }
 
 impl std::ops::BitXorAssign for Fingerprint {
@@ -570,109 +356,26 @@ impl std::ops::BitXorAssign for Fingerprint {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ThreeDRange {
-    pub paths: Range<Path>,
-    pub subspaces: Range<SubspaceId>,
-    pub times: Range<Timestamp>,
-}
-
-impl ThreeDRange {
-    pub fn includes_entry(&self, entry: &Entry) -> bool {
-        self.subspaces.includes(&entry.subspace_id)
-            && self.paths.includes(&entry.path)
-            && self.times.includes(&entry.timestamp)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
-pub struct Range<T> {
-    pub start: T,
-    pub end: RangeEnd<T>,
-}
-
-impl<T> Range<T> {
-    pub fn new(start: T, end: RangeEnd<T>) -> Self {
-        Self { start, end }
-    }
-    pub fn is_closed(&self) -> bool {
-        matches!(self.end, RangeEnd::Closed(_))
-    }
-    pub fn is_open(&self) -> bool {
-        matches!(self.end, RangeEnd::Open)
-    }
-}
-
-impl<T: Ord + PartialOrd> Range<T> {
-    pub fn includes(&self, value: &T) -> bool {
-        value >= &self.start && self.end.includes(value)
-    }
-
-    pub fn includes_range(&self, other: &Range<T>) -> bool {
-        self.start <= other.start && self.end >= other.end
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub enum RangeEnd<T> {
-    Closed(T),
-    Open,
-}
-
-impl<T: Ord + PartialOrd> PartialOrd for RangeEnd<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (self, other) {
-            (RangeEnd::Open, RangeEnd::Closed(_)) => Some(Ordering::Greater),
-            (RangeEnd::Closed(_), RangeEnd::Open) => Some(Ordering::Less),
-            (RangeEnd::Closed(a), RangeEnd::Closed(b)) => a.partial_cmp(b),
-            (RangeEnd::Open, RangeEnd::Open) => Some(Ordering::Equal),
-        }
-    }
-}
-
-// impl<T: Ord + PartialOrd> PartialOrd<T> for RangeEnd<T> {
-//     fn partial_cmp(&self, other: &T) -> Option<Ordering> {
-//         // match (self, other) {
-//         //     (RangeEnd::Open, RangeEnd::Closed(_)) => Some(Ordering::Greater),
-//         //     (RangeEnd::Closed(_), RangeEnd::Open) => Some(Ordering::Less),
-//         //     (RangeEnd::Closed(a), RangeEnd::Closed(b)) => a.partial_cmp(b),
-//         //     (RangeEnd::Open, RangeEnd::Open) => Some(Ordering::Equal),
-//         // }
-//     }
-// }
-
-impl<T: Ord + PartialOrd> RangeEnd<T> {
-    pub fn includes(&self, value: &T) -> bool {
-        match self {
-            Self::Open => true,
-            Self::Closed(end) => value < end,
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub enum ControlMessage {}
-
 /// Make a binding promise of available buffer capacity to the other peer
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ControlIssueGuarantee {
-    amount: u64,
-    channel: LogicalChannel,
+    pub amount: u64,
+    pub channel: LogicalChannel,
 }
 
 /// Allow the other peer to reduce its total buffer capacity by amount.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ControlAbsolve {
-    amount: u64,
-    channel: LogicalChannel,
+    pub amount: u64,
+    pub channel: LogicalChannel,
 }
 
 /// Ask the other peer to send an ControlAbsolve message
 /// such that the receiver remaining guarantees will be target.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ControlPlead {
-    target: u64,
-    channel: LogicalChannel,
+    pub target: u64,
+    pub channel: LogicalChannel,
 }
 
 /// The server notifies the client that it has started dropping messages and will continue
@@ -680,13 +383,13 @@ pub struct ControlPlead {
 /// guarantees of the logical channel before sending a AnnounceDropping message.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ControlAnnounceDropping {
-    channel: LogicalChannel,
+    pub channel: LogicalChannel,
 }
 
 /// The client notifies the server that it can stop dropping messages on this logical channel.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ControlApologise {
-    channel: LogicalChannel,
+    pub channel: LogicalChannel,
 }
 
 /// Ask the other peer to free a resource handle.
