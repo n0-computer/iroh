bb369956ed58015676631cf2d66dd828c0d83332
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-04-25 14:34:44 +0200

    test

---
+++ .github/scripts/upload_gh_assets.sh
@@ -0,0 +1,10 @@
+IFS=',' read -ra ASSETS <<< "${{ env.ASSET }}"
+for ASSET in "${ASSETS[@]}"; do
+    ASSET_NAME=$(basename $ASSET)
+    curl \
+    -H "Authorization: Bearer $0" \
+    -H "Content-Type: $(file -b --mime-type $ASSET)" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    --data-binary @"$ASSET" \
+    "$1=$ASSET_NAME"
+done
