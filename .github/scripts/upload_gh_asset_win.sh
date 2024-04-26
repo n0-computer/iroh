40875412407680c881fe9dcc88d5eaf9d4025b0f
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-04-26 15:10:34 +0200

    test

--- .github/scripts/upload_gh_asset_win.sh
+++ .github/scripts/upload_gh_asset_win.sh
@@ -3,9 +3,11 @@ url="$3"
 url=${url%%\{*}
 for ASSET in "${ASSETS[@]}"; do
   ASSET_NAME=$(basename $ASSET)
+  CONTENT_LENGTH=$(wc -c <"$ASSET")
   curl \
   -H "Authorization: Bearer $2" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Content-Length: $CONTENT_LENGTH" \
   -F "file=@$ASSET;type=$(file -b --mime-type $ASSET)" \
   "$url?name=$ASSET_NAME"
 done
