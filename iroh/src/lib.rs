bcf5720e29f969ee4cb485cced8528f0b5f3b0ee
dignifiedquire <me@dignifiedquire.com>
2024-05-02 19:13:07 +0200

    docs(iroh): inline reexported docs

--- iroh/src/lib.rs
+++ iroh/src/lib.rs
@@ -2,9 +2,13 @@
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
 // re-export the iroh crates
+#[doc(inline)]
 pub use iroh_base as base;
+#[doc(inline)]
 pub use iroh_bytes as bytes;
+#[doc(inline)]
 pub use iroh_net as net;
+#[doc(inline)]
 pub use iroh_sync as sync;
 
 pub mod client;
