7e6d8391fb25262fcd10f1aafe7a6ed5695124aa
Franz Heinzmann (Frando) <frando@unbiskant.org>
2024-05-12 01:29:55 +0200

    implement resource control and refactor channel opening

--- iroh-willow/src/session/channels.rs
+++ iroh-willow/src/session/channels.rs
@@ -7,34 +7,60 @@ use crate::{
 };
 
 #[derive(Debug, Clone)]
+pub struct LogicalChannelReceivers {
+    pub reconciliation: Receiver<Message>,
+    pub static_tokens: Receiver<Message>,
+}
+impl LogicalChannelReceivers {
+    pub fn close(&self) {
+        self.reconciliation.close();
+        self.static_tokens.close();
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct LogicalChannelSenders {
+    pub reconciliation: Sender<Message>,
+    pub static_tokens: Sender<Message>,
+}
+impl LogicalChannelSenders {
+    pub fn close(&self) {
+        self.reconciliation.close();
+        self.static_tokens.close();
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Channels {
     pub control_send: Sender<Message>,
     pub control_recv: Receiver<Message>,
-    pub reconciliation_send: Sender<Message>,
-    pub reconciliation_recv: Receiver<Message>,
+    pub logical_send: LogicalChannelSenders,
+    pub logical_recv: LogicalChannelReceivers,
 }
 
 impl Channels {
     pub fn close_all(&self) {
         self.control_send.close();
         self.control_recv.close();
-        self.reconciliation_send.close();
-        self.reconciliation_recv.close();
+        self.logical_send.close();
+        self.logical_recv.close();
     }
     pub fn close_send(&self) {
         self.control_send.close();
-        self.reconciliation_send.close();
+        self.logical_send.close();
     }
     pub fn sender(&self, channel: LogicalChannel) -> &Sender<Message> {
         match channel {
             LogicalChannel::Control => &self.control_send,
-            LogicalChannel::Reconciliation => &self.reconciliation_send,
+            LogicalChannel::Reconciliation => &self.logical_send.reconciliation,
+            LogicalChannel::StaticToken => &self.logical_send.static_tokens,
         }
     }
     pub fn receiver(&self, channel: LogicalChannel) -> &Receiver<Message> {
         match channel {
             LogicalChannel::Control => &self.control_recv,
-            LogicalChannel::Reconciliation => &self.reconciliation_recv,
+            LogicalChannel::Reconciliation => &self.logical_recv.reconciliation,
+            LogicalChannel::StaticToken => &self.logical_recv.static_tokens,
         }
     }
 
