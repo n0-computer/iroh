035808d099b69f39577c837df5d253ed01206956
dignifiedquire <me@dignifiedquire.com>
2024-07-05 17:57:59 +0200

    fix(iroh-docs): do not dial invalid peers

--- iroh-docs/src/engine/live.rs
+++ iroh-docs/src/engine/live.rs
@@ -447,27 +447,34 @@ impl<B: iroh_blobs::store::Store> LiveActor<B> {
         namespace: NamespaceId,
         peers: Vec<NodeAddr>,
     ) -> anyhow::Result<()> {
-        let peer_ids: Vec<PublicKey> = peers.iter().map(|p| p.node_id).collect();
+        let mut peer_ids = Vec::new();
 
         // add addresses of peers to our endpoint address book
         for peer in peers.into_iter() {
             let peer_id = peer.node_id;
-            if let Err(err) = self.endpoint.add_node_addr_with_source(peer, SOURCE_NAME) {
-                warn!(peer = %peer_id.fmt_short(), "failed to add known addrs: {err:?}");
+            match self.endpoint.add_node_addr_with_source(peer, SOURCE_NAME) {
+                Ok(()) => {
+                    peer_ids.push(peer_id);
+                }
+                Err(err) => {
+                    warn!(peer = %peer_id.fmt_short(), "failed to add known addrs: {err:?}");
+                }
             }
         }
 
-        // tell gossip to join
-        self.gossip_actor_tx
-            .send(ToGossipActor::Join {
-                namespace,
-                peers: peer_ids.clone(),
-            })
-            .await?;
+        if !peer_ids.is_empty() {
+            // tell gossip to join
+            self.gossip_actor_tx
+                .send(ToGossipActor::Join {
+                    namespace,
+                    peers: peer_ids.clone(),
+                })
+                .await?;
 
-        // trigger initial sync with initial peers
-        for peer in peer_ids {
-            self.sync_with_peer(namespace, peer, SyncReason::DirectJoin);
+            // trigger initial sync with initial peers
+            for peer in peer_ids {
+                self.sync_with_peer(namespace, peer, SyncReason::DirectJoin);
+            }
         }
         Ok(())
     }
