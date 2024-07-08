aff81520f836e4c572025989061744ba53367aff
Philipp Krüger <philipp.krueger1@gmail.com>
2024-07-08 17:03:13 +0200

    fix(iroh-net): unexpected `cfg` condition values / possible fix on netbsd (#2476)
    ## Description
    
    We had some CLI tests that had `#[cfg(feature = "fs-store")]`, but that
    feature doesn't exist in `iroh-cli`... so they were never compiled (and
    some related code needed for these tests was deleted. I reintroduced
    it).
    
    This also fixes what is likely a bug, where `#[cfg(target_arch =
    "netbsd")]` was used, but there is no "architecture" netbsd. Only an
    *OS* netbsd.
    
    ## Breaking Changes
    
    None
    
    ## Notes & open questions
    
    --
    
    ## Change checklist
    
    - [X] Self-review.
    - ~~[ ] Documentation updates following the [style
    guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text),
    if relevant.~~
    - ~~[ ] Tests if relevant.~~
    - [X] All breaking changes documented.

--- iroh-cli/tests/cli.rs
+++ iroh-cli/tests/cli.rs
@@ -108,11 +108,10 @@ fn cli_provide_tree() -> Result<()> {
     test_provide_get_loop(Input::Path(path), Output::Path)
 }
 
-#[cfg(feature = "fs-store")]
 #[test]
 #[ignore = "flaky"]
 fn cli_provide_tree_resume() -> Result<()> {
-    use iroh_blobs::store::file::test_support::{make_partial, MakePartialResult};
+    use iroh::blobs::store::fs::test_support::{make_partial, MakePartialResult};
 
     /// Get all matches for match group 1 (an explicitly defined match group)
     fn explicit_matches(matches: Vec<(usize, Vec<String>)>) -> Vec<String> {
@@ -214,11 +213,10 @@ fn cli_provide_tree_resume() -> Result<()> {
     Ok(())
 }
 
-#[cfg(feature = "fs-store")]
 #[test]
 #[ignore = "flaky"]
 fn cli_provide_file_resume() -> Result<()> {
-    use iroh_blobs::store::file::test_support::{make_partial, MakePartialResult};
+    use iroh::blobs::store::fs::test_support::{make_partial, MakePartialResult};
 
     /// Get all matches for match group 1 (an explicitly defined match group)
     fn explicit_matches(matches: Vec<(usize, Vec<String>)>) -> Vec<String> {
@@ -1033,4 +1031,52 @@ where
     }
 
     caps
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<usize> {
+    let src = src.as_ref();
+    let dst = dst.as_ref();
+    std::fs::create_dir_all(dst)?;
+    let mut len = 0;
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry.with_context(|| {
+            format!(
+                "failed to read directory entry in `{}`",
+                src.to_string_lossy()
+            )
+        })?;
+        let ty = entry.file_type().with_context(|| {
+            format!(
+                "failed to get file type for file `{}`",
+                entry.path().to_string_lossy()
+            )
+        })?;
+        let src = entry.path();
+        let dst = dst.join(entry.file_name());
+        if ty.is_dir() {
+            len += copy_dir_all(&src, &dst).with_context(|| {
+                format!(
+                    "failed to copy directory `{}` to `{}`",
+                    src.to_string_lossy(),
+                    dst.to_string_lossy()
+                )
+            })?;
+        } else {
+            std::fs::copy(&src, &dst).with_context(|| {
+                format!(
+                    "failed to copy file `{}` to `{}`",
+                    src.to_string_lossy(),
+                    dst.to_string_lossy()
+                )
+            })?;
+            len += 1;
+        }
+    }
+    Ok(len)
+}
+
+fn copy_blob_dirs(src: &Path, tgt: &Path) -> Result<()> {
+    let dir = &IrohPaths::BaoStoreDir;
+    copy_dir_all(dir.with_root(src), dir.with_root(tgt))?;
+    Ok(())
 }
