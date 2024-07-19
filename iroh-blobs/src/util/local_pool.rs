f06efd1641c45ce0e32e2d95482917218bd5ade7
Ruediger Klaehn <rklaehn@protonmail.com>
2024-07-19 19:06:43 +0300

    Rename shutdown to finish, use unbounded queue, add PanicMode (unused so far)

--- iroh-blobs/src/util/local_pool.rs
+++ iroh-blobs/src/util/local_pool.rs
@@ -1,4 +1,5 @@
 //! A local task pool with proper shutdown
+use core::panic;
 use std::{any::Any, future::Future, ops::Deref, pin::Pin, sync::Arc};
 use tokio::{sync::Semaphore, task::LocalSet};
 use tokio_util::sync::CancellationToken;
@@ -89,45 +90,73 @@ fn get_thread_name() -> String {
         .to_string()
 }
 
+/// What to do when a panic occurs in a pool thread
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PanicMode {
+    /// Log the panic and continue
+    ///
+    /// The panic will be re-thrown when the pool is dropped.
+    LogAndContinue,
+    /// Log the panic and immediately shut down the pool.
+    ///
+    /// The panic will be re-thrown when the pool is dropped.
+    Shutdown,
+}
+
 /// Local task pool configuration
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Debug)]
 pub struct Config {
     /// Number of threads in the pool
     pub threads: usize,
-    /// Size of the task queue, shared between threads
-    pub queue_size: usize,
     /// Prefix for thread names
     pub thread_name_prefix: &'static str,
+    /// Ignore panics in pool threads
+    pub panic_mode: PanicMode,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             threads: num_cpus::get(),
-            queue_size: 1024,
-            thread_name_prefix: "local-pool-",
+            thread_name_prefix: "local-pool",
+            panic_mode: PanicMode::Shutdown,
         }
     }
 }
 
+impl Default for LocalPool {
+    fn default() -> Self {
+        Self::new(Default::default())
+    }
+}
+
 impl LocalPool {
+    /// Create a new local pool with a single std thread.
+    pub fn single() -> Self {
+        Self::new(Config {
+            threads: 1,
+            ..Default::default()
+        })
+    }
+
     /// Create a new task pool with `n` threads and a queue of size `queue_size`
     pub fn new(config: Config) -> Self {
         let Config {
             threads,
-            queue_size,
             thread_name_prefix,
+            panic_mode,
         } = config;
         let cancel_token = CancellationToken::new();
-        let (send, recv) = flume::bounded::<Message>(queue_size);
+        let (send, recv) = flume::unbounded::<Message>();
         let shutdown_sem = Arc::new(Semaphore::new(0));
         let handles = (0..threads)
             .map(|i| {
-                Self::spawn_one(
+                Self::spawn_pool_thread(
                     format!("{thread_name_prefix}-{i}"),
                     recv.clone(),
                     cancel_token.clone(),
                     shutdown_sem.clone(),
+                    panic_mode,
                 )
             })
             .collect::<std::io::Result<Vec<_>>>()
@@ -146,11 +175,12 @@ impl LocalPool {
     }
 
     /// Spawn a new task in the pool.
-    fn spawn_one(
+    fn spawn_pool_thread(
         task_name: String,
         recv: flume::Receiver<Message>,
         cancel_token: CancellationToken,
         shutdown_sem: Arc<Semaphore>,
+        _panic_mode: PanicMode,
     ) -> std::io::Result<std::thread::JoinHandle<()>> {
         std::thread::Builder::new().name(task_name).spawn(move || {
             let ls = LocalSet::new();
@@ -181,29 +211,54 @@ impl LocalPool {
         })
     }
 
-    /// Cleanly shut down the pool
+    /// Gently shut down the pool
     ///
     /// Notifies all the pool threads to shut down and waits for them to finish.
     ///
     /// If you just want to drop the pool without giving the threads a chance to
-    /// process their remaining tasks, just use drop.
-    pub async fn shutdown(self) {
-        let threads = self
-            .threads
+    /// process their remaining tasks, just use [`Self::shutdown`].
+    ///
+    /// If you want to wait for only a limited time for the tasks to finish,
+    /// you can race this function with a timeout.
+    pub async fn finish(self) {
+        // we assume that there are exactly as many threads as there are handles.
+        // also, we assume that the threads are still running.
+        for _ in 0..self.threads_u32() {
+            println!("sending shutdown message");
+            // send the shutdown message
+            // sending will fail if all threads are already finished, but
+            // in that case we don't need to do anything.
+            //
+            // Threads will add a permit in any case, so await_thread_completion
+            // will then immediately return.
+            self.send.send(Message::Finish).ok();
+        }
+        self.await_thread_completion().await;
+    }
+
+    fn threads_u32(&self) -> u32 {
+        self.threads
             .len()
             .try_into()
-            .expect("invalid number of threads");
-        for _ in 0..threads {
-            self.send
-                .send_async(Message::Finish)
+            .expect("invalid number of threads")
+    }
+
+    async fn await_thread_completion(&self) {
+        // wait for all threads to finish.
+        // Each thread will add a permit to the semaphore.
+        let wait_for_semaphore = async move {
+            let _ = self
+                .shutdown_sem
+                .acquire_many(self.threads_u32())
                 .await
-                .expect("receiver dropped");
+                .expect("semaphore closed");
+        };
+        // race the semaphore wait with the cancel token in case somebody
+        // cancels the pool while we are waiting.
+        tokio::select! {
+            _ = wait_for_semaphore => {}
+            _ = self.cancel_token.cancelled() => {}
         }
-        let _ = self
-            .shutdown_sem
-            .acquire_many(threads)
-            .await
-            .expect("semaphore closed");
     }
 }
 
@@ -350,7 +405,7 @@ mod tests {
                 .await
                 .unwrap();
         }
-        pool.shutdown().await;
+        pool.finish().await;
         assert_eq!(counter.load(std::sync::atomic::Ordering::SeqCst), n);
     }
 }
