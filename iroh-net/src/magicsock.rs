ef259dc54617c4e3b7ecbeee8688381db11b4b3e
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-07-22 15:32:02 +0200

    rebase cleanup

--- iroh-net/src/magicsock.rs
+++ iroh-net/src/magicsock.rs
@@ -1776,7 +1776,7 @@ impl Actor {
                         "tick: direct addr heartbeat {} direct addrs",
                         self.msock.node_map.node_count(),
                     );
-		    inc!(Metrics, actor_tick_endpoint_heartbeat);
+                    inc!(Metrics, actor_tick_direct_addr_heartbeat);
                     // TODO: this might trigger too many packets at once, pace this
 
                     self.msock.node_map.prune_inactive();
@@ -1786,7 +1786,7 @@ impl Actor {
                 _ = direct_addr_update_receiver.changed() => {
                     let reason = *direct_addr_update_receiver.borrow();
                     trace!("tick: direct addr update receiver {:?}", reason);
-		    inc!(Metrics, actor_tick_endpoints_update_receiver);
+                    inc!(Metrics, actor_tick_direct_addr_update_receiver);
                     if let Some(reason) = reason {
                         self.update_direct_addrs(reason).await;
                     }
