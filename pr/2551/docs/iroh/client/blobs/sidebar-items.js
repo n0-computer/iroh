11c25ca9260bc389fcc9d5289d8a6a4dbd49578c
Arqu <Arqu@users.noreply.github.com>
2024-07-28 21:40:15 +0000

    deploy: 2ada53b5af3a76804a90114bd6dc0a4fb7f2bc5f

---
+++ pr/2551/docs/iroh/client/blobs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BlobStatus","DownloadMode","WrapOption"],"struct":["AddOutcome","AddProgress","BlobInfo","Client","CollectionInfo","DownloadOptions","DownloadOutcome","DownloadProgress","ExportOutcome","ExportProgress","IncompleteBlobInfo","Reader"]};
