d73c40d1bd13b49fb6259e6224a3bb2c7718da78
Franz Heinzmann (Frando) <frando@unbiskant.org>
2024-08-01 12:28:35 +0200

    refactor: move integration tests

---
+++ iroh-willow/tests/basic.rs
@@ -0,0 +1,341 @@
+
+use anyhow::Result;
+use futures_concurrency::future::TryJoin;
+use futures_lite::StreamExt;
+
+use iroh_willow::{
+    proto::{
+        grouping::Area,
+        willow::Path,
+    },
+    session::{intents::EventKind, Interests, SessionInit, SessionMode},
+};
+
+use self::util::{create_rng, spawn_two, insert, setup_and_delegate, Peer};
+
+#[tokio::test(flavor = "multi_thread")]
+async fn peer_manager_two_intents() -> Result<()> {
+    iroh_test::logging::setup_multithreaded();
+    let mut rng = create_rng("peer_manager_two_intents");
+
+    let [alfie, betty] = spawn_two(&mut rng).await?;
+    let (namespace, _alfie_user, betty_user) = setup_and_delegate(&alfie, &betty).await?;
+    let betty_node_id = betty.node_id();
+
+    insert(&betty, namespace, betty_user, &[b"foo", b"1"], "foo 1").await?;
+    insert(&betty, namespace, betty_user, &[b"bar", b"2"], "bar 2").await?;
+    insert(&betty, namespace, betty_user, &[b"bar", b"3"], "bar 3").await?;
+
+    let task_foo_path = tokio::task::spawn({
+        let alfie = alfie.clone();
+        async move {
+            let path = Path::new(&[b"foo"]).unwrap();
+
+            let init = SessionInit::new(
+                Interests::builder().add_area(namespace, [Area::path(path.clone())]),
+                SessionMode::ReconcileOnce,
+            );
+            let mut intent = alfie.sync_with_peer(betty_node_id, init).await.unwrap();
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::CapabilityIntersection {
+                    namespace,
+                    area: Area::full(),
+                }
+            );
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::InterestIntersection {
+                    namespace,
+                    area: Area::path(path.clone()).into()
+                }
+            );
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::Reconciled {
+                    namespace,
+                    area: Area::path(path.clone()).into()
+                }
+            );
+
+            assert_eq!(intent.next().await.unwrap(), EventKind::ReconciledAll);
+
+            assert!(intent.next().await.is_none());
+        }
+    });
+
+    let task_bar_path = tokio::task::spawn({
+        let alfie = alfie.clone();
+        async move {
+            let path = Path::new(&[b"bar"]).unwrap();
+
+            let interests = Interests::builder().add_area(namespace, [Area::path(path.clone())]);
+            let init = SessionInit::new(interests, SessionMode::ReconcileOnce);
+
+            let mut intent = alfie.sync_with_peer(betty_node_id, init).await.unwrap();
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::CapabilityIntersection {
+                    namespace,
+                    area: Area::full(),
+                }
+            );
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::InterestIntersection {
+                    namespace,
+                    area: Area::path(path.clone()).into()
+                }
+            );
+
+            assert_eq!(
+                intent.next().await.unwrap(),
+                EventKind::Reconciled {
+                    namespace,
+                    area: Area::path(path.clone()).into()
+                }
+            );
+
+            assert_eq!(intent.next().await.unwrap(), EventKind::ReconciledAll);
+
+            assert!(intent.next().await.is_none());
+        }
+    });
+
+    task_foo_path.await.unwrap();
+    task_bar_path.await.unwrap();
+
+    [alfie, betty].map(Peer::shutdown).try_join().await?;
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn peer_manager_update_intent() -> Result<()> {
+    iroh_test::logging::setup_multithreaded();
+    let mut rng = create_rng("peer_manager_update_intent");
+
+    let [alfie, betty] = spawn_two(&mut rng).await?;
+    let (namespace, _alfie_user, betty_user) = setup_and_delegate(&alfie, &betty).await?;
+    let betty_node_id = betty.node_id();
+
+    insert(&betty, namespace, betty_user, &[b"foo"], "foo 1").await?;
+    insert(&betty, namespace, betty_user, &[b"bar"], "bar 1").await?;
+
+    let path = Path::new(&[b"foo"]).unwrap();
+    let interests = Interests::builder().add_area(namespace, [Area::path(path.clone())]);
+    let init = SessionInit::new(interests, SessionMode::Live);
+    let mut intent = alfie.sync_with_peer(betty_node_id, init).await.unwrap();
+
+    assert_eq!(
+        intent.next().await.unwrap(),
+        EventKind::CapabilityIntersection {
+            namespace,
+            area: Area::full(),
+        }
+    );
+    assert_eq!(
+        intent.next().await.unwrap(),
+        EventKind::InterestIntersection {
+            namespace,
+            area: Area::path(path.clone()).into()
+        }
+    );
+    assert_eq!(
+        intent.next().await.unwrap(),
+        EventKind::Reconciled {
+            namespace,
+            area: Area::path(path.clone()).into()
+        }
+    );
+    assert_eq!(intent.next().await.unwrap(), EventKind::ReconciledAll);
+
+    let path = Path::new(&[b"bar"]).unwrap();
+    let interests = Interests::builder().add_area(namespace, [Area::path(path.clone())]);
+    intent.add_interests(interests).await?;
+
+    assert_eq!(
+        intent.next().await.unwrap(),
+        EventKind::InterestIntersection {
+            namespace,
+            area: Area::path(path.clone()).into()
+        }
+    );
+    assert_eq!(
+        intent.next().await.unwrap(),
+        EventKind::Reconciled {
+            namespace,
+            area: Area::path(path.clone()).into()
+        }
+    );
+
+    assert_eq!(intent.next().await.unwrap(), EventKind::ReconciledAll);
+
+    intent.close().await;
+
+    assert!(intent.next().await.is_none(),);
+
+    [alfie, betty].map(Peer::shutdown).try_join().await?;
+    Ok(())
+}
+
+mod util {
+    use std::sync::{Arc, Mutex};
+
+    use anyhow::Result;
+    use bytes::Bytes;
+    use futures_concurrency::future::TryJoin;
+    use iroh_net::{Endpoint, NodeId};
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha12Rng;
+    use rand_core::CryptoRngCore;
+    use tokio::task::JoinHandle;
+
+    use iroh_willow::{
+        auth::{CapSelector, DelegateTo, RestrictArea},
+        engine::{AcceptOpts, Engine},
+        form::EntryForm,
+        net::ALPN,
+        proto::{
+            keys::{NamespaceId, NamespaceKind, UserId},
+            meadowcap::AccessMode,
+            willow::Path,
+        },
+    };
+
+    pub fn create_rng(seed: &str) -> ChaCha12Rng {
+        let seed = iroh_base::hash::Hash::new(seed);
+        ChaCha12Rng::from_seed(*(seed.as_bytes()))
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Peer {
+        endpoint: Endpoint,
+        engine: Engine,
+        accept_task: Arc<Mutex<Option<JoinHandle<Result<()>>>>>,
+    }
+
+    impl Peer {
+        pub async fn spawn(
+            secret_key: iroh_net::key::SecretKey,
+            accept_opts: AcceptOpts,
+        ) -> Result<Self> {
+            let endpoint = Endpoint::builder()
+                .secret_key(secret_key)
+                .alpns(vec![ALPN.to_vec()])
+                .bind(0)
+                .await?;
+            let payloads = iroh_blobs::store::mem::Store::default();
+            let create_store = move || iroh_willow::store::memory::Store::new(payloads);
+            let engine = Engine::spawn(endpoint.clone(), create_store, accept_opts);
+            let accept_task = tokio::task::spawn({
+                let engine = engine.clone();
+                let endpoint = endpoint.clone();
+                async move {
+                    while let Some(mut conn) = endpoint.accept().await {
+                        let alpn = conn.alpn().await?;
+                        if alpn != ALPN {
+                            continue;
+                        }
+                        let conn = conn.await?;
+                        engine.handle_connection(conn).await?;
+                    }
+                    Result::Ok(())
+                }
+            });
+            Ok(Self {
+                endpoint,
+                engine,
+                accept_task: Arc::new(Mutex::new(Some(accept_task))),
+            })
+        }
+
+        pub async fn shutdown(self) -> Result<()> {
+            let accept_task = self.accept_task.lock().unwrap().take();
+            if let Some(accept_task) = accept_task {
+                accept_task.abort();
+                match accept_task.await {
+                    Err(err) if err.is_cancelled() => {}
+                    Ok(Ok(())) => {}
+                    Err(err) => Err(err)?,
+                    Ok(Err(err)) => Err(err)?,
+                }
+            }
+            self.engine.shutdown().await?;
+            self.endpoint.close(0u8.into(), b"").await?;
+            Ok(())
+        }
+
+        pub fn node_id(&self) -> NodeId {
+            self.endpoint.node_id()
+        }
+    }
+
+    impl std::ops::Deref for Peer {
+        type Target = Engine;
+        fn deref(&self) -> &Self::Target {
+            &self.engine
+        }
+    }
+
+    pub async fn spawn_two(rng: &mut impl CryptoRngCore) -> Result<[Peer; 2]> {
+        let peers = [
+            iroh_net::key::SecretKey::generate_with_rng(rng),
+            iroh_net::key::SecretKey::generate_with_rng(rng),
+        ]
+        .map(|secret_key| Peer::spawn(secret_key, Default::default()))
+        .try_join()
+        .await?;
+
+        peers[0]
+            .endpoint
+            .add_node_addr(peers[1].endpoint.node_addr().await?)?;
+
+        peers[1]
+            .endpoint
+            .add_node_addr(peers[0].endpoint.node_addr().await?)?;
+
+        Ok(peers)
+    }
+
+    pub async fn setup_and_delegate(
+        alfie: &Engine,
+        betty: &Engine,
+    ) -> Result<(NamespaceId, UserId, UserId)> {
+        let user_alfie = alfie.create_user().await?;
+        let user_betty = betty.create_user().await?;
+
+        let namespace_id = alfie
+            .create_namespace(NamespaceKind::Owned, user_alfie)
+            .await?;
+
+        let cap_for_betty = alfie
+            .delegate_caps(
+                CapSelector::widest(namespace_id),
+                AccessMode::ReadWrite,
+                DelegateTo::new(user_betty, RestrictArea::None),
+            )
+            .await?;
+
+        betty.import_caps(cap_for_betty).await?;
+        Ok((namespace_id, user_alfie, user_betty))
+    }
+
+    pub async fn insert(
+        handle: &Engine,
+        namespace_id: NamespaceId,
+        user: UserId,
+        path: &[&[u8]],
+        bytes: impl Into<Bytes>,
+    ) -> Result<()> {
+        let path = Path::new(path)?;
+        let entry = EntryForm::new_bytes(namespace_id, path, bytes);
+        handle.insert(entry, user).await?;
+        Ok(())
+    }
+}
