70deec3c03b829ada9c747601e1e5158c05483c9
Floris Bruynooghe <flub@n0.computer>
2024-08-05 11:06:49 +0200

    clippy

--- iroh-net/src/magicsock/node_map/udp_paths.rs
+++ iroh-net/src/magicsock/node_map/udp_paths.rs
@@ -122,7 +122,7 @@ impl NodeUdpPaths {
                             .filter_map(|path| path.udp_addr())
                             .filter(|addr| addr.is_ipv4() || have_ipv6)
                             .choose(&mut rand::thread_rng());
-                        self.chosen_candidate = addr.clone().map(IpPort::from);
+                        self.chosen_candidate = addr.map(IpPort::from);
                         addr
                     });
                 match addr {
