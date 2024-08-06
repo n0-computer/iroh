cb635eaf8b4d892bf575c27497c9c031bc291528
flub <flub@users.noreply.github.com>
2024-08-06 10:21:54 +0000

    deploy: 43baba84894a107f006babdf695f758258281cc7

---
+++ pr/2595/docs/iroh_docs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["actor","engine","metrics","net","store","sync"],"struct":["Author","AuthorHeads","AuthorId","AuthorPublicKey","DocTicket","NamespaceId","NamespacePublicKey","NamespaceSecret"]};
