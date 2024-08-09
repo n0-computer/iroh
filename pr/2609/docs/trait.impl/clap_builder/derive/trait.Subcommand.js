922ac2b189daafc6f08891da2b3c553621a64f1c
matheus23 <matheus23@users.noreply.github.com>
2024-08-09 13:18:52 +0000

    deploy: 3e747dcfe91f6ed9a0743232f645972d9abe387b

---
+++ pr/2609/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_net_bench":[["impl Subcommand for <a class=\"enum\" href=\"iroh_net_bench/enum.Commands.html\" title=\"enum iroh_net_bench::Commands\">Commands</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
