565c1d7a07fd1aeaee1080539e55efc6f8c5db4f
Ruediger Klaehn <rklaehn@protonmail.com>
2024-08-14 10:53:49 +0300

    Use oneshot channel from the oneshot crate instead of a capacity 1 async_channel
    we can't use the one from tokio bc recv_blocking panics when used in the runtime.

--- iroh-blobs/src/store/fs/test_support.rs
+++ iroh-blobs/src/store/fs/test_support.rs
@@ -7,8 +7,6 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use tokio::sync::oneshot;
-
 use super::{
     tables::{ReadableTables, Tables},
     ActorError, ActorMessage, ActorResult, ActorState, DataLocation, EntryState, FilterPredicate,
@@ -106,29 +104,29 @@ impl Store {
 impl StoreInner {
     #[cfg(test)]
     async fn entry_state(&self, hash: Hash) -> OuterResult<EntryStateResponse> {
-        let (tx, rx) = async_channel::bounded(1);
+        let (tx, rx) = ::oneshot::channel();
         self.tx.send(ActorMessage::EntryState { hash, tx }).await?;
-        Ok(rx.recv().await??)
+        Ok(rx.await??)
     }
 
     async fn set_full_entry_state(&self, hash: Hash, entry: Option<EntryData>) -> OuterResult<()> {
-        let (tx, rx) = async_channel::bounded(1);
+        let (tx, rx) = ::oneshot::channel();
         self.tx
             .send(ActorMessage::SetFullEntryState { hash, entry, tx })
             .await?;
-        Ok(rx.recv().await??)
+        Ok(rx.await??)
     }
 
     async fn get_full_entry_state(&self, hash: Hash) -> OuterResult<Option<EntryData>> {
-        let (tx, rx) = async_channel::bounded(1);
+        let (tx, rx) = ::oneshot::channel();
         self.tx
             .send(ActorMessage::GetFullEntryState { hash, tx })
             .await?;
-        Ok(rx.recv().await??)
+        Ok(rx.await??)
     }
 
     async fn all_blobs(&self) -> OuterResult<Vec<io::Result<Hash>>> {
-        let (tx, rx) = oneshot::channel();
+        let (tx, rx) = ::oneshot::channel();
         let filter: FilterPredicate<Hash, EntryState> =
             Box::new(|_i, k, v| Some((k.value(), v.value())));
         self.tx.send(ActorMessage::Blobs { filter, tx }).await?;
