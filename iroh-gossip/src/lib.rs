10892a85df909fa78c3ccaec6b1658789ebac0b4
Ruediger Klaehn <rklaehn@protonmail.com>
2024-08-16 12:10:01 +0300

    Add a bit more top level docs to iroh-gossip

--- iroh-gossip/src/lib.rs
+++ iroh-gossip/src/lib.rs
@@ -1,5 +1,10 @@
 //! Broadcast messages to peers subscribed to a topic
-
+//!
+//! The crate is designed to be used from the [iroh] crate, which provides a
+//! [high level interface](https://docs.rs/iroh/latest/iroh/client/gossip/index.html),
+//! but can also be used standalone.
+//!
+//! [iroh]: https://docs.rs/iroh
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
 pub mod metrics;
