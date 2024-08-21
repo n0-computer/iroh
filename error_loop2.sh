1f01e1c75303deffa3eec8f50a69f0bbdd7344f1
Franz Heinzmann (Frando) <frando@unbiskant.org>
2024-08-21 15:46:41 +0200

    tests: add connection loop test to maybe find our error

---
+++ error_loop2.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+echo "Building tests..."
+# run once for nice output
+cargo test --release -p iroh-net --test auth-fail || exit 1
+# run again for getting the executable path
+echo "Extract path..."
+executable_path=$(cargo test --release -p iroh-net --test auth-fail 2>&1 | grep "Running" | tail -n 1 | sed -n 's/.*(\(.*\)).*/\1/p')
+echo "Extracted path: $executable_path"
+
+total_runs=100
+failure_count=0
+
+mkdir -p ./logs
+
+for ((counter=1; counter<=total_runs; counter++)); do
+    echo -n "Running tests... Attempt #$counter"
+
+    start_time=$(date +%s%3N)
+    LOGFILE="logs/attempt-$counter.log"
+    RUST_LOG=trace "$executable_path" --nocapture >& $LOGFILE
+    err_code=$?
+    end_time=$(date +%s%3N)
+    duration=$((end_time - start_time))
+
+    echo ", ${duration} ms"
+    echo "\n\nTIME: ${duration}" >> $LOGFILE
+
+    if [ $err_code -ne 0 ]; then
+        failure_count=$((failure_count + 1))
+        echo "$(wc -l $LOGFILE) log line(s), tail:"
+        tail $LOGFILE
+        if grep "failed to auth" $LOGFILE; then
+            cp $LOGFILE logs/failure-$failure_count.log
+            echo "Error detected on attempt #$counter!"
+        else
+            echo "Apparently different error on attempt #$counter."
+        fi
+    fi
+done
+
+# Calculate failure probability
+failure_probability=$(echo "scale=4; $failure_count / $total_runs * 100" | bc)
+
+echo "Test completed: $total_runs runs, $failure_count failures."
+echo "Probability of failure: $failure_probability%"
---
+++ iroh-net/tests/auth-fail.rs
@@ -0,0 +1,207 @@
+use std::time::Instant;
+
+use anyhow::Result;
+use futures_buffered::try_join_all;
+use futures_util::FutureExt;
+use iroh_net::{
+    endpoint::get_remote_node_id,
+    key::SecretKey,
+    relay::{RelayMap, RelayUrl},
+    Endpoint, NodeAddr, NodeId,
+};
+use quinn::ConnectionError;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    task::JoinSet,
+};
+use tracing::{error, error_span, info, Instrument, Span};
+
+const ALPN: &[u8] = b"foo";
+
+/// Test that add_bytes adds the right data
+#[tokio::test(flavor = "multi_thread")]
+async fn auth_fail() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let (relay_map, relay_url, relay_server) = iroh_net::test_utils::run_relay_server().await?;
+
+    let endpoint_count = std::env::var("ENDPOINTS")
+        .map(|x| x.parse().expect("ENDPOINTS must be a number"))
+        .unwrap_or(3);
+
+    let connects_count = std::env::var("CONNECTS")
+        .map(|x| x.parse().expect("CONNECTS must be a number"))
+        .unwrap_or(2);
+    let accepts_count = connects_count * (endpoint_count - 1);
+
+    info!("starting run with {endpoint_count} endpoints and {connects_count} conns/endpoint");
+
+    let endpoints = try_join_all(
+        (0..endpoint_count).map(|i| create_endpoint(i as u8, relay_map.clone(), relay_url.clone())),
+    )
+    .await?;
+    info!("endpoints bound");
+
+    for a in &endpoints {
+        for b in &endpoints {
+            if b.1 == a.1 {
+                continue;
+            }
+            a.0.add_node_addr(b.2.clone())?;
+        }
+    }
+
+    let start = Instant::now();
+    let mut tasks = JoinSet::new();
+
+    for a in &endpoints {
+        let span = error_span!("accept", me = %a.1.fmt_short());
+        let span_clone = span.clone();
+        tasks.spawn(
+            accept_loop(a.0.clone(), accepts_count)
+                .map(move |res| (span, res))
+                .instrument(span_clone),
+        );
+        for b in &endpoints {
+            if b.1 == a.1 {
+                continue;
+            }
+            let span = error_span!("connect", me = %a.1.fmt_short(), remote = %b.1.fmt_short());
+            let span_clone = span.clone();
+            tasks.spawn(
+                connect_loop(a.0.clone(), b.1, connects_count)
+                    .map(move |res| (span, res))
+                    .instrument(span_clone),
+            );
+        }
+    }
+
+    while let Some(res) = tasks.join_next().await {
+        let (span, res) = res.expect("task panicked");
+        let _guard = span.enter();
+        if res.is_err() {
+            error!(?res, "FAILED task");
+        }
+    }
+
+    info!(time=?start.elapsed(), "all tasks finished, shutdown endpoints");
+
+    // drop, we finished everything, saves test time.
+    drop(endpoints);
+    relay_server.shutdown().await?;
+    // this would gracefully shutdown the endpoints instead:
+    // try_join_all(
+    //     endpoints
+    //         .into_iter()
+    //         .map(|(ep, _, _)| ep.close(1u32.into(), b"shutdown")),
+    // )
+    // .await?;
+    // info!(time=?start.elapsed(), "endpoints shutdown");
+
+    Ok(())
+}
+
+async fn create_endpoint(
+    id: u8,
+    relay_map: RelayMap,
+    relay_url: RelayUrl,
+) -> anyhow::Result<(Endpoint, NodeId, NodeAddr)> {
+    let secret_key = SecretKey::from_bytes(&[id; 32]);
+    // let port = 20000 + id as u16;
+    let endpoint = Endpoint::builder()
+        .secret_key(secret_key)
+        .alpns(vec![ALPN.to_vec()])
+        .relay_mode(iroh_net::relay::RelayMode::Custom(relay_map))
+        .insecure_skip_relay_cert_verify(true)
+        .bind(0)
+        .await?;
+    let node_id = endpoint.node_id();
+    let addr = NodeAddr::new(node_id).with_relay_url(relay_url);
+    Ok((endpoint, node_id, addr))
+}
+
+async fn connect_loop(endpoint: Endpoint, node_id: NodeId, count: usize) -> Result<()> {
+    let mut tasks = JoinSet::<(usize, Result<()>)>::new();
+    for i in 0..count {
+        let endpoint = endpoint.clone();
+        let fut = async move {
+            let start = Instant::now();
+            info!("connect");
+            let conn = endpoint.connect_by_node_id(node_id, ALPN).await?;
+            info!("connected");
+            let mut stream = conn.open_uni().await?;
+            stream.write_u8(23).await?;
+            let reason = conn.closed().await;
+            match reason {
+                ConnectionError::ApplicationClosed(reason) if reason.error_code == 42u32.into() => {
+                    info!(?i, time=?start.elapsed(), "closed gracefully");
+                    // conn.close(43u32.into(), b"bye-bye");
+                    Result::<_, anyhow::Error>::Ok(())
+                }
+                _ => {
+                    error!(?i, ?reason, "FAILED at close");
+                    // conn.close(1u32.into(), b"bad-close");
+                    Err(reason.into())
+                }
+            }
+        }
+        .map(move |res| (i, res))
+        .instrument(error_span!("conn", i));
+        tasks.spawn(fut);
+    }
+    while let Some(res) = tasks.join_next().await {
+        let (i, res) = res.expect("connect conn task panicked");
+        if res.is_err() {
+            error!(i, ?res, "FAILED at connect conn");
+        }
+        res?;
+    }
+    Ok(())
+}
+
+async fn accept_loop(endpoint: Endpoint, count: usize) -> Result<()> {
+    let mut tasks = JoinSet::<(usize, Result<()>)>::new();
+    for i in 0..count {
+        let incoming = endpoint
+            .accept()
+            .await
+            .expect("expected incoming connection");
+        let start = Instant::now();
+        tasks.spawn(
+            async move {
+                info!(?i, "incoming");
+                let conn = incoming.accept()?.await?;
+                let remote = get_remote_node_id(&conn)?;
+                Span::current().record("remote", tracing::field::display(&remote.fmt_short()));
+                info!(?i, "accepted");
+                let mut stream = conn.accept_uni().await?;
+                let data = stream.read_u8().await?;
+                assert_eq!(data, 23u8);
+                drop(stream);
+                conn.close(42u32.into(), b"bye");
+                let reason = conn.closed().await;
+                info!(time=?start.elapsed(), "closed");
+                match reason {
+                    ConnectionError::LocallyClosed => {
+                        info!(?i, time=?start.elapsed(), "closed gracefully");
+                        Ok(())
+                    }
+                    _ => {
+                        error!(?i, ?reason, time=?start.elapsed(), "FAILED at close");
+                        Err(reason.into())
+                    }
+                }
+            }
+            .map(move |res| (i, res))
+            .instrument(error_span!("conn", i, remote = tracing::field::Empty)),
+        );
+    }
+    while let Some(res) = tasks.join_next().await {
+        let (i, res) = res.expect("accept conn task panicked");
+        if res.is_err() {
+            error!(i, ?res, "FAILED at accept conn");
+        }
+        res?;
+    }
+
+    Ok(())
+}
