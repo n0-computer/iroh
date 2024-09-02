554856d8e382f02f5238da4757f807ff6ef6f305
matheus23 <matheus23@users.noreply.github.com>
2024-09-02 16:04:10 +0000

    deploy: d0cdb58dc20697126571122b534600315b2a41f3

---
+++ pr/2690/docs/iroh/blobs/downloader/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DownloadError","FailureAction","GetOutput"],"struct":["ConcurrencyLimits","DownloadHandle","DownloadKind","DownloadRequest","Downloader","IntentId","RetryConfig"],"trait":["Dialer","Getter","NeedsConn"]};
