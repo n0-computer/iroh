3f8d2870169335d1a116bf5f1eecf7f748c2c561
matheus23 <matheus23@users.noreply.github.com>
2024-09-04 16:49:23 +0000

    deploy: 7c87458396cdd25a885664ec713bc719cfdf8b90

---
+++ pr/2695/docs/iroh_willow/store/memory/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CapsStore","EntryStore","NamespaceStore","SecretStore","Store"]};
---
+++ pr/2695/docs/trait.impl/willow_data_model/parameters/trait.NamespaceId.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_willow":[["impl NamespaceId for <a class=\"struct\" href=\"iroh_willow/proto/keys/struct.NamespaceId.html\" title=\"struct iroh_willow::proto::keys::NamespaceId\">NamespaceId</a>"],["impl NamespaceId for <a class=\"struct\" href=\"iroh_willow/proto/keys/struct.NamespacePublicKey.html\" title=\"struct iroh_willow::proto::keys::NamespacePublicKey\">NamespacePublicKey</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
