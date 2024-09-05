1115336034abcc132230716f87b09a0640867fe1
Friedel Ziegelmayer <me@dignifiedquire.com>
2024-09-05 10:55:51 +0200

    Update iroh-net/examples/connect.rs
    Co-authored-by: Kasey <kasey@n0.computer>

--- iroh-net/examples/connect.rs
+++ iroh-net/examples/connect.rs
@@ -49,7 +49,7 @@ async fn main() -> anyhow::Result<()> {
         // Use `RelayMode::Disable` to disable holepunching and relaying over HTTPS
         // If you want to experiment with relaying using your own relay server, you must pass in the same custom relay url to both the `listen` code AND the `connect` code
         .relay_mode(RelayMode::Default)
-        // You can choose an addr to bind to, but passing in `None` will bind the socket to a random available port
+        // You can choose an address to bind to, but passing in `None` will bind the socket to a random available port
         .bind(None, None)
         .await?;
 
