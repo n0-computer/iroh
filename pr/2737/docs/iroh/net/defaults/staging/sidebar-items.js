30e2b15472add93283679245ee7db083ae610804
Arqu <Arqu@users.noreply.github.com>
2024-09-18 12:43:07 +0000

    deploy: efbada32570cb03b3e61c32f8a89711cce5047b4

---
+++ pr/2737/docs/iroh/net/defaults/staging/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["EU_RELAY_HOSTNAME","NA_RELAY_HOSTNAME"],"fn":["default_eu_relay_node","default_na_relay_node","default_relay_map"]};
---
+++ pr/2737/docs/iroh_blobs/get/progress/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BlobProgress"],"struct":["BlobState","TransferState"],"type":["ProgressId"]};
