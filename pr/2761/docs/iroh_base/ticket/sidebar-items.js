9a6f5539d3c1d6c8da7c8db576118842dffbcd01
flub <flub@users.noreply.github.com>
2024-09-30 15:44:08 +0000

    deploy: 18dbc76ab38bb0c44d50151e609dc17d598cc284

---
+++ pr/2761/docs/iroh_base/ticket/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"struct":["BlobTicket","NodeTicket"],"trait":["Ticket"]};
---
+++ pr/2761/docs/trait.impl/num_enum/trait.TryFromPrimitive.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_docs":[["impl TryFromPrimitive for <a class=\"enum\" href=\"iroh_docs/sync/enum.CapabilityKind.html\" title=\"enum iroh_docs::sync::CapabilityKind\">CapabilityKind</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
