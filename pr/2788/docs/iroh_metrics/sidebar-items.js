71a7d61dbc415e834fb08397a3df8a71306ace31
Arqu <Arqu@users.noreply.github.com>
2024-10-07 13:53:52 +0000

    deploy: 38b38c0bf691789d14c08155198476710de1bc47

--- pr/2788/docs/iroh_metrics/sidebar-items.js
+++ pr/2788/docs/iroh_metrics/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["parse_prometheus_metrics","report_usage_stats"],"macro":["inc","inc_by","set"],"mod":["core","metrics","service"]};
+window.SIDEBAR_ITEMS = {"fn":["parse_prometheus_metrics","report_usage_stats"],"macro":["inc","inc_by","set"],"mod":["core","metrics"],"struct":["PushMetricsConfig"]};
