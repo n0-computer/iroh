a7b0cc8933b9f20d75444ec12db1da3a7270a0c5
flub <flub@users.noreply.github.com>
2024-10-17 13:50:42 +0000

    deploy: a66c5e30f6df780bfaa2de7fd5382301c92b1dc8

---
+++ pr/2811/docs/trait.impl/iroh_blobs/format/collection/trait.SimpleStore.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh":[["impl <a class=\"trait\" href=\"iroh/blobs/format/collection/trait.SimpleStore.html\" title=\"trait iroh::blobs::format::collection::SimpleStore\">SimpleStore</a> for <a class=\"struct\" href=\"iroh/client/blobs/struct.Client.html\" title=\"struct iroh::client::blobs::Client\">Client</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
