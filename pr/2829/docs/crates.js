59336a81fc651fa269e5dbb1f820de257fea4a41
rklaehn <rklaehn@users.noreply.github.com>
2024-10-23 12:47:30 +0000

    deploy: 8266b3561f6dc04d0b82605277b0d8bc11b90f2b

--- pr/2829/docs/crates.js
+++ pr/2829/docs/crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["bulk","iroh","iroh_base","iroh_dns_server","iroh_docs","iroh_metrics","iroh_net","iroh_net_bench","iroh_relay","iroh_test"];
+window.ALL_CRATES = ["bulk","iroh","iroh_base","iroh_dns_server","iroh_metrics","iroh_net","iroh_net_bench","iroh_relay","iroh_test"];
