5405a99eadf8d1316a59e11116d5702d55781630
Arqu <Arqu@users.noreply.github.com>
2024-10-28 13:04:36 +0000

    deploy: 941e723a2da3104777c7f444cd3046ee46520e92

---
+++ pr/2851/docs/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_relay":[["impl ValueEnum for <a class=\"enum\" href=\"iroh_relay/enum.CertMode.html\" title=\"enum iroh_relay::CertMode\">CertMode</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
