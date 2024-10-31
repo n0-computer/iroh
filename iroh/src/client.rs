03b617876862b8c1c2cef99d81a5332a00c6d17d
Ruediger Klaehn <rklaehn@protonmail.com>
2024-10-31 12:40:27 +0200

    fmt

--- iroh/src/client.rs
+++ iroh/src/client.rs
@@ -21,8 +21,7 @@ pub use self::{docs::Doc, net::NodeStatus};
 pub mod authors;
 pub mod docs;
 pub mod net;
-pub use iroh_blobs::rpc::client::blobs;
-pub use iroh_blobs::rpc::client::tags;
+pub use iroh_blobs::rpc::client::{blobs, tags};
 
 // Keep this type exposed, otherwise every occurrence of `RpcClient` in the API
 // will show up as `RpcClient<RpcService, Connection<RpcService>>` in the docs.
