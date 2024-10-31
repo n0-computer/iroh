fd467ceab5bcb445de860a77be21a9b56e6ccd5f
Diva M <divma@protonmail.com>
2024-10-31 11:41:42 -0500

    remove MaybeFuture

--- iroh-relay/src/util.rs
+++ iroh-relay/src/util.rs
@@ -1,9 +1,3 @@
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
-
 pub(crate) mod chain {
     //! IO utility to chain `AsyncRead`s together.
 
@@ -126,31 +120,6 @@ pub(crate) mod chain {
             } else {
                 me.second.consume(amt)
             }
-        }
-    }
-}
-
-/// Resolves to pending if the inner is `None`.
-#[derive(Debug)]
-pub(crate) struct MaybeFuture<T> {
-    /// Future to be polled.
-    pub inner: Option<T>,
-}
-
-// NOTE: explicit implementation to bypass derive unnecessary bounds
-impl<T> Default for MaybeFuture<T> {
-    fn default() -> Self {
-        MaybeFuture { inner: None }
-    }
-}
-
-impl<T: Future + Unpin> Future for MaybeFuture<T> {
-    type Output = T::Output;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        match self.inner {
-            Some(ref mut t) => Pin::new(t).poll(cx),
-            None => Poll::Pending,
         }
     }
 }
