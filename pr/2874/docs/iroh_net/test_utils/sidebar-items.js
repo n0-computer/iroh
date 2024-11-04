442d6102e676449df63591a0d5941e97c1c2226f
rklaehn <rklaehn@users.noreply.github.com>
2024-11-04 15:15:21 +0000

    deploy: 19c4f55ce9a4167ebb6f057d29e038c2ffd0d5d6

--- pr/2874/docs/iroh_net/test_utils/sidebar-items.js
+++ pr/2874/docs/iroh_net/test_utils/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_dns_resolver","run_relay_server"],"struct":["CleanupDropGuard","DnsPkarrServer"]};
+window.SIDEBAR_ITEMS = {"fn":["create_dns_resolver","run_relay_server","run_relay_server_with"],"struct":["CleanupDropGuard","DnsPkarrServer"]};
