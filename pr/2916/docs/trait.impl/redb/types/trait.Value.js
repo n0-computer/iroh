17f8f9f2727af8d21cce4fe9d5a8bb99b55df36e
Arqu <Arqu@users.noreply.github.com>
2024-11-11 11:01:11 +0000

    deploy: 6588f3f63306a2caa2926153c5d820cc5c7145dc

---
+++ pr/2916/docs/trait.impl/redb/types/trait.Value.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_base":[["impl Value for <a class=\"struct\" href=\"iroh_base/hash/struct.Hash.html\" title=\"struct iroh_base::hash::Hash\">Hash</a>"],["impl Value for <a class=\"struct\" href=\"iroh_base/hash/struct.HashAndFormat.html\" title=\"struct iroh_base::hash::HashAndFormat\">HashAndFormat</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
