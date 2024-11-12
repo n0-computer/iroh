dc2387a0b2fb283c97f8a8116e0683fbb8c4bbac
Diva M <divma@protonmail.com>
2024-11-12 00:04:23 -0500

    improve crate docs based on README plus existing credits

--- iroh-relay/src/lib.rs
+++ iroh-relay/src/lib.rs
@@ -1,11 +1,23 @@
-//! Package `relay` implements a revised version of the Designated Encrypted Relay for Packets
-//! (DERP) protocol written by Tailscale.
-//
-//! The relay routes packets to clients using curve25519 keys as addresses.
-//
-//! The relay is used to proxy encrypted QUIC packets through the relay servers when a direct path
-//! cannot be found or opened. The relay is a last resort. If both sides have very aggressive NATs,
-//! or firewalls, or no IPv6, we use the relay connection. Based on tailscale/derp/derp.go
+//! Iroh's relay is a feature within [iroh](https://github.com/n0-computer/iroh), a peer-to-peer
+//! networking system designed to facilitate direct, encrypted connections between devices. Iroh
+//! aims to simplify decentralized communication by automatically handling connections through
+//! "relays" when direct connections aren't immediately possible. The relay server helps establish
+//! connections by temporarily routing encrypted traffic until a direct, P2P connection is
+//! feasible. Once this direct path is set up, the relay server steps back, and the data flows
+//! directly between devices. This approach allows Iroh to maintain a secure, low-latency
+//! connection, even in challenging network situations.
+//!
+//! This crate provides a complete setup for creating and interacting with iroh relays, including:
+//! - [`protos::relay`]: The protocol used to communicate between relay servers and clients. It's a
+//!   revised version of the Designated Encrypted Relay for Packets (DERP) protocol written by
+//!   Tailscale.
+//! - [`server`]: A fully-fledged iroh-relay server over HTTP or HTTPS. Optionally will also
+//!   expose a stun endpoint and metrics.
+//! - [`client`]: A client for establishing connections to the relay.
+//! - *Server Binary*: A CLI for running your own relay server. It can be configured to also offer
+//!   STUN support and expose metrics.
+//!
+//! Based on tailscale/derp/derp.go
 
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
 
