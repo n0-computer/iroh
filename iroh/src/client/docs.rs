76fe9a9cfafc952bc8d268e91e0985e1da373138
Philipp Krüger <philipp.krueger1@gmail.com>
2024-11-13 18:39:30 +0100

    Revert all remaining changes to `iroh/**` files

--- iroh/src/client/docs.rs
+++ iroh/src/client/docs.rs
@@ -753,9 +753,6 @@ mod tests {
 
     use super::*;
 
-    // TODO(Frando): This fails consistently with timeout, the task never joins.
-    // Something in the addition of willow engine to the node makes this
-    // test timeout - debugging outstanding.
     #[tokio::test]
     async fn test_drop_doc_client_sync() -> Result<()> {
         let _guard = iroh_test::logging::setup();
@@ -766,17 +763,12 @@ mod tests {
         let doc = client.docs().create().await?;
 
         let res = std::thread::spawn(move || {
-            println!("now drop doc");
             drop(doc);
-            println!("now drop node");
             drop(node);
-            println!("done");
         });
 
-        println!("wait task");
         tokio::task::spawn_blocking(move || res.join().map_err(|e| anyhow::anyhow!("{:?}", e)))
             .await??;
-        println!("task done");
 
         Ok(())
     }
