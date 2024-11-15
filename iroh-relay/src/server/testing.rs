bc486ff7b68485fe9981431fac4fedbb26939b47
Diva M <divma@protonmail.com>
2024-11-15 16:41:49 -0500

    fix merge

--- iroh-relay/src/server/testing.rs
+++ iroh-relay/src/server/testing.rs
@@ -20,12 +20,10 @@ pub fn tls_config() -> TlsConfig<()> {
     let cert =
         rcgen::generate_simple_self_signed(vec!["localhost".to_string(), "127.0.0.1".to_string()])
             .expect("valid");
-    let certs = vec![rustls::pki_types::CertificateDer::from(
-        cert.serialize_der().expect("valid cert"),
-    )];
-    let private_key =
-        rustls::pki_types::PrivatePkcs8KeyDer::from(cert.get_key_pair().serialize_der());
+    let rustls_cert = cert.cert.der();
+    let private_key = rustls::pki_types::PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
     let private_key = rustls::pki_types::PrivateKeyDer::from(private_key);
+    let certs = vec![rustls_cert.clone()];
     TlsConfig {
         cert: CertConfig::<(), ()>::Manual { private_key, certs },
         https_bind_addr: (Ipv4Addr::LOCALHOST, 0).into(),
