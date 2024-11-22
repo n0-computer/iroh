b0566e92971adfdd4fa39df3901d6201cf5c7fbc
flub <flub@users.noreply.github.com>
2024-11-22 15:34:21 +0000

    deploy: 0787b129de48154b5f70eab8084bd2e530f53a4e

---
+++ pr/2961/docs/iroh_net/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["defaults","dialer","discovery","dns","endpoint","key","metrics","test_utils","ticket","tls"],"struct":["AddrInfo","NodeAddr","RelayMap","RelayNode","RelayUrl"],"type":["NodeId"]};
