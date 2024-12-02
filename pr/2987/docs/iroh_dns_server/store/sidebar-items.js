e924e0f73bb5d851eb816b3b08b3362aca6e9cbc
Arqu <Arqu@users.noreply.github.com>
2024-12-02 13:40:54 +0000

    deploy: 60acb30d959dd375b95d3dfed8510b5cb2dae7a5

---
+++ pr/2987/docs/iroh_dns_server/store/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_CACHE_CAPACITY","DHT_CACHE_TTL"],"enum":["PacketSource"],"struct":["ZoneStore"]};
---
+++ pr/2987/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_node_util":[["impl Service for <a class=\"struct\" href=\"iroh_node_util/rpc/proto/struct.RpcService.html\" title=\"struct iroh_node_util::rpc::proto::RpcService\">RpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
