003d1ad2486351d623efb66acee9d6a6796d7573
flub <flub@users.noreply.github.com>
2024-12-05 09:35:36 +0000

    deploy: 2593a99fd83b920a1ef4382cf9aa3ff8e77a3674

---
+++ pr/3012/docs/trait.impl/iroh_metrics/core/trait.Metric.js
@@ -0,0 +1,6 @@
+(function() {var implementors = {
+"iroh":[["impl Metric for <a class=\"struct\" href=\"iroh/metrics/struct.MagicsockMetrics.html\" title=\"struct iroh::metrics::MagicsockMetrics\">Metrics</a>"]],
+"iroh_dns_server":[["impl Metric for <a class=\"struct\" href=\"iroh_dns_server/metrics/struct.Metrics.html\" title=\"struct iroh_dns_server::metrics::Metrics\">Metrics</a>"]],
+"iroh_net_report":[["impl Metric for <a class=\"struct\" href=\"iroh_net_report/struct.Metrics.html\" title=\"struct iroh_net_report::Metrics\">Metrics</a>"]],
+"iroh_relay":[["impl Metric for <a class=\"struct\" href=\"iroh_relay/server/struct.Metrics.html\" title=\"struct iroh_relay::server::Metrics\">Metrics</a>"],["impl Metric for <a class=\"struct\" href=\"iroh_relay/server/struct.StunMetrics.html\" title=\"struct iroh_relay::server::StunMetrics\">StunMetrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
