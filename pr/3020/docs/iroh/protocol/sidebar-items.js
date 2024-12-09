f8b4fc447a8975d80ba2a28eea74b7f81768228e
rklaehn <rklaehn@users.noreply.github.com>
2024-12-09 09:03:23 +0000

    deploy: 1883e2234850a554094994191fdfacb26a0859ca

---
+++ pr/3020/docs/iroh/protocol/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["Router","RouterBuilder"],"trait":["ProtocolHandler","UnboxedProtocolHandler"]};
---
+++ pr/3020/docs/iroh/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["AddrInfoOptions"],"mod":["defaults","dialer","discovery","dns","endpoint","hash","key","metrics","protocol","test_utils","ticket","tls"],"struct":["AddrInfo","NodeAddr","RelayMap","RelayNode","RelayUrl"],"type":["NodeId"]};
