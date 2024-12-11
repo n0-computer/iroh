edaf2b18a1a34c97d128e33753e6d4f76570e7ae
ramfox <ramfox@users.noreply.github.com>
2024-12-11 05:13:18 +0000

    deploy: 5adb25dea6c69f5ec43997707b5b5848fadc992f

---
+++ pr/3028/docs/trait.impl/iroh/dns/trait.ResolverExt.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
