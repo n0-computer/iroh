c37895bfccb716d44a0e23f997555f0689e9b5a9
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2024-12-12 16:02:26 +0100

    feat(relay): reloading certificate resolver (#2999)
    ## Description
    
    This sets us up so we can have manually/externally managed certs that
    will reload on a daily basis. This should close
    https://github.com/n0-computer/iroh/issues/1108 and we should follow up
    on the ops side to utilize this where needed.
    
    Ok, this turned out to be much grosser than expected. 
    - I don't like the new dependencies we introduce
    - I don't like the loader setup
    - I'm unsure whether to keep the reloading resolver in the `iroh` repo
    or move it into `tokio-rustls-acme` (think it should move over, but for
    the sake of this discussion, included it here)
    - Should I do anything with the `_handle` for the reloader in regards to
    shutting down?
    
    Most of this still feels necessary. I could maybe feature flag it?
    I had a really hard time trying to extract the loader creation into a
    standalone func which also aligns all the traits and error types so the
    thing compiles.
    
    Suggestions are welcome :)
    
    Example config:
    ```
    enable_relay = true
    http_bind_addr = "[::]:80"
    enable_stun = true
    stun_bind_addr = "[::]:3478"
    enable_metrics = true
    metrics_addr = "127.0.0.1:9090"
    
    [tls]
    manual_cert_path="certificate.der"
    manual_key_path="private_key.der"
    cert_mode = "Reloading"
    ```
    
    ## Breaking Changes
    
    <!-- Optional, if there are any breaking changes document them,
    including how to migrate older code. -->
    
    ## Notes & open questions
    
    <!-- Any notes, remarks or open questions you have to make about the PR.
    -->
    
    ## Change checklist
    
    - [ ] Self-review.
    - [ ] Documentation updates following the [style
    guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text),
    if relevant.
    - [ ] Tests if relevant.
    - [ ] All breaking changes documented.

---
+++ iroh-relay/src/server/resolver.rs
@@ -0,0 +1,93 @@
+use std::{sync::Arc, time::Duration};
+
+use anyhow::{anyhow, Result};
+use reloadable_state::Reloadable;
+use rustls::{
+    server::{ClientHello, ResolvesServerCert},
+    sign::CertifiedKey,
+};
+use tokio_util::{sync::CancellationToken, task::AbortOnDropHandle};
+
+/// The default certificate reload interval.
+pub const DEFAULT_CERT_RELOAD_INTERVAL: u64 = 60 * 60 * 24;
+
+/// A Certificate resolver that reloads the certificate every interval
+#[derive(Debug)]
+pub struct ReloadingResolver<Loader: Send + 'static> {
+    /// The inner reloadable value.
+    reloadable: Arc<Reloadable<CertifiedKey, Loader>>,
+    /// The handle to the task that reloads the certificate.
+    _handle: AbortOnDropHandle<()>,
+    /// Cancel token to shutdown the resolver.
+    cancel_token: CancellationToken,
+}
+
+impl<Loader> ReloadingResolver<Loader>
+where
+    Loader: Send + reloadable_state::core::Loader<Value = CertifiedKey> + 'static,
+{
+    /// Perform the initial load and construct the [`ReloadingResolver`].
+    pub async fn init(loader: Loader, interval: Duration) -> Result<Self> {
+        let (reloadable, _) = Reloadable::init_load(loader)
+            .await
+            .map_err(|_| anyhow!("Failed to load the certificate"))?;
+        let reloadable = Arc::new(reloadable);
+
+        let cancel_token = CancellationToken::new();
+
+        // Spawn a task to reload the certificate every interval.
+        let _reloadable = reloadable.clone();
+        let _cancel_token = cancel_token.clone();
+        let _handle = tokio::spawn(async move {
+            let mut interval = tokio::time::interval(interval);
+            loop {
+                tokio::select! {
+                    _ = interval.tick() => {
+                        let _ = _reloadable.reload().await;
+                        tracing::info!("Reloaded the certificate");
+                    },
+                    _ = _cancel_token.cancelled() => {
+                        tracing::trace!("shutting down");
+                        break;
+                    }
+                }
+            }
+        });
+        let _handle = AbortOnDropHandle::new(_handle);
+
+        Ok(Self {
+            reloadable,
+            _handle,
+            cancel_token,
+        })
+    }
+
+    /// Shutdown the resolver.
+    pub fn shutdown(self) {
+        self.cancel_token.cancel();
+    }
+
+    /// Reload the certificate.
+    pub async fn reload(&self) {
+        let _ = self.reloadable.reload().await;
+    }
+}
+
+impl<Loader> ResolvesServerCert for ReloadingResolver<Loader>
+where
+    Loader: reloadable_state::core::Loader<Value = CertifiedKey>,
+    Loader: Send,
+    Loader: std::fmt::Debug,
+{
+    fn resolve(&self, _client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
+        Some(self.reloadable.get())
+    }
+}
+
+impl<Loader: Send> std::ops::Deref for ReloadingResolver<Loader> {
+    type Target = Reloadable<CertifiedKey, Loader>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.reloadable
+    }
+}
