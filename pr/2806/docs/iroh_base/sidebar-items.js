8637a46a9a746b80f615943ab7d764bfc5b0cc98
matheus23 <matheus23@users.noreply.github.com>
2024-12-12 15:10:28 +0000

    deploy: 3235552df7d4e4bd937b28445eabef7fdf7c8f0f

--- pr/2806/docs/iroh_base/sidebar-items.js
+++ pr/2806/docs/iroh_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["PUBLIC_KEY_LENGTH"],"enum":["BlobFormat","KeyParsingError"],"mod":["base32","ticket"],"struct":["Hash","HashAndFormat","NodeAddr","PublicKey","RelayUrl","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
+window.SIDEBAR_ITEMS = {"constant":["PUBLIC_KEY_LENGTH"],"enum":["KeyParsingError"],"mod":["base32","ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
