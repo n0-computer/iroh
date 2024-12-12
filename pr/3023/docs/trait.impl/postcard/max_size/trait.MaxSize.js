288e72989b63ed34e008459ddba5db08286b2d7a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-12 10:19:42 +0000

    deploy: dee2ceb837e262585b3bf75f476c19a6e4ffdbae

--- pr/3023/docs/trait.impl/postcard/max_size/trait.MaxSize.js
+++ pr/3023/docs/trait.impl/postcard/max_size/trait.MaxSize.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"iroh_base":[["impl MaxSize for <a class=\"enum\" href=\"iroh_base/enum.BlobFormat.html\" title=\"enum iroh_base::BlobFormat\">BlobFormat</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/struct.Hash.html\" title=\"struct iroh_base::Hash\">Hash</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/struct.HashAndFormat.html\" title=\"struct iroh_base::HashAndFormat\">HashAndFormat</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["iroh_base",[["impl MaxSize for <a class=\"enum\" href=\"iroh_base/enum.BlobFormat.html\" title=\"enum iroh_base::BlobFormat\">BlobFormat</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/struct.Hash.html\" title=\"struct iroh_base::Hash\">Hash</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/struct.HashAndFormat.html\" title=\"struct iroh_base::HashAndFormat\">HashAndFormat</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[411]}
