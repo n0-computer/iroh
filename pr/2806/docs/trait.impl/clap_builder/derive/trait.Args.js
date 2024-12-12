eda15ae0ad082f5bdf43c98cfebcdec5c285304e
matheus23 <matheus23@users.noreply.github.com>
2024-12-12 08:37:12 +0000

    deploy: 13fe53e7e95db067fa3b0e1d82f1aad06c5afe48

---
+++ pr/2806/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_bench":[["impl Args for <a class=\"struct\" href=\"iroh_bench/s2n/struct.Opt.html\" title=\"struct iroh_bench::s2n::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"iroh_bench/struct.Opt.html\" title=\"struct iroh_bench::Opt\">Opt</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
