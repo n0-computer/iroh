20c27d46187392fe7432622e3337ab79ea443677
flub <flub@users.noreply.github.com>
2024-12-12 13:45:36 +0000

    deploy: 0e51ddfb4525406a6973e52f39b085b309aa4be4

--- pr/3027/docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
+++ pr/3027/docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"iroh_bench":[["impl CommandFactory for <a class=\"enum\" href=\"iroh_bench/enum.Commands.html\" title=\"enum iroh_bench::Commands\">Commands</a>"],["impl CommandFactory for <a class=\"struct\" href=\"iroh_bench/s2n/struct.Opt.html\" title=\"struct iroh_bench::s2n::Opt\">Opt</a>"],["impl CommandFactory for <a class=\"struct\" href=\"iroh_bench/struct.Opt.html\" title=\"struct iroh_bench::Opt\">Opt</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["iroh_bench",[["impl CommandFactory for <a class=\"enum\" href=\"iroh_bench/enum.Commands.html\" title=\"enum iroh_bench::Commands\">Commands</a>"],["impl CommandFactory for <a class=\"struct\" href=\"iroh_bench/s2n/struct.Opt.html\" title=\"struct iroh_bench::s2n::Opt\">Opt</a>"],["impl CommandFactory for <a class=\"struct\" href=\"iroh_bench/struct.Opt.html\" title=\"struct iroh_bench::Opt\">Opt</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[409]}
--- pr/3027/docs/trait.impl/futures_core/stream/trait.Stream.js
+++ pr/3027/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"iroh":[["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.ConnectionTypeStream.html\" title=\"struct iroh::endpoint::ConnectionTypeStream\">ConnectionTypeStream</a>"],["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.DirectAddrsStream.html\" title=\"struct iroh::endpoint::DirectAddrsStream\">DirectAddrsStream</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.ConnectionTypeStream.html\" title=\"struct iroh::endpoint::ConnectionTypeStream\">ConnectionTypeStream</a>"],["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.DirectAddrsStream.html\" title=\"struct iroh::endpoint::DirectAddrsStream\">DirectAddrsStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[351]}
