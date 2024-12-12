272bb62e1d2f5233615de4ff332e28f1296697b9
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-12 10:17:48 +0000

    deploy: 2afb8e389b77e9a25676574eb6fe67d9ce99dba9

---
+++ pr/3034/docs/iroh/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["defaults","discovery","dns","endpoint","hash","key","metrics","protocol","test_utils","ticket"],"struct":["NodeAddr","RelayMap","RelayNode","RelayUrl"],"type":["NodeId"]};
---
+++ pr/3034/docs/trait.impl/postcard/max_size/trait.MaxSize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_base",[["impl MaxSize for <a class=\"enum\" href=\"iroh_base/hash/enum.BlobFormat.html\" title=\"enum iroh_base::hash::BlobFormat\">BlobFormat</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/hash/struct.Hash.html\" title=\"struct iroh_base::hash::Hash\">Hash</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_base/hash/struct.HashAndFormat.html\" title=\"struct iroh_base::hash::HashAndFormat\">HashAndFormat</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[444]}
