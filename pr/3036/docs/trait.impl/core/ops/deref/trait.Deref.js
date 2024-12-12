70a744df2ebb9db29cbef5a9c30b5ea1934383b3
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-12 11:00:15 +0000

    deploy: f78c3ad10e2fa611a91e867639a5300c5ec890a7

---
+++ pr/3036/docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_base",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"iroh_base/struct.RelayUrl.html\" title=\"struct iroh_base::RelayUrl\">RelayUrl</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[282]}
