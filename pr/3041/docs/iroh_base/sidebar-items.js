6dfe28424b5baf57470cffcc1ab4993f83089d9b
flub <flub@users.noreply.github.com>
2024-12-13 15:32:35 +0000

    deploy: 82ba68f0cf6758dea57c80fd2263a0fba6e90481

---
+++ pr/3041/docs/iroh_base/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["PUBLIC_KEY_LENGTH"],"enum":["KeyParsingError"],"mod":["base32","ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
