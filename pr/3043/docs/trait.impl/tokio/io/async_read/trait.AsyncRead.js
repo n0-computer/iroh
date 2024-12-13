56b0430c6ba9a7d719a6e8d2bf6c93c272a4f9e9
rklaehn <rklaehn@users.noreply.github.com>
2024-12-13 16:29:07 +0000

    deploy: 6072bc678e82b0c53a910ad739f3b03f63c0bdc4

---
+++ pr/3043/docs/trait.impl/tokio/io/async_read/trait.AsyncRead.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl AsyncRead for <a class=\"enum\" href=\"iroh_relay/server/enum.MaybeTlsStreamServer.html\" title=\"enum iroh_relay::server::MaybeTlsStreamServer\">MaybeTlsStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[190]}
