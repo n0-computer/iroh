47ab9b6610bc70ed6b2b6408e1bf9bb7d389f726
ramfox <ramfox@users.noreply.github.com>
2024-12-14 03:06:28 +0000

    deploy: 330c40a31e14b733cb13d17df186ac00d8b559b0

--- pr/3032/docs/iroh_base/sidebar-items.js
+++ pr/3032/docs/iroh_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["base32","hash","key","node_addr","relay_map","ticket"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
--- pr/3032/docs/trait.impl/futures_core/stream/trait.Stream.js
+++ pr/3032/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"iroh":[["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.ConnectionTypeStream.html\" title=\"struct iroh::endpoint::ConnectionTypeStream\">ConnectionTypeStream</a>"],["impl Stream for <a class=\"struct\" href=\"iroh/endpoint/struct.DirectAddrsStream.html\" title=\"struct iroh::endpoint::DirectAddrsStream\">DirectAddrsStream</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; Stream for <a class=\"struct\" href=\"iroh/watchable/struct.WatcherStream.html\" title=\"struct iroh::watchable::WatcherStream\">WatcherStream</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[444]}
