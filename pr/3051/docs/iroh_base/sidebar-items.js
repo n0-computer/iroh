0eea0c282563f796295a609657c69c6502c1b122
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-12-16 11:36:47 +0000

    deploy: 763280d29f98b31e8bff32ec40ac503d3df08870

--- pr/3051/docs/iroh_base/sidebar-items.js
+++ pr/3051/docs/iroh_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature"],"type":["NodeId"]};
