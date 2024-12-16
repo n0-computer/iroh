0b7dd943be7b9f59b6de1b0c76527be51d95f852
flub <flub@users.noreply.github.com>
2024-12-16 11:34:32 +0000

    deploy: ccd4215cf698dd22d8ed9d4b4ce614290873ac33

---
+++ pr/3052/docs/iroh_base/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DecryptionError","KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","SharedSecret","Signature"],"type":["NodeId"]};
