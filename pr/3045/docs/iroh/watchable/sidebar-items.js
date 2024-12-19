1cdd7518f18bd6b7a5c0e898375cf2ba08469eec
matheus23 <matheus23@users.noreply.github.com>
2024-12-19 10:24:35 +0000

    deploy: 9f3997f9bc8d73d80c295c8bdb37446c563e9e62

---
+++ pr/3045/docs/iroh/watchable/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["DirectWatcher","Disconnected","MapWatcher","OrWatcher","WatchInitializedFut","WatchNextFut","Watchable","WatcherStream"],"trait":["Watcher"]};
---
+++ pr/3045/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl&lt;W: <a class=\"trait\" href=\"iroh/watchable/trait.Watcher.html\" title=\"trait iroh::watchable::Watcher\">Watcher</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>&gt; Stream for <a class=\"struct\" href=\"iroh/watchable/struct.WatcherStream.html\" title=\"struct iroh::watchable::WatcherStream\">WatcherStream</a>&lt;W&gt;<div class=\"where\">where\n    W::<a class=\"associatedtype\" href=\"iroh/watchable/trait.Watcher.html#associatedtype.Value\" title=\"type iroh::watchable::Watcher::Value\">Value</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[767]}
