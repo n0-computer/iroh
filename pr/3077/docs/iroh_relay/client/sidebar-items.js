8383a02882e2f0aef6fc07a8abafb8285eaa829f
flub <flub@users.noreply.github.com>
2024-12-28 21:49:20 +0000

    deploy: 64597c52d824e05ba3023c011101ab12eb24eba5

---
+++ pr/3077/docs/iroh_relay/client/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ClientError","ConnSendError","ReceivedMessage","SendMessage"],"fn":["make_dangerous_client_config"],"struct":["Client","ClientBuilder","ClientSink","ClientStream","ConnectedClient"]};
