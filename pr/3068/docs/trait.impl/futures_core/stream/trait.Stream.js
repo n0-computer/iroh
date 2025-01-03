4be0bad3d4d8ea7f5ce33754c440e87bd4677cba
flub <flub@users.noreply.github.com>
2025-01-03 10:30:26 +0000

    deploy: 9889a0bd2f0417d5412373211173edb714bfb9d2

--- pr/3068/docs/trait.impl/futures_core/stream/trait.Stream.js
+++ pr/3068/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; Stream for <a class=\"struct\" href=\"iroh/watchable/struct.WatcherStream.html\" title=\"struct iroh::watchable::WatcherStream\">WatcherStream</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>&gt; Stream for <a class=\"struct\" href=\"iroh/watchable/struct.WatcherStream.html\" title=\"struct iroh::watchable::WatcherStream\">WatcherStream</a>&lt;T&gt;"]]],["iroh_relay",[["impl Stream for <a class=\"struct\" href=\"iroh_relay/client/struct.Client.html\" title=\"struct iroh_relay::client::Client\">Client</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_relay/client/struct.ClientStream.html\" title=\"struct iroh_relay::client::ClientStream\">ClientStream</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[444]}
+//{"start":57,"fragment_lengths":[444,317]}
