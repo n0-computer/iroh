1cf77a31edf46f2d790d60370b071ed5c844cd99
flub <flub@users.noreply.github.com>
2025-01-03 08:29:31 +0000

    deploy: 7fe3deefe6c823952ec9ea612e3ec811d3b3083f

---
+++ pr/3088/docs/trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"iroh/dns/node_info/enum.IrohAttr.html\" title=\"enum iroh::dns::node_info::IrohAttr\">IrohAttr</a>"]]],["iroh_base",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_base/struct.PublicKey.html\" title=\"struct iroh_base::PublicKey\">PublicKey</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[390,385]}
