9f59584565f4af3f67a705a6bcb879ae75efee82
ramfox <ramfox@users.noreply.github.com>
2025-01-24 13:55:33 +0000

    deploy: c4923bafdc8bbd4b0dc536c3cab9b75c3f37f438

--- pr/3049/docs/trait.impl/core/ops/drop/trait.Drop.js
+++ pr/3049/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh/discovery/pkarr/struct.PkarrPublisher.html\" title=\"struct iroh::discovery::pkarr::PkarrPublisher\">PkarrPublisher</a>"]]],["iroh_test",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_test/struct.CallOnDrop.html\" title=\"struct iroh_test::CallOnDrop\">CallOnDrop</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh/discovery/pkarr/struct.PkarrPublisher.html\" title=\"struct iroh::discovery::pkarr::PkarrPublisher\">PkarrPublisher</a>"]]],["iroh_relay",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_relay/quic/struct.QuicClient.html\" title=\"struct iroh_relay::quic::QuicClient\">QuicClient</a>"]]],["iroh_test",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_test/struct.CallOnDrop.html\" title=\"struct iroh_test::CallOnDrop\">CallOnDrop</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[314,284]}
+//{"start":57,"fragment_lengths":[314,298,284]}
