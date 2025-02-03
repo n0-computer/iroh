22234fec7bd60b44353fe3da276f69bdb89d2d74
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-02-03 17:16:39 +0000

    deploy: e33e916ba9f2146dd339058a6b29a32bfe8c3e41

--- pr/2937/docs/iroh/sidebar-items.js
+++ pr/2937/docs/iroh/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["KeyParsingError","TlsAuthentication"],"mod":["defaults","discovery","dns","endpoint","metrics","protocol","test_utils","watchable"],"struct":["NodeAddr","PublicKey","RelayMap","RelayNode","RelayUrl","RelayUrlParseError","SecretKey"],"type":["NodeId"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["defaults","discovery","dns","endpoint","metrics","protocol","test_utils","watchable"],"struct":["NodeAddr","PublicKey","RelayMap","RelayNode","RelayUrl","RelayUrlParseError","SecretKey"],"type":["NodeId"]};
