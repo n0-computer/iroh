5eacb2428deb5e5d5a39ff13faed87e76fe8d4d7
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-02-11 09:25:03 +0000

    deploy: 8574c08d65e49800e9ab55954f7b4f0bb9cb94bd

--- pr/2937/docs/trait.impl/iroh_relay/dns/node_info/trait.NodeIdExt.js
+++ pr/2937/docs/trait.impl/iroh_relay/dns/node_info/trait.NodeIdExt.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[]]]);
+    var implementors = Object.fromEntries([["iroh",[]],["iroh_relay",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11]}
+//{"start":57,"fragment_lengths":[11,18]}
