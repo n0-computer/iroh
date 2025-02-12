9d6bbc8576bcae9347e589ded0b2a476715e97fb
Philipp Krüger <philipp.krueger1@gmail.com>
2025-02-12 15:47:03 +0100

    Codespell

--- iroh/src/endpoint.rs
+++ iroh/src/endpoint.rs
@@ -714,7 +714,7 @@ impl Endpoint {
         // have 63 maximum per DNS subdomain. Base32 is the next best alternative.
         // We use the `.invalid` TLD, as that's specified (in RFC 2606) to never actually resolve
         // "for real", unlike `.localhost` which is allowed to resolve to `127.0.0.1`.
-        // We also add "iroh" as a subdomain, although those 5 bytes might not be neccessary.
+        // We also add "iroh" as a subdomain, although those 5 bytes might not be necessary.
         // We *could* decide to remove that indicator in the future likely without breakage.
         let server_name = &format!("{}.iroh.invalid", BASE32_DNSSEC.encode(node_id.as_bytes()));
         let connect = self.msock.endpoint().connect_with(
