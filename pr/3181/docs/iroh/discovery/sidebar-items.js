e50762d9e7b6123fd081b2e0f0b1bc5f8b6f597e
Frando <Frando@users.noreply.github.com>
2025-02-18 22:22:19 +0000

    deploy: fc15ac8a83c9550ad4a49fee93d9ed892a5d3af3

--- pr/3181/docs/iroh/discovery/sidebar-items.js
+++ pr/3181/docs/iroh/discovery/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["dns","local_swarm_discovery","pkarr","static_provider"],"struct":["ConcurrentDiscovery","DiscoveryItem","Lagged"],"trait":["Discovery"]};
+window.SIDEBAR_ITEMS = {"mod":["dns","local_swarm_discovery","pkarr","static_provider"],"struct":["ConcurrentDiscovery","DiscoveryItem","Lagged","NodeData","NodeInfo"],"trait":["Discovery"]};
