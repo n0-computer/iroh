2377010a9e4ffbd573e34ced7a46068d3bb7219d
matheus23 <matheus23@users.noreply.github.com>
2025-02-21 16:16:17 +0000

    deploy: 0701b33073f0e0ff19eabf446a71a0a7c08705fa

--- pr/3189/docs/iroh_relay/node_info/sidebar-items.js
+++ pr/3189/docs/iroh_relay/node_info/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["IROH_TXT_NAME"],"struct":["NodeData","NodeInfo"],"trait":["NodeIdExt"]};
+window.SIDEBAR_ITEMS = {"constant":["IROH_TXT_NAME"],"struct":["MaxLengthExceededError","NodeData","NodeInfo","UserData"],"trait":["NodeIdExt"]};
