5d667d4a2bba175b1e59f3ae0fd5b38b6905189c
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2025-02-25 23:02:10 +0100

    allow selecting between der and pem format

--- iroh-relay/src/server/resolver.rs
+++ iroh-relay/src/server/resolver.rs
@@ -95,3 +95,30 @@ impl<Loader: Send> std::ops::Deref for ReloadingResolver<Loader> {
         &self.reloadable
     }
 }
+
+/// Format of the certificate/key file.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub enum CertFormat {
+    /// PEM format.
+    PEM,
+    /// DER format
+    /// PKCS8 specifically, other formats are not supported via this DER.
+    DER,
+}
+
+/// Error for the format parsing.
+#[derive(Debug, thiserror::Error)]
+#[error("unknown format: {0}")]
+pub struct FormatParseError(pub String);
+
+impl std::str::FromStr for CertFormat {
+    type Err = FormatParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "pem" => Self::PEM,
+            "der" => Self::DER,
+            other => return Err(FormatParseError(other.to_owned())),
+        })
+    }
+}
