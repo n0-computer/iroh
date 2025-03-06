711e039d1bd6c543aea4ef7c46d11a9a42d026fb
matheus23 <matheus23@users.noreply.github.com>
2025-03-06 13:57:14 +0000

    deploy: c66d307eabfbf54582b4026922586c66cda97001

---
+++ pr/3217/docs/trait.impl/futures_sink/trait.Sink.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl Sink&lt;<a class=\"enum\" href=\"iroh_relay/client/enum.SendMessage.html\" title=\"enum iroh_relay::client::SendMessage\">SendMessage</a>&gt; for <a class=\"struct\" href=\"iroh_relay/client/struct.Client.html\" title=\"struct iroh_relay::client::Client\">Client</a>"],["impl Sink&lt;<a class=\"enum\" href=\"iroh_relay/client/enum.SendMessage.html\" title=\"enum iroh_relay::client::SendMessage\">SendMessage</a>&gt; for <a class=\"struct\" href=\"iroh_relay/client/struct.ClientSink.html\" title=\"struct iroh_relay::client::ClientSink\">ClientSink</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[580]}
