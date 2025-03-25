365aa6c9d856b890e3dbf28fddd94f61390702eb
matheus23 <matheus23@users.noreply.github.com>
2025-03-25 12:45:11 +0000

    deploy: 725dfd04e18de85b89feca9b75a0b7a80c68a80f

--- pr/3217/docs/iroh/protocol/sidebar-items.js
+++ pr/3217/docs/iroh/protocol/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Router","RouterBuilder"],"trait":["ProtocolHandler"]};
+window.SIDEBAR_ITEMS = {"struct":["AccessLimit","Router","RouterBuilder"],"trait":["ProtocolHandler"]};
