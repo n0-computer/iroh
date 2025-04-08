d0c15c3096f439b54cb25907a8a98e0ec82551e8
Frando <Frando@users.noreply.github.com>
2025-04-08 12:21:22 +0000

    deploy: 53891385ce5af0da024e9170aaa34cd7227ee9c4

---
+++ pr/3262/docs/iroh/protocol/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["AccessLimit","Router","RouterBuilder"],"trait":["ProtocolHandler"]};
---
+++ pr/3262/docs/trait.impl/iroh_metrics/core/trait.MetricSet.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl MetricSet for <a class=\"struct\" href=\"iroh/metrics/struct.EndpointMetrics.html\" title=\"struct iroh::metrics::EndpointMetrics\">EndpointMetrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[171]}
