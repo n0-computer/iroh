156d60c8ba0d839cfa4176f6b24b82f983138999
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-05-05 17:49:32 +0000

    deploy: 1b83124b7f5cc1079592856bb50b8c52e8018a07

--- pr/3262/docs/trait.impl/iroh_metrics/base/trait.MetricsGroupSet.js
+++ pr/3262/docs/trait.impl/iroh_metrics/base/trait.MetricsGroupSet.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl MetricsGroupSet for <a class=\"struct\" href=\"iroh/metrics/struct.EndpointMetrics.html\" title=\"struct iroh::metrics::EndpointMetrics\">EndpointMetrics</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl MetricsGroupSet for <a class=\"struct\" href=\"iroh/metrics/struct.EndpointMetrics.html\" title=\"struct iroh::metrics::EndpointMetrics\">EndpointMetrics</a>"]]],["iroh_relay",[["impl MetricsGroupSet for <a class=\"struct\" href=\"iroh_relay/server/struct.RelayMetrics.html\" title=\"struct iroh_relay::server::RelayMetrics\">RelayMetrics</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[177]}
+//{"start":57,"fragment_lengths":[177,185]}
