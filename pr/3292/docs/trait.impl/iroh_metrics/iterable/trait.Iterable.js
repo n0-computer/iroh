2c083efe37be00394fdcef2a68bd19306e0a14c8
ramfox <ramfox@users.noreply.github.com>
2025-05-06 16:18:25 +0000

    deploy: f44f34fe83b29ddaa6c979987f9a2a8846345caf

---
+++ pr/3292/docs/trait.impl/iroh_metrics/iterable/trait.Iterable.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl Iterable for <a class=\"struct\" href=\"iroh/metrics/struct.MagicsockMetrics.html\" title=\"struct iroh::metrics::MagicsockMetrics\">Metrics</a>"],["impl Iterable for <a class=\"struct\" href=\"iroh/net_report/struct.Metrics.html\" title=\"struct iroh::net_report::Metrics\">Metrics</a>"]]],["iroh_dns_server",[["impl Iterable for <a class=\"struct\" href=\"iroh_dns_server/metrics/struct.Metrics.html\" title=\"struct iroh_dns_server::metrics::Metrics\">Metrics</a>"]]],["iroh_relay",[["impl Iterable for <a class=\"struct\" href=\"iroh_relay/server/struct.Metrics.html\" title=\"struct iroh_relay::server::Metrics\">Metrics</a>"],["impl Iterable for <a class=\"struct\" href=\"iroh_relay/server/struct.StunMetrics.html\" title=\"struct iroh_relay::server::StunMetrics\">StunMetrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[306,180,321]}
