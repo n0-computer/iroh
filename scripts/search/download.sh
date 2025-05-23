9cba200108601b1b19290e2f88caac3396633157
Hector Garcia <hmgarcia06@gmail.com>
2025-05-23 18:06:11 -0400

    Minor change in scripts/download.sh

--- scripts/search/download.sh
+++ scripts/search/download.sh
@@ -2,4 +2,4 @@
 
 # cargo run --example file-download -- download --node-id <node_id> --output-path <path>
 
-cargo run --quiet --example file-download --features=examples -- download  --output-path "$1" --node-id "$2"
+cargo run --quiet --example file-download --features=examples -- download  --relay-url "https://use1-1.relay.iroh.network./" --output-path "$1" --node-id "$2"
