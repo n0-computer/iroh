ff35d8921358bb69a5201df76e3eba3a2820610d
ramfox <ramfox@users.noreply.github.com>
2025-05-28 20:27:37 +0000

    deploy: e57582a74dac874bbf87862e1bfad540a0399449

--- pr/3161/docs/iroh/node_info/sidebar-items.js
+++ pr/3161/docs/iroh/node_info/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["IROH_TXT_NAME"],"enum":["Error"],"struct":["MaxLengthExceededError","NodeData","NodeInfo","UserData"],"trait":["NodeIdExt"]};
+window.SIDEBAR_ITEMS = {"constant":["IROH_TXT_NAME"],"enum":["DecodingError","EncodingError","LookupError","ParseError"],"struct":["MaxLengthExceededError","NodeData","NodeInfo","StaggeredError","UserData"],"trait":["NodeIdExt"]};
