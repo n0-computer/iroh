b90edc72cc78d57f4ba6bd16431cfe315e2d1928
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-06-02 21:49:56 +0000

    deploy: cd1a4e041ade4ead0b96a3d855828ddcccfab5b5

--- pr/3314/docs/iroh/net_report/sidebar-items.js
+++ pr/3314/docs/iroh/net_report/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["IfStateDetails","Metrics","Options","QuicConfig","RelayLatencies","Report"]};
+window.SIDEBAR_ITEMS = {"enum":["ReportError"],"struct":["IfStateDetails","Metrics","Options","QuicConfig","RelayLatencies","Report"]};
--- pr/3314/docs/iroh_base/sidebar-items.js
+++ pr/3314/docs/iroh_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature"],"type":["NodeId"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError"],"mod":["ticket"],"struct":["NodeAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature"],"type":["NodeId","SignatureError"]};
