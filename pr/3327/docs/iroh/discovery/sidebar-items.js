439129a9cf446f6eb85fa3f2c3846c5d87be843c
Frando <Frando@users.noreply.github.com>
2025-06-03 10:48:10 +0000

    deploy: 1c00398639ac540772fbbf601e831114204a1ff2

--- pr/3327/docs/iroh/discovery/sidebar-items.js
+++ pr/3327/docs/iroh/discovery/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DiscoveryError","IntoDiscoveryError","ParseError"],"mod":["dns","mdns","pkarr","static_provider"],"struct":["ConcurrentDiscovery","DiscoveryItem","Lagged","NodeData","NodeInfo","UserData"],"trait":["Discovery","IntoDiscovery"]};
+window.SIDEBAR_ITEMS = {"enum":["DiscoveryError","IntoDiscoveryError","ParseError"],"mod":["dns","mdns","pkarr","static_provider"],"struct":["ConcurrentDiscovery","DiscoveryContext","DiscoveryItem","Lagged","NodeData","NodeInfo","UserData"],"trait":["Discovery","IntoDiscovery"]};
