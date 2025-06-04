c2e12bc14c3c947b6a10987c2fff398373a67c56
matheus23 <matheus23@users.noreply.github.com>
2025-06-04 16:14:54 +0000

    deploy: a41009a5dfd6b3e0997526f589c80cefd9bb759b

--- pr/3331/docs/iroh/protocol/sidebar-items.js
+++ pr/3331/docs/iroh/protocol/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["AccessLimit","Router","RouterBuilder"],"trait":["ProtocolHandler"]};
+window.SIDEBAR_ITEMS = {"enum":["AcceptError"],"struct":["AccessLimit","Router","RouterBuilder"],"trait":["ProtocolHandler"]};
---
+++ pr/3331/docs/trait.impl/num_enum/trait.FromPrimitive.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl FromPrimitive for <a class=\"enum\" href=\"iroh_relay/protos/handshake/enum.FrameType.html\" title=\"enum iroh_relay::protos::handshake::FrameType\">FrameType</a>"],["impl FromPrimitive for <a class=\"enum\" href=\"iroh_relay/protos/relay/enum.FrameType.html\" title=\"enum iroh_relay::protos::relay::FrameType\">FrameType</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[352]}
