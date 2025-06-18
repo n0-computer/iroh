b19cb0e7a52867e84e97d18f5bc15d7275efbe49
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-06-18 14:42:57 +0000

    deploy: fec624b2da89d85919b3c0b520f0324e2ffa2b98

--- pr/3314/docs/trait.impl/iroh_metrics/base/trait.MetricsGroup.js
+++ pr/3314/docs/trait.impl/iroh_metrics/base/trait.MetricsGroup.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh/metrics/struct.MagicsockMetrics.html\" title=\"struct iroh::metrics::MagicsockMetrics\">Metrics</a>"],["impl MetricsGroup for <a class=\"struct\" href=\"iroh/net_report/struct.Metrics.html\" title=\"struct iroh::net_report::Metrics\">Metrics</a>"]]],["iroh_dns_server",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_dns_server/metrics/struct.Metrics.html\" title=\"struct iroh_dns_server::metrics::Metrics\">Metrics</a>"]]],["iroh_relay",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_relay/server/struct.Metrics.html\" title=\"struct iroh_relay::server::Metrics\">Metrics</a>"],["impl MetricsGroup for <a class=\"struct\" href=\"iroh_relay/server/struct.StunMetrics.html\" title=\"struct iroh_relay::server::StunMetrics\">StunMetrics</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh/metrics/struct.MagicsockMetrics.html\" title=\"struct iroh::metrics::MagicsockMetrics\">Metrics</a>"],["impl MetricsGroup for <a class=\"struct\" href=\"iroh/net_report/struct.Metrics.html\" title=\"struct iroh::net_report::Metrics\">Metrics</a>"]]],["iroh_dns_server",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_dns_server/metrics/struct.Metrics.html\" title=\"struct iroh_dns_server::metrics::Metrics\">Metrics</a>"]]],["iroh_relay",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_relay/server/struct.Metrics.html\" title=\"struct iroh_relay::server::Metrics\">Metrics</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[314,184,329]}
+//{"start":57,"fragment_lengths":[314,184,167]}
