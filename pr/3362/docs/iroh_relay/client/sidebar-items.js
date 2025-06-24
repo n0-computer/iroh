ab5dd96efcde560fc5b94430a6b6b12f9b4b9e00
ramfox <ramfox@users.noreply.github.com>
2025-06-24 16:04:45 +0000

    deploy: 14ce4e969a09673bd874d3743979c0679453ef22

---
+++ pr/3362/docs/iroh_relay/client/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ConnectError","DialError","ReceivedMessage","RecvError","SendError","SendMessage"],"fn":["make_dangerous_client_config"],"struct":["Client","ClientBuilder","ClientSink","ClientStream"]};
