fbe285e6f4217f1df2e1f65dc81ff4c0551dc8b9
dignifiedquire <me@dignifiedquire.com>
2025-07-11 14:01:32 +0200

    start tracking path events

--- iroh/src/magicsock/node_map/path_state.rs
+++ iroh/src/magicsock/node_map/path_state.rs
@@ -14,12 +14,6 @@ use super::{
 };
 use crate::disco::SendAddr;
 
-/// The minimum time between pings to an endpoint.
-///
-/// Except in the case of CallMeMaybe frames resetting the counter, as the first pings
-/// likely didn't through the firewall.
-const DISCO_PING_INTERVAL: Duration = Duration::from_secs(5);
-
 /// State about a particular path to another [`NodeState`].
 ///
 /// This state is used for both the relay path and any direct UDP paths.
@@ -31,13 +25,6 @@ pub(super) struct PathState {
     node_id: NodeId,
     /// The path this applies for.
     path: SendAddr,
-    /// The last (outgoing) ping time.
-    pub(super) last_ping: Option<Instant>,
-
-    /// If non-zero, means that this was an endpoint that we learned about at runtime (from an
-    /// incoming ping). If so, we keep the time updated and use it to discard old candidates.
-    // NOTE: tx_id Originally added in tailscale due to <https://github.com/tailscale/tailscale/issues/7078>.
-    last_got_ping: Option<(Instant, stun_rs::TransactionId)>,
 
     /// The time this endpoint was last advertised via a call-me-maybe DISCO message.
     pub(super) call_me_maybe_time: Option<Instant>,
@@ -61,8 +48,6 @@ impl PathState {
         Self {
             node_id,
             path,
-            last_ping: None,
-            last_got_ping: None,
             call_me_maybe_time: None,
             last_payload_msg: None,
             sources,
@@ -87,8 +72,6 @@ impl PathState {
         PathState {
             node_id,
             path,
-            last_ping: None,
-            last_got_ping: None,
             call_me_maybe_time: None,
             last_payload_msg: Some(now),
             sources,
@@ -109,11 +92,6 @@ impl PathState {
             .unwrap_or(false)
     }
 
-    /// Returns the instant the last incoming ping was received.
-    pub(super) fn last_incoming_ping(&self) -> Option<&Instant> {
-        self.last_got_ping.as_ref().map(|(time, _tx_id)| time)
-    }
-
     /// Reports the last instant this path was considered alive.
     ///
     /// Alive means the path is considered in use by the remote endpoint.  Either because we
@@ -130,7 +108,6 @@ impl PathState {
             .as_ref()
             .into_iter()
             .chain(self.call_me_maybe_time.as_ref())
-            .chain(self.last_incoming_ping())
             .max()
             .copied()
     }
@@ -138,7 +115,6 @@ impl PathState {
     /// The last control or DISCO message **about** this path.
     ///
     /// This is the most recent instant among:
-    /// - when last pong was received.
     /// - when this path was last advertised in a received CallMeMaybe message.
     /// - when the last ping from them was received.
     ///
@@ -149,32 +125,11 @@ impl PathState {
             .call_me_maybe_time
             .as_ref()
             .map(|call_me| (*call_me, ControlMsg::CallMeMaybe));
-        let last_ping = self
-            .last_incoming_ping()
-            .map(|ping| (*ping, ControlMsg::Ping));
 
         last_call_me_maybe
             .into_iter()
-            .chain(last_ping)
             .max_by_key(|(instant, _kind)| *instant)
             .map(|(instant, kind)| (now.duration_since(instant), kind))
-    }
-
-    pub(super) fn needs_ping(&self, now: &Instant) -> bool {
-        match self.last_ping {
-            None => true,
-            Some(last_ping) => {
-                let elapsed = now.duration_since(last_ping);
-
-                // TODO: remove!
-                // This logs "ping is too new" for each send whenever the endpoint does *not* need
-                // a ping. Pretty sure this is not a useful log, but maybe there was a reason?
-                // if !needs_ping {
-                //     debug!("ping is too new: {}ms", elapsed.as_millis());
-                // }
-                elapsed > DISCO_PING_INTERVAL
-            }
-        }
     }
 
     pub(super) fn add_source(&mut self, source: Source, now: Instant) {
@@ -182,8 +137,6 @@ impl PathState {
     }
 
     pub(super) fn clear(&mut self) {
-        self.last_ping = None;
-        self.last_got_ping = None;
         self.call_me_maybe_time = None;
     }
 
@@ -191,12 +144,6 @@ impl PathState {
         write!(w, "{{ ")?;
         if self.is_active() {
             write!(w, "active ")?;
-        }
-        if let Some(when) = self.last_incoming_ping() {
-            write!(w, "ping-received({:?} ago) ", when.elapsed())?;
-        }
-        if let Some(ref when) = self.last_ping {
-            write!(w, "ping-sent({:?} ago) ", when.elapsed())?;
         }
         if let Some(last_source) = self.sources.iter().max_by_key(|&(_, instant)| instant) {
             write!(
