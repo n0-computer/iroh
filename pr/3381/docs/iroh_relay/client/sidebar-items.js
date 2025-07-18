0af823d0e1d64b62cdac4f3a374ee9cc4b85a314
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-07-18 15:22:59 +0000

    deploy: cf6e98903b8d7c46c3e128ced9e2525371833c43

--- pr/3381/docs/iroh_relay/client/sidebar-items.js
+++ pr/3381/docs/iroh_relay/client/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ConnectError","DialError","ReceivedMessage","RecvError","SendError","SendMessage"],"fn":["make_dangerous_client_config"],"struct":["Client","ClientBuilder","ClientSink","ClientStream"]};
+window.SIDEBAR_ITEMS = {"enum":["ConnectError","DialError","RecvError","SendError"],"fn":["make_dangerous_client_config"],"struct":["Client","ClientBuilder","ClientSink","ClientStream"]};
