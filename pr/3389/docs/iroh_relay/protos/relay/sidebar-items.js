3707ec0ab2724b562826047d71621cc82b71f505
matheus23 <matheus23@users.noreply.github.com>
2025-07-18 07:50:54 +0000

    deploy: 804bc012495dd95d0edac5adf2e26a6a385bb1b3

--- pr/3389/docs/iroh_relay/protos/relay/sidebar-items.js
+++ pr/3389/docs/iroh_relay/protos/relay/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["MAX_PACKET_SIZE","MAX_PAYLOAD_SIZE"],"enum":["ClientToRelayMsg","Error","RelayToClientMsg"],"struct":["Datagrams"]};
+window.SIDEBAR_ITEMS = {"constant":["MAX_PACKET_SIZE"],"enum":["ClientToRelayMsg","Error","RelayToClientMsg"],"struct":["Datagrams"]};
