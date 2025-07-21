2ff9ebdfa0d44cc8d9b4fe10dc8b487b3527644b
dignifiedquire <me@dignifiedquire.com>
2025-07-21 12:50:42 +0200

    remove rtt_actor, this is now done inside quinn on a per path basis

--- iroh/src/endpoint.rs
+++ iroh/src/endpoint.rs
@@ -28,28 +28,6 @@ use n0_future::{time::Duration, Stream};
 use n0_watcher::Watcher;
 use nested_enum_utils::common_fields;
 use pin_project::pin_project;
-use snafu::{ensure, ResultExt, Snafu};
-use tracing::{debug, instrument, trace, warn};
-use url::Url;
-
-#[cfg(wasm_browser)]
-use crate::discovery::pkarr::PkarrResolver;
-#[cfg(not(wasm_browser))]
-use crate::{discovery::dns::DnsDiscovery, dns::DnsResolver};
-use crate::{
-    discovery::{
-        pkarr::PkarrPublisher, ConcurrentDiscovery, Discovery, DiscoveryContext, DiscoveryError,
-        DiscoveryItem, DiscoverySubscribers, DiscoveryTask, DynIntoDiscovery, IntoDiscovery,
-        IntoDiscoveryError, Lagged, UserData,
-    },
-    magicsock::{self, Handle, NodeIdMappedAddr, OwnAddressSnafu},
-    metrics::EndpointMetrics,
-    net_report::Report,
-    tls,
-};
-
-mod rtt_actor;
-
 // Missing still: SendDatagram and ConnectionClose::frame_type's Type.
 pub use quinn::{
     AcceptBi, AcceptUni, AckFrequencyConfig, ApplicationClose, Chunk, ClosedStream,
@@ -66,11 +44,28 @@ pub use quinn_proto::{
     },
     FrameStats, PathStats, TransportError, TransportErrorCode, UdpStats, Written,
 };
+use snafu::{ensure, ResultExt, Snafu};
+use tracing::{debug, instrument, trace, warn};
+use url::Url;
 
-use self::rtt_actor::RttMessage;
 pub use super::magicsock::{
     AddNodeAddrError, ConnectionType, ControlMsg, DirectAddr, DirectAddrInfo, DirectAddrType,
     RemoteInfo, Source,
+};
+#[cfg(wasm_browser)]
+use crate::discovery::pkarr::PkarrResolver;
+#[cfg(not(wasm_browser))]
+use crate::{discovery::dns::DnsDiscovery, dns::DnsResolver};
+use crate::{
+    discovery::{
+        pkarr::PkarrPublisher, ConcurrentDiscovery, Discovery, DiscoveryContext, DiscoveryError,
+        DiscoveryItem, DiscoverySubscribers, DiscoveryTask, DynIntoDiscovery, IntoDiscovery,
+        IntoDiscoveryError, Lagged, UserData,
+    },
+    magicsock::{self, Handle, NodeIdMappedAddr, OwnAddressSnafu},
+    metrics::EndpointMetrics,
+    net_report::Report,
+    tls,
 };
 
 /// The delay to fall back to discovery when direct addresses fail.
@@ -526,8 +521,6 @@ impl StaticConfig {
 pub struct Endpoint {
     /// Handle to the magicsocket/actor
     msock: Handle,
-    /// Handle to the actor that resets the quinn RTT estimator
-    rtt_actor: Arc<rtt_actor::RttHandle>,
     /// Configuration structs for quinn, holds the transport config, certificate setup, secret key etc.
     static_config: Arc<StaticConfig>,
 }
@@ -637,7 +630,6 @@ impl Endpoint {
         let metrics = msock.metrics.magicsock.clone();
         let ep = Self {
             msock,
-            rtt_actor: Arc::new(rtt_actor::RttHandle::new(metrics)),
             static_config: Arc::new(static_config),
         };
         Ok(ep)
@@ -1623,7 +1615,6 @@ impl Future for IncomingFuture {
             Poll::Ready(Err(err)) => Poll::Ready(Err(err)),
             Poll::Ready(Ok(inner)) => {
                 let conn = Connection::new(inner, None, &this.ep);
-                try_send_rtt_msg(&conn, this.ep, None);
                 Poll::Ready(Ok(conn))
             }
         }
@@ -1710,19 +1701,18 @@ impl Connecting {
     pub fn into_0rtt(self) -> Result<(Connection, ZeroRttAccepted), Self> {
         match self.inner.into_0rtt() {
             Ok((inner, zrtt_accepted)) => {
+                // This call is why `self.remote_node_id` was introduced.
+                // When we `Connecting::into_0rtt`, then we don't yet have `handshake_data`
+                // in our `Connection`, thus we won't be able to pick up
+                // `Connection::remote_node_id`.
+                // Instead, we provide `self.remote_node_id` here - we know it in advance,
+                // after all.
                 let conn = Connection::new(inner, self.remote_node_id, &self.ep);
                 let zrtt_accepted = ZeroRttAccepted {
                     inner: zrtt_accepted,
                     _discovery_drop_guard: self._discovery_drop_guard,
                 };
 
-                // This call is why `self.remote_node_id` was introduced.
-                // When we `Connecting::into_0rtt`, then we don't yet have `handshake_data`
-                // in our `Connection`, thus `try_send_rtt_msg` won't be able to pick up
-                // `Connection::remote_node_id`.
-                // Instead, we provide `self.remote_node_id` here - we know it in advance,
-                // after all.
-                try_send_rtt_msg(&conn, &self.ep, self.remote_node_id);
                 Ok((conn, zrtt_accepted))
             }
             Err(inner) => Err(Self {
@@ -1762,8 +1752,6 @@ impl Future for Connecting {
             Poll::Ready(Err(err)) => Poll::Ready(Err(err)),
             Poll::Ready(Ok(inner)) => {
                 let conn = Connection::new(inner, *this.remote_node_id, &this.ep);
-
-                try_send_rtt_msg(&conn, this.ep, *this.remote_node_id);
                 Poll::Ready(Ok(conn))
             }
         }
@@ -2135,30 +2123,6 @@ impl Connection {
     #[inline]
     pub fn set_max_concurrent_bi_streams(&self, count: VarInt) {
         self.inner.set_max_concurrent_bi_streams(count)
-    }
-}
-
-/// Try send a message to the rtt-actor.
-///
-/// If we can't notify the actor that will impact performance a little, but we can still
-/// function.
-fn try_send_rtt_msg(conn: &Connection, magic_ep: &Endpoint, remote_node_id: Option<NodeId>) {
-    // If we can't notify the rtt-actor that's not great but not critical.
-    let Some(node_id) = remote_node_id.or_else(|| conn.remote_node_id().ok()) else {
-        warn!(?conn, "failed to get remote node id");
-        return;
-    };
-    let Some(conn_type_changes) = magic_ep.conn_type(node_id) else {
-        warn!(?conn, "failed to create conn_type stream");
-        return;
-    };
-    let rtt_msg = RttMessage::NewConnection {
-        connection: conn.inner.weak_handle(),
-        conn_type_changes: conn_type_changes.stream(),
-        node_id,
-    };
-    if let Err(err) = magic_ep.rtt_actor.msg_tx.try_send(rtt_msg) {
-        warn!(?conn, "rtt-actor not reachable: {err:#}");
     }
 }
 
