4d6a83eef8b9ed52067dcd1da7fb2b354f457352
matheus23 <matheus23@users.noreply.github.com>
2025-07-22 15:18:15 +0000

    deploy: b345797ebbacb91d47738a6772b0119ca706c98d

---
+++ pr/3398/docs/iroh/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError","RelayProtocol"],"mod":["defaults","discovery","dns","endpoint","metrics","net_report","node_info","protocol","test_utils","watchable"],"struct":["NodeAddr","PublicKey","RelayMap","RelayNode","RelayUrl","RelayUrlParseError","SecretKey"],"type":["NodeId"]};
