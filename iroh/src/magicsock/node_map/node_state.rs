n0-computer/iroh

commit a3187caee475e83c2f6ad8cb57bbfd3825a7c917

fix(iroh): Track path validity for all paths and replace `BestAddr` with `PathValidity` (#3400)
## Description

#3398 but now rebased on `main`. See its description for more
information.

There's only one small change: We don't need to think about
`NodeMap::reset`, as that's removed in `main`.

## Breaking Changes

None.

## Notes & open questions

<!-- Any notes, remarks or open questions you have to make about the PR.
-->

## Change checklist
<!-- Remove any that are not relevant. -->
- [x] Self-review.

iroh/src/magicsock/node_map/udp_paths.rs
@@ -7,17 +7,9 @@
 //! [`NodeState`]: super::node_state::NodeState
 use std::{collections::BTreeMap, net::SocketAddr};
 
-use n0_future::time::{Duration, Instant};
-use rand::seq::IteratorRandom;
-use tracing::warn;
+use n0_future::time::Instant;
 
-use super::{
-    IpPort,
-    best_addr::{self, BestAddr},
-    node_state::PongReply,
-    path_state::PathState,
-};
-use crate::disco::SendAddr;
+use super::{IpPort, path_state::PathState};
 
 /// The address on which to send datagrams over UDP.
 ///
@@ -32,7 +24,7 @@ use crate::disco::SendAddr;
 ///
 /// [`MagicSock`]: crate::magicsock::MagicSock
 /// [`NodeState`]: super::node_state::NodeState
-#[derive(Debug)]
+#[derive(Debug, Default, Clone, Copy)]
 pub(super) enum UdpSendAddr {
     /// The UDP address can be relied on to deliver data to the remote node.
     ///
@@ -54,7 +46,19 @@ pub(super) enum UdpSendAddr {
     /// establish a connection.
     Unconfirmed(SocketAddr),
     /// No known UDP path exists to the remote node.
+    #[default]
     None,
+}
+
+impl UdpSendAddr {
+    pub fn get_addr(&self) -> Option<SocketAddr> {
+        match self {
+            UdpSendAddr::Valid(addr)
+            | UdpSendAddr::Outdated(addr)
+            | UdpSendAddr::Unconfirmed(addr) => Some(*addr),
+            UdpSendAddr::None => None,
+        }
+    }
 }
 
 /// The UDP paths for a single node.
@@ -72,10 +76,19 @@ pub(super) enum UdpSendAddr {
 pub(super) struct NodeUdpPaths {
     /// The state for each of this node's direct paths.
     pub(super) paths: BTreeMap<IpPort, PathState>,
-    /// Best UDP path currently selected.
-    pub(super) best_addr: BestAddr,
-    /// If we had to choose a path because we had no `best_addr` it is stored here.
-    chosen_candidate: Option<IpPort>,
+    /// The current address we use to send on.
+    ///
+    /// This is *almost* the same as going through `paths` and finding
+    /// the best one, except that this is
+    /// 1. Not updated in `send_addr`, but instead when there's changes to `paths`, so that `send_addr` can take `&self`.
+    /// 2. Slightly sticky: It only changes when
+    ///   - the current send addr is not a validated path anymore or
+    ///   - we received a pong with lower latency.
+    pub(super) best: UdpSendAddr,
+    /// The current best address to send on from all IPv4 addresses we have available.
+    ///
+    /// Follows the same logic as `best` above, but doesn't include any IPv6 addresses.
+    pub(super) best_ipv4: UdpSendAddr,
 }
 
 impl NodeUdpPaths {
@@ -84,97 +97,90 @@ impl NodeUdpPaths {
     }
 
     #[cfg(test)]
-    pub(super) fn from_parts(paths: BTreeMap<IpPort, PathState>, best_addr: BestAddr) -> Self {
+    pub(super) fn from_parts(paths: BTreeMap<IpPort, PathState>, best: UdpSendAddr) -> Self {
         Self {
             paths,
-            best_addr,
-            chosen_candidate: None,
+            best_ipv4: best, // we only use ipv4 addrs in tests
+            best,
         }
     }
 
     /// Returns the current UDP address to send on.
-    ///
-    /// TODO: The goal here is for this to simply return the already known send address, so
-    /// it should be `&self` and not `&mut self`.  This is only possible once the state from
-    /// [`NodeUdpPaths`] is no longer modified from outside.
-    pub(super) fn send_addr(&mut self, now: Instant, have_ipv6: bool) -> UdpSendAddr {
-        self.assign_best_addr_from_candidates_if_empty();
-        match self.best_addr.state(now) {
-            best_addr::State::Valid(addr) => UdpSendAddr::Valid(addr.addr),
-            best_addr::State::Outdated(addr) => UdpSendAddr::Outdated(addr.addr),
-            best_addr::State::Empty => {
-                // No direct connection has been used before.  If we know of any possible
-                // candidate addresses, randomly try to use one.  This path is most
-                // effective when folks use a NodeAddr with exactly one direct address which
-                // they know to work, effectively like using a traditional socket or QUIC
-                // endpoint.
-                let addr = self
-                    .chosen_candidate
-                    .and_then(|ipp| self.paths.get(&ipp))
-                    .and_then(|path| path.udp_addr())
-                    .filter(|addr| addr.is_ipv4() || have_ipv6)
-                    .or_else(|| {
-                        // Look for a new candidate in all the known paths.  This may look
-                        // like a RNG use on the hot-path but this is normally invoked at
-                        // most most once at startup.
-                        let addr = self
-                            .paths
-                            .values()
-                            .filter_map(|path| path.udp_addr())
-                            .filter(|addr| addr.is_ipv4() || have_ipv6)
-                            .choose(&mut rand::thread_rng());
-                        self.chosen_candidate = addr.map(IpPort::from);
-                        addr
-                    });
-                match addr {
-                    Some(addr) => UdpSendAddr::Unconfirmed(addr),
-                    None => UdpSendAddr::None,
-                }
-            }
+    pub(super) fn send_addr(&self, have_ipv6: bool) -> &UdpSendAddr {
+        if !have_ipv6 {
+            return &self.best_ipv4;
         }
+        &self.best
     }
 
-    /// Fixup best_addr from candidates.
+    /// Change the current best address(es) to ones chosen as described in [`Self::best_addr`] docs.
     ///
-    /// If somehow we end up in a state where we failed to set a best_addr, while we do have
-    /// valid candidates, this will chose a candidate and set best_addr again.  Most likely
-    /// this is a bug elsewhere though.
-    fn assign_best_addr_from_candidates_if_empty(&mut self) {
-        if !self.best_addr.is_empty() {
-            return;
-        }
+    /// This should be called any time that `paths` is modified.
+    pub(super) fn update_to_best_addr(&mut self, now: Instant) {
+        self.best_ipv4 = self.best_addr(false, now);
+        self.best = self.best_addr(true, now);
+    }
 
-        // The highest acceptable latency for an endpoint path.  If the latency is higher
-        // then this the path will be ignored.
-        const MAX_LATENCY: Duration = Duration::from_secs(60 * 60);
-        let best_pong = self.paths.iter().fold(None, |best_pong, (ipp, state)| {
-            let best_latency = best_pong
-                .map(|p: &PongReply| p.latency)
-                .unwrap_or(MAX_LATENCY);
-            match state.recent_pong {
-                // This pong is better if it has a lower latency, or if it has the same
-                // latency but on an IPv6 path.
-                Some(ref pong)
-                    if pong.latency < best_latency
-                        || (pong.latency == best_latency && ipp.ip().is_ipv6()) =>
-                {
-                    Some(pong)
+    /// Returns the current best address of all available paths, ignoring
+    /// the currently chosen best address.
+    ///
+    /// We try to find the lowest latency [`UdpSendAddr::Valid`], if one exists, otherwise
+    /// we try to find the lowest latency [`UdpSendAddr::Outdated`], if one exists, otherwise
+    /// we return essentially an arbitrary [`UdpSendAddr::Unconfirmed`].
+    ///
+    /// If we don't have any addresses, returns [`UdpSendAddr::None`].
+    ///
+    /// If `have_ipv6` is false, we only search among ipv4 candidates.
+    fn best_addr(&self, have_ipv6: bool, now: Instant) -> UdpSendAddr {
+        let Some((ipp, path)) = self
+            .paths
+            .iter()
+            .filter(|(ipp, _)| have_ipv6 || ipp.ip.is_ipv4())
+            .max_by_key(|(ipp, path)| {
+                // We find the best by sorting on a key of type (Option<ReverseOrd<Duration>>, Option<ReverseOrd<Duration>>, bool)
+                // where the first is set to Some(ReverseOrd(latency)) iff path.is_valid(now) and
+                // the second is set to Some(ReverseOrd(latency)) if path.is_outdated(now) and
+                // the third is set to whether the ipp is ipv6.
+                // This makes max_by_key sort for the lowest valid latency first, then sort for
+                // the lowest outdated latency second, and if latencies are equal, it'll sort IPv6 paths first.
+                let is_ipv6 = ipp.ip.is_ipv6();
+                if let Some(latency) = path.validity.latency_if_valid(now) {
+                    (Some(ReverseOrd(latency)), None, is_ipv6)
+                } else if let Some(latency) = path.validity.latency_if_outdated(now) {
+                    (None, Some(ReverseOrd(latency)), is_ipv6)
+                } else {
+                    (None, None, is_ipv6)
                 }
-                _ => best_pong,
-            }
-        });
+            })
+        else {
+            return UdpSendAddr::None;
+        };
 
-        // If we found a candidate, set to best addr
-        if let Some(pong) = best_pong {
-            if let SendAddr::Udp(addr) = pong.from {
-                warn!(%addr, "No best_addr was set, choose candidate with lowest latency");
-                self.best_addr.insert_if_better_or_reconfirm(
-                    addr,
-                    pong.latency,
-                    best_addr::Source::BestCandidate,
-                    pong.pong_at,
-                )
-            }
+        if path.validity.is_valid(now) {
+            UdpSendAddr::Valid((*ipp).into())
+        } else if path.validity.is_outdated(now) {
+            UdpSendAddr::Outdated((*ipp).into())
+        } else {
+            UdpSendAddr::Unconfirmed((*ipp).into())
         }
+    }
+}
+
+/// Implements the reverse [`Ord`] implementation for the wrapped type.
+///
+/// Literally calls [`std::cmp::Ordering::reverse`] on the inner value's
+/// ordering.
+#[derive(PartialEq, Eq)]
+struct ReverseOrd<N: PartialOrd + Ord + PartialEq + Eq>(N);
+
+impl<N: PartialOrd + Ord + PartialEq + Eq> Ord for ReverseOrd<N> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0).reverse()
+    }
+}
+
+impl<N: PartialOrd + Ord + PartialEq + Eq> PartialOrd for ReverseOrd<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
 }

iroh/src/magicsock/node_map.rs
@@ -11,18 +11,15 @@ use serde::{Deserialize, Serialize};
 use stun_rs::TransactionId;
 use tracing::{debug, info, instrument, trace, warn};
 
-use self::{
-    best_addr::ClearReason,
-    node_state::{NodeState, Options, PingHandled},
-};
+use self::node_state::{NodeState, Options, PingHandled};
 use super::{ActorMessage, NodeIdMappedAddr, metrics::Metrics, transports};
 use crate::disco::{CallMeMaybe, Pong, SendAddr};
 #[cfg(any(test, feature = "test-utils"))]
 use crate::endpoint::PathSelection;
 
-mod best_addr;
 mod node_state;
 mod path_state;
+mod path_validity;
 mod udp_paths;
 
 pub use node_state::{ConnectionType, ControlMsg, DirectAddrInfo, RemoteInfo};
@@ -196,7 +193,7 @@ impl NodeMap {
             .expect("poisoned")
             .get_mut(NodeStateKey::Idx(id))
         {
-            ep.ping_timeout(tx_id);
+            ep.ping_timeout(tx_id, Instant::now());
         }
     }
 
@@ -266,9 +263,10 @@ impl NodeMap {
     }
 
     pub(super) fn reset_node_states(&self) {
+        let now = Instant::now();
         let mut inner = self.inner.lock().expect("poisoned");
         for (_, ep) in inner.node_states_mut() {
-            ep.note_connectivity_change();
+            ep.note_connectivity_change(now);
         }
     }
 
@@ -317,7 +315,7 @@ impl NodeMap {
         self.inner
             .lock()
             .expect("poisoned")
-            .on_direct_addr_discovered(discovered);
+            .on_direct_addr_discovered(discovered, Instant::now());
     }
 }
 
@@ -378,24 +376,28 @@ impl NodeMapInner {
     }
 
     /// Prunes direct addresses from nodes that claim to share an address we know points to us.
-    pub(super) fn on_direct_addr_discovered(&mut self, discovered: BTreeSet<SocketAddr>) {
+    pub(super) fn on_direct_addr_discovered(
+        &mut self,
+        discovered: BTreeSet<SocketAddr>,
+        now: Instant,
+    ) {
         for addr in discovered {
-            self.remove_by_ipp(addr.into(), ClearReason::MatchesOurLocalAddr)
+            self.remove_by_ipp(addr.into(), now, "matches our local addr")
         }
     }
 
     /// Removes a direct address from a node.
-    fn remove_by_ipp(&mut self, ipp: IpPort, reason: ClearReason) {
+    fn remove_by_ipp(&mut self, ipp: IpPort, now: Instant, why: &'static str) {
         if let Some(id) = self.by_ip_port.remove(&ipp) {
             if let Entry::Occupied(mut entry) = self.by_id.entry(id) {
                 let node = entry.get_mut();
-                node.remove_direct_addr(&ipp, reason);
+                node.remove_direct_addr(&ipp, now, why);
                 if node.direct_addresses().count() == 0 {
                     let node_id = node.public_key();
                     let mapped_addr = node.quic_mapped_addr();
                     self.by_node_key.remove(node_id);
                     self.by_quic_mapped_addr.remove(mapped_addr);
-                    debug!(node_id=%node_id.fmt_short(), ?reason, "removing node");
+                    debug!(node_id=%node_id.fmt_short(), why, "removing node");
                     entry.remove();
                 }
             }
@@ -840,7 +842,7 @@ mod tests {
         }
 
         info!("Pruning addresses");
-        endpoint.prune_direct_addresses();
+        endpoint.prune_direct_addresses(Instant::now());
 
         // Half the offline addresses should have been pruned.  All the active and alive
         // addresses should have been kept.

iroh/src/magicsock/node_map/best_addr.rs
@@ -1,221 +1,0 @@
-//! The [`BestAddr`] is the currently active best address for UDP sends.
-
-use std::net::SocketAddr;
-
-use n0_future::time::{Duration, Instant};
-use tracing::{debug, info};
-
-/// How long we trust a UDP address as the exclusive path (without using relay) without having heard a Pong reply.
-const TRUST_UDP_ADDR_DURATION: Duration = Duration::from_millis(6500);
-
-#[derive(Debug, Default)]
-pub(super) struct BestAddr(Option<BestAddrInner>);
-
-#[derive(Debug)]
-struct BestAddrInner {
-    addr: AddrLatency,
-    trust_until: Option<Instant>,
-    confirmed_at: Instant,
-}
-
-impl BestAddrInner {
-    fn is_trusted(&self, now: Instant) -> bool {
-        self.trust_until
-            .map(|trust_until| trust_until >= now)
-            .unwrap_or(false)
-    }
-
-    fn addr(&self) -> SocketAddr {
-        self.addr.addr
-    }
-}
-
-#[derive(Debug)]
-pub(super) enum Source {
-    ReceivedPong,
-    BestCandidate,
-    Udp,
-}
-
-impl Source {
-    fn trust_until(&self, from: Instant) -> Instant {
-        match self {
-            Source::ReceivedPong => from + TRUST_UDP_ADDR_DURATION,
-            // TODO: Fix time
-            Source::BestCandidate => from + Duration::from_secs(60 * 60),
-            Source::Udp => from + TRUST_UDP_ADDR_DURATION,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub(super) enum State<'a> {
-    Valid(&'a AddrLatency),
-    Outdated(&'a AddrLatency),
-    Empty,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum ClearReason {
-    Inactive,
-    PongTimeout,
-    MatchesOurLocalAddr,
-}
-
-impl BestAddr {
-    #[cfg(test)]
-    pub fn from_parts(
-        addr: SocketAddr,
-        latency: Duration,
-        confirmed_at: Instant,
-        trust_until: Instant,
-    ) -> Self {
-        let inner = BestAddrInner {
-            addr: AddrLatency { addr, latency },
-            confirmed_at,
-            trust_until: Some(trust_until),
-        };
-        Self(Some(inner))
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.0.is_none()
-    }
-
-    /// Unconditionally clears the best address.
-    pub fn clear(&mut self, reason: ClearReason, has_relay: bool) {
-        let old = self.0.take();
-        if let Some(old_addr) = old.as_ref().map(BestAddrInner::addr) {
-            info!(?reason, ?has_relay, %old_addr, "clearing best_addr");
-        }
-    }
-
-    /// Clears the best address if equal to `addr`.
-    pub fn clear_if_equals(&mut self, addr: SocketAddr, reason: ClearReason, has_relay: bool) {
-        if self.addr() == Some(addr) {
-            self.clear(reason, has_relay)
-        }
-    }
-
-    pub fn clear_trust(&mut self, why: &'static str) {
-        if let Some(state) = self.0.as_mut() {
-            info!(
-                %why,
-                prev_trust_until = ?state.trust_until,
-                "clearing best_addr trust",
-            );
-            state.trust_until = None;
-        }
-    }
-
-    pub fn insert_if_better_or_reconfirm(
-        &mut self,
-        addr: SocketAddr,
-        latency: Duration,
-        source: Source,
-        confirmed_at: Instant,
-    ) {
-        match self.0.as_mut() {
-            None => {
-                self.insert(addr, latency, source, confirmed_at);
-            }
-            Some(state) => {
-                let candidate = AddrLatency { addr, latency };
-                if !state.is_trusted(confirmed_at) || candidate.is_better_than(&state.addr) {
-                    self.insert(addr, latency, source, confirmed_at);
-                } else if state.addr.addr == addr {
-                    state.confirmed_at = confirmed_at;
-                    state.trust_until = Some(source.trust_until(confirmed_at));
-                }
-            }
-        }
-    }
-
-    /// Reset the expiry, if the passed in addr matches the currently used one.
-    #[cfg(not(wasm_browser))]
-    pub fn reconfirm_if_used(&mut self, addr: SocketAddr, source: Source, confirmed_at: Instant) {
-        if let Some(state) = self.0.as_mut() {
-            if state.addr.addr == addr {
-                state.confirmed_at = confirmed_at;
-                state.trust_until = Some(source.trust_until(confirmed_at));
-            }
-        }
-    }
-
-    fn insert(
-        &mut self,
-        addr: SocketAddr,
-        latency: Duration,
-        source: Source,
-        confirmed_at: Instant,
-    ) {
-        let trust_until = source.trust_until(confirmed_at);
-
-        if self
-            .0
-            .as_ref()
-            .map(|prev| prev.addr.addr == addr)
-            .unwrap_or_default()
-        {
-            debug!(
-                %addr,
-                latency = ?latency,
-                trust_for = ?trust_until.duration_since(Instant::now()),
-               "re-selecting direct path for node"
-            );
-        } else {
-            info!(
-               %addr,
-               latency = ?latency,
-               trust_for = ?trust_until.duration_since(Instant::now()),
-               "selecting new direct path for node"
-            );
-        }
-        let inner = BestAddrInner {
-            addr: AddrLatency { addr, latency },
-            trust_until: Some(trust_until),
-            confirmed_at,
-        };
-        self.0 = Some(inner);
-    }
-
-    pub fn state(&self, now: Instant) -> State<'_> {
-        match &self.0 {
-            None => State::Empty,
-            Some(state) => match state.trust_until {
-                Some(expiry) if now < expiry => State::Valid(&state.addr),
-                Some(_) | None => State::Outdated(&state.addr),
-            },
-        }
-    }
-
-    pub fn addr(&self) -> Option<SocketAddr> {
-        self.0.as_ref().map(BestAddrInner::addr)
-    }
-}
-
-/// A `SocketAddr` with an associated latency.
-#[derive(Debug, Clone)]
-pub struct AddrLatency {
-    pub addr: SocketAddr,
-    pub latency: Duration,
-}
-
-impl AddrLatency {
-    /// Reports whether `self` is a better addr to use than `other`.
-    fn is_better_than(&self, other: &Self) -> bool {
-        if self.addr == other.addr {
-            return false;
-        }
-        if self.addr.is_ipv6() && other.addr.is_ipv4() {
-            // Prefer IPv6 for being a bit more robust, as long as
-            // the latencies are roughly equivalent.
-            if self.latency / 10 * 9 < other.latency {
-                return true;
-            }
-        } else if self.addr.is_ipv4() && other.addr.is_ipv6() && other.is_better_than(self) {
-            return false;
-        }
-        self.latency < other.latency
-    }
-}

commit bcb60d42fecd79fcc5e4d064f410367a3f7aa0b2

fix(iroh): Only clear `last_call_me_maybe` when the best addr became invalid (#3415)
## Description

Fixes a bug introduced in #3400

We used to only reset `last_call_me_maybe` to `None` when the best
address was cleared. The above PR accidentally changed that to always
reset.

## Change checklist
<!-- Remove any that are not relevant. -->
- [x] Self-review.
- [x] Documentation updates following the [style
guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text),
if relevant.

iroh/src/magicsock/node_map/node_state.rs
@@ -1025,10 +1025,12 @@ impl NodeState {
                 }
             }
         }
-        // Clear trust on our best_addr if it is not included in the updated set.  Also
-        // clear the last call-me-maybe send time so we will send one again.
-        self.udp_paths.update_to_best_addr(now);
-        self.last_call_me_maybe = None;
+        // Clear trust on our best_addr if it is not included in the updated set.
+        let changed = self.udp_paths.update_to_best_addr(now);
+        if changed {
+            // Clear the last call-me-maybe send time so we will send one again.
+            self.last_call_me_maybe = None;
+        }
         debug!(
             paths = %summarize_node_paths(&self.udp_paths.paths),
             "updated endpoint paths from call-me-maybe",

iroh/src/magicsock/node_map/udp_paths.rs
@@ -8,7 +8,7 @@
 use std::{collections::BTreeMap, net::SocketAddr};
 
 use n0_future::time::Instant;
-use tracing::debug;
+use tracing::{Level, event};
 
 use super::{IpPort, path_state::PathState};
 
@@ -114,20 +114,34 @@ impl NodeUdpPaths {
         &self.best
     }
 
-    /// Change the current best address(es) to ones chosen as described in [`Self::best_addr`] docs.
+    /// Changes the current best address(es) to ones chosen as described in [`Self::best_addr`] docs.
+    ///
+    /// Returns whether one of the best addresses had to change.
     ///
     /// This should be called any time that `paths` is modified.
-    pub(super) fn update_to_best_addr(&mut self, now: Instant) {
+    pub(super) fn update_to_best_addr(&mut self, now: Instant) -> bool {
         let best_ipv4 = self.best_addr(false, now);
         let best = self.best_addr(true, now);
+        let mut changed = false;
         if best_ipv4 != self.best_ipv4 {
-            debug!(?best_ipv4, "update best addr (IPv4)");
+            event!(
+                target: "iroh::_events::udp::best_ipv4",
+                Level::DEBUG,
+                ?best_ipv4,
+            );
+            changed = true;
         }
         if best != self.best {
-            debug!(?best, "update best addr (IPv4 or IPv6)");
+            event!(
+                target: "iroh::_events::udp::best",
+                Level::DEBUG,
+                ?best,
+            );
+            changed = true;
         }
         self.best_ipv4 = best_ipv4;
         self.best = best;
+        changed
     }
 
     /// Returns the current best address of all available paths, ignoring