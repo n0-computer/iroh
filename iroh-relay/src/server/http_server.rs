n0-computer/iroh

commit 0776687e982050c7a7c46e8d430b350a24481718

refactor(iroh,iroh-relay): Remove legacy relay path, make websocket connections default (#3384)
## Description

Split out from #3331. 

Switch to websockets and remove the old code path.

Also:
- I've removed `governor` for rate limiting
- We used to guess the amount of bytes a frame was encoded as after
we've decoded it, because that's how governor works (you do it on
`Stream`s, not the underlying `AsyncRead`).
- Instead we have our own couple of lines of rate-limiting logic (with
tests) on the `AsyncRead` level.
- The reason is we can't guess the size of websocket frames as easily
anymore.

## Breaking Changes

In iroh:
- Removed `iroh::endpoint::Builder::relay_conn_protocol`. It's now
always websockets
- Removed the `iroh::RelayProtocol` re-export (the type was removed in
`iroh-relay`)

In iroh-relay:
- Removed `ClientBuilder::protocol`
- Removed `http::Protocol` type
- Renamed `frames_rx_ratelimited_total` metric to
`bytes_rx_ratelimited_total`, which now tracks bytes not frames
- Removed `relay_accepts` and `websocket_accepts` metrics

## Change checklist
<!-- Remove any that are not relevant. -->
- [x] Self-review.

iroh-relay/src/server/http_server.rs
@@ -18,20 +18,24 @@ use nested_enum_utils::common_fields;
 use snafu::{Backtrace, ResultExt, Snafu};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_rustls_acme::AcmeAcceptor;
-use tokio_util::{codec::Framed, sync::CancellationToken, task::AbortOnDropHandle};
+use tokio_util::{sync::CancellationToken, task::AbortOnDropHandle};
 use tracing::{debug, debug_span, error, info, info_span, trace, warn, Instrument};
 
-use super::{clients::Clients, streams::StreamError, AccessConfig, SpawnError};
+use super::{
+    clients::Clients,
+    streams::{InvalidBucketConfig, StreamError},
+    AccessConfig, SpawnError,
+};
 use crate::{
     defaults::{timeouts::SERVER_WRITE_TIMEOUT, DEFAULT_KEY_CACHE_CAPACITY},
-    http::{Protocol, LEGACY_RELAY_PATH, RELAY_PATH, SUPPORTED_WEBSOCKET_VERSION},
+    http::{RELAY_PATH, SUPPORTED_WEBSOCKET_VERSION, WEBSOCKET_UPGRADE_PROTOCOL},
     protos::relay::{
-        recv_client_key, Frame, RelayCodec, PER_CLIENT_SEND_QUEUE_DEPTH, PROTOCOL_VERSION,
+        recv_client_key, Frame, MAX_FRAME_SIZE, PER_CLIENT_SEND_QUEUE_DEPTH, PROTOCOL_VERSION,
     },
     server::{
         client::Config,
         metrics::Metrics,
-        streams::{MaybeTlsStream, RelayedStream},
+        streams::{MaybeTlsStream, RateLimited, RelayedStream},
         BindTcpListenerSnafu, ClientRateLimit, NoLocalAddrSnafu,
     },
     KeyCache,
@@ -230,6 +234,8 @@ pub enum AcceptError {
         #[snafu(implicit)]
         span_trace: n0_snafu::SpanTrace,
     },
+    #[snafu(display("rate limiting misconfigured"))]
+    RateLimitingMisconfigured { source: InvalidBucketConfig },
 }
 
 /// Server connection errors, includes errors that can happen on `accept`.
@@ -464,47 +470,42 @@ impl RelayService {
         async move {
             {
                 // Send a 400 to any request that doesn't have an `Upgrade` header.
-                let Some(protocol) = req.headers().get(UPGRADE).and_then(Protocol::parse_header)
-                else {
+                if req.headers().get(UPGRADE)
+                    != Some(&HeaderValue::from_static(WEBSOCKET_UPGRADE_PROTOCOL))
+                {
                     return Ok(builder
                         .status(StatusCode::BAD_REQUEST)
                         .body(body_empty())
                         .expect("valid body"));
                 };
 
-                let websocket_headers = if protocol == Protocol::Websocket {
-                    let Some(key) = req.headers().get("Sec-WebSocket-Key").cloned() else {
-                        warn!("missing header Sec-WebSocket-Key for websocket relay protocol");
-                        return Ok(builder
-                            .status(StatusCode::BAD_REQUEST)
-                            .body(body_empty())
-                            .expect("valid body"));
-                    };
-
-                    let Some(version) = req.headers().get("Sec-WebSocket-Version").cloned() else {
-                        warn!("missing header Sec-WebSocket-Version for websocket relay protocol");
-                        return Ok(builder
-                            .status(StatusCode::BAD_REQUEST)
-                            .body(body_empty())
-                            .expect("valid body"));
-                    };
-
-                    if version.as_bytes() != SUPPORTED_WEBSOCKET_VERSION.as_bytes() {
-                        warn!("invalid header Sec-WebSocket-Version: {:?}", version);
-                        return Ok(builder
-                            .status(StatusCode::BAD_REQUEST)
-                            // It's convention to send back the version(s) we *do* support
-                            .header("Sec-WebSocket-Version", SUPPORTED_WEBSOCKET_VERSION)
-                            .body(body_empty())
-                            .expect("valid body"));
-                    }
-
-                    Some((key, version))
-                } else {
-                    None
+                let Some(key) = req.headers().get("Sec-WebSocket-Key").cloned() else {
+                    warn!("missing header Sec-WebSocket-Key for websocket relay protocol");
+                    return Ok(builder
+                        .status(StatusCode::BAD_REQUEST)
+                        .body(body_empty())
+                        .expect("valid body"));
                 };
 
-                debug!(?protocol, "upgrading connection");
+                let Some(version) = req.headers().get("Sec-WebSocket-Version").cloned() else {
+                    warn!("missing header Sec-WebSocket-Version for websocket relay protocol");
+                    return Ok(builder
+                        .status(StatusCode::BAD_REQUEST)
+                        .body(body_empty())
+                        .expect("valid body"));
+                };
+
+                if version.as_bytes() != SUPPORTED_WEBSOCKET_VERSION.as_bytes() {
+                    warn!("invalid header Sec-WebSocket-Version: {:?}", version);
+                    return Ok(builder
+                        .status(StatusCode::BAD_REQUEST)
+                        // It's convention to send back the version(s) we *do* support
+                        .header("Sec-WebSocket-Version", SUPPORTED_WEBSOCKET_VERSION)
+                        .body(body_empty())
+                        .expect("valid body"));
+                }
+
+                debug!("upgrading connection");
 
                 // Setup a future that will eventually receive the upgraded
                 // connection and talk a new protocol, and spawn the future
@@ -517,15 +518,10 @@ impl RelayService {
                     async move {
                         match hyper::upgrade::on(&mut req).await {
                             Ok(upgraded) => {
-                                if let Err(err) =
-                                    this.0.relay_connection_handler(protocol, upgraded).await
-                                {
-                                    warn!(
-                                        ?protocol,
-                                        "error accepting upgraded connection: {err:#}",
-                                    );
+                                if let Err(err) = this.0.relay_connection_handler(upgraded).await {
+                                    warn!("error accepting upgraded connection: {err:#}",);
                                 } else {
-                                    debug!(?protocol, "upgraded connection completed");
+                                    debug!("upgraded connection completed");
                                 };
                             }
                             Err(err) => warn!("upgrade error: {err:#}"),
@@ -535,20 +531,17 @@ impl RelayService {
                 );
 
                 // Now return a 101 Response saying we agree to the upgrade to the
-                // HTTP_UPGRADE_PROTOCOL
-                builder = builder
-                    .status(StatusCode::SWITCHING_PROTOCOLS)
-                    .header(UPGRADE, HeaderValue::from_static(protocol.upgrade_header()));
+                // websocket upgrade protocol
+                builder = builder.status(StatusCode::SWITCHING_PROTOCOLS).header(
+                    UPGRADE,
+                    HeaderValue::from_static(WEBSOCKET_UPGRADE_PROTOCOL),
+                );
 
-                if let Some((key, _version)) = websocket_headers {
-                    Ok(builder
-                        .header(SEC_WEBSOCKET_ACCEPT, derive_accept_key(&key))
-                        .header(CONNECTION, "upgrade")
-                        .body(body_full("switching to websocket protocol"))
-                        .expect("valid body"))
-                } else {
-                    Ok(builder.body(body_empty()).expect("valid body"))
-                }
+                Ok(builder
+                    .header(SEC_WEBSOCKET_ACCEPT, derive_accept_key(&key))
+                    .header(CONNECTION, "upgrade")
+                    .body(body_full("switching to websocket protocol"))
+                    .expect("valid body"))
             }
         }
         .boxed()
@@ -564,7 +557,7 @@ impl Service<Request<Incoming>> for RelayService {
         // Create a client if the request hits the relay endpoint.
         if matches!(
             (req.method(), req.uri().path()),
-            (&hyper::Method::GET, LEGACY_RELAY_PATH | RELAY_PATH)
+            (&hyper::Method::GET, RELAY_PATH)
         ) {
             let this = self.clone();
             return Box::pin(async move { this.call_client_conn(req).await.map_err(Into::into) });
@@ -612,16 +605,15 @@ impl Inner {
     /// having sent off the connection this handler returns.
     async fn relay_connection_handler(
         &self,
-        protocol: Protocol,
         upgraded: Upgraded,
     ) -> Result<(), ConnectionHandlerError> {
-        debug!(?protocol, "relay_connection upgraded");
+        debug!("relay_connection upgraded");
         let (io, read_buf) = downcast_upgrade(upgraded)?;
         if !read_buf.is_empty() {
             return Err(BufferNotEmptySnafu { buf: read_buf }.build());
         }
 
-        self.accept(protocol, io).await?;
+        self.accept(io).await?;
         Ok(())
     }
 
@@ -635,25 +627,23 @@ impl Inner {
     ///
     /// [`AsyncRead`]: tokio::io::AsyncRead
     /// [`AsyncWrite`]: tokio::io::AsyncWrite
-    async fn accept(&self, protocol: Protocol, io: MaybeTlsStream) -> Result<(), AcceptError> {
+    async fn accept(&self, io: MaybeTlsStream) -> Result<(), AcceptError> {
         use snafu::ResultExt;
 
-        trace!(?protocol, "accept: start");
-        let mut io = match protocol {
-            Protocol::Relay => {
-                self.metrics.relay_accepts.inc();
-                RelayedStream::Relay(Framed::new(io, RelayCodec::new(self.key_cache.clone())))
-            }
-            Protocol::Websocket => {
-                self.metrics.websocket_accepts.inc();
-                // Since we already did the HTTP upgrade in the previous step,
-                // we use tokio-websockets to handle this connection
-                // Create a server builder with default config
-                let builder = tokio_websockets::ServerBuilder::new();
-                // Serve will create a WebSocketStream on an already upgraded connection
-                let websocket = builder.serve(io);
-                RelayedStream::Ws(websocket, self.key_cache.clone())
-            }
+        trace!("accept: start");
+
+        let io = RateLimited::from_cfg(self.rate_limit, io, self.metrics.clone())
+            .context(RateLimitingMisconfiguredSnafu)?;
+
+        self.metrics.accepts.inc();
+        // Create a server builder with default config
+        let websocket = tokio_websockets::ServerBuilder::new()
+            .limits(tokio_websockets::Limits::default().max_payload_len(Some(MAX_FRAME_SIZE)))
+            // Serve will create a WebSocketStream on an already upgraded connection
+            .serve(io);
+        let mut io = RelayedStream {
+            inner: websocket,
+            key_cache: self.key_cache.clone(),
         };
         trace!("accept: recv client key");
         let (client_key, info) = recv_client_key(&mut io).await.context(RecvClientKeySnafu)?;
@@ -683,7 +673,6 @@ impl Inner {
             stream: io,
             write_timeout: self.write_timeout,
             channel_capacity: PER_CLIENT_SEND_QUEUE_DEPTH,
-            rate_limit: self.rate_limit,
         };
         trace!("accept: create client");
         let node_id = client_conn_builder.node_id;
@@ -869,7 +858,6 @@ mod tests {
     use crate::{
         client::{
             conn::{Conn, ReceivedMessage, SendMessage},
-            streams::MaybeTlsStreamChained,
             Client, ClientBuilder, ConnectError,
         },
         dns::DnsResolver,
@@ -1085,8 +1073,9 @@ mod tests {
     }
 
     async fn make_test_client(client: tokio::io::DuplexStream, key: &SecretKey) -> Result<Conn> {
-        let client = MaybeTlsStreamChained::Mem(client);
-        let client = Conn::new_relay(client, KeyCache::test(), key).await?;
+        let client = crate::client::streams::MaybeTlsStream::Test(client);
+        let client = tokio_websockets::ClientBuilder::new().take_over(client);
+        let client = Conn::new(client, KeyCache::test(), key).await?;
         Ok(client)
     }
 
@@ -1108,10 +1097,8 @@ mod tests {
         let public_key_a = key_a.public();
         let (client_a, rw_a) = tokio::io::duplex(10);
         let s = service.clone();
-        let handler_task = tokio::spawn(async move {
-            s.0.accept(Protocol::Relay, MaybeTlsStream::Test(rw_a))
-                .await
-        });
+        let handler_task =
+            tokio::spawn(async move { s.0.accept(MaybeTlsStream::Test(rw_a)).await });
         let mut client_a = make_test_client(client_a, &key_a).await?;
         handler_task.await.context("join")??;
 
@@ -1120,10 +1107,8 @@ mod tests {
         let public_key_b = key_b.public();
         let (client_b, rw_b) = tokio::io::duplex(10);
         let s = service.clone();
-        let handler_task = tokio::spawn(async move {
-            s.0.accept(Protocol::Relay, MaybeTlsStream::Test(rw_b))
-                .await
-        });
+        let handler_task =
+            tokio::spawn(async move { s.0.accept(MaybeTlsStream::Test(rw_b)).await });
         let mut client_b = make_test_client(client_b, &key_b).await?;
         handler_task.await.context("join")??;
 
@@ -1196,10 +1181,8 @@ mod tests {
         let public_key_a = key_a.public();
         let (client_a, rw_a) = tokio::io::duplex(10);
         let s = service.clone();
-        let handler_task = tokio::spawn(async move {
-            s.0.accept(Protocol::Relay, MaybeTlsStream::Test(rw_a))
-                .await
-        });
+        let handler_task =
+            tokio::spawn(async move { s.0.accept(MaybeTlsStream::Test(rw_a)).await });
         let mut client_a = make_test_client(client_a, &key_a).await?;
         handler_task.await.context("join")??;
 
@@ -1208,10 +1191,8 @@ mod tests {
         let public_key_b = key_b.public();
         let (client_b, rw_b) = tokio::io::duplex(10);
         let s = service.clone();
-        let handler_task = tokio::spawn(async move {
-            s.0.accept(Protocol::Relay, MaybeTlsStream::Test(rw_b))
-                .await
-        });
+        let handler_task =
+            tokio::spawn(async move { s.0.accept(MaybeTlsStream::Test(rw_b)).await });
         let mut client_b = make_test_client(client_b, &key_b).await?;
         handler_task.await.context("join")??;
 
@@ -1254,10 +1235,8 @@ mod tests {
         info!("Create client B and connect it to the server");
         let (new_client_b, new_rw_b) = tokio::io::duplex(10);
         let s = service.clone();
-        let handler_task = tokio::spawn(async move {
-            s.0.accept(Protocol::Relay, MaybeTlsStream::Test(new_rw_b))
-                .await
-        });
+        let handler_task =
+            tokio::spawn(async move { s.0.accept(MaybeTlsStream::Test(new_rw_b)).await });
         let mut new_client_b = make_test_client(new_client_b, &key_b).await?;
         handler_task.await.context("join")??;
 

iroh-relay/Cargo.toml
@@ -89,7 +89,7 @@ rustls-cert-file-reader = { version = "0.4.1", optional = true }
 rustls-pemfile = { version = "2.1", optional = true }
 time = { version = "0.3.37", optional = true }
 tokio-rustls-acme = { version = "0.7.1", optional = true }
-tokio-websockets = { version = "0.11.3", features = ["rustls-bring-your-own-connector", "ring", "getrandom", "rand", "server"], optional = true } # server-side websocket implementation
+tokio-websockets = { version = "0.12", features = ["rustls-bring-your-own-connector", "ring", "getrandom", "rand", "server"], optional = true } # server-side websocket implementation
 simdutf8 = { version = "0.1.5", optional = true } # minimal version fix
 toml = { version = "0.8", optional = true }
 tracing-subscriber = { version = "0.3", features = [
@@ -110,7 +110,7 @@ tokio = { version = "1", features = [
     "signal",
     "process",
 ] }
-tokio-websockets = { version = "0.11.3", features = ["rustls-bring-your-own-connector", "ring", "getrandom", "rand", "client"] }
+tokio-websockets = { version = "0.12", features = ["rustls-bring-your-own-connector", "ring", "getrandom", "rand", "client"] }
 
 # wasm-in-browser dependencies
 [target.'cfg(all(target_family = "wasm", target_os = "unknown"))'.dependencies]

iroh-relay/src/client.rs
@@ -25,11 +25,7 @@ use url::Url;
 pub use self::conn::{ReceivedMessage, RecvError, SendError, SendMessage};
 #[cfg(not(wasm_browser))]
 use crate::dns::{DnsError, DnsResolver};
-use crate::{
-    http::{Protocol, RELAY_PATH},
-    protos::relay::SendError as SendRelayError,
-    KeyCache,
-};
+use crate::{http::RELAY_PATH, protos::relay::SendError as SendRelayError, KeyCache};
 
 pub(crate) mod conn;
 #[cfg(not(wasm_browser))]
@@ -125,8 +121,6 @@ pub struct ClientBuilder {
     is_prober: bool,
     /// Server url.
     url: RelayUrl,
-    /// Relay protocol
-    protocol: Protocol,
     /// Allow self-signed certificates from relay servers
     #[cfg(any(test, feature = "test-utils"))]
     insecure_skip_cert_verify: bool,
@@ -153,9 +147,6 @@ impl ClientBuilder {
             is_prober: false,
             url: url.into(),
 
-            // Resolves to websockets in browsers and relay otherwise
-            protocol: Protocol::default(),
-
             #[cfg(any(test, feature = "test-utils"))]
             insecure_skip_cert_verify: false,
 
@@ -165,13 +156,6 @@ impl ClientBuilder {
             dns_resolver,
             key_cache: KeyCache::new(128),
         }
-    }
-
-    /// Sets whether to connect to the relay via websockets or not.
-    /// Set to use non-websocket, normal relaying by default.
-    pub fn protocol(mut self, protocol: Protocol) -> Self {
-        self.protocol = protocol;
-        self
     }
 
     /// Returns if we should prefer ipv6
@@ -216,41 +200,97 @@ impl ClientBuilder {
     }
 
     /// Establishes a new connection to the relay server.
+    #[cfg(not(wasm_browser))]
     pub async fn connect(&self) -> Result<Client, ConnectError> {
-        let (conn, local_addr) = match self.protocol {
-            #[cfg(wasm_browser)]
-            Protocol::Websocket => {
-                let conn = self.connect_ws().await?;
-                let local_addr = None;
-                (conn, local_addr)
+        use tls::MaybeTlsStreamBuilder;
+
+        use crate::protos::relay::MAX_FRAME_SIZE;
+
+        let mut dial_url = (*self.url).clone();
+        dial_url.set_path(RELAY_PATH);
+        // The relay URL is exchanged with the http(s) scheme in tickets and similar.
+        // We need to use the ws:// or wss:// schemes when connecting with websockets, though.
+        dial_url
+            .set_scheme(match self.url.scheme() {
+                "http" => "ws",
+                "ws" => "ws",
+                _ => "wss",
+            })
+            .map_err(|_| {
+                InvalidWebsocketUrlSnafu {
+                    url: dial_url.clone(),
+                }
+                .build()
+            })?;
+
+        debug!(%dial_url, "Dialing relay by websocket");
+
+        #[allow(unused_mut)]
+        let mut builder = MaybeTlsStreamBuilder::new(dial_url.clone(), self.dns_resolver.clone())
+            .prefer_ipv6(self.prefer_ipv6())
+            .proxy_url(self.proxy_url.clone());
+
+        #[cfg(any(test, feature = "test-utils"))]
+        if self.insecure_skip_cert_verify {
+            builder = builder.insecure_skip_cert_verify(self.insecure_skip_cert_verify);
+        }
+
+        let stream = builder.connect().await?;
+        let local_addr = stream
+            .as_ref()
+            .local_addr()
+            .map_err(|_| NoLocalAddrSnafu.build())?;
+        let (conn, response) = tokio_websockets::ClientBuilder::new()
+            .uri(dial_url.as_str())
+            .map_err(|_| {
+                InvalidRelayUrlSnafu {
+                    url: dial_url.clone(),
+                }
+                .build()
+            })?
+            .limits(tokio_websockets::Limits::default().max_payload_len(Some(MAX_FRAME_SIZE)))
+            .connect_on(stream)
+            .await?;
+
+        if response.status() != hyper::StatusCode::SWITCHING_PROTOCOLS {
+            UnexpectedUpgradeStatusSnafu {
+                code: response.status(),
             }
-            #[cfg(not(wasm_browser))]
-            Protocol::Websocket => {
-                let (conn, local_addr) = self.connect_ws().await?;
-                (conn, Some(local_addr))
-            }
-            #[cfg(not(wasm_browser))]
-            Protocol::Relay => {
-                let (conn, local_addr) = self.connect_relay().await?;
-                (conn, Some(local_addr))
-            }
-            #[cfg(wasm_browser)]
-            Protocol::Relay => return Err(RelayProtoNotAvailableSnafu.build()),
-        };
+            .fail()?;
+        }
+
+        let conn = Conn::new(conn, self.key_cache.clone(), &self.secret_key).await?;
 
         event!(
             target: "events.net.relay.connected",
             Level::DEBUG,
             url = %self.url,
-            protocol = ?self.protocol,
         );
 
         trace!("connect done");
-        Ok(Client { conn, local_addr })
+
+        Ok(Client {
+            conn,
+            local_addr: Some(local_addr),
+        })
     }
 
+    /// Reports whether IPv4 dials should be slightly
+    /// delayed to give IPv6 a better chance of winning dial races.
+    /// Implementations should only return true if IPv6 is expected
+    /// to succeed. (otherwise delaying IPv4 will delay the connection
+    /// overall)
+    #[cfg(not(wasm_browser))]
+    fn prefer_ipv6(&self) -> bool {
+        match self.address_family_selector {
+            Some(ref selector) => selector(),
+            None => false,
+        }
+    }
+
+    /// Establishes a new connection to the relay server.
     #[cfg(wasm_browser)]
-    async fn connect_ws(&self) -> Result<Conn, ConnectError> {
+    pub async fn connect(&self) -> Result<Client, ConnectError> {
         let mut dial_url = (*self.url).clone();
         dial_url.set_path(RELAY_PATH);
         // The relay URL is exchanged with the http(s) scheme in tickets and similar.
@@ -271,9 +311,20 @@ impl ClientBuilder {
         debug!(%dial_url, "Dialing relay by websocket");
 
         let (_, ws_stream) = ws_stream_wasm::WsMeta::connect(dial_url.as_str(), None).await?;
-        let conn =
-            Conn::new_ws_browser(ws_stream, self.key_cache.clone(), &self.secret_key).await?;
-        Ok(conn)
+        let conn = Conn::new(ws_stream, self.key_cache.clone(), &self.secret_key).await?;
+
+        event!(
+            target: "events.net.relay.connected",
+            Level::DEBUG,
+            url = %self.url,
+        );
+
+        trace!("connect done");
+
+        Ok(Client {
+            conn,
+            local_addr: None,
+        })
     }
 }
 

commit 8c558a1c2e6d53ca0a20b0c2a4d78efe3beab45a

fix(iroh-relay): Don't double-count connection accepts (#3436)
## Description

- Wrote a test checking that accepts == disconnects in relay metrics
- Fixed duplicate accept counts

Bug was originally introduced in #3384: We used to
`metrics.relay_accepts.inc()` or `metrics.websocket_accepts.inc()` and I
thought there should be a replacement for these calls,
`metrics.accepts.inc()` (along with removing the above two).
Turns out the accepts are already tracked somewhere else, so I ended up
counting them twice.

## Change checklist
<!-- Remove any that are not relevant. -->
- [x] Self-review.

iroh-relay/src/server/http_server.rs
@@ -665,7 +665,6 @@ impl Inner {
         let io = RateLimited::from_cfg(self.rate_limit, io, self.metrics.clone())
             .context(RateLimitingMisconfiguredSnafu)?;
 
-        self.metrics.accepts.inc();
         // Create a server builder with default config
         let websocket = tokio_websockets::ServerBuilder::new()
             .limits(tokio_websockets::Limits::default().max_payload_len(Some(MAX_FRAME_SIZE)))
@@ -1115,13 +1114,14 @@ mod tests {
     #[traced_test]
     async fn test_server_basic() -> Result {
         info!("Create the server.");
+        let metrics = Arc::new(Metrics::default());
         let service = RelayService::new(
             Default::default(),
             Default::default(),
             None,
             KeyCache::test(),
             AccessConfig::Everyone,
-            Default::default(),
+            metrics.clone(),
         );
 
         info!("Create client A and connect it to the server.");
@@ -1199,6 +1199,14 @@ mod tests {
             .await;
         assert!(res.is_err());
         assert!(client_b.next().await.is_none());
+
+        drop(client_a);
+        drop(client_b);
+
+        service.shutdown().await;
+
+        assert_eq!(metrics.accepts.get(), metrics.disconnects.get());
+
         Ok(())
     }
 

iroh/src/discovery/pkarr/dht.rs
@@ -216,7 +216,7 @@ impl Builder {
                 }
                 if let Some(url) = &self.pkarr_relay {
                     builder
-                        .relays(&[url.clone()])
+                        .relays(std::slice::from_ref(url))
                         .map_err(|e| IntoDiscoveryError::from_err("pkarr", e))?;
                 }
                 builder