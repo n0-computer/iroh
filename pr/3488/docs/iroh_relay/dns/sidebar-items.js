869039d90c1f59af4ce50b89c4de84007da43e80
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-09-30 10:44:38 +0000

    deploy: dc9886a6f584e68db1ff8b9d060e28c8b7866480

---
+++ pr/3488/docs/iroh_relay/dns/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["N0_DNS_NODE_ORIGIN_PROD","N0_DNS_NODE_ORIGIN_STAGING"],"enum":["DnsError","LookupError"],"struct":["DnsResolver","StaggeredError","TxtRecordData"],"trait":["Resolver"],"type":["BoxIter"]};
