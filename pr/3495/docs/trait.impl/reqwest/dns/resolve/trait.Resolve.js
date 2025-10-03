2d18e28de12972d12510f85f14282569636c38cd
ramfox <ramfox@users.noreply.github.com>
2025-10-03 02:47:56 +0000

    deploy: fcbcd78f9bbe1ab764c4b3add2c92b31dbac8fab

---
+++ pr/3495/docs/trait.impl/reqwest/dns/resolve/trait.Resolve.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl Resolve for <a class=\"struct\" href=\"iroh_relay/dns/struct.DnsResolver.html\" title=\"struct iroh_relay::dns::DnsResolver\">DnsResolver</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[167]}
