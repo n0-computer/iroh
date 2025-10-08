eb085d1a0ece0e44fe624589edbcac36dda6eb6b
“ramfox” <kasey@n0.computer>
2025-10-08 10:07:58 -0400

    docs: fix doc path

--- iroh/src/endpoint.rs
+++ iroh/src/endpoint.rs
@@ -967,8 +967,8 @@ impl Endpoint {
     ///
     /// This has no timeout, so if that is needed, you need to wrap it in a
     /// timeout. We recommend using a timeout slightly larger than the
-    /// [`iroh::net_report::defaults::PROBES_TIMEOUT`], so you can be sure that at least one
-    /// [`iroh::net_report::Report`] has been attempted.
+    /// [`crate::net_report::defaults::PROBES_TIMEOUT`], so you can be sure that at least one
+    /// [`crate::net_report::Report`] has been attempted.
     ///
     /// To understand if the endpoint has gone back "offline",
     /// you must use the [`Endpoint::watch_node_addr`] method, to
