c2162d8b8d4b7154cdef8c9b0fc89d1d73a4c8e1
Frando <Frando@users.noreply.github.com>
2025-10-21 12:00:03 +0000

    deploy: 04d26e3b031d59d09d3292a3e13fdd492f30c24a

--- pr/3563/docs/iroh_base/sidebar-items.js
+++ pr/3563/docs/iroh_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["KeyParsingError","TransportAddr"],"mod":["ticket"],"struct":["EndpointAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature","SignatureError"],"type":["EndpointId"]};
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError","TransportAddr"],"struct":["EndpointAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature","SignatureError"],"type":["EndpointId"]};
