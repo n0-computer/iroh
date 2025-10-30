850560fe7b5de777c6d69d5fb29ff265af2baa60
ramfox <ramfox@users.noreply.github.com>
2025-10-30 22:31:59 +0000

    deploy: 19a6c034d2aedf17c23c77cf3a494250373f694f

--- pr/3556/docs/trait.impl/core/future/future/trait.Future.js
+++ pr/3556/docs/trait.impl/core/future/future/trait.Future.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.Accept.html\" title=\"struct iroh::endpoint::Accept\">Accept</a>&lt;'_&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.Connecting.html\" title=\"struct iroh::endpoint::Connecting\">Connecting</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.IncomingFuture.html\" title=\"struct iroh::endpoint::IncomingFuture\">IncomingFuture</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/connection/struct.Accept.html\" title=\"struct iroh::endpoint::connection::Accept\">Accept</a>&lt;'_&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/connection/struct.Connecting.html\" title=\"struct iroh::endpoint::connection::Connecting\">Connecting</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"iroh/endpoint/connection/struct.IncomingFuture.html\" title=\"struct iroh::endpoint::connection::IncomingFuture\">IncomingFuture</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[899]}
+//{"start":57,"fragment_lengths":[968]}
