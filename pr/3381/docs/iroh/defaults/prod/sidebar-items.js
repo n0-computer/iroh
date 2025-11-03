6210732c87e3c7af1eef015c7d0a8c326059d938
dignifiedquire <dignifiedquire@users.noreply.github.com>
2025-11-03 12:10:08 +0000

    deploy: 007723775df734c02067e530fd7c009951b283e3

--- pr/3381/docs/iroh/defaults/prod/sidebar-items.js
+++ pr/3381/docs/iroh/defaults/prod/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["AP_RELAY_HOSTNAME","EU_RELAY_HOSTNAME","NA_RELAY_HOSTNAME"],"fn":["default_ap_relay_node","default_eu_relay_node","default_na_relay_node","default_relay_map"]};
+window.SIDEBAR_ITEMS = {"constant":["AP_RELAY_HOSTNAME","EU_RELAY_HOSTNAME","NA_RELAY_HOSTNAME"],"fn":["default_ap_relay","default_eu_relay","default_na_relay","default_relay_map"]};
