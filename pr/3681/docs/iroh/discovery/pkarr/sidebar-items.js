3c09dff1266b30e45140f9f7c4c020bf344066ad
matheus23 <matheus23@users.noreply.github.com>
2025-11-20 09:19:02 +0000

    deploy: 58f21a98fefaa49ef5a6d44df8826a0cae97b384

---
+++ pr/3681/docs/iroh/discovery/pkarr/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_PKARR_TTL","DEFAULT_REPUBLISH_INTERVAL","N0_DNS_PKARR_RELAY_PROD","N0_DNS_PKARR_RELAY_STAGING"],"enum":["PkarrError"],"mod":["dht"],"struct":["PkarrPublisher","PkarrPublisherBuilder","PkarrRelayClient","PkarrResolver","PkarrResolverBuilder"]};
