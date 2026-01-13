6b5cd770b21f89bdcd2ae7bcfc8512d13d44f56e
rklaehn <rklaehn@users.noreply.github.com>
2026-01-13 11:08:31 +0000

    deploy: 88a8b8a7802382af7047fc212ad808abae3bfe39

---
+++ pr/3845/docs/iroh_base/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["KeyParsingError","TransportAddr"],"struct":["EndpointAddr","PublicKey","RelayUrl","RelayUrlParseError","SecretKey","Signature","SignatureError","UserAddr"],"type":["EndpointId"]};
---
+++ pr/3845/docs/iroh_dns_server/http/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["CertMode","RateLimitConfig"],"struct":["HttpConfig","HttpServer","HttpsConfig"]};
