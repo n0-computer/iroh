4d647c10621a0312aa730ba2d8bce09fca4c6226
rklaehn <rklaehn@users.noreply.github.com>
2026-01-29 13:57:36 +0000

    deploy: ba7e9b1ae23e664d9f69cd0e1a3b745019c697af

---
+++ pr/3898/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl Stream for <a class=\"struct\" href=\"iroh_relay/client/struct.Client.html\" title=\"struct iroh_relay::client::Client\">Client</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_relay/client/struct.ClientStream.html\" title=\"struct iroh_relay::client::ClientStream\">ClientStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[316]}
