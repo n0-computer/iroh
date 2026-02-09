e90911614ffc109c0cb396333144cbc722525105
Frando <Frando@users.noreply.github.com>
2026-02-09 11:43:10 +0000

    deploy: c58a63604bff0fd2f6b751f9baa8579b9bc19e95

---
+++ pr/3899/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.PathInfoListIntoIter.html\" title=\"struct iroh::endpoint::PathInfoListIntoIter\">PathInfoListIntoIter</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[354]}
