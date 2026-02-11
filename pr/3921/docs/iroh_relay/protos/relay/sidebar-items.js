de1e8e75bc714b2bffdf7bcecf92d8b8b8acf48c
Frando <Frando@users.noreply.github.com>
2026-02-11 12:10:43 +0000

    deploy: b7de7c3b0f6a65d9e25ed9bb0dcba8e091b31e30

--- pr/3921/docs/iroh_relay/protos/relay/sidebar-items.js
+++ pr/3921/docs/iroh_relay/protos/relay/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["MAX_PACKET_SIZE","PER_CLIENT_SEND_QUEUE_DEPTH"],"enum":["ClientToRelayMsg","CloseReason","Error","RelayToClientMsg"],"struct":["Datagrams"]};
+window.SIDEBAR_ITEMS = {"constant":["MAX_PACKET_SIZE","PER_CLIENT_SEND_QUEUE_DEPTH"],"enum":["ClientToRelayMsg","Error","RelayToClientMsg"],"struct":["Datagrams"]};
