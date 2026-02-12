70ca9da0d947336b95f3eecdaa8591bcda872c9b
Frando <Frando@users.noreply.github.com>
2026-02-12 10:28:09 +0000

    deploy: 9328a5d5e0f8d9cba24435f27be1b9df0e39eede

--- pr/3899/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ pr/3899/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.PathInfoListIntoIter.html\" title=\"struct iroh::endpoint::PathInfoListIntoIter\">PathInfoListIntoIter</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.PathInfoListIter.html\" title=\"struct iroh::endpoint::PathInfoListIter\">PathInfoListIter</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[354]}
+//{"start":57,"fragment_lengths":[342]}
