93925fef488cf52f0136996dae6bbf635d34f305
Frando <Frando@users.noreply.github.com>
2026-02-18 15:03:11 +0000

    deploy: f1a4c00d6fd09252a73ec3d18ed40415d4226814

---
+++ pr/3955/docs/trait.impl/strum/trait.VariantArray.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_relay",[["impl VariantArray for <a class=\"enum\" href=\"iroh_relay/http/enum.ProtocolVersion.html\" title=\"enum iroh_relay::http::ProtocolVersion\">ProtocolVersion</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[180]}
