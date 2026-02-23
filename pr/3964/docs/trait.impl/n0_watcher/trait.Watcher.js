81f2ae2cf699234ff0fc16a78984ef567bcacaf8
Frando <Frando@users.noreply.github.com>
2026-02-23 11:15:05 +0000

    deploy: d76070c209cdb444cd7fd2b0b470c95e06a25bdf

---
+++ pr/3964/docs/trait.impl/n0_watcher/trait.Watcher.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"iroh/trait.Watcher.html\" title=\"trait iroh::Watcher\">Watcher</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.PathWatcher.html\" title=\"struct iroh::endpoint::PathWatcher\">PathWatcher</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[245]}
