a2ff33aa42c15a93a78a129091b1fac330bee1c7
Frando <frando@unbiskant.org>
2026-03-11 14:46:28 +0100

    fix: make DNS_TIMEOUT public

--- iroh-relay/src/defaults.rs
+++ iroh-relay/src/defaults.rs
@@ -34,7 +34,7 @@ pub mod timeouts {
     /// Default timeout for DNS queries issued by [`DnsResolver`].
     ///
     /// [`DnsResolver`]: crate::dns::DnsResolver
-    pub(crate) const DNS_TIMEOUT: Duration = Duration::from_secs(3);
+    pub const DNS_TIMEOUT: Duration = Duration::from_secs(3);
 
     /// Maximum time the server will attempt to get a successful write to the connection.
     #[cfg(feature = "server")]
