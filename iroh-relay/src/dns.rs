546b2818351aa64388f946343ff7633a0d1e5dcc
dignifiedquire <me@dignifiedquire.com>
2026-03-13 14:53:53 +0100

    feat: minimal dns resolver using simple-dns

--- iroh-relay/src/dns.rs
+++ iroh-relay/src/dns.rs
@@ -6,6 +6,17 @@
 //! See the [`endpoint_info`] module documentation for details on how iroh endpoint records
 //! are structured.
 
+#[cfg(not(wasm_browser))]
+mod cache;
+#[cfg(not(wasm_browser))]
+mod query;
+#[cfg(not(wasm_browser))]
+mod resolver;
+#[cfg(not(wasm_browser))]
+mod system_config;
+#[cfg(not(wasm_browser))]
+mod transport;
+
 use std::{
     fmt,
     future::Future,
@@ -13,11 +24,6 @@ use std::{
     sync::Arc,
 };
 
-use hickory_resolver::{
-    TokioResolver,
-    config::{ResolverConfig, ResolverOpts},
-    name_server::TokioConnectionProvider,
-};
 use iroh_base::EndpointId;
 use n0_error::{StackError, e, stack_error};
 use n0_future::{
@@ -26,9 +32,10 @@ use n0_future::{
     time::{self, Duration},
 };
 use tokio::sync::RwLock;
-use tracing::debug;
 use url::Url;
 
+#[cfg(not(wasm_browser))]
+use self::resolver::SimpleDnsResolver;
 use crate::{
     defaults::timeouts::DNS_TIMEOUT,
     endpoint_info::{self, EndpointInfo, ParseError},
@@ -84,10 +91,21 @@ pub enum DnsError {
     },
     #[error("missing host")]
     MissingHost {},
-    #[error(transparent)]
-    Resolve {
-        source: hickory_resolver::ResolveError,
+    #[cfg(not(wasm_browser))]
+    #[error("invalid DNS packet")]
+    InvalidPacket {
+        #[error(std_err)]
+        source: simple_dns::SimpleDnsError,
     },
+    #[error("DNS transport error")]
+    Transport { source: std::io::Error },
+    #[error("DNS-over-HTTPS transport error")]
+    HttpTransport {
+        #[error(std_err)]
+        source: reqwest::Error,
+    },
+    #[error("DNS server returned error: {rcode}")]
+    ServerError { rcode: String },
     #[error("invalid DNS response: not a query for _iroh.z32encodedpubkey")]
     InvalidResponse {},
 }
@@ -156,20 +174,6 @@ pub enum DnsProtocol {
     Https,
 }
 
-impl DnsProtocol {
-    fn to_hickory(self) -> hickory_resolver::proto::xfer::Protocol {
-        use hickory_resolver::proto::xfer::Protocol;
-        match self {
-            DnsProtocol::Udp => Protocol::Udp,
-            DnsProtocol::Tcp => Protocol::Tcp,
-            #[cfg(with_crypto_provider)]
-            DnsProtocol::Tls => Protocol::Tls,
-            #[cfg(with_crypto_provider)]
-            DnsProtocol::Https => Protocol::Https,
-        }
-    }
-}
-
 impl Builder {
     /// Makes the builder respect the host system's DNS configuration.
     ///
@@ -208,7 +212,8 @@ impl Builder {
 
     /// Builds the DNS resolver.
     pub fn build(self) -> DnsResolver {
-        DnsResolver(Arc::new(RwLock::new(HickoryResolver::new(self))))
+        let resolver = SimpleDnsResolver::new(self);
+        DnsResolver(DnsResolverInner::Simple(Arc::new(RwLock::new(resolver))))
     }
 }
 
@@ -500,111 +505,59 @@ impl reqwest::dns::Resolve for DnsResolver {
     }
 }
 
-#[derive(Debug)]
-struct HickoryResolver {
-    resolver: TokioResolver,
-    builder: Builder,
+/// Wrapper enum that contains either the built-in simple-dns resolver or a custom resolver.
+///
+/// We do this to save the cost of boxing the futures and iterators when using
+/// the built-in resolver.
+#[derive(Debug, Clone)]
+enum DnsResolverInner {
+    Simple(Arc<RwLock<SimpleDnsResolver>>),
+    Custom(Arc<RwLock<dyn Resolver>>),
 }
 
-impl HickoryResolver {
-    fn new(builder: Builder) -> Self {
-        let resolver = Self::build_resolver(&builder);
-        Self { resolver, builder }
-    }
-
-    fn build_resolver(builder: &Builder) -> TokioResolver {
-        let (mut config, mut options) = if builder.use_system_defaults {
-            match Self::system_config() {
-                Ok((config, options)) => (config, options),
-                Err(error) => {
-                    debug!(%error, "Failed to read the system's DNS config, using fallback DNS servers.");
-                    (ResolverConfig::google(), ResolverOpts::default())
-                }
-            }
-        } else {
-            (ResolverConfig::new(), ResolverOpts::default())
-        };
-
-        #[cfg(with_crypto_provider)]
-        if let Some(client_config) = builder.tls_client_config.clone() {
-            options.tls_config = client_config;
-        }
-
-        for (addr, proto) in builder.nameservers.iter() {
-            let nameserver =
-                hickory_resolver::config::NameServerConfig::new(*addr, proto.to_hickory());
-            config.add_name_server(nameserver);
-        }
-
-        // see [`DnsResolver::lookup_ipv4_ipv6`] for info on why we avoid `LookupIpStrategy::Ipv4AndIpv6`
-        options.ip_strategy = hickory_resolver::config::LookupIpStrategy::Ipv4thenIpv6;
-        options.negative_max_ttl = Some(Duration::ZERO);
-
-        let mut hickory_builder =
-            TokioResolver::builder_with_config(config, TokioConnectionProvider::default());
-        *hickory_builder.options_mut() = options;
-        hickory_builder.build()
-    }
-
-    fn system_config() -> Result<(ResolverConfig, ResolverOpts), hickory_resolver::ResolveError> {
-        let (system_config, options) = hickory_resolver::system_conf::read_system_conf()?;
-
-        // Copy all of the system config, but strip the bad windows nameservers.  Unfortunately
-        // there is no easy way to do this.
-        let mut config = hickory_resolver::config::ResolverConfig::new();
-        if let Some(name) = system_config.domain() {
-            config.set_domain(name.clone());
-        }
-        for name in system_config.search() {
-            config.add_search(name.clone());
-        }
-        for nameserver_cfg in system_config.name_servers() {
-            if !WINDOWS_BAD_SITE_LOCAL_DNS_SERVERS.contains(&nameserver_cfg.socket_addr.ip()) {
-                config.add_name_server(nameserver_cfg.clone());
-            }
-        }
-        Ok((config, options))
-    }
-}
-
-impl Resolver for HickoryResolver {
-    fn lookup_ipv4(&self, host: String) -> BoxFuture<Result<BoxIter<Ipv4Addr>, DnsError>> {
-        let resolver = self.resolver.clone();
-        Box::pin(async move {
-            let lookup = resolver.ipv4_lookup(host).await?;
-            let iter: BoxIter<Ipv4Addr> = Box::new(lookup.into_iter().map(Ipv4Addr::from));
-            Ok(iter)
+impl DnsResolverInner {
+    async fn lookup_ipv4(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = Ipv4Addr> + use<>, DnsError> {
+        Ok(match self {
+            Self::Simple(resolver) => Either::Left(resolver.read().await.lookup_ipv4(host).await?),
+            Self::Custom(resolver) => Either::Right(resolver.read().await.lookup_ipv4(host).await?),
         })
     }
 
-    fn lookup_ipv6(&self, host: String) -> BoxFuture<Result<BoxIter<Ipv6Addr>, DnsError>> {
-        let resolver = self.resolver.clone();
-        Box::pin(async move {
-            let lookup = resolver.ipv6_lookup(host).await?;
-            let iter: BoxIter<Ipv6Addr> = Box::new(lookup.into_iter().map(Ipv6Addr::from));
-            Ok(iter)
+    async fn lookup_ipv6(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = Ipv6Addr> + use<>, DnsError> {
+        Ok(match self {
+            Self::Simple(resolver) => Either::Left(resolver.read().await.lookup_ipv6(host).await?),
+            Self::Custom(resolver) => Either::Right(resolver.read().await.lookup_ipv6(host).await?),
         })
     }
 
-    fn lookup_txt(&self, host: String) -> BoxFuture<Result<BoxIter<TxtRecordData>, DnsError>> {
-        let resolver = self.resolver.clone();
-        Box::pin(async move {
-            let lookup = resolver.txt_lookup(host).await?;
-            let iter: BoxIter<TxtRecordData> = Box::new(
-                lookup
-                    .into_iter()
-                    .map(|txt| TxtRecordData::from_iter(txt.iter().cloned())),
-            );
-            Ok(iter)
+    async fn lookup_txt(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = TxtRecordData> + use<>, DnsError> {
+        Ok(match self {
+            Self::Simple(resolver) => Either::Left(resolver.read().await.lookup_txt(host).await?),
+            Self::Custom(resolver) => Either::Right(resolver.read().await.lookup_txt(host).await?),
         })
     }
 
-    fn clear_cache(&self) {
-        self.resolver.clear_cache()
+    async fn clear_cache(&self) {
+        match self {
+            Self::Simple(resolver) => resolver.read().await.clear_cache(),
+            Self::Custom(resolver) => resolver.read().await.clear_cache(),
+        }
     }
 
-    fn reset(&mut self) {
-        self.resolver = Self::build_resolver(&self.builder);
+    async fn reset(&self) {
+        match self {
+            Self::Simple(resolver) => resolver.write().await.reset(),
+            Self::Custom(resolver) => resolver.write().await.reset(),
+        }
     }
 }
 
@@ -650,19 +603,22 @@ impl From<Vec<Box<[u8]>>> for TxtRecordData {
     }
 }
 
-/// Deprecated IPv6 site-local anycast addresses still configured by windows.
-///
-/// Windows still configures these site-local addresses as soon even as an IPv6 loopback
-/// interface is configured.  We do not want to use these DNS servers, the chances of them
-/// being usable are almost always close to zero, while the chance of DNS configuration
-/// **only** relying on these servers and not also being configured normally are also almost
-/// zero.  The chance of the DNS resolver accidentally trying one of these and taking a
-/// bunch of timeouts to figure out they're no good are on the other hand very high.
-const WINDOWS_BAD_SITE_LOCAL_DNS_SERVERS: [IpAddr; 3] = [
-    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 1)),
-    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 2)),
-    IpAddr::V6(Ipv6Addr::new(0xfec0, 0, 0, 0xffff, 0, 0, 0, 3)),
-];
+/// Helper enum to give a unified type to either of two iterators
+enum Either<A, B> {
+    Left(A),
+    Right(B),
+}
+
+impl<T, A: Iterator<Item = T>, B: Iterator<Item = T>> Iterator for Either<A, B> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Either::Left(iter) => iter.next(),
+            Either::Right(iter) => iter.next(),
+        }
+    }
+}
 
 /// Helper enum to give a unified type to the iterators of [`DnsResolver::lookup_ipv4_ipv6`].
 enum LookupIter<A, B> {
---
+++ iroh-relay/src/dns/resolver.rs
@@ -0,0 +1,329 @@
+//! Built-in DNS resolver using `simple-dns` for packet construction/parsing
+//! and tokio for transport.
+
+use std::{
+    net::{Ipv4Addr, Ipv6Addr, SocketAddr},
+    sync::Arc,
+};
+
+use n0_error::e;
+use rustls::ClientConfig;
+use simple_dns::TYPE;
+use tracing::{debug, trace};
+
+use super::{
+    Builder, DnsError, DnsProtocol, TxtRecordData,
+    cache::{CachedRecord, DnsCache, QueryType},
+    query, system_config, transport,
+};
+
+#[derive(Debug)]
+pub(super) struct SimpleDnsResolver {
+    nameservers: Vec<(SocketAddr, DnsProtocol)>,
+    tls_config: Option<Arc<ClientConfig>>,
+    /// Lazily initialized, cached reqwest client for DNS-over-HTTPS queries.
+    https_client: tokio::sync::Mutex<Option<reqwest::Client>>,
+    cache: std::sync::RwLock<DnsCache>,
+    builder: Builder,
+}
+
+impl SimpleDnsResolver {
+    pub(super) fn new(builder: Builder) -> Self {
+        let nameservers = Self::build_nameservers(&builder);
+        debug!(count = nameservers.len(), "configured DNS nameservers");
+        for (addr, proto) in &nameservers {
+            trace!(%addr, ?proto, "nameserver");
+        }
+        let tls_config = builder
+            .tls_client_config
+            .as_ref()
+            .map(|c: &ClientConfig| Arc::new(c.clone()));
+        Self {
+            nameservers,
+            tls_config,
+            https_client: tokio::sync::Mutex::new(None),
+            cache: std::sync::RwLock::new(DnsCache::new()),
+            builder,
+        }
+    }
+
+    fn build_nameservers(builder: &Builder) -> Vec<(SocketAddr, DnsProtocol)> {
+        let mut nameservers = Vec::new();
+
+        if builder.use_system_defaults {
+            nameservers.extend(system_config::system_nameservers());
+        }
+
+        nameservers.extend(builder.nameservers.iter().copied());
+
+        if nameservers.is_empty() {
+            nameservers.extend(system_config::fallback_nameservers());
+        }
+
+        nameservers
+    }
+
+    /// Returns a clone of the cached reqwest client, creating it on first use.
+    ///
+    /// `reqwest::Client` uses an inner `Arc`, so cloning is cheap.
+    async fn get_or_init_https_client(&self) -> Result<reqwest::Client, DnsError> {
+        let mut guard = self.https_client.lock().await;
+        if let Some(client) = guard.as_ref() {
+            return Ok(client.clone());
+        }
+        let client = transport::build_https_client(self.tls_config.as_ref())?;
+        *guard = Some(client.clone());
+        Ok(client)
+    }
+
+    /// Send a query to the first responding nameserver.
+    async fn send_query(&self, query_bytes: &[u8]) -> Result<Vec<u8>, DnsError> {
+        let mut last_err = None;
+        for (addr, proto) in &self.nameservers {
+            trace!(%addr, ?proto, "sending DNS query");
+            let result = match proto {
+                DnsProtocol::Udp => {
+                    let resp = transport::udp_query(*addr, query_bytes).await?;
+                    // Check for truncation, fallback to TCP
+                    if query::is_truncated(&resp) {
+                        debug!(%addr, "UDP response truncated, retrying over TCP");
+                        transport::tcp_query(*addr, query_bytes).await
+                    } else {
+                        Ok(resp)
+                    }
+                }
+                DnsProtocol::Tcp => transport::tcp_query(*addr, query_bytes).await,
+                DnsProtocol::Tls => {
+                    let tls_config = self.tls_config.as_ref().ok_or_else(|| {
+                        e!(DnsError::Transport {
+                            source: std::io::Error::new(
+                                std::io::ErrorKind::InvalidInput,
+                                "TLS config required for DNS-over-TLS",
+                            ),
+                        })
+                    })?;
+                    transport::tls_query(*addr, query_bytes, tls_config).await
+                }
+                DnsProtocol::Https => {
+                    let client = self.get_or_init_https_client().await?;
+                    transport::https_query(*addr, query_bytes, &client).await
+                }
+            };
+            match result {
+                Ok(resp) => {
+                    trace!(%addr, ?proto, len = resp.len(), "DNS query succeeded");
+                    return Ok(resp);
+                }
+                Err(e) => {
+                    trace!(%addr, ?proto, err = %e, "DNS query failed, trying next nameserver");
+                    last_err = Some(e);
+                }
+            }
+        }
+        Err(last_err.unwrap_or_else(|| e!(DnsError::NoResponse)))
+    }
+
+    pub(super) async fn lookup_ipv4(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = Ipv4Addr> + use<>, DnsError> {
+        if let Ok(mut cache) = self.cache.write()
+            && let Some(CachedRecord::A(addrs)) = cache.get(&host, QueryType::A)
+        {
+            trace!(%host, count = addrs.len(), "A lookup cache hit");
+            return Ok(addrs.into_iter());
+        }
+
+        trace!(%host, "resolving A record");
+        let (id, query_bytes) = query::build_query(&host, TYPE::A)?;
+        let response = self.send_query(&query_bytes).await?;
+        let (addrs, ttl) = query::parse_a_response(&response, id)?;
+        debug!(%host, count = addrs.len(), ttl, "resolved A record");
+
+        if let Ok(mut cache) = self.cache.write() {
+            cache.insert(&host, QueryType::A, CachedRecord::A(addrs.clone()), ttl);
+        }
+
+        Ok(addrs.into_iter())
+    }
+
+    pub(super) async fn lookup_ipv6(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = Ipv6Addr> + use<>, DnsError> {
+        if let Ok(mut cache) = self.cache.write()
+            && let Some(CachedRecord::Aaaa(addrs)) = cache.get(&host, QueryType::AAAA)
+        {
+            trace!(%host, count = addrs.len(), "AAAA lookup cache hit");
+            return Ok(addrs.into_iter());
+        }
+
+        trace!(%host, "resolving AAAA record");
+        let (id, query_bytes) = query::build_query(&host, TYPE::AAAA)?;
+        let response = self.send_query(&query_bytes).await?;
+        let (addrs, ttl) = query::parse_aaaa_response(&response, id)?;
+        debug!(%host, count = addrs.len(), ttl, "resolved AAAA record");
+
+        if let Ok(mut cache) = self.cache.write() {
+            cache.insert(
+                &host,
+                QueryType::AAAA,
+                CachedRecord::Aaaa(addrs.clone()),
+                ttl,
+            );
+        }
+
+        Ok(addrs.into_iter())
+    }
+
+    pub(super) async fn lookup_txt(
+        &self,
+        host: String,
+    ) -> Result<impl Iterator<Item = TxtRecordData> + use<>, DnsError> {
+        if let Ok(mut cache) = self.cache.write()
+            && let Some(CachedRecord::Txt(records)) = cache.get(&host, QueryType::TXT)
+        {
+            trace!(%host, count = records.len(), "TXT lookup cache hit");
+            return Ok(records.into_iter());
+        }
+
+        trace!(%host, "resolving TXT record");
+        let (id, query_bytes) = query::build_query(&host, TYPE::TXT)?;
+        let response = self.send_query(&query_bytes).await?;
+        let (records, ttl) = query::parse_txt_response(&response, id)?;
+        debug!(%host, count = records.len(), ttl, "resolved TXT record");
+
+        if let Ok(mut cache) = self.cache.write() {
+            cache.insert(
+                &host,
+                QueryType::TXT,
+                CachedRecord::Txt(records.clone()),
+                ttl,
+            );
+        }
+
+        Ok(records.into_iter())
+    }
+
+    pub(super) fn clear_cache(&self) {
+        if let Ok(mut cache) = self.cache.write() {
+            cache.clear();
+        }
+    }
+
+    pub(super) fn reset(&mut self) {
+        self.nameservers = Self::build_nameservers(&self.builder);
+        self.tls_config = self
+            .builder
+            .tls_client_config
+            .as_ref()
+            .map(|c: &ClientConfig| Arc::new(c.clone()));
+        // Clear cached HTTPS client so it gets rebuilt with the new TLS config on next use
+        *self.https_client.get_mut() = None;
+        if let Ok(mut cache) = self.cache.write() {
+            cache.clear();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+    use n0_future::time::Duration;
+
+    use super::super::{DnsProtocol, DnsResolver};
+
+    const TIMEOUT: Duration = Duration::from_secs(5);
+    const GOOGLE_DNS: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)), 53);
+    const CLOUDFLARE_DNS: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1)), 53);
+    const GOOGLE_DNS_TLS: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)), 853);
+    const CLOUDFLARE_DNS_HTTPS: SocketAddr =
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1)), 443);
+
+    fn with_proto(addr: SocketAddr, proto: DnsProtocol) -> DnsResolver {
+        let mut builder = DnsResolver::builder().with_nameserver(addr, proto);
+        if proto == DnsProtocol::Tls {
+            let root_store =
+                rustls::RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+            builder = builder.tls_client_config(
+                rustls::ClientConfig::builder()
+                    .with_root_certificates(root_store)
+                    .with_no_client_auth(),
+            );
+        }
+        builder.build()
+    }
+
+    async fn assert_resolves_ipv4(resolver: &DnsResolver, host: &str) {
+        let addrs: Vec<_> = resolver.lookup_ipv4(host, TIMEOUT).await.unwrap().collect();
+        assert!(!addrs.is_empty(), "{host} should have IPv4 addresses");
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_ipv4_udp() {
+        assert_resolves_ipv4(&with_proto(GOOGLE_DNS, DnsProtocol::Udp), "google.com").await;
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_ipv6_udp() {
+        let resolver = with_proto(GOOGLE_DNS, DnsProtocol::Udp);
+        let addrs: Vec<_> = resolver
+            .lookup_ipv6("google.com", TIMEOUT)
+            .await
+            .unwrap()
+            .collect();
+        assert!(!addrs.is_empty());
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_ipv4_tcp() {
+        assert_resolves_ipv4(&with_proto(CLOUDFLARE_DNS, DnsProtocol::Tcp), "google.com").await;
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_ipv4_tls() {
+        assert_resolves_ipv4(&with_proto(GOOGLE_DNS_TLS, DnsProtocol::Tls), "google.com").await;
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_ipv4_https() {
+        assert_resolves_ipv4(
+            &with_proto(CLOUDFLARE_DNS_HTTPS, DnsProtocol::Https),
+            "google.com",
+        )
+        .await;
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_txt_udp() {
+        let resolver = with_proto(GOOGLE_DNS, DnsProtocol::Udp);
+        let records: Vec<_> = resolver
+            .lookup_txt("google.com", TIMEOUT)
+            .await
+            .unwrap()
+            .collect();
+        assert!(!records.is_empty());
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_system_defaults() {
+        assert_resolves_ipv4(&DnsResolver::new(), "google.com").await;
+    }
+
+    #[tokio::test]
+    #[ignore = "requires network access"]
+    async fn resolve_multiple_sites() {
+        let resolver = DnsResolver::new();
+        for host in ["google.com", "cloudflare.com", "example.com"] {
+            assert_resolves_ipv4(&resolver, host).await;
+        }
+    }
+}
