443d453f8dfe30a087f6748c00655fdf690ddb2f
rklaehn <rklaehn@users.noreply.github.com>
2026-03-19 09:22:27 +0000

    deploy: de265b64d37db55c9f6ac19eec1d696b7a0beb88

--- pr/4002/docs/trait.impl/core/convert/trait.AsRef.js
+++ pr/4002/docs/trait.impl/core/convert/trait.AsRef.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_base",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_base/struct.PublicKey.html\" title=\"struct iroh_base::PublicKey\">PublicKey</a>"]]],["iroh_relay",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"iroh_relay/endpoint_info/struct.UserData.html\" title=\"struct iroh_relay::endpoint_info::UserData\">UserData</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;T&gt; for <a class=\"struct\" href=\"iroh/address_lookup/struct.FilteredAddressLookup.html\" title=\"struct iroh::address_lookup::FilteredAddressLookup\">FilteredAddressLookup</a>&lt;T&gt;"]]],["iroh_base",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_base/struct.PublicKey.html\" title=\"struct iroh_base::PublicKey\">PublicKey</a>"]]],["iroh_relay",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"iroh_relay/endpoint_info/struct.UserData.html\" title=\"struct iroh_relay::endpoint_info::UserData\">UserData</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[384,414]}
+//{"start":57,"fragment_lengths":[359,385,414]}
