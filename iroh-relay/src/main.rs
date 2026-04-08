a38e3eeaa0e4ec95486b4e050a39be569aedc876
Frando <frando@unbiskant.org>
2026-04-08 13:13:35 +0200

    fix: set default crypto provider for iroh-relay binary

--- iroh-relay/src/main.rs
+++ iroh-relay/src/main.rs
@@ -496,6 +496,14 @@ async fn main() -> Result<()> {
         .with(EnvFilter::from_default_env())
         .init();
 
+    // Install `ring` as default crypto provider for rustls.
+    // `ring` is enabled by the `tls-ring` feature, which is included in the `server`
+    // feature, which is required for the main.rs binary.
+    // Therefore, this does not need any feature flags.
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("failed to set default crypto provider");
+
     let cli = Cli::parse();
     let mut cfg = Config::load(&cli).await?;
     if cfg.enable_quic_addr_discovery && cfg.tls.is_none() {
