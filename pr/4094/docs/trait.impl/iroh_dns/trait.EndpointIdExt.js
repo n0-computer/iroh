811a8b341d5dafb004e8bde02558af2371aaa7aa
rklaehn <rklaehn@users.noreply.github.com>
2026-04-09 13:57:20 +0000

    deploy: b4fff5b991cbcefb467a006845ddb11280d38d2d

--- pr/4094/docs/trait.impl/iroh_dns/trait.EndpointIdExt.js
+++ pr/4094/docs/trait.impl/iroh_dns/trait.EndpointIdExt.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh",[]],["iroh_dns",[]],["iroh_relay",[]]]);
+    var implementors = Object.fromEntries([["iroh",[]],["iroh_dns",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11,16,18]}
+//{"start":57,"fragment_lengths":[11,16]}
