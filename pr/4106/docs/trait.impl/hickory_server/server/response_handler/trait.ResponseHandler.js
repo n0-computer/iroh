dd219f8614ccf84789f347fe12348fe7b3e98fbe
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-04-13 10:03:11 +0000

    deploy: 1b5ae81a09ef65b6ae4ecd5d5b3c083756f8c3da

---
+++ pr/4106/docs/trait.impl/hickory_server/server/response_handler/trait.ResponseHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_dns_server",[["impl ResponseHandler for <a class=\"struct\" href=\"iroh_dns_server/dns/struct.Handle.html\" title=\"struct iroh_dns_server::dns::Handle\">Handle</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[175]}
