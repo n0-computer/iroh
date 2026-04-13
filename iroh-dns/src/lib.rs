a094cf87b49e4d1486cc1b556ba182ae4f704004
Ruediger Klaehn <rklaehn@protonmail.com>
2026-04-13 12:11:41 +0300

    Use iroh_base decoding error for the z32 decoding as well

--- iroh-dns/src/lib.rs
+++ iroh-dns/src/lib.rs
@@ -9,7 +9,7 @@ pub mod pkarr;
 use std::sync::LazyLock;
 
 use iroh_base::{EndpointId, KeyParsingError};
-use n0_error::{e, stack_error};
+use n0_error::e;
 
 /// z-base-32 encoding as used by pkarr.
 static Z_BASE_32: LazyLock<data_encoding::Encoding> = LazyLock::new(|| {
@@ -18,21 +18,6 @@ static Z_BASE_32: LazyLock<data_encoding::Encoding> = LazyLock::new(|| {
     spec.encoding().expect("valid z-base-32 spec")
 });
 
-#[allow(missing_docs)]
-#[stack_error(derive, add_meta)]
-#[non_exhaustive]
-pub enum DecodingError {
-    #[error("endpoint id was not encoded in valid z32")]
-    InvalidEncodingZ32 {
-        #[error(std_err)]
-        source: data_encoding::DecodeError,
-    },
-    #[error("length must be 32 bytes, but got {len} byte(s)")]
-    InvalidLength { len: usize },
-    #[error("endpoint id is not a valid public key")]
-    InvalidKey { source: KeyParsingError },
-}
-
 /// Extension methods for [`EndpointId`] to encode to and decode from z-base-32,
 /// which is the encoding used by [pkarr](https://pkarr.org) domain names.
 pub trait EndpointIdExt {
@@ -40,7 +25,7 @@ pub trait EndpointIdExt {
     fn to_z32(&self) -> String;
 
     /// Parses a [`EndpointId`] from [z-base-32](https://philzimmermann.com/docs/human-oriented-base-32-encoding.txt) encoding.
-    fn from_z32(s: &str) -> Result<EndpointId, DecodingError>;
+    fn from_z32(s: &str) -> Result<EndpointId, KeyParsingError>;
 }
 
 impl EndpointIdExt for EndpointId {
@@ -48,12 +33,10 @@ impl EndpointIdExt for EndpointId {
         Z_BASE_32.encode(self.as_bytes())
     }
 
-    fn from_z32(s: &str) -> Result<EndpointId, DecodingError> {
+    fn from_z32(s: &str) -> Result<EndpointId, KeyParsingError> {
         let bytes = Z_BASE_32
             .decode(s.as_bytes())
-            .map_err(|e| e!(DecodingError::InvalidEncodingZ32, e))?;
-        let pk =
-            EndpointId::try_from(bytes.as_slice()).map_err(|e| e!(DecodingError::InvalidKey, e))?;
-        Ok(pk)
+            .map_err(|_| e!(KeyParsingError::FailedToDecodeBase32))?;
+        EndpointId::try_from(bytes.as_slice())
     }
 }
