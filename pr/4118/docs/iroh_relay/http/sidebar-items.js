3c8a0d26f5671f4ed21e4f949a1792a604d55383
Frando <Frando@users.noreply.github.com>
2026-04-15 10:02:28 +0000

    deploy: 9e30326a4e8dc8de13620e4627cf400d12569549

--- pr/4118/docs/iroh_relay/http/sidebar-items.js
+++ pr/4118/docs/iroh_relay/http/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["CLIENT_AUTH_HEADER","RELAY_PATH","RELAY_PROBE_PATH","RELAY_PROTOCOL_VERSION"]};
+window.SIDEBAR_ITEMS = {"constant":["CLIENT_AUTH_HEADER","RELAY_PATH","RELAY_PROBE_PATH"],"enum":["ProtocolVersion"],"struct":["UnsupportedRelayProtocolVersion"]};
