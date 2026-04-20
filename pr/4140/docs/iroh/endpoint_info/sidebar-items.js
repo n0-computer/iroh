c75e56bfdb76f2de759327a245d01ca3e412f426
Frando <Frando@users.noreply.github.com>
2026-04-20 09:15:51 +0000

    deploy: 54cb58560433a6e37ec7064d59e94f7b37c341d7

---
+++ pr/4140/docs/iroh/endpoint_info/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["IROH_TXT_NAME"],"enum":["EncodingError","ParseError"],"struct":["AddrFilter","EndpointData","EndpointInfo","MaxLengthExceededError","UserData"]};
