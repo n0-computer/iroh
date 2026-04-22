a28218562c87ce8ef8bc2d68059fe540a4b0e5c9
rklaehn <rklaehn@users.noreply.github.com>
2026-04-22 12:49:11 +0000

    deploy: ed0ccdcd73c6bc3dac4b1b2a38ed61d750faa9dc

---
+++ pr/4094/docs/trait.impl/reqwest/dns/resolve/trait.Resolve.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_dns",[["impl Resolve for <a class=\"struct\" href=\"iroh_dns/dns/struct.DnsResolver.html\" title=\"struct iroh_dns::dns::DnsResolver\">DnsResolver</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[161]}
