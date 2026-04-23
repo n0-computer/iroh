79ed106cac118178b42a500dcb9b0f6a1ac27dc5
Frando <frando@unbiskant.org>
2026-04-23 14:03:26 +0200

    chore: fmt

--- iroh-dns/src/lib.rs
+++ iroh-dns/src/lib.rs
@@ -2,7 +2,6 @@
 //!
 //! This crate contains the core types for publishing and resolving iroh endpoint
 //! information via DNS, using the [pkarr](https://pkarr.org) signed packet format.
-//!
 #![deny(missing_docs, rustdoc::broken_intra_doc_links, unreachable_pub)]
 #![cfg_attr(not(test), deny(clippy::unwrap_used))]
 
