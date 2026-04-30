ecbe062d26bfd9193d4d52ecd7b516c6c4e289a5
rklaehn <rklaehn@users.noreply.github.com>
2026-04-30 11:28:35 +0000

    deploy: ec39abde67e8384d9c0b3be9e97acab8c5cd0bbf

--- pr/4214/docs/iroh/endpoint/transports/sidebar-items.js
+++ pr/4214/docs/iroh/endpoint/transports/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Addr","AddrKind"],"struct":["Transmit","TransportBias"],"trait":["CustomEndpoint","CustomSender","CustomTransport"]};
+window.SIDEBAR_ITEMS = {"enum":["Addr","AddrKind"],"struct":["RecvInfo","Transmit","TransportBias"],"trait":["CustomEndpoint","CustomSender","CustomTransport"]};
