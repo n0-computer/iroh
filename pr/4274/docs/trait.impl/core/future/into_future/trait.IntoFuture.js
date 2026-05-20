73e6f72da4d3e46a896b524244d777b609a88aea
Frando <Frando@users.noreply.github.com>
2026-05-20 08:49:22 +0000

    deploy: be713e4f266913e025c4e99b114015f74137f78e

---
+++ pr/4274/docs/trait.impl/core/future/into_future/trait.IntoFuture.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/into_future/trait.IntoFuture.html\" title=\"trait core::future::into_future::IntoFuture\">IntoFuture</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.Incoming.html\" title=\"struct iroh::endpoint::Incoming\">Incoming</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[319]}
