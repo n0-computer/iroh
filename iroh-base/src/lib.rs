b37c6edace5eb4ce761e86f38dab6d4bb85b589b
dignifiedquire <me@dignifiedquire.com>
2026-05-26 21:11:17 +0200

    fixup: fmt

--- iroh-base/src/lib.rs
+++ iroh-base/src/lib.rs
@@ -13,6 +13,9 @@ mod relay_url;
 #[cfg(feature = "key")]
 pub use self::endpoint_addr::{CustomAddr, EndpointAddr, TransportAddr};
 #[cfg(feature = "key")]
-pub use self::key::{EndpointId, KeyParsingError, PublicKey, SecretKey, Signature, SignatureError, SignatureParsingError};
+pub use self::key::{
+    EndpointId, KeyParsingError, PublicKey, SecretKey, Signature, SignatureError,
+    SignatureParsingError,
+};
 #[cfg(feature = "relay")]
 pub use self::relay_url::{RelayUrl, RelayUrlParseError};
