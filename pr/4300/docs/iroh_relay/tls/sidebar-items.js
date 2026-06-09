a596dc9140ce60f95761c1a2c5a89d4b1d5f009d
Frando <Frando@users.noreply.github.com>
2026-06-09 10:56:56 +0000

    deploy: ab6e8f97b4d3d599b37b4e0cb174a6eacd758521

--- pr/4300/docs/iroh_relay/tls/sidebar-items.js
+++ pr/4300/docs/iroh_relay/tls/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["default_provider","make_dangerous_client_config"],"struct":["CaTlsConfig"],"type":["ServerCertVerifierBuilder"]};
+window.SIDEBAR_ITEMS = {"fn":["default_provider","make_dangerous_client_config"],"struct":["CaTlsConfig"],"type":["CaRootsConfig","ServerCertVerifierBuilder"]};
