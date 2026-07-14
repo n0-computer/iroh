a3db78ee6f037e4e86a6d70c8b0cca093d1ad59f
Arqu <Arqu@users.noreply.github.com>
2026-07-14 10:51:18 +0000

    deploy: 3a2825b9db5737a649f84425399ae5f452671815

---
+++ pr/4411/docs/trait.impl/iroh_metrics/labels/trait.EncodeLabelSet.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl EncodeLabelSet for <a class=\"struct\" href=\"iroh/address_lookup/struct.ServiceLabels.html\" title=\"struct iroh::address_lookup::ServiceLabels\">ServiceLabels</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[184]}
