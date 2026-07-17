0f29465feba6ec5805a48c460c4fef01ee2eed5e
Floris Bruynooghe <flub@n0.computer>
2026-07-17 12:00:44 +0100

    fix: remove vergen use from build.rs files
    This removes the vergen dependency and removes it from the build.rs
    files where it was used. It was a no-op: the only environment variable
    we used from it was one we explicitly configured vergen to not
    generate. Since no one has missed this information for so long there's
    no point in fixing it.
    
    Vergen's use of git is doing the wrong thing in some situations: when
    the crate itself is build from a registry archive there is no git repo
    for iroh itself. But in some build setups there might be a git repo
    somewhere in a parent directory which git than finds and uses. This
    not only results in finding the wrong info, but also results in
    triggering rebuilds when they are not needed due to unrelated changes
    in the found git repo. See https://github.com/n0-computer/iroh/issues/4418

--- iroh-relay/build.rs
+++ iroh-relay/build.rs
@@ -8,15 +8,4 @@ fn main() {
         wasm_browser: { all(target_family = "wasm", target_os = "unknown") },
         with_crypto_provider: { any(feature = "tls-ring", feature = "tls-aws-lc-rs") }
     }
-
-    // Emit build-time environment variables
-    if let Err(e) = emit_vergen() {
-        eprintln!("vergen error: {e}");
-    }
-}
-
-fn emit_vergen() -> Result<(), Box<dyn std::error::Error>> {
-    let gitcl = GitclBuilder::default().sha(false).build()?;
-    Emitter::default().add_instructions(&gitcl)?.emit()?;
-    Ok(())
 }
