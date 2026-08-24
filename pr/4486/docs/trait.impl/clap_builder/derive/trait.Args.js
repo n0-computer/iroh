be651430efc42e0cef7bf4bf76cb1044c1b6564e
flub <flub@users.noreply.github.com>
2026-08-24 14:34:47 +0000

    deploy: 273dcfdf39d82ee35bcac773bf3c629e3f0905d0

---
+++ pr/4486/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_bench",[["impl Args for <a class=\"struct\" href=\"iroh_bench/s2n/struct.Opt.html\" title=\"struct iroh_bench::s2n::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"iroh_bench/struct.Opt.html\" title=\"struct iroh_bench::Opt\">Opt</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[255]}
