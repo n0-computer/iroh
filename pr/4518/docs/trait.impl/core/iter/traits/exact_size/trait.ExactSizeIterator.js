bb099bf531b59c51a7ae8d8eacf63c43ae26b568
ramfox <ramfox@users.noreply.github.com>
2026-09-09 17:03:19 +0000

    deploy: 18c87383279e617e6206fd101581362b536e2b4c

---
+++ pr/4518/docs/trait.impl/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"iroh/endpoint/struct.PathListIter.html\" title=\"struct iroh::endpoint::PathListIter\">PathListIter</a>&lt;'_&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[371]}
