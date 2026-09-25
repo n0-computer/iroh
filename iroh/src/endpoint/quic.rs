3c912827c42c0304265a126d90c6231248054ab4
ramfox <kasey@n0.computer>
2026-09-24 20:25:32 -0400

    chore: fix `qlog` feature gate error (#4556)
    A combination of human error and `cargo make format-check` meant that the last commit made the `qlog` feature gate our `noq` exports.
    
    This fixes that.

--- iroh/src/endpoint/quic.rs
+++ iroh/src/endpoint/quic.rs
@@ -6,9 +6,9 @@
 //! to limit or expand how those structs are used in iroh. By encapsulating them
 //! we can ensure the functionality needed to make iroh work.
 
-use std::{path::Path, sync::Arc, time::Duration};
-
 #[cfg(feature = "qlog")]
+use std::path::Path;
+use std::{sync::Arc, time::Duration};
 
 /// `noq` types that are used in the public iroh API.
 // Each type is notated with the iroh type or noq type that uses it.
